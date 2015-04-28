26608088be5750aa5b17ee3f4e5ebee08c16f6cc
Phillip <phil.winston2@gmail.com>
2015-04-28 19:45:48 -0400

    commit 6

--- pixart.js
+++ pixart.js
@@ -4,13 +4,13 @@ var brush = document.querySelector(".brush");
 var field = document.querySelector("#color-field");
 var form = document.querySelector("#form");
 
-  button.addEventListener("click", function(event){
-  event.preventDefault();
-  brush.style.background = field.value
+button.addEventListener("click", function(event){
+event.preventDefault();
+brush.style.background = field.value
 })
 
 
-for(var i = 0; i < 20; i++) {
+for(var i = 0; i < 8000; i++) {
   var square = document.createElement("div");
   square.setAttribute("class", "square");
   document.body.appendChild(square);
@@ -19,12 +19,8 @@ for(var i = 0; i < 20; i++) {
 var draw = document.querySelectorAll(".square");
 
 for (var x = 0; x < draw.length; x++) {
-  draw[x].addEventListener("click", function(event){
+  draw[x].addEventListener("mouseover", function(event){
   event.preventDefault();
   this.style.background = field.value
   })
 }
-// <form id="form">
-//   <input type="text" id="color-field"></input>
-//   <button id="set-color">Set Color</button>
-// </form>
