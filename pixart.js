aa08f13485be2d298632fcd32d08411b674e77cd
Noah Levine <nclevine@gmail.com>
2015-04-28 17:21:34 -0400

    event listeners active for button click and enter key

--- pixart.js
+++ pixart.js
@@ -1,1 +1,17 @@
+var colorEntry = document.getElementById('color-field');
+var colorButton = document.getElementById('set-color');
+var brushBox = document.querySelector('.brush');
 
+console.log(brushBox);
+
+colorButton.addEventListener('click', function(action){
+  action.preventDefault();
+  brushBox.style.background = colorEntry.value;
+})
+
+colorEntry.addEventListener('keydown', function(action){
+  if(action.keyCode === 13){
+    action.preventDefault();
+    brushBox.style.background = colorEntry.value;
+  }
+})
