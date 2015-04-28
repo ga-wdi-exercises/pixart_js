165832ee991151dee351b9767c1b6f54d808e89c
Lindsey <lmdragun@gmail.com>
2015-04-28 18:38:58 -0400

    first step

--- pixart.js
+++ pixart.js
@@ -1,1 +1,15 @@
+// var color = document.getElementById("color-field").value;
+// var newColor = document.forms["form"]["color-field"].value;
+// console.log(newColor);
+// function switchColor(){
+// 	document.getElementsByClassName("brush").style.backgroundColor = newColor;
+// }
 
+var button = document.getElementById("set-color");
+button.addEventListener("click", function(myEvent){
+	myEvent.preventDefault();
+	console.log("Hi");
+	var newColor = document.getElementById("color-field").value;
+	console.log(newColor);
+	document.querySelector(".brush").style.backgroundColor = newColor;
+})
