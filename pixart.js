c9be78ae7204b97f69f5de6ef3bbac7ae50f8240
Parmeet Singh <parmeetmander90@gmail.COM>
2015-04-28 18:25:29 -0400

    div square changes color

--- pixart.js
+++ pixart.js
@@ -4,9 +4,17 @@ document.getElementById("set-color").addEventListener("click", function(evt){
 	document.getElementsByClassName("brush")[0].style.backgroundColor= input.value;
 });
 
-for (i=0; i<19; i++) {
+for (var i=0; i<19; i++) {
 	var addSquare= document.createElement('DIV');
 	document.body.appendChild(addSquare);
 	addSquare.className= "square";
 }
 
+var squares = document.getElementsByClassName('square');
+for (var j=0; j<squares.length; j++){
+	document.getElementsByClassName("square")[j].addEventListener("click", function(evt){
+		var color = document.getElementsByClassName("brush")[0].style.backgroundColor;
+		console.log(color);
+		evt.toElement.style.backgroundColor= color;
+	});
+}
