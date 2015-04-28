cf50c9a956b5b623058178f1afabf37335d02ff1
alitopaloglu <ali_t@gwmail.gwu.edu>
2015-04-28 18:13:20 -0400

    commit one

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
 
+
+
+var color = document.getElementById("color-field");
+
+document.getElementById("set-color").addEventListener("click", function(evt){
+	evt.preventDefault();
+    document.getElementsByClassName("brush")[0].style.backgroundColor = color.value
+
+})
+
