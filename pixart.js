231e55d9376c1c0ea122fed8fe8c97ec693d5eaf
keithknox <keithwknox@gmail.com>
2015-04-28 19:00:16 -0400

    commit #'s 2 & 3

--- pixart.js
+++ pixart.js
@@ -6,3 +6,9 @@ document.getElementById("set-color").addEventListener("click", function(evt){
   var classy = document.getElementsByClassName("brush")[0];
   classy.style.background = input.value;
 })
+
+  for(i = 0; i<=20; i++) {
+    var addSquare = document.createElement("div");
+    document.body.appendChild(addSquare);
+    addSquare.className = "square";
+  }
