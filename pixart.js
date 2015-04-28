415791a91fdd27a724215a0a55c07ef1c017ed6b
Enoch Shin <seekpeace@gmail.com>
2015-04-28 18:40:46 -0400

    brush color change allowed

--- pixart.js
+++ pixart.js
@@ -1,9 +1,12 @@
 
 var colorButton = document.querySelector("#set-color");
 colorButton.addEventListener("click",changeBrush);
+// #element.method(event_name,function)
 
 function changeBrush(){
   event.preventDefault();
   var brush =document.querySelector(".brush");
-  brush.style.backgroundColor = "red";
+  // brush.style.backgroundColor = "red";
+  var colorField = document.querySelector("#color-field");
+  brush.style.backgroundColor = colorField.value;
 }
