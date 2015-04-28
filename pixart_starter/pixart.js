79933b8052e897f5e54adf2eb179a9ee04e3acf4
presto2116 <preston.straight@gmail.com>
2015-04-28 17:52:35 -0400

    completed 1 and 2

--- pixart_starter/pixart.js
+++ pixart_starter/pixart.js
@@ -1,1 +1,9 @@
+var input = document.getElementById("color-field");
 
+document.getElementById("set-color").addEventListener("click", function(evt){
+	evt.preventDefault();
+	console.log(input.value);
+	var classy = document.getElementsByClassName("brush")[0];
+	console.log(classy)
+	classy.style.background = input.value;
+});
