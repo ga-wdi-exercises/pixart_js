87e29c983968acfb85e977149841fdeb8e537520
Damon Emde <damon.emde@gmail.com>
2015-04-28 17:45:52 -0400

    box color functionality

--- pixart.js
+++ pixart.js
@@ -1,1 +1,9 @@
+var form = document.querySelector('#form')
+var field = document.querySelector('#color-field')
+var box = document.querySelector(".brush")
 
+form.addEventListener("submit", function( event ){
+  event.preventDefault();
+  console.log(field.value)
+  box.style.backgroundColor = field.value
+})
