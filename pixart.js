ca12b92e2b9c11e7c0a2e6bf37fdd23d50d4d06d
theeltahir <theeltahir@gmail.com>
2015-04-28 17:05:28 -0400

    Commit 1: change color of brush

--- pixart.js
+++ pixart.js
@@ -1,1 +1,10 @@
+var form = document.getElementById("form");
+var brush = document.querySelector("div.controls div.brush");
+var colorField = document.getElementById("color-field");
 
+form.addEventListener('submit', changeBrush);
+
+function changeBrush(event){
+    event.preventDefault();
+    brush.style.backgroundColor = colorField.value;
+}
