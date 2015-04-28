e93827c72857061db702bb20c5d17fc408713fb0
Ryan Keiper <ryankeiper@gmail.com>
2015-04-28 19:11:12 -0400

    finished the bonus, made a nice swatch

--- pixart.js
+++ pixart.js
@@ -2,9 +2,24 @@ var box = document.querySelector(".brush");
 var form = document.querySelector("#form");
 var field = document.querySelector("#color-field");
 
+for(i = 0; i < 3; i++){
+	var swatch = document.createElement("div");
+	swatch.className = "swatch";
+	document.body.firstElementChild.appendChild(swatch);
+	swatch.addEventListener("click", function(event){
+		box.style.backgroundColor = this.style.backgroundColor;
+		field.value = this.style.backgroundColor;
+	})
+}
+
+var swatches = document.querySelectorAll(".swatch");
+
 form.addEventListener("submit", function(event){
 	event.preventDefault();
 	box.style.backgroundColor = field.value;
+	swatches[2].style.backgroundColor = swatches[1].style.backgroundColor;
+	swatches[1].style.backgroundColor = swatches[0].style.backgroundColor;
+	swatches[0].style.backgroundColor = field.value;
 })
 
 for(i = 0; i < 8000; i++){
