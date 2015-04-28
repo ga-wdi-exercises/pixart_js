cf65d74e429c48d742b3b77c5fca1ab0430c4db8
presto2116 <preston.straight@gmail.com>
2015-04-28 18:04:21 -0400

    finished commit 3

--- pixart_starter/pixart.js
+++ pixart_starter/pixart.js
@@ -2,8 +2,11 @@ var input = document.getElementById("color-field");
 
 document.getElementById("set-color").addEventListener("click", function(evt){
 	evt.preventDefault();
-	console.log(input.value);
 	var classy = document.getElementsByClassName("brush")[0];
-	console.log(classy)
 	classy.style.background = input.value;
 });
+for(var i = 0; i < 19; ++i){
+var addSquare = document.createElement('div');
+	document.body.appendChild(addSquare);
+	addSquare.className = "square";
+}
