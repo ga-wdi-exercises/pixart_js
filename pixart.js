4d20bc857fa8a4af49df6f314189ab1239acf2ea
Lindsey <lmdragun@gmail.com>
2015-04-28 19:20:35 -0400

    painting function

--- pixart.js
+++ pixart.js
@@ -5,25 +5,29 @@
 // 	document.getElementsByClassName("brush").style.backgroundColor = newColor;
 // }
 
+var newColor = document.getElementById("color-field");
 var button = document.getElementById("set-color");
-button.addEventListener("click", function(myEvent){
-	myEvent.preventDefault();
-	console.log("Hi");
-	var newColor = document.getElementById("color-field").value;
-	console.log(newColor);
-	document.querySelector(".brush").style.backgroundColor = newColor;
+var form = document.querySelector("form");
+var brush = document.querySelector(".brush");
+
+
+console.log("Hi");
+console.log(newColor);
+
+form.addEventListener("submit", function(event){
+	event.preventDefault();
+	brush.style.backgroundColor = newColor.value;
 })
 
-for(i = 1; i < 21; i++){
+//create 8000 squares
+for(i = 0; i < 8000; i++){
 	var createSquares = document.createElement('div');
-	document.body.appendChild(createSquares);
-    createSquares.className = "square";
+		document.body.appendChild(createSquares).className = "square";
 }
-
+//paint on hover over the squares
 var squares = document.querySelectorAll(".square");
-
 for(i=0; i < 8000; i++){
-squares[i].addEventListener("mouseover", function(paintEvent){
-	this.style.backgroundColor = "green";
+	squares[i].addEventListener("mouseover", function(paintEvent){
+	this.style.backgroundColor = newColor.value;
 })
 }
