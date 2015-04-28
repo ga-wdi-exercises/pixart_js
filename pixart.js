9a8c50168eded97de288dbb6485499f966d935d0
GWEN <gwenmak@gmail.com>
2015-04-28 17:27:39 -0400

    commit 1: change brush color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,10 @@
+var setBoxColor = function( event ){
+	event.preventDefault();
+	var colorField = document.getElementById("color-field")
+	var chosenColor = colorField.value
+	var brush = document.querySelector(".brush")
+	brush.style.backgroundColor = chosenColor
+}
 
+var setColorButton = document.getElementById("set-color")
+setColorButton.addEventListener("click", setBoxColor);
