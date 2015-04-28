f206aa2ef612b1586ee94876ff2d0a9fd3ecf9e3
Parmeet Singh <parmeetmander90@gmail.COM>
2015-04-28 17:57:35 -0400

    added 20 div with class square

--- pixart.js
+++ pixart.js
@@ -1,8 +1,12 @@
 var input = document.getElementById("color-field");
 document.getElementById("set-color").addEventListener("click", function(evt){
 	evt.preventDefault();
-	console.log(input.value);
 	document.getElementsByClassName("brush")[0].style.backgroundColor= input.value;
 });
 
+for (i=0; i<19; i++) {
+	var addSquare= document.createElement('DIV');
+	document.body.appendChild(addSquare);
+	addSquare.className= "square";
+}
 
