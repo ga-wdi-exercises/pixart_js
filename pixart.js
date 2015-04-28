beda5d766df7599522f14fb1ab3f16839258d4e8
Chad Smith <chadxsmith@ymail.com>
2015-04-28 19:30:05 -0400

    mouse hover

--- pixart.js
+++ pixart.js
@@ -2,8 +2,19 @@ function changeBackground(evt) {
 
   evt.preventDefault();
 
-  var color = document.getElementById("color-field").value;
+  color = document.getElementById("color-field").value;
       document.querySelector(".brush").style.backgroundColor = color;
  }
 
  document.getElementById("set-color").addEventListener("click", changeBackground);
+
+
+
+ for (i = 0; i < 801; i++) {
+    var square = document.createElement('div');             // this creates divs
+    square.className ="square";                             // for each newly created div, give it the class properities of "square"
+    square.addEventListener("mouseover", function(event) {  //for each newly created div w/ the class properities of "square", run the function below
+      this.style.backgroundColor = color                     //on this one "square", apply the "style" "backgroundColor" of color
+    })
+    document.body.appendChild(square);                        //add the square to the row of squares, which is the end of the file
+ }
