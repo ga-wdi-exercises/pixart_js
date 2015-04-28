5eae78e0ec4d6172a6a71a06ef340c4c5c2d44d0
JANICE MIN <JANICEMIN@GMAIL.COM>
2015-04-28 18:16:09 -0400

    up to commit 4

--- pixart.js
+++ pixart.js
@@ -1,1 +1,26 @@
+var form = document.querySelector("#form")
+var brush = document.querySelector("div.brush")
+
+var colorChange = document.querySelector("#color-field")
+form.addEventListener("submit", changeIt)
+
+function changeIt (event){
+	event.preventDefault(); 
+
+	brush.style.backgroundColor = colorChange.value;
+}
+
+
+
+
+for(var i = 0; i < 21; i++) {
+	var square = document.createElement('div');
+	square.className = "square"
+	document.body.appendChild(square);
+}
+
+
+
+
+
 
