ba33492c2ccafb0bb54716b159787c4b5972dc97
Sarah Greene <sarahmaudegreene@gmail.com>
2015-04-28 17:56:19 -0400

    commit 1 of pixel art

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
 
+  var color = document.getElementById("color-field");
+  var brush = document.getElementsByClassName("brush")[0];
+
+  document.getElementById("set-color").addEventListener("click", function(evt) {
+    evt.preventDefault();
+    console.log(color.value);
+    brush.style.backgroundColor = color.value
+  })
+
+  
