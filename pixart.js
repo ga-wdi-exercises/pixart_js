b27328cd05613339651b739d79d7c381f6ef9291
Haleigh Harper <haleighharper@Haleighs-MacBook-Pro.local>
2015-04-28 18:06:05 -0400

    set color button functioning

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
+// When I click the "Set Color" button, it should change the color of the "brush" box to the color I specify in the input field. (**Hint:** You will need to use `event.preventDefault()` somewhere in your code.)
+var form = document.querySelector("form")
+var button = document.querySelector("#set-color")
+var brush = document.querySelector(".brush")
+var colorField = document.querySelector("#color-field")
 
+form.addEventListener("submit", function( event ){
+    event.preventDefault();
+
+    brush.style.backgroundColor = colorField.value
+})
