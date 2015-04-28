a0346fe51efd22d5882b8e4cf3af4b82334411cc
Parmeet Singh <parmeetmander90@gmail.COM>
2015-04-28 17:49:46 -0400

    got div brush to change color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,8 @@
+var input = document.getElementById("color-field");
+document.getElementById("set-color").addEventListener("click", function(evt){
+	evt.preventDefault();
+	console.log(input.value);
+	document.getElementsByClassName("brush")[0].style.backgroundColor= input.value;
+});
+
 
