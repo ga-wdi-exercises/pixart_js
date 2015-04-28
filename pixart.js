c2d2e8f93593cdce292aee3b2850636d3ae31b6b
Matt Wuertz <mattwuertz@gmail.com>
2015-04-28 18:23:09 -0400

    first commit

--- pixart.js
+++ pixart.js
@@ -1,1 +1,12 @@
+function changeBoxColor(event){
+  
+  event.preventDefault();
+  // event.target // equals the button that was clicked
+  // so we don't have to document.getElementById("set-color") again
+  var input = document.getElementById("color-field").value;
+  var brush = document.querySelector(".brush");
+  brush.style.background = input
+  }
 
+document.getElementById("set-color").addEventListener("click", changeBoxColor);
+document.getElementById("set-color").addEventListener("onkeypress", changeBoxColor);
