b62359400dd36f00c702c50906801945e4b20513
Chad Smith <chadxsmith@ymail.com>
2015-04-28 18:12:55 -0400

    change color of box by response

--- pixart.js
+++ pixart.js
@@ -1,1 +1,9 @@
+function changeBackground(evt) {
 
+  evt.preventDefault();
+
+  var color = document.getElementById("color-field").value;
+      document.querySelector(".brush").style.backgroundColor = color;
+ }
+
+ document.getElementById("set-color").addEventListener("click", changeBackground);
