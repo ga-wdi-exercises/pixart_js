46f6a5d43fa2d8b2f20fb0f818f23be1be201e2b
Brad Foster <btf5048@gmail.com>
2015-04-28 18:36:15 -0400

    add event listener for set color button

--- pixart.js
+++ pixart.js
@@ -1,1 +1,8 @@
+var colorButton = document.getElementById("set-color");
+var colorField = document.getElementById("color-field");
+var brush = document.querySelector(".brush");
 
+colorButton.addEventListener("click", function(event){
+  event.preventDefault();
+  brush.style.background = colorField.value
+});
