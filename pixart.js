76e02d6fa56c7eaec372b551cefe0a521d440c5c
theeltahir <theeltahir@gmail.com>
2015-04-28 17:59:13 -0400

    Bonus: this needs refactoring but it works

--- pixart.js
+++ pixart.js
@@ -1,15 +1,29 @@
 var form = document.getElementById("form");
-var brush = document.querySelector("div.controls div.brush");
+var brush = document.querySelector("div#current-brush");
 var colorField = document.getElementById("color-field");
+var swatch = document.getElementById("swatch")
 
 form.addEventListener('submit', changeBrush);
+
 
 function changeBrush(event){
     event.preventDefault();
     brush.style.backgroundColor = colorField.value;
+    var newSwatch = document.createElement("div");
+    newSwatch.className = "brush";
+    newSwatch.style.backgroundColor = colorField.value;
+    newSwatch.addEventListener('click', selectSwatch);
+    swatch.appendChild(newSwatch);
+    while(swatch.childNodes.length > 3){
+        swatch.removeChild(swatch.firstChild);
+    }
 }
 function changeSquare(event){
     event.target.style.backgroundColor = colorField.value;
+}
+function selectSwatch(event){
+    colorField.value = event.target.style.backgroundColor;
+    brush.style.backgroundColor = colorField.value;
 }
 
 
