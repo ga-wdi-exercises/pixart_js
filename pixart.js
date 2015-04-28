e9e32912c125cbb862f94c5c17269630c3c36ad9
alitopaloglu <ali_t@gwmail.gwu.edu>
2015-04-28 18:58:55 -0400

    commit two redo

--- pixart.js
+++ pixart.js
@@ -1,11 +1,23 @@
-
-
-
+// var form = document.getElementById("form");
+// var brush = document.querySelector("div.controls div.brush");
 var color = document.getElementById("color-field");
 
-document.getElementById("set-color").addEventListener("submit", function(evt){
+document.getElementById("set-color").addEventListener("click", function(evt){
 	evt.preventDefault();
     document.getElementsByClassName("brush")[0].style.backgroundColor = color.value
 
 })
 
+document.getElementById("color-field").addEventListener("keydown", function(evt){
+	if (evt.keyCode === 83){
+		evt.preventDefault();}
+		document.getElementsByClassName("brush")[0].style.backgroundColor = color.value
+
+})
+
+
+// for(var i = 0; i < 20; i ++){
+//     var square = document.createElement("div");
+//     square.className = "square";
+//     document.body.appendChild(square);
+// }
