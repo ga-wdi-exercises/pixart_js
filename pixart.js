c0a26c713b288c2358a6fcc18e530653c1232ea8
keithknox <keithwknox@gmail.com>
2015-04-28 18:03:12 -0400

    commit #1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,8 @@
+var input = document.getElementById("color-field");
 
+document.getElementById("set-color").addEventListener("click", function(evt){
+  evt.preventDefault();
+  // console.log(input.value);
+  var classy = document.getElementsByClassName("brush")[0];
+  classy.style.background = input.value;
+})
