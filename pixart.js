f88450b9b3c61805106718ee2a37405658e9ec94
Matt Wuertz <mattwuertz@gmail.com>
2015-04-28 19:43:51 -0400

    fourth commit

--- pixart.js
+++ pixart.js
@@ -1,3 +1,18 @@
+for(var i=0;i<20; i++){
+  elemDiv= document.createElement('div');
+  elemDiv.className = "square";
+ document.body.appendChild(elemDiv);
+}
+
+document.getElementById("set-color").addEventListener("click", changeBoxColor);
+document.getElementById("set-color").addEventListener("onkeypress", changeBoxColor);
+
+var square =document.querySelectorAll(".square")
+for(var i=0; i<square.length; i++){
+  square[i].addEventListener("click", changeDivColor);
+}
+
+
 function changeBoxColor(event){
 
   event.preventDefault();
@@ -6,13 +21,17 @@ function changeBoxColor(event){
   var input = document.getElementById("color-field").value;
   var brush = document.querySelector(".brush");
   brush.style.background = input
+}
+
+
+
+function changeDivColor(event){
+  var brush = document.querySelector(".brush");
+  if (brush.style.background === ""){
+    var brushColor = window.getComputedStyle(document.querySelector(".brush"))["background-color"];
+    event.target.style.background = brushColor;
+  }else{
+    var brushColor = brush.style.background;
+    event.target.style.background = brushColor;
   }
-
-document.getElementById("set-color").addEventListener("click", changeBoxColor);
-document.getElementById("set-color").addEventListener("onkeypress", changeBoxColor);
-
-for(var i=0;i<20; i++){
-  elemDiv= document.createElement('div');
-  elemDiv.className = "square";
- document.body.appendChild(elemDiv);
 }
