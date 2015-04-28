0d1a9ba5dff8bad39333897c86a7a24b0f51cd4e
Nabil Hashmi <nabil2011@gmail.com>
2015-04-28 18:25:55 -0400

    Set color on click or enter

--- pixart.js
+++ pixart.js
@@ -1,1 +1,18 @@
+// Get the form so that we can prevent the submit action on it
+var color_form = document.getElementById("form")
+// Get the new color
+var new_color = document.getElementById("color-field")
+// Get the brush 
+var brush = document.querySelector(".brush")
 
+// Change the color 
+function changeColor(evt){
+  // Prevent the default behavior of whatever event is passed to changeColor
+  evt.preventDefault();
+  // Replace the brush's color with a new color
+  brush.style.backgroundColor = new_color.value;
+}
+
+// Take the form and call changeColor when the form is submitted 
+// submit.preventDefault() prevents the form from being submitted 
+color_form.addEventListener('submit', changeColor);
