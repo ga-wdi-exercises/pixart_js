1d3067db85a2cdf768da0f9dc64d4dbfdb9cde39
Phillip <phil.winston2@gmail.com>
2015-04-28 18:32:42 -0400

    commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,21 @@
 
+var button = document.querySelector("#set-color");
+var brush = document.querySelector(".brush");
+var field = document.querySelector("#color-field");
+var form = document.querySelector("#form");
+
+
+button.addEventListener("click", function(event){
+  event.preventDefault();
+  // console.log(brush.style.background)
+
+  brush.style.background = field.value
+  // console.log(brush.style.background)
+
+})
+
+
+// <form id="form">
+//   <input type="text" id="color-field"></input>
+//   <button id="set-color">Set Color</button>
+// </form>
