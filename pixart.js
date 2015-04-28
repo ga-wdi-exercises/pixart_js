02648738e865faf7ce6327f0aeaefe6884fbf8d4
Ryan Keiper <ryankeiper@gmail.com>
2015-04-28 18:09:54 -0400

    pixel art commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,8 @@
+var box = document.querySelector(".brush");
+var form = document.querySelector("#form");
+var field = document.querySelector("#color-field");
 
+form.addEventListener("submit", function(event){
+	event.preventDefault();
+	box.style.backgroundColor = field.value;
+})
