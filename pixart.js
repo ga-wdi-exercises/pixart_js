f598caa37e4c6dbf898ccec85b446afc4d2de894
DIANA JURGIELEWICZ <DMJURG@GMAIL.COM>
2015-04-28 22:08:20 -0400

    commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,6 @@
-
+var form = document.querySelector("#form")
+var field = document.querySelector("#color-field")
+form.addEventListener("submit", function(event){
+  event.preventDefault();
+  document.querySelector(".brush").style.background = field.value
+})
