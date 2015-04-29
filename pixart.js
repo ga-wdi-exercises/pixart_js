1e234d4a13af1a4782a7dff67297d9c455222f48
Freetown5 <iconteh21@yahoo.com>
2015-04-28 21:32:34 -0400

    made button work

--- pixart.js
+++ pixart.js
@@ -1,1 +1,15 @@
+var form = document.querySelector("#form");
+var setColor = document.querySelector("#set-color");
+var colorField = document.querySelector("#color-field");
+var brush = document.querySelector(".brush");
 
+// when I click on the set color button
+
+  // get input
+  // change the color attribute of the clicker to the users input
+
+  setColor.addEventListener("click", function(event){
+    event.preventDefault();
+    brush.style.background = colorField.value;
+
+  })
