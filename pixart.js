1cc2eb32c2d75c04cf594c1e9aee90fe14b481cf
Sarah Greene <sarahmaudegreene@gmail.com>
2015-04-28 21:56:55 -0400

    commit 3: added 20 divs with square class

--- pixart.js
+++ pixart.js
@@ -8,4 +8,12 @@
     brush.style.backgroundColor = color.value
   })
 
-  
+  function createSquares () {
+    for (var i = 0; i < 21; i++) {
+      var newDiv = document.createElement("div");
+      newDiv.className = "square";
+      document.body.appendChild(newDiv);
+    }
+  }
+
+  createSquares();
