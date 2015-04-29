3b72a493b1ad6ec4245290df77152b6474c2e3b1
Karen-O <karen.e.och@gmail.com>
2015-04-28 21:26:08 -0400

    Pixart step#3 complete (20 div-squares created)

--- pixart.js
+++ pixart.js
@@ -8,3 +8,14 @@ form.addEventListener("submit", function(formEvent){     // ("event being listen
 	colorPalette.style.backgroundColor = colorField.value;  // tested on page for click and then 'enter' to submit color
 })
 
+// Using JavaScript, create 20 divs of the "square" class and append them to the body.
+
+for (i = 0; i < 20; i++) { 				// a for loop needs curly braces too
+	var pixelSquare = document.createElement("div");
+	pixelSquare.className = "square";		// method for giving the style of an existing Class to something new.  // new variable "square" is a new created element within the DOM (a div) and we're giving it a Class (CSS) attribute of "square".
+	document.body.appendChild(pixelSquare);   // appends pixelSquare 20 times to the doc. body
+}     
+// for loop = (3 parts) using any variable [think of "i = iterator"] --> (1) i=0 ["starting point"]; 
+// (2) i < 20 [#20 = # of times to iterate, NOT like the "upper point of a range"... aka "keep iterating as long as the number of iterations is less than 20; or really, "as long as 'this middle condition' is < 20"]; and
+// (3) 'i++' instructs HOW the iterator should increment with each iteration--> ++ means "go up by 1 each time" (opposite is '--' insofar as "syntactic sugar" goes)
+
