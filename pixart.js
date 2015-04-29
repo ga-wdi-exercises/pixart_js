f469c748e737d694b22bba4cff8753518872609d
tsetoung <jtmoua@yahoo.com>
2015-04-29 08:19:05 -0400

    adjustments

--- pixart.js
+++ pixart.js
@@ -1,6 +1,6 @@
 //* When I click the "Set Color" button, it should change the color of the "brush" box to the color I specify in the input field. (**Hint:** You will need to use `event.preventDefault()` somewhere in your code.)
 function colorChange(){
-  var color = document.getElementById("SetColor");
+  var color = window.getComputedStyle(document.getElementById("SetColor"))["background-color"];
   input.addEventListener("onclick", function(event))
   if(event.keyCode === 9){
     event.preventDefault();
@@ -11,3 +11,5 @@ function colorChange(){
     });
   }
 }
+
+//* The same thing should happen when I press the enter key from inside the input field
