48f9c687865a442c1e31e3478945ddb2751a9b0c
Adrian Maseda <amaseda@gmail.com>
2015-04-28 23:09:39 -0400

    Commit #4

--- pixart.js
+++ pixart.js
@@ -1,15 +1,15 @@
+// INITIALIZE VARIABLES
 var colorInput = document.querySelector( "#color-field" );
 var colorButton = document.querySelector( "#set-color" );
 var brushBox = document.querySelector( ".brush" );
+var squares = document.getElementsByClassName( "square" );
 
+
+// FUNCTIONS
 var changeBrushBox = function( event ) {
   event.preventDefault();
   brushBox.style.backgroundColor = colorInput.value;
 }
-
-colorButton.addEventListener( "click", changeBrushBox );
-colorButton.addEventListener( "change", changeBrushBox );
-
 
 var createSquares = function( number ) {
   for( var i=0; i < number; i++ ) {
@@ -19,4 +19,19 @@ var createSquares = function( number ) {
   }
 }
 
+var changeSquareColor = function() {
+  this.style.backgroundColor = "green";
+}
+
+
+// EXECUTE CODE
 createSquares( 20 );
+
+
+// EVENT LISTENERS
+colorButton.addEventListener( "click", changeBrushBox );
+colorButton.addEventListener( "change", changeBrushBox );
+
+for ( var i=0; i < squares.length; i++ ) {
+  squares[i].addEventListener( "click", changeSquareColor );
+}
