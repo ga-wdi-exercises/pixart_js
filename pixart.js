910dce3b4f2e5724964b08670f90d6fefe506da1
Adrian Maseda <amaseda@gmail.com>
2015-04-28 22:50:10 -0400

    Commit #1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,10 @@
+var colorInput = document.querySelector( "#color-field" );
+var colorButton = document.querySelector( "#set-color" );
+var brushBox = document.querySelector( ".brush" );
 
+var changeBrushBox = function( event ) {
+  event.preventDefault();
+  brushBox.style.backgroundColor = colorInput.value;
+}
+
+colorButton.addEventListener( "click", changeBrushBox );
