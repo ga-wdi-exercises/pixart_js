1e3f17c9cf51d7aa81995478a9153a4cb5129f7d
Biboy <opbiboy@gmail.com>
2015-04-29 09:26:18 -0400

    Commit

--- pixart.js
+++ pixart.js
@@ -1,1 +1,14 @@
+for(var i = 0; i < 8000; i++) {
+ var square = document.createElement("div");
+ square.setAttribute("class", "square");
+ document.body.appendChild(square);
+}
 
+var draw = document.querySelectorAll(".square");
+
+for (var x = 0; x < draw.length; x++) {
+ draw[x].addEventListener("mouseover", function(event){
+ event.preventDefault();
+ this.style.background = field.value
+ })
+}
