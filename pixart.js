45adf003e409fb0e1a8eb63960f62693779d26d1
Tammy Peters <tamarapeters2@gmail.com>
2015-04-29 07:22:43 -0400

    step 2 already done - bc used 'submit' instead of 'click'

--- pixart.js
+++ pixart.js
@@ -1,15 +1,4 @@
-// var entries = document.querySelector("#entries")
-// var total = document.querySelector("#total")
-// var form = document.querySelector("#entry")
-// var field = document.querySelector("#newEntry")
-//
-// form.addEventListener("submit", function( event ){
-//   event.preventDefault();
-//
-//   console.log( field.value )
-//
-// })
-//
+
 // * When I click the "Set Color" button, it should change the color of the "brush"
 // box to the color I specify in the input field. (**Hint:** You will need to use `event.preventDefault()` somewhere in your code.)
 
@@ -19,7 +8,7 @@ var brush = document.querySelector(".brush")
 var colorField = document.querySelector("#color-field")
 
 
-form.addEventListener('submit', function(event){
+form.addEventListener("submit", function(event){
   event.preventDefault()
   brush.style.backgroundColor = colorField.value
 })
