5e2071542f472d273caecfdc0012629aea158822
Aaron <aaronpark87@gmail.com>
2015-04-29 10:26:32 -0400

    w07d02 hw

--- pixart.js
+++ pixart.js
@@ -1,1 +1,20 @@
 
+
+var form = document.querySelector("#form")
+var field = document.querySelector("#text")
+var color = document.getElementById("color-field");
+
+// form.addEventListener("click", function(event)) {
+// 	event.preventDefault();
+// 	console.log(field.value)
+// }
+document.getElementById("set-color").addEventListener("submit", function(evt){
+    evt.preventDefault();
+    document.getElementsByClassName("brush")[0].style.backgroundColor = color.value
+
+})
+
+
+
+
+
