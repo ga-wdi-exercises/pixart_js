1d890c072c5e837b5bfa46f74d19678e4c383f51
Nabil Hashmi <nabil2011@gmail.com>
2015-04-28 21:49:47 -0400

    User can now change color by selecting swatch

--- pixart.js
+++ pixart.js
@@ -4,6 +4,25 @@ var color_form = document.getElementById("form")
 var new_color = document.getElementById("color-field")
 // Get the brush 
 var brush = document.querySelector(".brush")
+colorSwatches = ["#333", "#fff", "#000"];
+
+// Draw swatches 
+function createSwatches(){
+  for (var i = 0; i < colorSwatches.length; i++) {
+    var swatch = document.createElement("div");
+    swatch.style.height = "100px";
+    swatch.style.width = "100px";
+    swatch.className = "recent-swatch"
+    swatch.style.display = "inline-block";
+    swatch.style.marginBottom = "30px";
+    swatch.style.marginLeft = "30px";
+    swatch.style.marginRight = "30px";
+    swatch.style.backgroundColor = colorSwatches[i];
+    document.querySelector(".controls").appendChild(swatch);
+  };
+}
+
+createSwatches();
 
 // Change the color 
 function changeColor(evt){
@@ -11,11 +30,27 @@ function changeColor(evt){
   evt.preventDefault();
   // Replace the brush's color with a new color
   brush.style.backgroundColor = new_color.value;
+  colorSwatches.pop();
+  colorSwatches.unshift(new_color.value);
+  swatches = document.querySelectorAll(".recent-swatch");
+  for (var i = 0; i < swatches.length; i++){
+    swatches[i].style.backgroundColor = colorSwatches[i]; 
+  }
 }
 
 // Take the form and call changeColor when the form is submitted 
 // submit.preventDefault() prevents the form from being submitted 
 color_form.addEventListener('submit', changeColor);
+
+function changeBySwatch(evt){
+  document.getElementById("color-field").value = this.style.backgroundColor
+}
+
+swatches = document.querySelectorAll(".recent-swatch");
+for (var i = 0; i < swatches.length; i++) {
+  swatches[i].addEventListener('click', changeBySwatch);
+  swatches[i].addEventListener('click', changeColor);
+};
 
 
 function createSquares(){
@@ -39,6 +74,3 @@ function changeSquare(evt){
 for (var i = 0; i < squares.length; i++) {
   squares[i].addEventListener('mouseover', changeSquare);
 };
-
-
-
