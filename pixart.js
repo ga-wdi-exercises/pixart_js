3e27a2ef2c0d2143b1515d6885f6b1155dbcc502
GWEN <gwenmak@gmail.com>
2015-04-28 23:07:32 -0400

    commit 7: thoughts on bonus and some comments

--- pixart.js
+++ pixart.js
@@ -1,10 +1,8 @@
-
-
 var setBoxColor = function( event ){
-	event.preventDefault();
-	colorField = document.getElementById("color-field")
+	event.preventDefault(); //prevent the page refreshing when the button is clicked
+	colorField = document.getElementById("color-field") //get the colour input from the user via the text field
 	chosenColor = colorField.value
-	var brush = document.querySelector(".brush")
+	var brush = document.querySelector(".brush") //find the brush object
 	brush.style.backgroundColor = chosenColor
 }
 
@@ -18,5 +16,17 @@ for(i = 0; i < 8001; i++) {
 		this.style.backgroundColor = chosenColor;
 	})
 	document.body.appendChild(square);
+}
+
+//bonus
+for(i = 0; i < 4; i++) {
+	var swatch = document.createElement('div'); //make swatches
+	swatch.className = "swatch" //associate css for swatches
+	swatch.addEventListener("submit", function(event){//collect colour information when user submits
+		this.style.backgroundColor = chosenColor; //change colour of swatches
+	})
+	document.body.insertBefore(swatch, square); //put the swatch between the brush box and the field of squares
+	//somehow need to keep the colours, maybe move the existing colour over and replace with the new colour when the set color button is clicked?
+	//make the swatches clickable so that they change the brush box color - something like swatch.addEvent Listener("click", function(event){this.style.backgroundColor = chosenColor;})
 }
 
