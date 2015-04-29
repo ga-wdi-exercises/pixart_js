08a1139a19e522beecd84026dd55d4c57b89da47
Karen-O <karen.e.och@gmail.com>
2015-04-28 22:08:40 -0400

    finished Pixart assignment

--- pixart.js
+++ pixart.js
@@ -14,10 +14,10 @@ form.addEventListener("submit", function(formEvent){     // ("event being listen
 // (2) i < 20 [#20 = # of times to iterate, NOT like the "upper point of a range"... aka "keep iterating as long as the number of iterations is less than 20; or really, "as long as 'this middle condition' is < 20"]; and
 // (3) 'i++' instructs HOW the iterator should increment with each iteration--> ++ means "go up by 1 each time" (opposite is '--' insofar as "syntactic sugar" goes)
 
-for (i = 0; i < 20; i++) { 				// a for loop needs curly braces too
+for (i = 0; i < 8000; i++) { 				// a for loop needs curly braces too
 	var pixelSquare = document.createElement("div");
 	pixelSquare.className = "square";		// method for giving the style of an existing Class to something new.  // new variable "square" is a new created element within the DOM (a div) and we're giving it a Class (CSS) attribute of "square".
-	pixelSquare.addEventListener("click", function(){      // the EventListener is "listening" for the CLICK event!  // Second item in parentheses = function which creates the action to follow the event (the click)
+	pixelSquare.addEventListener("mouseover", function(){      // the EventListener is "listening" for the CLICK event!  // Second item in parentheses = function which creates the action to follow the event (the click)
 		this.style.backgroundColor = colorField.value;		// 'this' replaces pixelSquare here because as each square is subsequently named "pixelSquare" (1 for each iteration, replacing the former square with variable name pixelSquare from the previous iteration), after the 'for' loop ends only the last square will have name "pixelSquare" -- so only that one would get changed to 'green' --> 'this' solves the problem by telling the element to refer to itSELF rather than an external veriable. 
 	});
 	document.body.appendChild(pixelSquare);   // appends pixelSquare 20 times to the doc. body
@@ -27,6 +27,8 @@ for (i = 0; i < 20; i++) { 				// a for loop needs curly braces too
 
 // Modify your code so that when I click on each "square", it changes to the color I set using my input instead of "green" every time.
 
+// Modify your code so that you are creating 8000 divs instead of 20.
+// Change the event that changes your box colors from 'click' to 'mouseover'     // we want to be able to hover to change the color-- not have to click.
 
 
 
