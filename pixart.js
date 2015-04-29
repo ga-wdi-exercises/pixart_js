2d64a012c20a4dd79c6245dbedcdd71c3ab5acdd
Tammy Peters <tamarapeters2@gmail.com>
2015-04-29 07:17:21 -0400

    initial commit change color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,25 @@
+// var entries = document.querySelector("#entries")
+// var total = document.querySelector("#total")
+// var form = document.querySelector("#entry")
+// var field = document.querySelector("#newEntry")
+//
+// form.addEventListener("submit", function( event ){
+//   event.preventDefault();
+//
+//   console.log( field.value )
+//
+// })
+//
+// * When I click the "Set Color" button, it should change the color of the "brush"
+// box to the color I specify in the input field. (**Hint:** You will need to use `event.preventDefault()` somewhere in your code.)
 
+
+var form = document.getElementById("form")
+var brush = document.querySelector(".brush")
+var colorField = document.querySelector("#color-field")
+
+
+form.addEventListener('submit', function(event){
+  event.preventDefault()
+  brush.style.backgroundColor = colorField.value
+})
