b76bb85d707ed6427c53f90fcf0584d8453f42a6
Karen-O <karen.e.och@gmail.com>
2015-04-28 21:56:04 -0400

    Pixart step 5 complete-- clicking a square turns it any color indicated in colorField

--- pixart.js
+++ pixart.js
@@ -18,9 +18,29 @@ for (i = 0; i < 20; i++) { 				// a for loop needs curly braces too
 	var pixelSquare = document.createElement("div");
 	pixelSquare.className = "square";		// method for giving the style of an existing Class to something new.  // new variable "square" is a new created element within the DOM (a div) and we're giving it a Class (CSS) attribute of "square".
 	pixelSquare.addEventListener("click", function(){      // the EventListener is "listening" for the CLICK event!  // Second item in parentheses = function which creates the action to follow the event (the click)
-		this.style.backgroundColor = "green";		// 'this' replaces pixelSquare here because as each square is subsequently named "pixelSquare" (1 for each iteration, replacing the former square with variable name pixelSquare from the previous iteration), after the 'for' loop ends only the last square will have name "pixelSquare" -- so only that one would get changed to 'green' --> 'this' solves the problem by telling the element to refer to itSELF rather than an external veriable. 
+		this.style.backgroundColor = colorField.value;		// 'this' replaces pixelSquare here because as each square is subsequently named "pixelSquare" (1 for each iteration, replacing the former square with variable name pixelSquare from the previous iteration), after the 'for' loop ends only the last square will have name "pixelSquare" -- so only that one would get changed to 'green' --> 'this' solves the problem by telling the element to refer to itSELF rather than an external veriable. 
 	});
 	document.body.appendChild(pixelSquare);   // appends pixelSquare 20 times to the doc. body
 }     											
 
+// Add functionality so that when I click on each "square", it changes the color of that individual square to "green"
+
+// Modify your code so that when I click on each "square", it changes to the color I set using my input instead of "green" every time.
+
+
+
+
+
+
+// BELOW = UP THROUGH STEP 4 --> Comments!!
+//////////////
+// for (i = 0; i < 20; i++) { 				// a for loop needs curly braces too
+// 	var pixelSquare = document.createElement("div");
+// 	pixelSquare.className = "square";		// method for giving the style of an existing Class to something new.  // new variable "square" is a new created element within the DOM (a div) and we're giving it a Class (CSS) attribute of "square".
+// 	pixelSquare.addEventListener("click", function(){      // the EventListener is "listening" for the CLICK event!  // Second item in parentheses = function which creates the action to follow the event (the click)
+// 		this.style.backgroundColor = "green";		// 'this' replaces pixelSquare here because as each square is subsequently named "pixelSquare" (1 for each iteration, replacing the former square with variable name pixelSquare from the previous iteration), after the 'for' loop ends only the last square will have name "pixelSquare" -- so only that one would get changed to 'green' --> 'this' solves the problem by telling the element to refer to itSELF rather than an external veriable. 
+// 	});
+// 	document.body.appendChild(pixelSquare);   // appends pixelSquare 20 times to the doc. body
+// }     											
+///////////////
 // Add functionality so that when I click on each "square", it changes the color of that individual square to "green"
