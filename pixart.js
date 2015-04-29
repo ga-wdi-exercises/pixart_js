5d5f691ee1408652fcc99e55640401e9030cc881
Noah Levine <nclevine@gmail.com>
2015-04-29 10:08:45 -0400

    fixed swatches, full functionality

--- pixart.js
+++ pixart.js
@@ -9,31 +9,58 @@ for (var i = 0; i < 3; i++) {
   document.querySelector('.swatches').appendChild(swatch);
 };
 
+var swatchArray = [
+  window.getComputedStyle(brushBox)['background-color'],
+  window.getComputedStyle(swatch3)['background-color'],
+  window.getComputedStyle(swatch2)['background-color']
+];
+
+function resetSwatchArray(){
+  swatchArray = [
+    window.getComputedStyle(brushBox)['background-color'],
+    window.getComputedStyle(swatch3)['background-color'],
+    window.getComputedStyle(swatch2)['background-color']
+  ];
+};
+
 function shiftSwatches(newColor){
-  var lastBrush = brushBox.style.backgroundColor;
-  var color3 = swatch3.style.backgroundColor;
-  var color2 = swatch2.style.backgroundColor;
-  console.log(lastBrush + ' ' + color3 + ' ' + color2);
-  swatch1.style.backgroundColor = color2;
-  swatch2.style.backgroundColor = color3;
-  swatch3.style.backgroundColor = lastBrush;
   brushBox.style.backgroundColor = newColor;
-}
+  swatch3.style.backgroundColor = swatchArray[0];
+  swatch2.style.backgroundColor = swatchArray[1];
+  swatch1.style.backgroundColor = swatchArray[2];
+  resetSwatchArray();
+};
 
 colorButton.addEventListener('click', function(action){
   action.preventDefault();
   var newColor = colorEntry.value;
   shiftSwatches(newColor);
-})
-
-colorEntry.addEventListener('keydown', function(action){
-  if(action.keyCode === 13){
-    var newColor = colorEntry.value;
-    shiftSwatches(newColor);
-  }
-})
+});
 
 
+swatch1.addEventListener('click', function(){
+    var swatchColor = window.getComputedStyle(this)['background-color'];
+    var currentBrush = window.getComputedStyle(brushBox)['background-color'];
+    brushBox.style.backgroundColor = swatchColor;
+    this.style.backgroundColor = currentBrush;
+    resetSwatchArray();
+});
+
+swatch2.addEventListener('click', function(){
+    var swatchColor = window.getComputedStyle(this)['background-color'];
+    var currentBrush = window.getComputedStyle(brushBox)['background-color'];
+    brushBox.style.backgroundColor = swatchColor;
+    this.style.backgroundColor = currentBrush;
+    resetSwatchArray();
+});
+
+swatch3.addEventListener('click', function(){
+    var swatchColor = window.getComputedStyle(this)['background-color'];
+    var currentBrush = window.getComputedStyle(brushBox)['background-color'];
+    brushBox.style.backgroundColor = swatchColor;
+    this.style.backgroundColor = currentBrush;
+    resetSwatchArray();
+});
 
 for (var i = 1; i <= 8000; i++) {
   var paintedPixel = document.createElement('div');
@@ -42,5 +69,5 @@ for (var i = 1; i <= 8000; i++) {
   paintedPixel.addEventListener('mouseover', function(action){
     var brushColor = window.getComputedStyle(brushBox)["background-color"];
     this.style.background = brushColor;
-  })
+  });
 };
