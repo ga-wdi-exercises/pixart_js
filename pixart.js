07a4c2ba3f9fd458032c5bd9507e0d4ce3382045
Nick Bumbarger <nbumbarger@gmail.com>
2015-04-28 22:00:40 -0400

    Commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,7 @@
-
+button = document.getElementById("set-color");
+button.addEventListener("click", function(event){
+	event.preventDefault()
+	brush = document.getElementsByClassName("brush")[0]
+	new_color = document.getElementById("color-field").value
+	brush.style.backgroundColor = new_color
+	})
