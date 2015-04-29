ec1483508978f83dee0841cd67cb525ac9189cc4
Tammy Peters <tamarapeters2@gmail.com>
2015-04-29 07:40:44 -0400

    started step 3 without success

--- pixart.js
+++ pixart.js
@@ -3,6 +3,7 @@
 // box to the color I specify in the input field. (**Hint:** You will need to use `event.preventDefault()` somewhere in your code.)
 
 
+
 var form = document.getElementById("form")
 var brush = document.querySelector(".brush")
 var colorField = document.querySelector("#color-field")
@@ -12,3 +13,10 @@ form.addEventListener("submit", function(event){
   event.preventDefault()
   brush.style.backgroundColor = colorField.value
 })
+
+/* Using JavaScript, create 20 divs of the "square" class and append them to the body*/
+var square = document.querySelector(".square")
+
+var div = document.createElement("div")
+div.appendChild( square )
+document.querySelector( "div" ).appendChild( div )
