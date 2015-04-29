673fc8ed0bb9287f61f170c5cf7c33d26b067bfc
Julia Rose <julmrose@gmail.com>
2015-04-28 22:48:51 -0400

    commit 1: set color changes brush box

--- pixart.js
+++ pixart.js
@@ -1,1 +1,16 @@
+function preventDef(event) {
+  event.preventDefault();
+}
+
+var setColor = document.getElementById("set-color");
+var brush = document.querySelector(".brush");
+var input = document.getElementById("color-field");
+
+function colorChange() {
+	var newColor = input.value;
+	brush.style.backgroundColor = newColor;
+}
+
+setColor.addEventListener("click", preventDef);
+setColor.addEventListener("click", colorChange);
 
