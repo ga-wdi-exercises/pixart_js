151cd79223580e0eca2e786489b8cd5982796165
Sarah Greene <sarahmaudegreene@gmail.com>
2015-04-28 23:14:33 -0400

    Bonus: just barely started thinking about it

--- pixart.js
+++ pixart.js
@@ -8,8 +8,22 @@
     brush.style.backgroundColor = color.value;
   })
 
+  function colorSwatch() {
+    for (var i = 0; i < 3; i++) {
+      var newDiv = document.createElement("div");
+      newDiv.className = "swatch";
+      document.body.appendChild(newDiv);
+      document.getElementById("set-color").addEventListener("click", function(){
+        document.getElementsByClassName("swatch")[2].style.backgroundColor = color.value;
+      })
+
+    }
+  }
+
+  colorSwatch();
+
   function createSquares () {
-    for (var i = 0; i < 8001; i++) {
+    for (var i = 0; i < 8000; i++) {
       var newDiv = document.createElement("div");
       newDiv.className = "square";
       document.body.appendChild(newDiv);
