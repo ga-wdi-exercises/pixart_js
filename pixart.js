515fb83bf1e6373c3c20273f4c9aa14d69804776
Karen-O <karen.e.och@gmail.com>
2015-04-28 20:38:04 -0400

    w07d02 HW-- Commits #1 & #2 (in one!)

--- pixart.js
+++ pixart.js
@@ -1,1 +1,10 @@
+var form = document.querySelector("#form");
+var colorPalette = document.querySelector(".brush");
+var colorField = document.querySelector("#color-field");
+
+
+form.addEventListener("submit", function(formEvent){     // ("event being listened for", [action to take when it occurs, in the form of a function])
+	formEvent.preventDefault();							// Prevents any default actions that would normally be taken by the event.
+	colorPalette.style.backgroundColor = colorField.value;  // tested on page for click and then 'enter' to submit color
+})
 
