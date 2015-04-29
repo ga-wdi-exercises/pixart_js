ab4055c1953dfb32637916f5f1dad320b91321e2
Enoch Shin <seekpeace@gmail.com>
2015-04-28 21:13:16 -0400

    one more step then done

--- pixart.js
+++ pixart.js
@@ -1,4 +1,4 @@
-
+var color;
 var colorButton = document.querySelector("#set-color");
 colorButton.addEventListener("click",changeBrush);
 // #element.method(event_name,function)
@@ -9,10 +9,39 @@ function changeBrush(){
   // brush.style.backgroundColor = "red";
   var colorField = document.querySelector("#color-field");
   brush.style.backgroundColor = colorField.value;
+  color = brush.style.backgroundColor;
 }
 
 for(var i =0;i < 20;i++){
   var createSquare = document.createElement("div");
   createSquare.className = 'square';
-  document.body.appendChild(createSquare);
+  var singleSquare = document.body.appendChild(createSquare);
+
 }
+
+
+var twentySquares = document.querySelectorAll(".square");
+// var twenty_squares = document.getElementsByClassName("square");
+
+// for(var i in twentySquares)
+for(var i = 0;i < twentySquares.length;i++)
+{
+  twentySquares[i].addEventListener("click",changeSquare);
+}
+// twentySquares.addEventListener("mousedown",changeSquare);
+//above doesn't work, have to lope through each one in the array
+
+function changeSquare(){
+  //below is for part 4//
+  // this.style.backgroundColor = "green";
+  this.style.backgroundColor = color;
+
+  // twentySquares[i].style.backgroundColor = "green";
+}
+// twentySquares[i].style.backgroundColor = window.getComputedStyle(document.querySelector(".brush"))["background-color"];
+// the above gives me the values in memory when value is not defined in style
+// troubleshooting steps below.  console.dir gives you all the attribute values
+// console.log("TEST");
+// console.dir(document.querySelector("#set-color"));
+// console.dir(document.querySelector("#color-field").value);
+// apply the color to twentySquares
