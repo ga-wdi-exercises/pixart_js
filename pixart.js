8b9ca70ecf3d5ad9fec97f6f5fa1ed83976479ed
Nick Bumbarger <nbumbarger@gmail.com>
2015-04-28 23:50:56 -0400

    Commi 4

--- pixart.js
+++ pixart.js
@@ -8,15 +8,23 @@ function addEvents(){
 	var button = document.getElementById("set-color");
 	var newColorField = document.getElementById("color-field");
 	button.addEventListener("click",
-	function(event){ event.preventDefault(); setBrushToInputColor()});
+		function(event){ event.preventDefault(); setBrushToInputColor()});
 	newColorField.addEventListener("keypress",
-	function(event){ var key = event.which; if (key === 13) {setBrushToInputColor;}});
+		function(event){ var key = event.which; if (key === 13) {setBrushToInputColor;}});
+}
+
+function colorBackground(divId) {
+	document.getElementById(divId).style.backgroundColor = "green";
 }
 
 function appendDivs(divCount){
-	for(var i = 0; i < divCount; i++) {
+	for (var i = 0; i < divCount; i++) {
 		var div = document.createElement("DIV");
 		div.className = "square";
+		div.id = "div" + i;
+		div.addEventListener("click", function(event) {
+			colorBackground(event.srcElement.id);
+		})
 		document.body.appendChild(div);
 	}
 }
