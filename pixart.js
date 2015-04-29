e09b810b66bc712ddbc6370c2c732911f84b81d1
Freetown5 <iconteh21@yahoo.com>
2015-04-29 09:00:55 -0400

    homework first half

--- pixart.js
+++ pixart.js
@@ -2,6 +2,8 @@ var form = document.querySelector("#form");
 var setColor = document.querySelector("#set-color");
 var colorField = document.querySelector("#color-field");
 var brush = document.querySelector(".brush");
+var body = document.querySelector("body")
+var square = document.querySelector(".square")
 
 // when I click on the set color button
 
@@ -19,3 +21,16 @@ var brush = document.querySelector(".brush");
     brush.style.background = colorField.value;
 
   })
+
+//create body variable
+//create a div class called square (in CSS)- never mind, already exists in CSS
+//write a function that adds the square div to body - call it to test it
+//get the function to replicate 20 times
+
+
+  function createSquare(){
+    body.appendChild(square);
+  }
+
+  createSquare();
+//get javascript to add it to the body
