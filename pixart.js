9a211368e5835317e8197cc20c07c336ede21c7e
Adrian Maseda <amaseda@gmail.com>
2015-04-29 07:14:44 -0400

    Bonus round 1: colors cycle through three swatches. Can't click them for new color yet.

--- pixart.js
+++ pixart.js
@@ -1,13 +1,25 @@
-// INITIALIZE VARIABLES
+// INITIALIZE
 var colorInput = document.querySelector( "#color-field" );
 var colorButton = document.querySelector( "#set-color" );
 var brushBox = document.querySelector( ".brush" );
 var squares = document.getElementsByClassName( "square" );
+var swatches = document.getElementsByClassName( "swatch" );
 
 
 // FUNCTIONS
+var createSwatches = function( number ) {
+  for ( var i=0; i < number; i++ ) {
+    swatch = document.createElement( "div" );
+    swatch.classList.add( "swatch" );
+    document.body.appendChild( swatch );
+  }
+}
+
 var changeBrushBox = function( event ) {
   event.preventDefault();
+  swatches[2].style.backgroundColor = swatches[1].style.backgroundColor;
+  swatches[1].style.backgroundColor = swatches[0].style.backgroundColor;
+  swatches[0].style.backgroundColor = brushBox.style.backgroundColor;
   brushBox.style.backgroundColor = colorInput.value;
 }
 
@@ -25,6 +37,7 @@ var changeSquareColor = function() {
 
 
 // EXECUTE CODE
+createSwatches( 3 );
 createSquares( 8000 );
 
 
