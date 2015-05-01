072397d55b52a56b99ec77f1a59a3a80af57cc56
DIANA JURGIELEWICZ <DMJURG@GMAIL.COM>
2015-04-30 22:56:27 -0400

    1-3 in JQuery

--- pixart.js
+++ pixart.js
@@ -1,24 +1,34 @@
-// * When I click the "Set Color" button, it should change the color of the "brush" box to the color I specify in the input field. (**Hint:** You will need to use `event.preventDefault()` somewhere in your code.)
+// * When I click the "Set Color" button, it should change the color of the "brush" box to the color I specify in the input field.
 // * The same thing should happen when I press the enter key from inside the input field
 
-var form = document.querySelector("#form")
-var field = document.querySelector("#color-field")
-form.addEventListener("submit", function(event){
-  event.preventDefault();
-  document.querySelector(".brush").style.background = field.value
-})
+$(document).ready(function(){
 
+var button = $("#set-color")
+var colorField = $("#color-field")
+var brush = $(".brush")
+
+function setColor(event){
+  event.preventDefault();
+  var newColor = colorField.val();
+  brush.css({
+    "background": newColor
+  })
+}
+
+button.on("click", setColor);
 
 // * Using JavaScript, create 20 divs of the "square" class and append them to the body
-for(x = 0; x < 21; x++){
-  var board = document.createElement('div');
-  board.className = "square";
-  document.querySelector("body").appendChild(board);
+
+for(var pixel = 0; pixel < 21; pixel++){
+  var squareDiv = $("<div></div>").addClass("square");
+  $("body").append(squareDiv);
 }
+
+// // * Add functionality so that when I click on each "square", it changes the color of that individual square to "green"
+// var pixel = document.querySelector(".square")
+// pixel.addEventListener("click", function(event){
+//   event.preventDefault();
 //
-// * Add functionality so that when I click on each "square", it changes the color of that individual square to "green"
-var pixel = document.querySelector(".square")
-pixel.addEventListener("click", function(event){
-  event.preventDefault();
-  
 })
+
+// })
