de7be44577614db96c71fb878f27b394ecbe6995
DIANA JURGIELEWICZ <DMJURG@GMAIL.COM>
2015-04-30 23:15:47 -0400

    squares turn color of the brush

--- pixart.js
+++ pixart.js
@@ -1,11 +1,9 @@
-// * When I click the "Set Color" button, it should change the color of the "brush" box to the color I specify in the input field.
-// * The same thing should happen when I press the enter key from inside the input field
-
 $(document).ready(function(){
 
 var button = $("#set-color")
 var colorField = $("#color-field")
 var brush = $(".brush")
+var color = "#1B4370"
 
 function setColor(event){
   event.preventDefault();
@@ -13,21 +11,17 @@ function setColor(event){
   brush.css({
     "background": newColor
   })
+  color = newColor
 }
 
 button.on("click", setColor);
 
-// * Using JavaScript, create 20 divs of the "square" class and append them to the body
-// * Add functionality so that when I click on each "square", it changes the color of that individual square to "green"
-
 for(var pixel = 0; pixel < 21; pixel++){
   var squareDiv = $("<div></div>").addClass("square");
   squareDiv.on("click", function() {
-    $(this).css("background-color", "green")
+    $(this).css("background-color", color)
     });
   $("body").append(squareDiv);
 }
 
 })
-
-// })
