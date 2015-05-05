6248701e42100ff886f34eaad2b1d8de6cb0c1cd
luminousbeam <jocelyn.jeriah@gmail.com>
2015-05-04 21:54:51 -0400

    completed pixart hw

--- pixart.js
+++ pixart.js
@@ -1,1 +1,26 @@
+var button = document.getElementById("set-color");
+var brush = document.getElementsByClassName("brush")[0];
+var field = document.getElementById("color-field");
+var form = document.querySelector("#form");
+var draw = document.getElementsByClassName("square");
 
+
+button.addEventListener("click", function(event){
+        event.preventDefault();
+        brush.style.background = field.value
+})
+
+
+for(var a = 0; a < 8000; a++) {
+        var square = document.createElement("div");
+        square.setAttribute("class", "square");
+        document.body.appendChild(square);
+}
+
+
+for (var b = 0; b < draw.length; b++) {
+        draw[b].addEventListener("mouseover", function(event){
+        event.preventDefault();
+        this.style.background = field.value
+  })
+}
