36328a3ad3dc5a23c6a5029c9b0fb2eeb45cc7a9
htylerbrooks <htb5qh@gmail.com>
2015-05-05 20:04:38 -0400

    commit 1: set color button changes the color of the brush box

--- pixart.js
+++ pixart.js
@@ -1,1 +1,6 @@
+var form = document.getElementById('form');
 
+form.addEventListener('submit',function(event){
+  event.preventDefault();
+  document.querySelector('.brush').style.backgroundColor = document.querySelector('#color-field').value;
+});
