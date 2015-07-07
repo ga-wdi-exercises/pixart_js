1fa3ed1ea1b47eb00d2e7e4e68c319b50fb872be
Ksenia Coulter <kzherdeva@ucdavis.edu>
2015-07-07 12:25:42 -0400

    button press changes brush field color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,13 @@
 
+var newColor;
+//add event handler to set color button
+$('#set-color').on("click", setColor)
+
+//Set color function will take the inner HTML from input field as an argument
+function setColor() {
+  event.preventDefault();
+  //get value from input field
+  newColor = document.getElementById("color-field").value;
+  console.log(newColor)
+  $(".brush").css("background-color", newColor)
+}
