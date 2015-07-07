81a2cdbd88d6c8298a834f97d700e87e5f26d1ef
Jeff Jones <jeff.jones1@gmail.com>
2015-07-07 11:51:40 -0400

    Commit 1: Set Color button alters brush box color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,9 @@
+var setColor = document.querySelector('#set-color');
+var colorField = document.querySelector('#color-field');
 
+
+setColor.addEventListener('click', function (event){
+  event.preventDefault();
+  $('.brush').css('background-color', $('#color-field').val());
+
+})
