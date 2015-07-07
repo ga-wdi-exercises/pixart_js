9f003b00d58ac847ba131b784eb795971c7bef66
Ryan Kahle <jrkahle@hotmail.com>
2015-07-07 13:22:07 -0400

    Grabs input and changes color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,10 @@
 
+
+// Grabs color from input and changes background of square
+
+$( "#set-color" )[0].addEventListener( "click", function(event) {
+  event.preventDefault();
+  var color = $(" #color-field").val();
+  $( ".brush" ).css("background-color", color);
+
+})
