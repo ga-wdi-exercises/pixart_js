79007b23931f87482648c442cba7e8124ee2c602
Melissa Steffan <msteffan2012@gmail.com>
2015-07-07 19:09:39 -0400

    solved bonus

--- pixart.js
+++ pixart.js
@@ -1,34 +1,51 @@
 $("#set-color").on("click", setNewBackground).on("submit", setNewBackground);
 // added variables for color switcher
-var thirdBackground = $("#2");
-var secondBackground = $("#1");
-var firstBackground = $("#0");
+// var thirdBackground = $("#2");
+// var secondBackground = $("#1");
+// var firstBackground = $("#0");
 var clickCounter = 0;
 
 function setNewBackground(){
     newBackground = document.getElementById('color-field').value;
+    var backgroundForFirstBox = $(".brush").css("background-color");
     event.preventDefault();
+
     $(".brush").css("background-color", newBackground);
+
 // added line for color switcher
     if(clickCounter == 0){
         $("#2").css("background-color", newBackground);
     }else if (clickCounter == 1){
         $("#1").css("background-color", newBackground);
+//        secondBackground = $("#1").css("background-color");
     } else if (clickCounter == 2){
         $("#0").css("background-color", newBackground);
+//        firstBackground = $("#0").css("background-color");
+    } else if (clickCounter === 3){
+
+    } else {
+        // loop through existing values of squares and set variables
+        var firstBackground = $("#0").css("background-color");
+        var secondBackground = $("#1").css("background-color");
+
+    // #0 needs to take new background. whatever is in #0 needs to shift to #1, #1 shift to #2
+        $("#0").css("background-color", backgroundForFirstBox);
+        $("#1").css("background-color", firstBackground);
+        $("#2").css("background-color", secondBackground);
+
     }
     clickCounter++;
 }
 
-for (var i=1;i<200;i++){
+for (var i=1;i<1000;i++){
     $("body").append("<div></div>");
     $("body div").last().addClass("square");
 
 }
-$(".square").on('mouseover', changeBackground);
-function changeBackground(){
-    var newBackground = $(".brush").css("background-color");
-    $(this).css("background-color", newBackground);
+$(".square").on('mouseover', changeBackgroundOfTinySquares);
+function changeBackgroundOfTinySquares(){
+    var newTinySquareBackground = $(".brush").css("background-color");
+    $(this).css("background-color", newTinySquareBackground);
 }
 //invoke function to create color-switcher swatch after the little divs
 addSwatch();
@@ -61,5 +78,5 @@ function addSwatch() {
 $(".color-switcher").on("click", function() {
         var switchBackground = $(this).css("background-color");
         $(".brush").css("background-color", switchBackground);
-        $(".square").on('mouseover', changeBackground);
+        $(".square").on('mouseover', changeBackgroundOfTinySquares);
     });
