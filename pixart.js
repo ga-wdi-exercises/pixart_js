fd9a877e436cd7d84d9bbc592eee4d76e81e8565
Melissa Steffan <msteffan2012@gmail.com>
2015-07-07 11:38:53 -0400

    Finished Commit 1: Click the set-color button to change to color specified in field

--- pixart.js
+++ pixart.js
@@ -1,1 +1,7 @@
+$("#set-color").on("click", function(){
+    var newBackground = document.getElementById('color-field').value;
+//    console.log(newBackground);
+    event.preventDefault();
+    $(".brush").css("background-color", newBackground);
 
+});
