90219feb3a70748cd4b81d8f64cbf20df83ee27a
Melissa Steffan <msteffan2012@gmail.com>
2015-07-07 12:33:58 -0400

    Finished commit 4: divs now change color to match newBackground on mouseover

--- pixart.js
+++ pixart.js
@@ -1,5 +1,6 @@
 $("#set-color").on("click", setNewBackground).on("submit", setNewBackground);
 
+
 function setNewBackground(){
     var newBackground = document.getElementById('color-field').value;
     event.preventDefault();
@@ -7,12 +8,22 @@ function setNewBackground(){
 
 }
 
-function addNewDivs() {
-$('body').append('<div></div>');
-$('body div').last().attr("class", "square");
-}
+for (var i=1;i<21;i++){
+    $("body").append("<div></div>");
+    $("body div").last().addClass("square");
+    $(".square").on('mouseover', changeBackground);
+    }
 
-for (var i=1;i<21;i++)
-{
-  addNewDivs(i);
-}
+function changeBackground(){
+    var newBackground = document.getElementById('color-field').value;
+    $(this).css("background-color", newBackground);
+    }
+//
+// for( var i = 0; i < 8000; i++){
+//   var div = document.createElement('div');
+//   div.classList.add('square');
+//   div.addEventListener('mouseover', function(){
+//     this.style.backgroundColor = brush.style.backgroundColor;
+//   });
+//   document.body.appendChild(div);
+// }
