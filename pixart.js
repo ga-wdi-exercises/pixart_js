4ac0184474b7375efa1ffdb0fc562d2aab84b6ba
Anand Moses <moses.adg@gmail.com>
2015-07-07 13:25:03 -0400

    added 20divs

--- pixart.js
+++ pixart.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
     $("#set-color").click(function(){
-      event.preventDefault();  
+      event.preventDefault();
 
     $(".brush").css("background-color", $("#color-field").val());
 
+//$("body").append(" <div class='square'> -- </div>.<br>");
+
+for(i=0;i<20; i++){
+    $("body").append("<div>_</div>");
+}
+//$("p").append(" <b>Appended text</b>.");
     });
 });
