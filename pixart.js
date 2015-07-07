ae29df4351822f1aa029bd8bd58100c7871950d8
Anand Moses <moses.adg@gmail.com>
2015-07-07 14:42:38 -0400

    pixart-HW

--- pixart.js
+++ pixart.js
@@ -1,25 +1,14 @@
 $(document).ready(function(){
     $("#set-color").click(function(){
       event.preventDefault();
-
     $(".brush").css("background-color", $("#color-field").val());
-
     });
 });
 
-
-
 for(i=0;i<8000; i++){
   $("body").append("<div class='square'></div>");
-
 }
 
-
 $(".square").mouseover("click", function(){
-    //console.log("hi")
-
-  // $(".square").css("background-color",$("#color-field").val());
    $(this).css("background-color",$("#color-field").val());
-
   })
-    //$( "div:nth-child(2)" )
