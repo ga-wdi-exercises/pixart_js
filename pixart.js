ceee9b89ec90d05c389d5ed96ff3ec21a96548a1
Jeremy Koulish <jkoulish@gmail.com>
2015-07-07 14:53:45 -0400

    color changes when click button

--- pixart.js
+++ pixart.js
@@ -1,1 +1,6 @@
+function changeColor() {
+  event.preventDefault();
+ $(".brush").css("background", (document.getElementById("color-field")).value);
+};
 
+$("button")[0].addEventListener("click", changeColor);
