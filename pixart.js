60788dbd5f27bf0c5a4cb578c09cfb4ac9189015
Melissa Steffan <msteffan2012@gmail.com>
2015-07-07 11:55:30 -0400

    Finished Commit #3 - added 20 new divs and appended them to body

--- pixart.js
+++ pixart.js
@@ -1,9 +1,18 @@
-$("#set-color").on("click", setNewBackground);
-$("#set-color").on("submit", setNewBackground);
+$("#set-color").on("click", setNewBackground).on("submit", setNewBackground);
 
 function setNewBackground(){
     var newBackground = document.getElementById('color-field').value;
     event.preventDefault();
     $(".brush").css("background-color", newBackground);
 
+}
+
+function addNewDivs() {
+$('body').append('<div></div>');
+$('body div').last().attr("class", "square");
+}
+
+for (var i=1;i<21;i++)
+{
+  addNewDivs(i);
 }
