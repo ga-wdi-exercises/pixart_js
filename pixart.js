ad6c0c9b940373ed58ff5a639cd620d9a3617a88
John Master <john.s.master@gmail.com>
2015-07-07 13:39:05 -0400

    add event listener to button which sets brush box color to input

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
+var button = $("#set-color")[0];
 
+button.addEventListener("click", changeColor);
+
+function changeColor(){ //Why don't I need to pass event as an argument?
+  event.preventDefault();
+  var color = $("#color-field").val();
+  $("#color-field").val("");
+  console.log(color);
+  $(".brush").css("background", color);
+}
