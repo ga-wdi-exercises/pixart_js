c7ae322227a8f0f7417bc07f0e765d86c3648ef3
Alex Bannon <alexbannon@gmail.com>
2015-07-08 15:36:55 -0400

    clicking a div with class square assigns it the color currently being displayed in the box

--- pixart.js
+++ pixart.js
@@ -1,8 +1,13 @@
+var currentColorChoice = $(".brush").css("background-color");
 $("#set-color, #color-field").on("click", function(event){
   event.preventDefault();
   var userInput = $("input").val();
   $(".brush").css("background-color", userInput);
+  currentColorChoice = $(".brush").css("background-color");
 })
 for(var i = 0; i < 20; i++) {
   $("body").append("<div class='square'></div>");
 }
+$(".square").on("click", function(){
+  $(this).css("background-color", currentColorChoice);
+})
