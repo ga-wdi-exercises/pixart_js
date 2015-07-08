fd4ce9b7900fae53708510c3c905e195301db14a
John Master <john.s.master@gmail.com>
2015-07-08 00:58:12 -0400

    Move event listener from each individual square to body, rewrote some jquery in vanilla

--- pixart.js
+++ pixart.js
@@ -1,4 +1,19 @@
-var button = $("#set-color")[0];
+var button = document.getElementById("set-color");
+var brush = document.getElementsByClassName("brush")[0];
+var body = document.getElementsByTagName("body")[0];
+
+for (var i = 0; i < 8000; i++){
+  $( "body" ).append( "<div class='square'></div>");
+}
+
+body.addEventListener("mouseover", function(e){
+  if (e.target !== e.currentTarget && e.target.className == "square"){
+        e.target.style.background = window.getComputedStyle(brush).backgroundColor;
+  }
+  e.stopPropagation(); //Was worried that this would interfere with tbe event listener on the button but it doesn't
+  }
+);
+
 
 button.addEventListener("click", changeColor);
 
@@ -8,13 +23,4 @@ function changeColor(){ //Why don't I need to pass event as an argument?
   $("#color-field").val("");
   console.log(color);
   $(".brush").css("background", color);
-}
-
-for (var i = 0; i < 8000; i++){
-  $( "body" ).append( "<div class='square'></div>");
-  $( ".square" )[i].addEventListener("mouseover", colorSquare)
-}
-
-function colorSquare(event) {
-  $(this).css("background", $(".brush").css("background"));
 }
