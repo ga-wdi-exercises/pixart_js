83189dc9a4392ab3d38708b79efe75077b35de0b
Erica Irving <contact.eirving@gmail.com>
2015-07-08 17:26:29 -0400

    condensed into two functions

--- pixart.js
+++ pixart.js
@@ -1,26 +1,26 @@
 $("document").ready(function() {
 
-function setBrushColor() {
-  event.preventDefault();
-  $(".brush").css("background-color", $("input").val());
-}
+  function setBrushColor() {
+    $("#set-color").on("click", function() {
+      event.preventDefault();
+      $(".brush").css("background-color", $("input").val());
+    });
+  }
 
-function setPixelColor() {
-  $(this).css("background-color", $("input").val());
-}
+  function makePaintableCanvas () {
+    for (var i = 1; i <= 8000; i++) {
+      $("<div class='square'></div>").appendTo($("body")).on("mouseover", function () {
+        $(this).css("background-color", $("input").val());
+      })
+    };
+  }
 
-$("#set-color").on("click", setBrushColor);
+  setBrushColor();
+  makePaintableCanvas();
+})
+
 // ###Commit 2
 // * The same thing should happen when I press the enter key from inside the input field
 
-function makeCanvas () {
-  for (var i = 1; i <= 8000; i++) {
-    $("<div class='square'></div>").appendTo($("body")).on("mouseover", setPixelColor)
-  };
-}
-makeCanvas();
-
 // ## Bonus
-
 // * Add a color swatch. You should have 3 boxes with the most recent 3 colors used. When you click on each of those boxes, it should set the current brush color back to that color.
-})
