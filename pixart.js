5e155c7abb5121114889f4bc0daa4b9cdcdaf17b
Silvia Brown <siltastic@gmail.com>
2015-07-08 17:06:36 -0400

    Removed extra spaces.

--- pixart.js
+++ pixart.js
@@ -1,43 +1,3 @@
-
-// When user wants to change the color to create pixel art.
-// User should locate the input field.
-// User types in a color.
-
-// var setColor = document.getElementById("set-color");
-
-// $(document).ready(function() {
-// 	console.log("Page is ready.");
-
-// 	// Click the submit button.
-// 	$("form")[0].addEventListener("submit", function(event) {
-// 		event.preventDefault(); // Prevet the trigger.
-
-// 		// Change the brush box color that relates to the input value.
-// 		var submitColor = $("#color-field").val();
-// 		$(".brush").css("background", submitColor)
-// 			console.log("Changed color in the brush box.");
-// 	});
-
-// 	// Create 20 divs of the "square" class and append them to the body
-// 	for (var i = 0; i < 20; i++) {
-// 		$("body").append($("<div class='square'/>"));
-// 		// On append, use <div /> instead of </div> tag.
-
-// 	}
-
-// 	// Changes the color of that individual square to typed color.
-	
-
-// 	$(".square").mouseover(function() {
-// 		var submitColor = $("#color-field").val();
-
-// 		$(".square").css("background", submitColor)
-// 	});
-
-// });
-
-
-
 $(document).ready(function() {
 	console.log("Page is ready.");
 
@@ -48,7 +8,6 @@ $(document).ready(function() {
 		// Add color value to the brush.
 		$(".brush").css("background",$("#color-field").val());
 			console.log("Changed color in the brush box.");
-
 	});
 
 	// Create 20 DIVs of the "square" class. 
@@ -63,5 +22,4 @@ $(document).ready(function() {
 		$(this).css("background",$("#color-field").val());
 			console.log("Individually changed each .square color value.");
 	});
-
 });
