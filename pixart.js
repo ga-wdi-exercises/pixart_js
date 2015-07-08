2b6ec01b0d0e2753fefc0985ba99955d499a6765
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-07-08 13:05:20 -0400

    added listener to button. changed color to user input

--- pixart.js
+++ pixart.js
@@ -1,1 +1,14 @@
+// add event listener to set color button
+// button auto refreshes page...use preventDefault
+// save color from input field to a variable userInput
+// change color of brush box to value of userInput variable
+// clear text box
 
+var userInput = $("#color-field");
+$("#set-color")[0].addEventListener("click", function(){
+  event.preventDefault();
+//  console.log("button clicked");
+//  console.log(userInput.val());
+  $(".brush").css("background", userInput.val());
+  userInput.val("");
+})
