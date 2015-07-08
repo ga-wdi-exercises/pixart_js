d55149815be7c54eb7a3c2f296c08ea4f2edc7bf
Mostafa Habib <MostafaSHabib@gmail.com>
2015-07-08 17:03:46 -0400

    20 squares added

--- pixart.js
+++ pixart.js
@@ -6,3 +6,12 @@ $('#set-color').click(function(){
   console.log("this works");
   $('.brush').css('background' , $('input').val());
 });
+
+
+for (var i = 0; i<20; i++)
+  $('body').append('<square>');
+
+
+// $('.square').addClass().each(function(){
+//   $(this).append();
+// })
