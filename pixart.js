6bc405e96eb8d4335daa3d23230c665c11e88856
Robert <i.am.robert.benjamin@gmail.com>
2015-07-08 17:20:15 -0400

    Add 20 divs with class square
    Uses a for loop and appends then to the body.

--- pixart.js
+++ pixart.js
@@ -1,6 +1,10 @@
 $("#set-color").click(function (e){ 
 	e.preventDefault();
 	var color = $("#color-field").val();
-	console.log(color)
-	$(".brush").css("background-color", color)
-})
+	$(".brush").css("background-color", color);
+	$("input").val('');
+});
+
+for(i = 0; i < 20; i++) {
+	$("body").append("<div class = 'square'></div>");
+}
