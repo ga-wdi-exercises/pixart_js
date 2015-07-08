96e22e2b7d69588028e5eb188a4c151104f1be78
Samuel Key <s.key08@gmail.com>
2015-07-08 08:36:07 -0400

    homework

--- pixart.js
+++ pixart.js
@@ -1,6 +1,7 @@
 var colorButton = $("#set-color")[0];
-var inputBox = $("#color-field")
+var inputBox = $("#color-field");
 var colorInput;
+var divBoxes = $(".square");
 //document.getElementById( "set-color");
 //console.log(colorButton)
 //input a color into the text field
@@ -9,14 +10,35 @@ var colorInput;
 function changeSquareColor(){
   event.preventDefault()
   colorInput=$("input").val();
-  $( ".brush" ).css( "background-color", colorInput)
+  $( ".brush" ).css( "background", colorInput)
 }
 colorButton.addEventListener("click", changeSquareColor);
 
-for(var i = 1; i < 21; i++) {
+for(var i = 1; i < 8001; i++) {
  $("body").append('<div class="square" / div>' )
  }
+
+ // divBoxes.addEventListener("click", function(){
+ //   event.preventDefault()
+ //   $( ".square" ).css( "background-color", "green"  );
+
+ function changeDivColor(){
+   //event.preventDefault()
+   //colorInput=$("input").val();
+   divBoxes.addEventListener("click", changeDivColor);
+   $( ".square" ).css( "background", "green")
+ }
+ // colorButton.addEventListener("click", function(){
+ //   event.preventDefault()
+ //   $( ".brush" ).css( "background-color", "green" );
 //$("#set-color").on("click", changeSquareColor);
 //find the brush box
 //var brushBox = $(".brush")[0];
 //make brush box change to color entered
+
+//when user clicks one of the div squares
+  //that square turns green
+  //square remains green
+
+//divBoxes.addeventListener("mouseover", changeDivColor);
+//$( ".square" ).css( "background", "green")
