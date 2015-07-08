6a888b6138182895c469db33d76bf1ad82ccd0db
Christine Lovett <clovett07@gmail.com>
2015-07-07 22:20:32 -0400

    commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,10 @@
+var input = document.getElementById("form");
+var button = $("button");
+var brushBox = $("div").eq(1);
+var value = null;
 
+button.addEventListener("submit", function(event){
+    event.preventDefault;
+    brushBox.style.background = input.value;
+    })
+//code doesn't work. I get an uncaught type error - button.listener isn't a function
