6d3c330ee2b4e2a32bfb5718d246f0b66f6e0853
Walter <walter.augustine10@gmail.com>
2015-07-08 17:16:22 -0400

    added change brush to input color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
+$(document).ready (function() {
 
+})
+// set brush to color, get from color field, listen for button click, handle body clicks
+$("button").on("click", function(event){
+  event.preventDefault();
+  var newColor = $("#color-field").val();
+  console.log(newColor);
+  $(".brush").css("background-color", newColor);
+
+})
