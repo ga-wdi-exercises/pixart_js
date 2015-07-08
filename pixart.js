c2958e45eb8825d30770bc7cc98a93a7e44c897b
tomBeach <tbeach2k@gmail.com>
2015-07-08 15:26:34 -0400

    making colored divs

--- pixart.js
+++ pixart.js
@@ -2,48 +2,77 @@
 
 var getColorBtn = $('#set-color');
 var brushBox = $('.brush');
-console.log('getColorBtn: ' + getColorBtn.attr("id"));
-console.log('brushBox: ' + brushBox.attr("id"));
 
 // click  "Set Color" button => change "brush" box color to input field value
-$("#set-color").click(function(event){
+$("#set-color").on('click', function(event){
   event.preventDefault()
-  console.log('getColorBtn:' + getColorBtn.attr("id"));
   var nextColor = $("#color-field").val();
-  console.log('nextColor:' + nextColor);
   $('.brush').css('background-color', nextColor);
-  var offset = $(this).offset();
-  console.log('this:' + this);
-  var relativeX = (event.pageX - offset.left);
-  var relativeY = (event.pageY - offset.top);
-  console.log('relativeX:' + relativeX);
-  console.log('relativeY:' + relativeY);
-
 });
 
 // same as above for  enter key press (from inside the input field)
-$('#color-field').keydown(function (e){
-    if(e.keyCode == 13){
+$('#color-field').keydown(function (event){
+    if(event.keyCode == 13){
         console.log('you pressed enter ^_^');
     }
 })
 
-// Create 20 divs of the "square" class and append them to the body (.append())
-for (var i = 0; i < 20; i++) {
-
-}
-
-$(function() {
-  $("#demo-box").click(function(e) {
-
-    var offset = $(this).offset();
-    var relativeX = (e.pageX - offset.left);
-    var relativeY = (e.pageY - offset.top);
-
-    alert("X: " + relativeX + "  Y: " + relativeY);
-
-  });
+$( "body" ).mousemove(function( event, nextColor ) {
+  var msg = "event.pageXY ";
+  msg += event.pageX + ", " + event.pageY;
+  console.log(msg);
+  var paintTrail = document.createElement("div");
+  $("body").append(paintTrail);
+  $(paintTrail).width(10);
+  $(paintTrail).height(10);
+  $(paintTrail).css('position', 'absolute');
+  $(paintTrail).css('background-color', 'black');
+  $(paintTrail).css('border', '1px solid black');
+  $(paintTrail).css('left', event.pageX);
+  $(paintTrail).css('top', event.pageY);
+  console.log($(paintTrail).css('left'));
 });
+
+// $('body').mousemove(function (event) {
+//   var paintTrail = document.createElement("div");
+//   $("body").append(paintTrail);
+//   $(paintTrail).width(10);
+//   $(paintTrail).height(10);
+//   $(paintTrail).css('position', 'absolute');
+//   $(paintTrail).css('offset', 100);
+//   console.log('position:' + $(paintTrail).css('position'));
+//   // console.log('$(paintTrail).width():' + $(paintTrail).width());
+//   // console.log('$(paintTrail).css.position:' + $(paintTrail).css.position);
+//   // $("body").append(document.createElement("div"));
+//   $('div').length;
+//   console.log('$(div).length:' + $('div').length);
+//   // console.log('$paintTrail: ' + $paintTrail);
+//   // $(paintTrail).offset({top:event.pageY,left:event.pageX});
+//   // console.log('event.pageXY: ' + event.pageX + ' ' + event.pageY);
+// });
+
+
+
+
+// Create 20 divs of the "square" class and append them to the body (.append())
+// var divHtml = "<div id='squareTrails'></div>";
+// var divString = '';
+// for (var i = 0; i < 20; i++) {
+//   $('.brush').offset({top:event.pageY,left:event.pageX});
+//   console.log('i:' + i);
+// }
+
+// $(function() {
+//   $("#demo-box").click(function(e) {
+//
+//     var offset = $(this).offset();
+//     var relativeX = (e.pageX - offset.left);
+//     var relativeY = (e.pageY - offset.top);
+//
+//     alert("X: " + relativeX + "  Y: " + relativeY);
+//
+//   });
+// });
 
 // click on each "square", it changes the color of that individual square to "green"
 // Hint: add event listener while creating squares, or listen for events on the body element
