96cd8e41e1ab29855f2a7daefdf10f7257ee433e
Omari Mayers-Walker <omari@fastmail.fm>
2015-07-07 21:56:56 -0400

    README.md: changed addEventListener to click. pixart.js: added event listener that changes color of brush box with the enter key or button push

--- pixart.js
+++ pixart.js
@@ -1,1 +1,16 @@
+var submitButton = $("#set-color");
 
+// submitButton.addEventListener("click", function(event){
+    // event.preventDefault();
+    // console.log("test");
+// });
+
+submitButton.click('click', function(event){
+    event.preventDefault();
+
+    // changes box color based on input
+    var colorChoice = $("#color-field").val();
+    var brushBox = $(".brush");
+
+    brushBox.css("background-color", colorChoice);
+});
