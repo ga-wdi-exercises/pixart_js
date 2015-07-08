af38952c83c292ee486f87556cc44548b7f257ed
Preston Davies <pdavies88@gmail.com>
2015-07-08 16:59:21 -0400

    commit 2 completed

--- pixart.js
+++ pixart.js
@@ -1,6 +1,13 @@
-$("#set-color").on("click", function(event) {
+/*$("#set-color").on("click", function(event) {
   event.preventDefault();
   console.log("set color clicked");
   var inputColor = $("input").val()
+  $(".brush").css("background", inputColor)
+});*/
+
+$("form").submit(function(event) {
+  event.preventDefault();
+  console.log("set color clicked");
+ var inputColor = $("input").val()
   $(".brush").css("background", inputColor)
 });
