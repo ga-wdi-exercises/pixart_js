0b3ce16f40f2e7ee5d3ce1c00f436e640b54898f
Matt Curtis <Curtismn87@yahoo.com>
2015-07-08 16:33:03 -0400

    commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,8 @@
 
+//var input = $("#color-field")
+
+$("form").on("submit", function(){
+  console.log("working");
+  event.preventDefault();
+  $(".brush").css("background-color", $("#color-field").val())
+  })
