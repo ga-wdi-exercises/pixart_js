2a63e1777edda2306e559e1e24838aea594b7b68
Erica Irving <contact.eirving@gmail.com>
2015-07-08 17:17:59 -0400

    restored working code

--- pixart.js
+++ pixart.js
@@ -1,12 +1,5 @@
 $("document").ready(function() {
 
-// ###Commit 1
-// * When I click the "Set Color" button, it should change the color of the "brush" box to the color I specify in the input field. (**Hint:** You will need to use `event.preventDefault()` somewhere in your code.)
-// * Use jQuery to select the element, and `addEventListener` to handle clicks
-// document.getElementById("set-color").addEventListener("click", function (){
-//   event.preventDefault();
-//   $(".brush").css("background-color", $("input").val());
-// });
 function setBrushColor() {
   event.preventDefault();
   $(".brush").css("background-color", $("input").val());
@@ -19,8 +12,6 @@ function setPixelColor() {
 $("#set-color").on("click", setBrushColor);
 // ###Commit 2
 // * The same thing should happen when I press the enter key from inside the input field
-//{$("input").on("keyup", setBrushColor);}
-//if((event.keyCode || event.which) == 13)
 
 function makeCanvas () {
   for (var i = 1; i <= 8000; i++) {
@@ -30,6 +21,6 @@ function makeCanvas () {
 makeCanvas();
 
 // ## Bonus
-//
+
 // * Add a color swatch. You should have 3 boxes with the most recent 3 colors used. When you click on each of those boxes, it should set the current brush color back to that color.
 })
