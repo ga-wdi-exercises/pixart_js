dce57f3cd761e8fd59592193b32fe27ea9d6dc7f
lon phelps <puzzleboks@gmail.com>
2015-07-07 21:43:33 -0400

    created function to change color of box from input

--- pixart.js
+++ pixart.js
@@ -1,1 +1,13 @@
-
+//commit 1
+  //use event.preventDefault
+  //when use clickes on "set color" button:
+    //change the color of the "brush" box to the color specified in the input field
+    //get value of input
+    //brush box: set background
+$(document).ready( function(){
+  $("#set-color").on("click", function(){//(event)?
+    event.preventDefault();
+    var colorVal = $("#color-field").val();
+    $( ".brush").css( "background-color", colorVal);
+  })
+})
