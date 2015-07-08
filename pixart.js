e4eae1139f4da9772a9342d21f2f99693c9ff95b
Omari Mayers-Walker <omari@fastmail.fm>
2015-07-07 22:45:47 -0400

    added event listener; turns squares green

--- pixart.js
+++ pixart.js
@@ -17,9 +17,18 @@ submitButton.click('click', function(event){
 
 function squareMaker (){
     for (var i = 0; i < 20; i++){
-        var twentySquares = $("body").after($('<div></>').addClass(".square"));
+        var twentySquares = $("body").after($('<div></>').addClass("square"));
     }
     return twentySquares;
 }
 
 squareMaker();
+
+$('body').click('click', function(event){
+    event.preventDefault();
+
+    var colorChoice = $("#color-field").val();
+    console.log(colorChoice);
+    $(".square").css("background-color", "green");
+});
+
