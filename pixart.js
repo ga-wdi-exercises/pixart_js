012be2d277eb6ba46d01e25cb01eb7d088b6f48a
Alex Bannon <alexbannon@gmail.com>
2015-07-08 15:41:17 -0400

    finished div creation and mouseover to be able to paint picture

--- pixart.js
+++ pixart.js
@@ -5,9 +5,9 @@ $("#set-color, #color-field").on("click", function(event){
   $(".brush").css("background-color", userInput);
   currentColorChoice = $(".brush").css("background-color");
 })
-for(var i = 0; i < 20; i++) {
+for(var i = 0; i < 8000; i++) {
   $("body").append("<div class='square'></div>");
 }
-$(".square").on("click", function(){
+$(".square").on("mouseover", function(){
   $(this).css("background-color", currentColorChoice);
 })
