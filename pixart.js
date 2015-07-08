2128d01212995d1f90e1294829fd2f16cd554974
Matt Curtis <Curtismn87@yahoo.com>
2015-07-08 17:41:00 -0400

    Final commit of pixart

--- pixart.js
+++ pixart.js
@@ -6,11 +6,13 @@ $("form").on("submit", function(){
   })
 
 // Create 20 divs of the "square" class and append them to the body
-for (var i = 0; i < 20; i++){
+for (var i = 0; i < 8000; i++){
   $("body").append('<div class="square"></div>');
-  $(".square").on("click", function(){ $(this).css('background' , 'green')
-  })
   }
+$(".square").on("mouseover", function(){
+  $(this).css('background-color' , 'green')}
+  )
+
 
 //* Add functionality so that when I click on each "square", it changes the color of that individual square to "green"
 // **Hint**: either add the event listener while creating the squares, or listen for events on the `body` element
