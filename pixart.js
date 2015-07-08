47e504c72dfa4cc50968c3a4e9a616c5260af32f
tomBeach <tbeach2k@gmail.com>
2015-07-08 12:29:56 -0400

    initial commit

--- pixart.js
+++ pixart.js
@@ -1,1 +1,15 @@
 
+
+// click  "Set Color" button => change "brush" box color to input field value
+// Use jQuery to select the element, and addEventListener to handle clicks
+
+// same as above for  enter key press (from inside the input field)
+
+// Create 20 divs of the "square" class and append them to the body (.append())
+
+// click on each "square", it changes the color of that individual square to "green"
+// Hint: add event listener while creating squares, or listen for events on the body element
+
+// modify: changes to the color I set using my input instead of "green" every time.
+
+// Modify the CSS so that the "square" class has a height and width of 10px and a margin of 0.
