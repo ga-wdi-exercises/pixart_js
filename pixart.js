592ca60cf05a249b3a94b02c21eb80da3dcdf8a1
Alex Bannon <alexbannon@gmail.com>
2015-07-08 15:30:27 -0400

    for looped 20 divs with class square

--- pixart.js
+++ pixart.js
@@ -3,3 +3,6 @@ $("#set-color, #color-field").on("click", function(event){
   var userInput = $("input").val();
   $(".brush").css("background-color", userInput);
 })
+for(var i = 0; i < 20; i++) {
+  $("body").append("<div class='square'></div>");
+}
