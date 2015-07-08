60cf4b65ccac42b9fd2ea7a0ee0278191a2d9869
LEIGH VARLEY <LEIGHVARLEY@GMAIL.COM>
2015-07-07 21:19:31 -0400

    Commit 1: brush box

--- pixart.js
+++ pixart.js
@@ -1,1 +1,10 @@
-
+//When user clicks "set color" button, change the color of the "brush" box to the color specified in the input field.
+//I think that event.preventDefault is necessary so that the program doesn't set the color on click before reading the rest of the function.
+//Replace the background color with the "color field" value.
+       //.val() is used to get values from input forms.
+//Used console.log to make sure function worked before moving on.
+$( "#set-color" ).click(function() {
+  event.preventDefault();
+  $( ".brush" ).css( "background-color" , $( "#color-field" ).val());
+  //console.log("working");
+})
