2e898a6b38f48fb6d54277ef2a3b7cf5402b833c
Rachel Warren <raelwa@gmail.com>
2015-07-07 22:09:58 -0400

    changed to jquery value getter on line 16

--- pixart.js
+++ pixart.js
@@ -1,1 +1,18 @@
+// var colorField = document.getElementById("color-field");
 
+var colorInput;
+//button=setColor//
+// var button = $("#set-color")
+
+//funtion to understand form input value?
+
+$("#set-color").on("click", setColor) //when the button is clicked, do function setColor
+$("#set-color").on("")
+
+//when the button is clicked, the box should change color to match value of input field
+function setColor(){
+  event.preventDefault();
+  //needs to understand the value of the color input.
+  colorInput = $("#color-field").value;
+  $(".brush").css("background-color", colorInput)
+}
