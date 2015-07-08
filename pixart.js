884e9ba158a5bd4d3d1fa88cfda75c3405acc4ff
Silvia Brown <siltastic@gmail.com>
2015-07-08 15:15:17 -0400

    Added addEventListener to submit a color.

--- pixart.js
+++ pixart.js
@@ -1,1 +1,19 @@
 
+// When user wants to change the color to create pixel art.
+// User should locate the input field.
+// User types in a color.
+
+var setColor = document.getElementById("set-color");
+
+$(document).ready(function() {
+
+	// Click the submit button.
+	$("form")[0].addEventListener("submit", function(event) {
+		event.preventDefault(); // Prevet the trigger.
+
+		// Change the brush box color that relates to the input value.
+		var submitColor = $("#color-field").val();
+		$(".brush").css("background", submitColor)
+			console.log("Changed color in the brush box.");
+	})
+})
