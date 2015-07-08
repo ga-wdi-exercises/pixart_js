206975a0601f7b7caa042b410b66292e2f443a8f
Erica Andersen <andersenek@gmail.com>
2015-07-08 17:13:47 -0400

    Functionality for individual squares to be clicked and changed to green

--- pixart.js
+++ pixart.js
@@ -1,15 +1,22 @@
 // Select a DOM element with an id of set-color
 var setColor = document.getElementById( "set-color" ); // Selects "Set Color" button
+var squareColor = document.getElementsByClassName( "square" ); // Selects ".square"
 
 // Use jQuery to select the element, and addEventListener to handle clicks on "Set Color" button
 $( setColor ).click(function() {
   event.preventDefault(); // event.preventDefault() prevents the auto page refresh
-  var applyColor = $('#color-field').val(); // Get Color Value
+  var applyColor = $( "#color-field" ).val(); // Get Color Value
   $( ".brush" ).css( "background", applyColor ); // Set Color Value
   console.log("click listener is working")
 });
 
 // Create 20 divs of the "square" class and append them to the body
 for( var i = 0; i < 20; i++){
-  $("body").append("<div class=square></div>"); // this adds the div as the last child of body with .square
+  $("body").append("<div class=square></div>"); // This adds the div as the last child of body with .square
+  $( squareColor ).each(function() { // Add functionality so that when I click on each "square"
+    $(this).click(function() { // Hint: either add the event listener while creating the squares
+      $(this).css( "background", "green" ); // Changes the color of that individual square to "green"
+      console.log("square click listener is working")
+    });
+  });
 }
