3fff041a59ec448ab87bc8bfa9b17575157c64d3
Matt Curtis <Curtismn87@yahoo.com>
2015-07-08 16:57:02 -0400

    added 20 div class squares

--- pixart.js
+++ pixart.js
@@ -1,8 +1,10 @@
 
-//var input = $("#color-field")
-
 $("form").on("submit", function(){
   console.log("working");
   event.preventDefault();
   $(".brush").css("background-color", $("#color-field").val())
   })
+
+// Create 20 divs of the "square" class and append them to the body
+for (var i = 0; i < 20; i++){
+  $("body").append('<div class="square"></div>')}
