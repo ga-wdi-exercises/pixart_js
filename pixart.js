875f78c74b8de60f68b43ed6b452c759e4a07261
Ryan Kahle <jrkahle@hotmail.com>
2015-07-07 21:49:09 -0400

    Final product

--- pixart.js
+++ pixart.js
@@ -1,28 +1,28 @@
 
 
-
+// Global for color
 var color;
 
+// EventListener for button
 $( "#set-color" )[0].addEventListener( "click", function(event) {
   event.preventDefault();
-  color = $(" #color-field" ).val();
+  color = $(" #color-field" ).val(); // Setting color
   $( ".brush" ).css( "background-color" , color);
+  $( "#color-field" ).val(" "); // Gettng rid of that nasty input
 })
 
+// Adding Divs
 function appendDiv() {
-  for(var i = 0; i < 20; i++) {
+  for(var i = 0; i < 8000; i++) {
     $( "body" ).append( "<div class='square'></div>" );
   }
-
 }
 
 appendDiv();
 
-$ ( "body div.square" ).click(function(){
+// Mouseover, background change
+$ ( "div.square" ).on( "mouseover" , function() {
 
   $( this ).css( "background-color" , color );
+
 });
-
-
-
-// Modify your code so that when I click on each "square", it changes to the color I set using my input instead of "green" every time.
