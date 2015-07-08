52f5864e91784dd0bda3e68c8c7e73f15124e1c0
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-07-08 15:10:55 -0400

    appended divs change to color of brush when clicked

--- pixart.js
+++ pixart.js
@@ -4,7 +4,7 @@ $("#set-color")[0].addEventListener("click", function(){
   event.preventDefault();
 //  console.log("button clicked");
 //  console.log(userInput.val());
-  $(".brush").css("background", userInput.val());
+  $(".brush").css("background-color", userInput.val());
   userInput.val("");
 })
 
@@ -19,13 +19,14 @@ $("set-color").keypress(function (e) {
 
 // commit 3 - append 20 divs
 // commit 4 - add click event listener and change to green
+// commit 5 - change green to color of brush background
 $("window").load(appendSquares());
 
 function appendSquares(){
   for (var i = 0; i < 20; i++) {
     console.log("window has loaded");
     $("body").append( $("<div></div>").addClass("square").click(function(){
-      $(this).css("background-color", "green");
+      $(this).css("background-color",$(".brush").css("background-color"));
     }));
   }
 }
