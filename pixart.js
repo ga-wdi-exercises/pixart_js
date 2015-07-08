ff557e1e72d63d4031ef9b2306399c12c429b0d6
lanak1301 <musiconstellation@gmail.com>
2015-07-08 00:42:31 -0400

    back to before (working as object)

--- pixart.js
+++ pixart.js
@@ -1,4 +1,33 @@
 
+var pixelArt = {
+
+	color : $( "input" ) ,
+	brushBox : $( ".brush" ) ,
+
+	canvas : function() {
+		var self = this ; 
+
+		for ( var i = 0; i < 8000; i++ ) {
+			$( "<div></div>" ).addClass('square').appendTo( "body" ).hover( function() {
+				$(this).css("background", self.color.val()) ;
+			}) ;
+		} 
+	} ,
+
+	eventListener : function() {
+		var self = this ;
+
+		$( "#form" ).submit( function( event ) {
+			event.preventDefault() ; 
+			self.brushBox.css( "background-color", self.color.val() ) ;
+		} ) 
+	}
+}
+
+pixelArt.canvas() ;
+pixelArt.eventListener() ; 
+
+/*
 var color = $( "input" ) ;
 var brushBox = $( ".brush" ) ;
 
@@ -15,91 +44,6 @@ $( "#form").submit( function( event ) {
 	brushBox.css( "background-color", color.val() ) ;
 } );
 
-// inputField.submit( function( event ) {
-// 	event.preventDefault() ; 
-// 	brushBox.css( "background-color", color.val() ) ;
-// } ) ; 
-
 canvas() ;
-
-
-
-
-/*
-
-var pixelArt = {
-	clickMe : document.getElementById( "set-color") ,
-	color : $( "input" ) ,
-	myText : $( "span" ) ,
-	brushBox : $( ".brush" ) ,
-	inputField : document.getElementById( "color-field" ) ,
-	body : document.body ,
-
-	// changeColor : function() {
-	// 	.css( "background-color", "red" ) ; 
-	// } ,
-
-	changeColor : function() {
-			document.getElementsByClassName( "square" ).css("background-color", "red" ) ;
-		} , 
-
-	canvas : function() {
-		var self = this ; 
-
-		for ( var i = 0; i < 8000; i++ ) {
-			$( "<div></div>" ).addClass('square').appendTo( "body" ) ;
-			document.getElementsByClassName( "square" ).addEventListener( "mouseover", this.changeColor ) ;
-			} 
-		} ,
-
-	eventListener : function() {
-		var self = this;
-
-		self.clickMe.addEventListener( "click", function() {
-			event.preventDefault() ; 
-			self.brushBox.css( "background-color", self.color.val() ) ;
-		} ) , 
-
-		self.inputField.addEventListener( "submit", function() {
-			event.preventDefault() ; 
-			self.brushBox.css( "background-color", self.color.val() ) ;
-		} ) 
-	}
-
-}
-
-pixelArt.canvas() ;
-pixelArt.eventListener() ; 
-
-/*
-	var clickMe = document.getElementById( "set-color") ; 
-	var color = $( "input" ).val() ; 
-	// var brushBox = $( ".brush" ) ;
-
-//clicking on button -> change brushBox
-	clickMe.addEventListener( "click", function() {
-		event.preventDefault() 
-		var self = this ; 
-		console.log( "blah" ) ;
-		// getValue();
-		console.log( self.color ) ;
-		$( "span" ).text( self.color );
-		$( ".brush" ).css( "background" ) ;
-		var bla = $('input').val();
-		alert(bla);
-		
-		
-	})
-
-// function getValue() {
-// 	$( "input" )
-//   .keyup(function() {
-//     var value = $( this ).val();
-//     $( "span" ).text( value );
-//     return value;
-//   })
-//   .keyup();
-
-// }
 
 */
