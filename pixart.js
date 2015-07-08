d4e0dfaae165e3d44e0a02600d1546a9407f6e50
Kevin Cole Jr <KevinColeLr@gmail.com>
2015-07-08 16:47:01 -0400

    color changes now upon pressing enter

--- pixart.js
+++ pixart.js
@@ -7,7 +7,14 @@ $("#set-color").on('click' , function(t){
 //When you click the set color button what do you want it to do
 t.preventDefault();
 //set value of the input field...
- var theseColors = $("input").val();
+ var clickToChangeColors = $("input").val();
  //Take the brush and change it's background to whatever theseColors input is
- $(".brush").css("background", theseColors);
+ $(".brush").css("background", clickToChangeColors);
+});
+
+
+// same thing should happen upon hitting enter
+$("input").keypress(function(){
+  var enterChangeColor = $("input").val();
+  $(".brush").css("background", enterChangeColor);
 });
