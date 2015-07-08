d66ed86cf95b2e562ec5f163497900d36e3abba5
Alex Bannon <alexbannon@gmail.com>
2015-07-08 17:28:16 -0400

    changed variable for clarity and changed initial brush colors and styling

--- pixart.js
+++ pixart.js
@@ -1,30 +1,35 @@
 var currentColorChoice = $(".brush").css("background-color");
-var userPicksTally = 0;
+var numberOfUserPicksTally = 0;
+$(".brush").css({
+  "display": "inline-block",
+  "margin": "15px 15px",
+  "background-color": "#1B4370"
+})
 function userPickedAColor(event) {
   event.preventDefault();
   var userInput = $("input").val();
   $(".brush").css("background-color", userInput);
   currentColorChoice = $(".brush").css("background-color");
   $("input").val("");
-  if(userPicksTally > 0 && userPicksTally < 2) {
+  if(numberOfUserPicksTally > 0 && numberOfUserPicksTally < 2) {
     $(".controls").append("<div class='brush3'></div>");
-    userPicksTally++;
+    numberOfUserPicksTally++;
     $(".brush3").css({
-      "width": "100px",
-      "height": "100px",
-      "background": "#1B4370",
-      "margin": "15px 15px",
-      "display": "inline-block"
+    "width": "100px",
+    "height": "100px",
+    "background-color": "#1B4370",
+    "margin": "15px 15px",
+    "display": "inline-block"
     })
-    userPicksTally++;
+    numberOfUserPicksTally++;
   }
-  if(userPicksTally < 1) {
+  if(numberOfUserPicksTally < 1) {
     $(".controls").append("<div class='brush2'></div>");
-    userPicksTally++;
+    numberOfUserPicksTally++;
     $(".brush2").css({
       "width": "100px",
       "height": "100px",
-      "background": "#1B4370",
+      "background-color": "#1B4370",
       "margin": "15px 15px",
       "display": "inline-block"
     })
@@ -32,7 +37,23 @@ function userPickedAColor(event) {
 }
 $("#set-color").on("click", userPickedAColor);
 $("#color-field").on("submit", userPickedAColor);
-
+$(".brush").on("click", function(event){
+  event.preventDefault();
+  console.log("user clicked!");
+  currentColorChoice = $(".brush").css("background-color");
+});
+if(numberOfUserPicksTally < 1){
+  $(".brush2").on("click", function(event){
+    event.preventDefault();
+    console.log("user clicked!");
+    currentColorChoice = $(".brush2").css("background-color");
+  });
+}
+$(".brush3").on("click", function(event){
+  event.preventDefault();
+  console.log("user clicked!");
+  currentColorChoice = $(".brush3").css("background-color");
+});
 for(var i = 0; i < 8000; i++) {
   $("body").append("<div class='square'></div>");
 }
