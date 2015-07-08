cb783b4c430496a856fcf97a1b66dd3e98f295b0
Kevin Cole Jr <KevinColeLr@gmail.com>
2015-07-08 16:35:21 -0400

    Makes Color button and input fields work

--- pixart.js
+++ pixart.js
@@ -1,1 +1,13 @@
+//Get set color button
+$("#set-color");
+//When change color selected... it should change to whatever is in input field
 
+//setup event listener for click button
+$("#set-color").on('click' , function(t){
+//When you click the set color button what do you want it to do
+t.preventDefault();
+//set value of the input field...
+ var theseColors = $("input").val();
+ //Take the brush and change it's background to whatever theseColors input is
+ $(".brush").css("background", theseColors);
+});
