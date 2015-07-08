7af94568d1fe6bee599bd48cf3d60f82c8be668d
tomBeach <tbeach2k@gmail.com>
2015-07-08 17:51:27 -0400

    20 squares color change working

--- pixart.js
+++ pixart.js
@@ -2,77 +2,65 @@
 
 var getColorBtn = $('#set-color');
 var brushBox = $('.brush');
+var nextColor;
 
 // click  "Set Color" button => change "brush" box color to input field value
 $("#set-color").on('click', function(event){
-  event.preventDefault()
-  var nextColor = $("#color-field").val();
+  event.preventDefault();
+  nextColor = $("#color-field").val();
   $('.brush').css('background-color', nextColor);
 });
 
-// same as above for  enter key press (from inside the input field)
+// same as above for enter key press (from inside the input field)
 $('#color-field').keydown(function (event){
     if(event.keyCode == 13){
         console.log('you pressed enter ^_^');
     }
-})
-
-$( "body" ).mousemove(function( event, nextColor ) {
-  var msg = "event.pageXY ";
-  msg += event.pageX + ", " + event.pageY;
-  console.log(msg);
-  var paintTrail = document.createElement("div");
-  $("body").append(paintTrail);
-  $(paintTrail).width(10);
-  $(paintTrail).height(10);
-  $(paintTrail).css('position', 'absolute');
-  $(paintTrail).css('background-color', 'black');
-  $(paintTrail).css('border', '1px solid black');
-  $(paintTrail).css('left', event.pageX);
-  $(paintTrail).css('top', event.pageY);
-  console.log($(paintTrail).css('left'));
 });
 
-// $('body').mousemove(function (event) {
-//   var paintTrail = document.createElement("div");
-//   $("body").append(paintTrail);
-//   $(paintTrail).width(10);
-//   $(paintTrail).height(10);
-//   $(paintTrail).css('position', 'absolute');
-//   $(paintTrail).css('offset', 100);
-//   console.log('position:' + $(paintTrail).css('position'));
-//   // console.log('$(paintTrail).width():' + $(paintTrail).width());
-//   // console.log('$(paintTrail).css.position:' + $(paintTrail).css.position);
-//   // $("body").append(document.createElement("div"));
-//   $('div').length;
-//   console.log('$(div).length:' + $('div').length);
-//   // console.log('$paintTrail: ' + $paintTrail);
-//   // $(paintTrail).offset({top:event.pageY,left:event.pageX});
-//   // console.log('event.pageXY: ' + event.pageX + ' ' + event.pageY);
-// });
-
-
-
-
 // Create 20 divs of the "square" class and append them to the body (.append())
-// var divHtml = "<div id='squareTrails'></div>";
-// var divString = '';
-// for (var i = 0; i < 20; i++) {
-//   $('.brush').offset({top:event.pageY,left:event.pageX});
-//   console.log('i:' + i);
-// }
+var startX = 100;
+var startY = 100;
+for (var i = 0; i < 20; i++) {
+  startX = startX + 20;
+  startY = startY + 20;
+  var newDiv = document.createElement("div");
+  newDivId = 'newDiv' + i;
+  $(newDiv).addClass("square");
+  $(newDiv).attr("id", newDivId);;
+  $(newDiv).width(20);
+  $(newDiv).height(20);
+  $(newDiv).css({
+    position: 'absolute',
+    backgroundColor: 'blue',
+    left: startX,
+    top: startY
+  })
+  $("body").append(newDiv);
 
-// $(function() {
-//   $("#demo-box").click(function(e) {
-//
-//     var offset = $(this).offset();
-//     var relativeX = (e.pageX - offset.left);
-//     var relativeY = (e.pageY - offset.top);
-//
-//     alert("X: " + relativeX + "  Y: " + relativeY);
-//
-//   });
-// });
+  $(newDiv).on( "click", function(event) {
+    console.log("newDiv was clicked!");
+    $(event.target).css({ backgroundColor: 'green' });
+  });
+}
+
+
+$("body").mousemove(function(event) {
+  var paintTrail = document.createElement("div");
+  var nextColor = $("#color-field").val();
+  console.log('nextColor: ' + nextColor);
+  // $("body").append(paintTrail);
+  // $(paintTrail).width(10);
+  // $(paintTrail).height(10);
+  // $(paintTrail).css({
+  //   position: 'absolute',
+  //   backgroundColor: 'blue',
+  //   left: event.pageX,
+  //   top: event.pageY
+  // })
+
+});
+
 
 // click on each "square", it changes the color of that individual square to "green"
 // Hint: add event listener while creating squares, or listen for events on the body element
