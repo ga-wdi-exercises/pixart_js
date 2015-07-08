c7a458ef68786484042cd2c03e530600c3493f3f
Erica Irving <contact.eirving@gmail.com>
2015-07-08 16:01:41 -0400

    commit 6. change occurs on mouseover. canvas is 8000 divs.

--- pixart.js
+++ pixart.js
@@ -22,24 +22,13 @@ $("#set-color").on("click", setBrushColor);
 //{$("input").on("keyup", setBrushColor);}
 //if((event.keyCode || event.which) == 13)
 
-
-
-// ###Commit 5
 function makeCanvas () {
-  for (var i = 1; i <= 20; i++) {
-    $("<div class='square'></div>").appendTo($("body")).on("click", setPixelColor)
+  for (var i = 1; i <= 8000; i++) {
+    $("<div class='square'></div>").appendTo($("body")).on("mouseover", setPixelColor)
   };
 }
 makeCanvas();
-// * Modify your code so that when I click on each "square", it changes to the color I set using my input instead of "green" every time.
-//
-// ###Commit 6
-//
-// * Modify the CSS so that the "square" class has a height and width of 10px and a margin of 0.
-// * Modify your code so that you are creating 8000 divs instead of 20.
-// * Change the event that changes your box colors from 'click' to 'mouseover'
-// * Paint a picture!
-//
+
 // ## Bonus
 //
 // * Add a color swatch. You should have 3 boxes with the most recent 3 colors used. When you click on each of those boxes, it should set the current brush color back to that color.
