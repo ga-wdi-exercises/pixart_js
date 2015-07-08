0f37c286ba123e088c853f70633313bc97a34afa
ktw1222 <ktw1222@gmail.com>
2015-07-07 21:15:34 -0400

    commit 5 Modify

--- pixart.js
+++ pixart.js
@@ -13,7 +13,7 @@ $("#set-color").on("click", function(event){
 // The same thing should happen when I press the enter key from inside the input field
 $("#set-color").on("submit", function(event){
   event.preventDefault();
-  $(".brush").css("background-color", $("#color-field").val());
+  $(".brush").css("background-color", $("#color-field").val())
 })
 
 // Commit 3
@@ -35,14 +35,12 @@ for (i=0; i<20; i++) {
 //   $(".square").css("background-color", $("#color-field").val())
 // }
 document.body.addEventListener("click", function(event){
-    $(event.target).css("background-color", "green")
+    $(event.target).css("background-color", $("color-field").val())
 })
-
 
 // Commit 5
 // Modify your code so that when I click on each "square",
 // it changes to the color I set using my input instead of "green" every time.
-
 
 
 // Commit 6
