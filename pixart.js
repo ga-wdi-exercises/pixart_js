2eaf5c81367bc850e1224dca8837c04343685489
Erik Anderson <itserikanderson@gmail.com>
2015-07-09 18:32:02 -0400

    added js

--- pixart.js
+++ pixart.js
@@ -1,1 +1,21 @@
+for(var i = 0; i < 8000; i++){
+$("body").append("<div class=square></div>")
+  }
 
+var colorVal
+
+$( "#set-color" ).click(function() {
+  colorVal= $("#color-field").val()
+  event.preventDefault();
+  $(".brush").css("background-color", colorVal);
+  $("input").val("")
+});
+
+$("#color-field").keyup(function(e){
+  if(e.which === 13){
+  $(".brush").css("background-color", colorVal);
+  }
+})
+$(".square").mouseover(function(){
+   $(this).css("background-color", colorVal)
+ });
