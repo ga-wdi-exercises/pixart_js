70a6f9e632ad3b6ed5cc91237721a5520c139ed9
PETER CUTLER <PETERGCUTLER@GMAIL.COM>
2015-07-09 17:42:45 -0400

    commit 6 as instructed

--- pixart.js
+++ pixart.js
@@ -3,29 +3,11 @@ $(document).ready( function() {
     event.preventDefault();
     $(".brush").css("background-color", $("#color-field").val());
   })
-  var htmlAppend = "<div class='square'></div>" +
-                   "<div class='square'></div>" +
-                   "<div class='square'></div>" +
-                   "<div class='square'></div>" +
-                   "<div class='square'></div>" +
-                   "<div class='square'></div>" +
-                   "<div class='square'></div>" +
-                   "<div class='square'></div>" +
-                   "<div class='square'></div>" +
-                   "<div class='square'></div>" +
-                   "<div class='square'></div>" +
-                   "<div class='square'></div>" +
-                   "<div class='square'></div>" +
-                   "<div class='square'></div>" +
-                   "<div class='square'></div>" +
-                   "<div class='square'></div>" +
-                   "<div class='square'></div>" +
-                   "<div class='square'></div>" +
-                   "<div class='square'></div>" +
-                   "<div class='square'></div>" +
-                   "<div class='square'></div>"
-  $("body").append(htmlAppend);
-  $(".square").on("click", function (){
+  var htmlAppend = "<div class='square'></div>";
+  for (var i = 1; i <= 8000; i++) {
+      $("body").append(htmlAppend);
+    };
+  $(".square").mouseover(function (){
     $(this).css("background-color", $("#color-field").val());
   })
 })
