4c24254c8c357e6160206457a6bbe06ccb4ddb18
Amy <amruta.ghate@gmail.com>
2015-07-08 21:42:59 -0400

    commit 3 and 4 - add square divs that turn green on click

--- pixart.js
+++ pixart.js
@@ -1,4 +1,28 @@
+$(document).ready(function (){
+
 $("#set-color")[0].addEventListener("click", function(){
   event.preventDefault();
   $(".brush").css("background-color", $("#color-field").val());
 })
+var i = 0;
+while( i < 20){
+$("body").append("<div class='square'></div>");
+i++;
+}
+var squares = $("div.square");
+for(var x = 0; x < squares.length; x++){
+squares.eq(x).on("click", function(){
+  $(this).css("background-color", "green");
+})
+}
+
+
+
+
+
+
+
+
+
+
+});
