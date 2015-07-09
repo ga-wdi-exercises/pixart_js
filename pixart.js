239feb86e82b1f589e9851a0a767296420fa83eb
PETER CUTLER <PETERGCUTLER@GMAIL.COM>
2015-07-09 15:32:45 -0400

    commit 1 as instructed

--- pixart.js
+++ pixart.js
@@ -1,1 +1,16 @@
+// pure jquery
 
+$(document).ready( function() {
+  $("#set-color").on("click", function() {
+    event.preventDefault();
+    $(".brush").css("background-color", $("#color-field").val());
+  })
+})
+
+// broken jquery + javascript
+// $(document).ready( function() {
+//   $("#set-color").addEventListener("click", function() {
+//     event.preventDefault();
+//     $(".brush").css("background-color", $("#color-field").val());
+//   })
+// })
