d10735e9e92f5b017eca9bbcf6a7b3229a84d9c1
Amy <amruta.ghate@gmail.com>
2015-07-08 21:56:19 -0400

    commit 6 - add mouseover to action to 8k divs

--- pixart.js
+++ pixart.js
@@ -2,17 +2,20 @@ $(document).ready(function (){
 
 $("#set-color")[0].addEventListener("click", function(){
   event.preventDefault();
-  $(".brush").css("background-color", $("#color-field").val());
+  $(".brush").css("background-color", $("#color-field").val() || "#1B4370");
 })
+
 var i = 0;
-while( i < 20){
+
+while( i < 8000){
 $("body").append("<div class='square'></div>");
 i++;
 }
+
 var squares = $("div.square");
 for(var x = 0; x < squares.length; x++){
-squares.eq(x).on("click", function(){
-  $(this).css("background-color", $("#color-field").val());
+squares.eq(x).on("mouseover", function(){
+  $(this).css("background-color", $("#color-field").val() || "#1B4370");
 })
 }
 
