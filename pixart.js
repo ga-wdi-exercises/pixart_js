285ec5ee908a157c5a5fbec67063909c5e1b8119
Erica Irving <contact.eirving@gmail.com>
2015-07-09 16:53:58 -0400

    with extra keypress script

--- pixart.js
+++ pixart.js
@@ -1,19 +1,23 @@
 $("document").ready( function() {
 
+  function setBrushColor() {
+    event.preventDefault();
+    $(".brush").css("background-color", $("input").val());
+    $("<div class='history'></div>").appendTo($(".controls")).css("background-color", $("input").val());
+    if ($(".history").length > 3){
+      $(".history").eq(0).remove();
+    };
+  };
+
   $("#set-color").on("click", function(event) {
       event.preventDefault();
-      $(".brush").css("background-color", $("input").val());
-      $("<div class='history'></div>").appendTo($(".controls")).css("background-color", $("input").val());
-      if ($(".history").length > 3){
-        $(".history").eq(0).remove();
-      };
+      setBrushColor();
     });
 
-  $('#set-color').keypress(function (e) {
-    var key = e.which;
-    if(key == 13) {
-      $(".brush").css("background-color", $("input").val());
-    }
+  $("#set-color").on("keypress", function(e) {
+    if(e.which == 13) {
+      setBrushColor();
+      };
   });
 
   for (var i = 1; i <= 8000; i++) {
@@ -22,9 +26,6 @@ $("document").ready( function() {
       })
     };
   })
-
-// ###Commit 2
-// * The same thing should happen when I press the enter key from inside the input field
 
 // ## Bonus
 // * Add a color swatch. You should have 3 boxes with the most recent 3 colors used. When you click on each of those boxes, it should set the current brush color back to that color.
