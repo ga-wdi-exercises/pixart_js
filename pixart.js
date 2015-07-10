346dcb53cea87e40bbf97a0ec32c1f1c772c4aa9
Philip Engles <philipengles@Philips-MacBook-Pro-2.local>
2015-07-10 09:11:53 -0400

    finished doing pixel ar project with using jquery

--- pixart.js
+++ pixart.js
@@ -1,1 +1,31 @@
+$(document).ready(function (){
 
+$("#set-color").on("click", function(event){
+  event.preventDefault();
+  $(".brush").css("background-color", $("#color-field").val() || "#1B4370");
+})
+
+var i = 0;
+
+while( i < 8000){
+$("body").append("<div class='square'></div>");
+i++;
+}
+
+var squares = $("div.square");
+for(var x = 0; x < squares.length; x++){
+squares.eq(x).on("mouseover", function(){
+  $(this).css("background-color", $("#color-field").val() || "#1B4370");
+})
+}
+
+
+
+
+
+
+
+
+
+
+});
