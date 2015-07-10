597d2fc657f69f8225abe63ab9b89d4719b0692c
Matt Ficke <matthew.ficke@gmail.com>
2015-07-09 21:57:47 -0400

    choose a color

--- pixart.js
+++ pixart.js
@@ -1,8 +1,9 @@
 //When I click the "Set Color" button, it should change the color of the "brush" box to the color I specify in the input field. (Hint: You will need to use event.preventDefault() somewhere in your code.)
+var color;
 
 $("#set-color").on("click", function() {
   event.preventDefault();
-  var color = $("#color-field").val();
+  color = $("#color-field").val();
   $(".brush")[0].style.background=color;
 })
 
@@ -19,7 +20,6 @@ $("#set-color").keydown(function(event){
 //Add functionality so that when I click on each "square", it changes the color of that individual square to "green"
 for (var i=0; i<20; i++) {
   $(document.createElement("div")).attr("class", "square").appendTo("body").on("click", function() {
-    var color = prompt("choose a color");
     $(this).css("background", color);
   });
 }
