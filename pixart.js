ba5e0caf3f0ab1a4bda9c46e25cfe33c2b215291
Nayana Davis <nayana487@gmail.com>
2015-07-09 20:17:50 -0400

    changed brush color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,13 @@
+/*When I click the "Set Color" button, it should change the color of the "brush" box to the color I specify in the input field. (Hint: You will need to use event.preventDefault() somewhere in your code.)
+Use jQuery to select the element, and addEventListener to handle clicks
+$("body")[0].addEventListener...*/
 
+
+$( document ).ready(function(){
+  $("#set-color").on("click", function(){
+    event.preventDefault();
+    var color = $("#color-field").val();
+    $(".brush").css("background-color", color);
+
+  });
+});
