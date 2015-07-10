5d92f67ce92e84e8388f94411ee054066b25df03
Nayana Davis <nayana487@gmail.com>
2015-07-09 21:06:52 -0400

    made paint

--- pixart.js
+++ pixart.js
@@ -5,11 +5,11 @@ $( document ).ready(function(){
     $(".brush").css("background-color", color);
   });
 
-  for(i=0; i<20; i++) {
+  for(i=0; i<8000; i++) {
     $("body").append("<div class='square'><div>");
     };
 
-  $(".square").on("click", function(){
+  $(".square").on("mouseover", function(){
     var color = $(".brush").css("background-color");
     $(this).css("background-color",color);
 
