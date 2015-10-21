394a188fbb6efbf78829b0024c57ca5c5e6296e4
ckib16 <ckib16@gmail.com>
2015-10-21 14:14:02 -0400

    Color changes with both button press and enter key

--- pixart.js
+++ pixart.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function () {
+  var inputField = $('input:text')
+  console.log(inputField)
+  $('#set-color').on('click', function (evt) {
+    evt.preventDefault()
+    var userInput = $('input').val()
+    $('.brush').css('background', userInput)
+    console.log(userInput)
+  })
+})
