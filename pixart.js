8925427e3af3f8ea0214a47d3f5b43d47effad8f
Starr Z. Chen <chenstarr@gmail.com>
2015-10-21 14:27:40 -0400

    Add 20 divs w/ class 'square' to body

--- pixart.js
+++ pixart.js
@@ -1,8 +1,11 @@
 //change $("brush").css("background-color") to $("#form").val() on click of $("#set-color")
-console.log("ready freddy");
-
 $("#set-color").on("click", function(e){
   e.preventDefault();
-  console.log("click");
+  // console.log("click"); -- for testing
   $(".brush").css("background-color", $("#color-field").val())
 })
+
+//append 20 of <div class = "square"></div> to body
+for (var i = 0; i <= 20; i++) {
+  $("body").append("<div class = 'square'></div>")
+}
