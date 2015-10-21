3b160ce2c07f597049ce6a11dd8b5f5fdbb44fc0
Matthew Murgia <matthew.murgia@gmail.com>
2015-10-21 18:14:17 -0400

    added 20 pixels to picture

--- pixart.js
+++ pixart.js
@@ -1,5 +1,6 @@
 // When I clock "set color", it should change the color of the "brush" box to the color I specify in the input field.
 var setColor = $('#set-color');
+var pixelCount = 20;
 // $('#set-color');
 setColor.on("click", function(evt) {
    evt.preventDefault();
@@ -20,3 +21,11 @@ setColor.on("keypress", function(evt) {
    console.log("change color");
    $(".brush").css( "background", $("#color-field").val() );
   });
+
+// Create 20 divs of the "square" class and append them to the body
+var createCanvas = function(pixelCount) {
+  for(var i=0; i<pixelCount; i++){
+    $("body").append("<div class = 'square'></div>");
+  }
+};
+createCanvas(pixelCount);
