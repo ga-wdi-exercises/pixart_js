e8e68794c43ad9f51891728d25fadb9e484967b9
Timothy Gregg <timtgregg@gmail.com>
2015-10-21 12:12:48 -0400

    commit 1down!

--- pixart.js
+++ pixart.js
@@ -1,7 +1,8 @@
 var setColorButton = $('#set-color');
+var inputSelector = $('#color-field');
 var swathDiv = $('.brush');
 setColorButton.on('click', function(evt){
   evt.preventDefault();
   console.log('click');
-  swathDiv.css( 'background-color', 'red');}
+  swathDiv.css( 'background-color', inputSelector.val());}
 );
