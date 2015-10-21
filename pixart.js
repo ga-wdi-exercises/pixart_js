fd6114ecb220dc9d5beeddf2d617c50cf76b328b
Starr Z. Chen <chenstarr@gmail.com>
2015-10-21 14:14:22 -0400

    Clicking 'Set Color' changes the color of the 'brush' box to the color specified in the input field. (Not using addEventListener)

--- pixart.js
+++ pixart.js
@@ -1,1 +1,8 @@
+//change $("brush").css("background-color") to $("#form").val() on click of $("#set-color")
+console.log("ready freddy");
 
+$("#set-color").on("click", function(e){
+  e.preventDefault();
+  console.log("click");
+  $(".brush").css("background-color", $("#color-field").val())
+})
