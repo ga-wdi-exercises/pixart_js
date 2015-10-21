7d014261764eaad80084b1ff082a6485e7c2e719
Timothy Gregg <timtgregg@gmail.com>
2015-10-21 12:08:08 -0400

    sort of working!!

--- pixart.js
+++ pixart.js
@@ -1,1 +1,7 @@
-
+var setColorButton = $('#set-color');
+var swathDiv = $('.brush');
+setColorButton.on('click', function(evt){
+  evt.preventDefault();
+  console.log('click');
+  swathDiv.css( 'background-color', 'red');}
+);
