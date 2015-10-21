78bb782b4730cf2239eb71a81fa263164ff4623c
alea q <aleaq@aleas-AppleSmoothie.local>
2015-10-21 14:02:59 -0400

    added new divs and class

--- pixart.js
+++ pixart.js
@@ -10,3 +10,11 @@
 $("input[type='text']").keyup(function(event){
   $(".brush").css("background" , "pink") //gets same background when enter is pushed in the input section
 })
+
+//3
+//create div tag put the class in the tag and then append to body / loop however many times
+for(i=0; i<20; i++){
+var lotsOfDivs = document.createElement("div");
+lotsOfDivs.className = "square"
+document.body.appendChild(lotsOfDivs)
+}
