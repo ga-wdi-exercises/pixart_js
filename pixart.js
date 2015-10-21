32f0a3fadd47f4b604fb0c1dca206f3dcb6aefa4
Elise Kain <elise.kain@gmail.com>
2015-10-21 11:56:02 -0400

    Allow user input to change color of brush div

--- pixart.js
+++ pixart.js
@@ -1,1 +1,12 @@
+$(document).ready(function() {
 
+  var setColorButton = $("#set-color");
+  var brushColorBox = $(".brush");
+
+  setColorButton.on("click", function(evt) {
+    evt.preventDefault();
+    var userInput = $("#color-field").val();
+    brushColorBox.css("background", userInput)
+  });
+
+});
