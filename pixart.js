54b748e31a36005eda8d4b97f7cd15c6066a6479
Alexis Johnson <alexis.johnson052@gmail.com>
2015-10-21 14:20:10 -0400

    create 20 divs, class square

--- pixart.js
+++ pixart.js
@@ -1,19 +1,21 @@
 $(document).ready(function(){
 
-  $('button').on('click', function(e){
+  $("button").on("click", function(e){
     e.preventDefault();
-    var color = $('input').val()
-    $('.brush').css('background', color);
+    var color = $("input").val()
+    $(".brush").css("background", color);
   })
 
-  $('#color-field').on('keyup', function(e){
+  $("#color-field").on("keyup", function(e){
     if(e.keyCode === 13){
-      console.log('Enter')
+      console.log("Enter")
       e.preventDefault();
-      var color = $('input').val()
-      $('.brush').css('background', color);
+      var color = $("input").val()
+      $(".brush").css("background", color);
     }
   })
-
-  
+for(i=2; i<=21; i++){
+  $("body").append("<div></div>");
+  $("div").eq(i).attr("class", "square")
+}
 })
