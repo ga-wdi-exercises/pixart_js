67301075f380117a70014dffe73513efd508328c
Brendan Lewis <lewis2ba@dukes.jmu.edu>
2015-10-21 12:07:25 -0400

    ability to change brush color via input and button

--- pixart.js
+++ pixart.js
@@ -1,1 +1,20 @@
+var setColor = $("#set-color")
+var colorField = $("#color-field")
+var color
+$(document).ready(function(){
+  setColor.click(function(evt){
+    evt.preventDefault();
+    color = colorField.val()
+    $(".brush").css("background-color",color)
+})
 
+
+
+
+
+
+
+
+
+
+})
