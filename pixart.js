7c06aa30cf8583645cbdd43d36574d9b2a65e3f7
Cassidy Harpster <cassidy.j.harpster@gmail.com>
2015-10-21 14:17:18 -0400

    initial commit oh man it works

--- pixart.js
+++ pixart.js
@@ -1,1 +1,9 @@
+// Change the color based on what is typed into the input field
 
+$(document).ready(function(){
+  $("#set-color").on("click", function(){
+    event.preventDefault();
+    var theColor = document.getElementById("color-field").value;
+    $(".brush").css("background-color", theColor);
+  });
+})
