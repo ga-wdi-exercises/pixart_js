ddc6cefe5203c103a809dbe0ff52777702970477
alea q <aleaq@aleas-AppleSmoothie.local>
2015-10-21 13:23:47 -0400

    set color button changes brush background

--- pixart.js
+++ pixart.js
@@ -1,1 +1,13 @@
 
+
+
+   $("button").click(function(event){
+     $(".brush").css("background" , "pink")
+     event.preventDefault()
+   })
+
+
+//  function whenClick() {
+// document.querySelector("#set-color").addEventListener("click", changeColor);
+//
+// };
