25f5ef163071536c8af3a5935f4789fabede522b
Dumitru Gorgan <dgorgan3@gmail.com>
2015-10-21 14:22:10 -0400

    1st and 2nd commit - functionality from second commit automatically added with solituon for 1st commit

--- pixart.js
+++ pixart.js
@@ -1,1 +1,15 @@
+$(document).ready(function(){
 
+  $('#set-color').on('click', function(e){
+    e.preventDefault();
+    var input = $('#color-field').val();
+    $('.brush').css('background', input);
+});
+
+
+
+
+
+
+
+})
