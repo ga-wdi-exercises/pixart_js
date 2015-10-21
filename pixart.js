348e681f6be3e7dbb8c30fb41c7a6eff0935090b
Elise Kain <elise.kain@gmail.com>
2015-10-21 12:25:28 -0400

    Canvas divs turn green when clicked

--- pixart.js
+++ pixart.js
@@ -2,16 +2,26 @@ $(document).ready(function() {
   // Set Brush Color
   var setColorButton = $("#set-color");
   var brushColorBox = $(".brush");
+  var userInput = "#1B4370";
   var setColor = function(evt) {
     evt.preventDefault();
-    var userInput = $("#color-field").val();
+    userInput = $("#color-field").val();
     brushColorBox.css("background", userInput);
   }
 
   setColorButton.on("click", setColor);
 
   // Append Canvas to Body
-  for (i = 0; i < 20; i++) {
-    $("body").append("<div class='square'></div>");
+  var appendCanvas = function() {
+    // Create Canvas
+    for (i = 0; i < 20; i++) {
+      $("body").append("<div class='square'></div>");
+    }
+    // Add Event Listeners for divs
+    $(".square").on("click", function() {
+      $(this).css("background", "green");
+    });
   }
+
+  appendCanvas();
 });
