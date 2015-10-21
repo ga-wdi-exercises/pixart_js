1b0404b33763e45c830f516d47283113122b196c
Josh Cohen <jacohen10@EMAIL.COM>
2015-10-21 13:35:26 -0400

    finished part 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,16 @@
+$(document).ready(function(){
 
+
+  $("#set-color").click(function(){
+    event.preventDefault();
+    $(".brush").css("background", $("input").val());
+  });
+
+  // $("#set-color").click(function(){
+  //   event.preventDefault();
+  //   $(".brush").css("background", $("input").val());
+  // });
+
+  //
+
+});
