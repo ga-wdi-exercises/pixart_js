4ca7ac0b15cd9302d3765564c410fc7223826aa3
Chase Gruber <chasegruber@gmail.com>
2015-10-21 15:11:17 -0400

    bonus: added color swatch

--- pixart.js
+++ pixart.js
@@ -4,15 +4,33 @@ $(document).ready(function(){
     evt.preventDefault();
     setColor();
   });
-  $("#color-field").on('keyup', function(evt){
+
+  // had to comment this out to prevent setColor() from firing twice
+  // input default behavior?
+  /*$("#color-field").on("keyup",function(evt){
+    evt.preventDefault();
     if (evt.keyCode === 13){
       setColor();
     }
-  });
+  });*/
 
   function setColor(){
     color = $("#color-field").val();
     $(".brush").css('background',color);
+
+    var swatchCount = $("#swatch").children().length;
+
+    if (swatchCount == 3){
+      $(".swatchBrush").eq(0).remove();
+    }
+
+    var swatchDiv = document.createElement('div');
+    swatchDiv.className = 'swatchBrush';
+    $(swatchDiv).css('background',color).attr('color',color).on("click",function(){
+      color = $(this).attr('color');
+      $(".brush").css('background',color);
+    });
+    $("#swatch").append(swatchDiv);
   }
 
   for (var i=0;i<8000;i++){
