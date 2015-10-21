1992e1db58701ca3911f17727e3262260ec4ee09
Tory Burgett <vdburgett@gmail.com>
2015-10-21 14:18:14 -0400

    Initial Commit

--- pixart.js
+++ pixart.js
@@ -1,1 +1,26 @@
+$(document).ready(function(){
+  //don't reload the page
+  event.preventDefault();
 
+  //What color do you want
+  var userColor;
+  var userColorChoose = function(){
+  userColor = $("#color-field").val();
+  event.preventDefault();
+};
+//put this color in the box
+var getColor = function(){
+  $(".brush").css("background", userColor);
+  event.preventDefault();
+};
+//get the color on button click
+$("button").on("click", function(){
+  userColorChoose();
+  getColor();
+});
+
+
+
+
+
+});
