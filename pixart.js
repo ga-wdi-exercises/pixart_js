117a1ef16032551cb5262008f68a52abc38b8755
Thomas Solow <thomassolow@gmail.com>
2015-10-21 19:15:12 -0400

    added comments

--- pixart.js
+++ pixart.js
@@ -1,19 +1,24 @@
 
+//object to hold all vars and functions
 var pixArtPainter = {
-  clickButton: $("#set-color"),
-  numPixels: 4000,
-  pickColor: function(event) {
-    event.preventDefault();
-    $(".brush").css("background",$("#color-field").val());
-  },
+  clickButton: $("#set-color"), //stores the button
+  numPixels: 4000, //number of pixels in the canvas
+  //attaches a listener to the brush
   setUpBrushListener: function (){
     this.clickButton.bind("click", this.pickColor);
   },
+  //sets the color to the brush
+  pickColor: function(event) {
+    event.preventDefault(); //cancels default event on click
+    $(".brush").css("background",$("#color-field").val()); //changes brush background to the value entered in the input box
+  },
+  //adds the pixels to the body
   setUpCanvas: function(pixelCount){
     for (var i =0; i < pixelCount; i++){
       $("body").append("<div class = 'square'></div>");
     }
   },
+  //adds listeners to the pixels
   addCanvasListeners: function() {
     $(".square").each(function(index){
       $(this).on("mouseover", function(){
@@ -21,6 +26,7 @@ var pixArtPainter = {
       })
     })
   },
+  //launch everything
   launchPaint: function() {
     this.setUpCanvas(this.numPixels);
     this.addCanvasListeners();
