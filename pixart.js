605983076729a02b0a09b0bff00bed1f22b8a78e
Jordan Brauner <jordan.brauner@gmail.com>
2015-10-21 14:25:10 -0400

    Commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,8 @@
+var colorName = $('#color-field');
+var colorSet = $('#set-color');
+var brushBox = $('.brush');
 
+colorSet.on("click", function(evt) {
+  evt.preventDefault();
+  brushBox.css("background", colorName.val());
+});
