1d9c7fe07218269db3147b87d266dea64734c04b
Samir Mehta <sammehta88@gmail.com>
2015-10-21 13:56:41 -0400

    add color swatch

--- pixart.js
+++ pixart.js
@@ -2,29 +2,34 @@ var $setColor = $('#set-color');
 var $input = $('input');
 var $brush = $('.brush');
 var $body = $('body');
-var color = $brush.css('background');
+var color = $brush.css('background-color');
+var colorSwatchColors = [];
+var colorSwatchDivs = [];
+var swatchSize = 3;
 
 $setColor.on('click', setColor);
-$input.on('keyup', function(evt) {
-  if(evt.keyCode === 13) {
-    setColor(evt);
-  }
-});
+// $input.on('keyup', function(evt) {
+//   evt.preventDefault();
+//   if(evt.keyCode === 13) {
+//     setColor(evt);
+//   }
+// });
 
 function setColor(evt) {
   evt.preventDefault();
+  updateColorSwatchColors();
   color = $input.val();
   $brush.css({
     'background': color
   });
+  updateSwatch();
 }
 
 function createCanvas(num) {
   for(var i = 0; i < num; i++) {
-    var $div = $('<div></div>');
+    var $div = $('<div/>');
     $div.toggleClass('square');
     $div.on('mouseover', function(evt){
-      console.log(evt);
       var $targetDiv = $(evt.target);
       $targetDiv.css({
         'background': color
@@ -35,3 +40,56 @@ function createCanvas(num) {
 }
 
 createCanvas(8000);
+
+function updateColorSwatchColors() {
+  if (!colorSwatchColors) {
+    createSwatch();
+  }
+  var newLength = colorSwatchColors.push(color);
+
+  if(newLength > swatchSize) {
+    colorSwatchColors.shift();
+  }
+}
+
+function updateSwatch() {
+  if (colorSwatchDivs.length < 3){
+    newRecentColor();
+  }
+
+  for (var i = 0; i < colorSwatchColors.length; i++) {
+    colorSwatchDivs[i].css({
+      'background-color': colorSwatchColors[i]
+    });
+  }
+}
+
+function newRecentColor() {
+  var $swatch = $('<div/>');
+  $swatch.toggleClass('brush');
+  $swatch.on('click', function(evt) {
+    var $targetDiv = $(evt.target);
+    color = $targetDiv.css('background-color');
+  });
+  $brush.after($swatch);
+  colorSwatchDivs.push($swatch);
+}
+// function createSwatch() {
+//   for (var i = 0; i < swatchSize; i++) {
+//       // if (colorSwatchColors[i]){
+//       var $swatch = $('<div/>');
+//       $swatch.css({
+//         'background': colorSwatchColors[i]
+//       });
+//       $swatch.toggleClass('brush');
+//       $swatch.on('click', function(evt) {
+//         var $targetDiv = $(evt.target);
+//         color = $targetDiv.css('background-color');
+//       });
+//       $brush.after($swatch);
+//       colorSwatchDivs.push($swatch);
+//     // }
+//   }
+// }
+
+// createSwatch();
