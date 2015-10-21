fe606deb0058748e6c2987e5b356fbf20a34b49c
Brendan Mahoney <BMahoney08@gmail.com>
2015-10-21 17:55:34 -0400

    commit 4:

--- pixart.js
+++ pixart.js
@@ -1,14 +1,18 @@
-var changeColor = function () {
-  var button = $("#set-color");
-  button.on("click", function(evt) {
-    evt.preventDefault();
-    var input = $("#color-field").val()
-    $(".brush").css("background-color",input);
-  })
+var button = $("#set-color");
+var input = $("#color-field");
+
+var newColor = function(evt){
+  evt.preventDefault();
+  var color = $("#color-field").val();
+  $(".brush").css("background-color",color);
 }
 
-changeColor();
+button.on("click", newColor);
 
 for(var i = 1; i <+ 20; i++){
-  $("body").append("<div/>")
+  var newDiv = $("<div/>", {"class": "square"});
+  newDiv.on("click", function() {
+    $(this).css("background-color", "green")
+  });
+  $("body").append(newDiv);
 }
