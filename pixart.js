b4879ba1e0cc83947fc3befe3d5c2abc3eee5a57
Sarah Hersh <schersh@gmail.com>
2015-10-21 12:11:10 -0400

    Commit 1 for set color button

--- pixart.js
+++ pixart.js
@@ -1,1 +1,7 @@
-
+$("#set-color").on("click", function (evt){
+  evt.preventDefault();
+  var userInput = $("#color-field").val();
+  var brushBox = $(".brush").css({
+    background: userInput
+  });
+})
