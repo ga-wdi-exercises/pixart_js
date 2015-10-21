41f5999dcefe8d52a9b527a19273242d105b5906
Bryce Lively <bryce.lively@gmail.com>
2015-10-21 14:37:57 -0400

    need to work on creating 20 divs

--- pixart.js
+++ pixart.js
@@ -1,9 +1,29 @@
-$(document).ready(function(){
-  $("button").on("click", function(evt){
+$(document).ready(function() {
+  $("button").on("click", function(evt) {
     evt.preventDefault();
-    var colorSelected = $("input:text").val(); //This is correct.
+    colorSelected = $("input:text").val(); //This is correct.
     console.log("the button works...");
     console.log("User input is: " + colorSelected);
     $(".brush").css("background-color", colorSelected);
   })
 });
+//There is an open loop somewhere in here?!!?
+
+function build20divs() {
+  for (var i = 1; i < 20; i++) {
+    $(document).ready(function() {
+      $('<div />', {
+        "class": 'square',
+        click: function(e) {
+          e.preventDefault;
+          //colorSelected2 = $("input:text").val();
+          $("square").css("background-color", green);
+        },
+      });
+      //CANNOT USE VANILLA JS WITH jQuery!
+      /*var myElement = document.getElementsByClassName("square");
+      document.body.appendChild(myElement); /*
+    });
+  }
+}
+build20divs();
