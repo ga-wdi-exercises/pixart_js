86fffccac078c27b2c4eba6a3b1569d8920122c8
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2015-10-21 14:29:08 -0400

    click each square changes green

--- pixart.js
+++ pixart.js
@@ -6,9 +6,16 @@ $(document).ready(function(){
     $(".brush").css("background", colorText);
   })
 
+  var index = 0;
   for (var i=0; i<=20; i++){
     $( "body" ).append( "<div class = 'square'></div>" );
 
-  };
+    // $( ".square" ).eq(i).attr( "name","sqr"+[i]);
 
+    $(".square").each(function(index){
+      $(this).on("click", function(){
+        $(".square").eq(index).css("background", "green");
+      })
+    });
+  }
 })
