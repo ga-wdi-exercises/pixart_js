53d28a3b93eb9a6d38b24957b4f07013ce7caad0
Bryce Lively <bryce.lively@gmail.com>
2015-10-21 16:13:33 -0400

    creating 20 divs in <body> but displaying oddly

--- pixart.js
+++ pixart.js
@@ -2,27 +2,21 @@ $(document).ready(function() {
   $("button").on("click", function(evt) {
     evt.preventDefault();
     colorSelected = $("input:text").val(); //This is correct.
-    console.log("the button works...");
     console.log("User input is: " + colorSelected);
     $(".brush").css("background-color", colorSelected);
     $("input:text").val("");
   })
 });
-//The following is not creating the divs....
-
+//Divs are showing up... oddly. ?
 $(document).ready(function() {
-  for (var i = 1; i < 20; i++) {
-    $("div", {
+  for (var i = 0; i < 20; console.log("made div " + i, i++)) {
+    $(".controls").after($('<div />', {
       "class": "square",
-      "style": "width = 200px;",
-      "style": "height = 200px;",
       click: function(e) {
         e.preventDefault;
         //colorSelected2 = $("input:text").val();
         $(".square").css("background-color", "green");
       },
-    });
-    var square = $(".square");
-    $("body").append(square);
+    }));
   }
 });
