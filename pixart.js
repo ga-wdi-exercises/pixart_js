dd7744d8db5210ca980a5e5fcd6b6ca4f719f866
Thomas Solow <thomassolow@gmail.com>
2015-10-21 13:33:13 -0400

    added color picker brush button

--- pixart.js
+++ pixart.js
@@ -1,1 +1,12 @@
-
+var pixArtPainter = {
+  clickButton: $("#set-color"),
+  pickColor: function(event) {
+    event.preventDefault();
+      $(".brush").css("background-color",$("#color-field").val());
+  },  
+  setUpListener: function (){
+    //event.preventDefault();
+    this.clickButton.bind("click", this.pickColor);
+  },
+}
+pixArtPainter.setUpListener();
