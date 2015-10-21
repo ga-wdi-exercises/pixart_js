f0a340ed46398d15bb1f0ff32cef5bcb48cd8164
Brendan Lewis <lewis2ba@dukes.jmu.edu>
2015-10-21 12:41:11 -0400

    created 20 divs of the square class and appended

--- pixart.js
+++ pixart.js
@@ -1,13 +1,20 @@
 var setColor = $("#set-color")
 var colorField = $("#color-field")
 var color
+
 $(document).ready(function(){
-  setColor.click(function(evt){
+
+  setColor.on("click",function(evt){
     evt.preventDefault();
     color = colorField.val()
     $(".brush").css("background-color",color)
-})
+  })
 
+  for(var i=0;i<=20;i++){
+    var div = document.createElement("div")
+    div.classList.add('square')
+    document.body.appendChild(div)
+  }
 
 
 
