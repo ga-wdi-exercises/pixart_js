a4202fcedbad802b125dc57f1a8cd94b684dadec
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2015-10-21 14:34:48 -0400

    commit 5 - change each square to color in text field

--- pixart.js
+++ pixart.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
 
   $("#set-color").on("click", function(evt){
-    colorText = $("#color-field").val();
+    var colorText = $("#color-field").val();
     evt.preventDefault();
     $(".brush").css("background", colorText);
   })
 
-  var index = 0;
   for (var i=0; i<=20; i++){
     $( "body" ).append( "<div class = 'square'></div>" );
 
     // $( ".square" ).eq(i).attr( "name","sqr"+[i]);
-
     $(".square").each(function(index){
       $(this).on("click", function(){
-        $(".square").eq(index).css("background", "green");
+        colorText = $("#color-field").val();
+        $(".square").eq(index).css("background", colorText);
       })
     });
   }
+
 })
