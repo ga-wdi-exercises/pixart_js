9f670eacf737f40e41d18b378fa3d530ef3ad65a
Thom Householder <thomhouseholder@gmail.com>
2015-10-21 13:47:12 -0400

    commit 1 - Set Color sets brush color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,15 @@
 
+//event listener
+$('#set-color').on('click', function(evt) {
+  console.log('Clicked');
+
+  //stop link from going to google - interrupt
+  evt.preventDefault();
+  //click setup
+//var clickButton=$('#set-color');
+
+  //chosen color
+  brushColor= $('input').val()
+  //change color attribute
+  $('.brush').css('background', brushColor);
+});
