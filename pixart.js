7760496c42ec0c6b5fc41083d3e98b0c83c3eaa4
Chase Gruber <chasegruber@gmail.com>
2015-10-21 12:01:44 -0400

    step 2: handle input enter keypress

--- pixart.js
+++ pixart.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
   $("#set-color").on("click",function(evt){
     evt.preventDefault();
+    setColor();
+  });
+  $("#color-field")[0].addEventListener('keyup', function(evt){
+    if (evt.keyCode === 13){
+      setColor();
+    }
+  })
+
+  function setColor(){
     var color = $("#color-field").val();
     $(".brush").css('background',color);
-  });
+  }
+
 });
