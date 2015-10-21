2967ec6b1756b70006d198a6da426ea2d79fe195
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2015-10-21 13:52:11 -0400

    set-color button working

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
+$(document).ready(function(){
 
+  $("#set-color").on("click", function(evt){
+    colorText = $("#color-field").val();
+    evt.preventDefault();
+    $(".brush").css("background", colorText);
+  })
+
+
+
+})
