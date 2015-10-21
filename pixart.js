ca3000b769dc2330383bd7396438fbb1c64495d6
Wonsik Jeong <wsjeong21@hotmail.com>
2015-10-21 13:45:15 -0400

    nicely done until commit2

--- pixart.js
+++ pixart.js
@@ -1,1 +1,9 @@
-
+//commit 1
+function updateC(evt)
+{
+ evt.preventDefault();
+ var matchC=$('#color-field').val();
+  $('div.brush').css("background", matchC);
+}
+var buttonNode=$('#set-color');
+buttonNode.click( updateC);
