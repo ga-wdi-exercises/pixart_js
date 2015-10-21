917fe8fc7480cf6e1c66e43cbd14f48dcbc0f9fd
Wonsik Jeong <wsjeong21@hotmail.com>
2015-10-21 14:25:57 -0400

    commit 5 done

--- pixart.js
+++ pixart.js
@@ -2,6 +2,11 @@
 function clickSquare(){
       $(this).css("background", "green");
     }
+
+function chooseMyC(){
+  var matchC=$('#color-field').val();
+  $(this).css("background", matchC);
+}
 //commit 1
 function updateC(evt)
 {
@@ -22,3 +27,6 @@ for(var i=0; i<20; i++){
 // commit 3
 var squareNode=$('div.square');
 squareNode.click(clickSquare);
+
+//commit 4
+squareNode.click(chooseMyC);
