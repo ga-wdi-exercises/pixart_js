8b5826c42e6f01ab3e89e8bea8650bc56d77a78c
Bryce Lively <bryce.lively@gmail.com>
2015-10-21 12:17:38 -0400

    User input and color box work

--- pixart.js
+++ pixart.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function(){
+  $("button").on("click", function(evt){
+    evt.preventDefault();
+    var colorSelected = $("input:text").val(); //This is correct.
+    console.log("the button works...");
+    console.log("User input is: " + colorSelected);
+    $(".brush").css("background-color", colorSelected);
+  })
+});
