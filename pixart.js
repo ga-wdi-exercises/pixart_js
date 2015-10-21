d27c5d335b805a66b4e89d114ef3d9eeb68b7d45
James Moon <mooniker@gmail.com>
2015-10-21 18:48:46 -0400

    Reimplement as JS object

--- pixart.js
+++ pixart.js
@@ -1,22 +1,35 @@
 $(document).ready(function() {
-  console.log("Page loaded.");
-
-  // Use jQuery to select the element, and `addEventListener` to handle clicks
-  // $("body")[0].addEventListener...`
 
   // var buttonSetColor = $("#set-color");
   // var divBrush = $("div.brush");
-  var formInput = $("#form input");
+  // var formInput = $("#form input");
 
-  function setColor() {
-    var color = $("#color-field").val();
-    console.log("Set color to", color);
-    $("div.brush").css("background", color);
-  }
+  var drawing = {
 
-  $("#set-color").on ("click", function (e) {
-    e.preventDefault();
-    setColor();
-  });
+    setColor: function () {
+      var color = $("#color-field").val();
+      console.log("Set color to", color);
+      $("div.brush").css("background", color);
+    },
+    setupListeners: function () {
+      $("#set-color").on ("click", function (e) {
+        e.preventDefault();
+        this.setColor();
+      }.bind(this));
+    },
+  };
+
+
+  // function setColor() {
+  //   var color = $("#color-field").val();
+  //   console.log("Set color to", color);
+  //   $("div.brush").css("background", color);
+  // }
+
+  // for ( var i = 0; i < 20; i++ ) {
+  //   $("body").append()
+  // }
+
+  drawing.setupListeners();
 
 });
