6fa508f1aaad913bc7fab4987bb5f5be4909628f
Timothy Gregg <timtgregg@gmail.com>
2015-10-21 12:43:28 -0400

    working on final color swap

--- pixart.js
+++ pixart.js
@@ -6,3 +6,13 @@ setColorButton.on('click', function(evt){
   console.log('click');
   swathDiv.css( 'background-color', inputSelector.val());}
 );
+
+for(var a=0;a<8000;a++){
+  $('body').append("<div class='square'></div>");
+}
+
+$('.square').on('click',function(evt){
+  console.log('div clicked');
+  console.log(evt);
+  evt.toElement.style.color='red';
+});
