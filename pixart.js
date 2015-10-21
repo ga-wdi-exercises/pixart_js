e532fe34aeffa1f0a9ca50c5d6a0723951a663fe
Samir Mehta <sammehta88@gmail.com>
2015-10-21 12:13:20 -0400

    FEAT: set color of brush

--- pixart.js
+++ pixart.js
@@ -1,1 +1,13 @@
+var $setColor = $('#set-color');
+var input = $('input');
+var $color;
+var $brush = $('.brush');
 
+$setColor.on('click', function(evt) {
+  evt.preventDefault();
+  color = input.val();
+  console.log(color);
+  $brush.css({
+    'background': color
+  });
+});
