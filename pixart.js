c531e3efee9c810b782b7d97bd98e1f6529cfce4
Josh Cohen <jacohen10@EMAIL.COM>
2015-10-21 13:37:23 -0400

    got 2 as well

--- pixart.js
+++ pixart.js
@@ -6,10 +6,13 @@ $(document).ready(function(){
     $(".brush").css("background", $("input").val());
   });
 
-  // $("#set-color").click(function(){
-  //   event.preventDefault();
-  //   $(".brush").css("background", $("input").val());
-  // });
+  $("#set-color").keydown(function(event){
+    if (event.keyCode === 13); {
+      event.preventDefault();
+    $(".brush").css("background", $("input").val());
+    }
+
+  });
 
   //
 
