8898cab72dfd831eeaf94862e34c5e3490f4f4c4
Alexis Johnson <alexis.johnson052@gmail.com>
2015-10-21 12:46:50 -0400

    select color on enter

--- pixart.js
+++ pixart.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
 
-  $('button').on('click', function(evt){
-    evt.preventDefault();
+  $('button').on('click', function(e){
+    e.preventDefault();
     var color = $('input').val()
     $('.brush').css('background', color);
   })
+
+  $('#color-field').on('keyup', function(e){
+    if(e.keyCode === 13){
+      console.log('Enter')
+      e.preventDefault();
+      var color = $('input').val()
+      $('.brush').css('background', color);
+    }
+  })
+
+  
 })
