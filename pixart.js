9304c92dbd49cc96a77133958c9fea5e1f516f2c
Wonsik Jeong <wsjeong21@hotmail.com>
2015-10-21 14:31:35 -0400

    changed square size to 15px by 15px since default was already 10px

--- pixart.js
+++ pixart.js
@@ -19,8 +19,8 @@ function updateC(evt)
   var buttonNode=$('#set-color');
   buttonNode.click( updateC);
 
-//commit 2
-for(var i=0; i<20; i++){
+//commit 2, commit 6
+for(var i=0; i<8000; i++){
   $('body').append('<div class="square"> </div>');
 }
 
@@ -30,3 +30,8 @@ squareNode.click(clickSquare);
 
 //commit 4
 squareNode.click(chooseMyC);
+
+// commit 6
+squareNode.css("width", "15px");
+squareNode.css("height", "15px");
+squareNode.css("margin", "0px");
