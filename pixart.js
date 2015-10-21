5d0d9d6d8327d273c6d5b10ab9f9855a7de5e56c
Samir Mehta <sammehta88@gmail.com>
2015-10-21 12:20:39 -0400

    Add event handler for enter key in input

--- pixart.js
+++ pixart.js
@@ -1,13 +1,19 @@
 var $setColor = $('#set-color');
-var input = $('input');
-var $color;
+var $input = $('input');
+var color;
 var $brush = $('.brush');
 
-$setColor.on('click', function(evt) {
+$setColor.on('click', setColor);
+$input.on('keyup', function(evt) {
+  if(evt.keyCode === 13) {
+    setColor(evt);
+  }
+});
+
+function setColor(evt) {
   evt.preventDefault();
-  color = input.val();
-  console.log(color);
+  color = $input.val();
   $brush.css({
     'background': color
   });
-});
+}
