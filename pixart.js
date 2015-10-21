51e01a803785cd072fdd909d67e18abf752d6b79
Alexis Johnson <alexis.johnson052@gmail.com>
2015-10-21 17:06:49 -0400

    change color to green on click

--- pixart.js
+++ pixart.js
@@ -1,21 +1,30 @@
 $(document).ready(function(){
 
-  $("button").on("click", function(e){
+  $('button').on('click', function(e){
     e.preventDefault();
-    var color = $("input").val()
-    $(".brush").css("background", color);
+    var color = $('input').val()
+    $('.brush').css('background', color);
   })
 
-  $("#color-field").on("keyup", function(e){
+  $('#color-field').on('keyup', function(e){
     if(e.keyCode === 13){
-      console.log("Enter")
       e.preventDefault();
-      var color = $("input").val()
-      $(".brush").css("background", color);
+      var color = $('input').val()
+      $('.brush').css('background', color);
     }
   })
-for(i=2; i<=21; i++){
-  $("body").append("<div></div>");
-  $("div").eq(i).attr("class", "square")
-}
+
+  for(i=1; i<=20; i++){
+    divNum = i+1;
+    $('body').append('<div></div>');
+    $('div').eq(divNum).attr('class', 'square');
+    }
+
+  $('.square').each(function(){
+    $(this).on('click', function(e){
+      $(this).css('background', 'green')
+    })
+  })
+
+
 })
