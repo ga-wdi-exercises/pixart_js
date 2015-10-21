72136c112a2ba11345cd13d550957d433f93086f
Daniel Alexander <daniel.elliot.alexander@gmail.com>
2015-10-21 14:20:38 -0400

    commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,10 @@
 
+function changeColor(color) {
+  $(".brush").css("background-color",color)
+}
+
+$("#set-color")[0].addEventListener("click", function(event){
+  event.preventDefault();
+  var colorChoice = $("#color-field").val();
+  changeColor(colorChoice)
+});
