4a70daa87ab1ee25a5c23a7bc9096888f99b9e04
Becky Beauchamp <beckybeauchamp1@gmail.com>
2015-10-21 14:21:17 -0400

    Complete Step 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,13 @@
-
+$(document).ready(function(){
+    $("button").on("click", (function(){
+      event.preventDefault();
+      var colorID = $("input:text").val();
+      if(colorID === " " || colorID === ""){
+        alert("Please choose a CSS color!");
+      }
+      else{
+        alert("This is your color: " + colorID);
+        $(".brush").css("background", colorID);
+      }
+  }));
+});
