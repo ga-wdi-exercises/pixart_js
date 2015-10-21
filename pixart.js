bd78c0976d86050e28c4c782181588f986670354
Eric Stennett <estennett@gmail.com>
2015-10-21 19:33:50 -0400

    made paint brush on click event and 20 divs

--- pixart.js
+++ pixart.js
@@ -1,1 +1,12 @@
+$(document).ready(function(){
+  $('#set-color').on('click', function(e){
+    e.preventDefault();
+    $('.brush').css('background-color', $('#color-field').val())
+  })
+})
 
+$(document).ready(function(){
+    for (i = 0; i < 20; i++){
+      $('body').append("<div></div>")
+    }
+  })
