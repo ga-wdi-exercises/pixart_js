98ebbb0ac1ab18ab589f107dbb885cb1f8ac091d
Davis Green <davismgreen@gmail.com>
2015-10-21 12:04:53 -0400

    Initial Commit, Change pallet to input text value

--- pixart.js
+++ pixart.js
@@ -1,1 +1,9 @@
 
+var setColor = function(evt){
+  var colorSelect = $(":text").val()
+  console.log("Hey")
+  evt.preventDefault()
+  $(".brush").css("background-color", colorSelect)
+}
+
+$("button").on("click",setColor)
