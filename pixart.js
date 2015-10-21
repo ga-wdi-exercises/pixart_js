52a072c8573709321543dca0bfe1946ca0e882f1
ckib16 <ckib16@gmail.com>
2015-10-21 14:28:05 -0400

    appended 20 divs with square class

--- pixart.js
+++ pixart.js
@@ -7,4 +7,11 @@ $(document).ready(function () {
     $('.brush').css('background', userInput)
     console.log(userInput)
   })
+
+  // * Create 20 divs of the "square" class and append them to the body
+  //   * **Hint**: use `.append()`
+  var amountSquareDivs = 20
+  for (var i = 0; i < amountSquareDivs; i++) {
+    $('body').append('<div class="square"></div>')
+  }
 })
