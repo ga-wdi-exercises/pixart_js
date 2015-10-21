4b2a1e8c1bc121d47dcb47a527ee7f5145349db6
Cassidy Harpster <cassidy.j.harpster@gmail.com>
2015-10-21 15:39:54 -0400

    Second commit. I forgot a few this is step 5

--- pixart.js
+++ pixart.js
@@ -1,3 +1,9 @@
+
+// Ask about this
+// $(document).ready(function(){
+//   event.preventDefault();
+// });
+
 // Change the color based on what is typed into the input field
 
 $(document).ready(function(){
@@ -6,4 +12,34 @@ $(document).ready(function(){
     var theColor = document.getElementById("color-field").value;
     $(".brush").css("background-color", theColor);
   });
-})
+});
+
+// Change the color with the enter key in the submit field. By targetting the #form form instead of #set-color, the page will not refresh upon "submission" of the form with the enter key.
+$('#form').submit(function(e){
+      e.preventDefault();
+      if(e.keyCode == 13){
+        $("#set-color").click();
+      };
+ });
+
+// Appending 20 div class = square divs to the document
+$(document).ready(function() {
+  for (var i = 0; i < 20; i++) {
+    $('body').append($("<div></div>", {
+      class: 'square'
+    }));
+
+// And now make each square turn green when you click on that div square
+//     $(".square").on("click", function() {
+//       $(this).css("background-color", "green");
+//     })
+//   }
+// });
+
+// And now make it so instead of green, they turn to whatever is in the input field
+    $(".square").on("click", function() {
+      var theColor = document.getElementById("color-field").value;
+      $(this).css("background-color", theColor);
+    })
+  }
+});
