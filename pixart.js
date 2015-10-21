8399c8dcc349cfae20b7704a6d0a860cdc94f678
MiaKoda Grodsky <mcgrodsky@gmail.com>
2015-10-21 12:36:42 -0400

    committing to view other branch

--- pixart.js
+++ pixart.js
@@ -1,1 +1,23 @@
-//when you hit set-color, change to color that is specific in the box. so...type color in input-field ID = "#color-field", then
+//1)type color you want (hex code, rgb) in input-field ID = "#color-field", then hit "set-color", then have the box
+// "brush" change color to the one you specified
+
+$(document).ready(function(){
+
+   // jQuery methods go here...
+
+
+//
+function pickColor() {
+  var color = $("color-field").val()
+    event.preventDefault();
+  }
+
+
+
+  //on click have the box "brush" change to color you specified in pickColor
+
+
+$("#set-color").on("click", function(){
+$(".brush").html("hey")
+})
+});
