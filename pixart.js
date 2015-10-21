94db425cb6a2fb9956a2f780c2f11027f9f42cfd
Elise Kain <elise.kain@gmail.com>
2015-10-21 12:32:32 -0400

    Made canvas 8000 divs and reacts to mouseover

--- pixart.js
+++ pixart.js
@@ -11,17 +11,27 @@ $(document).ready(function() {
 
   setColorButton.on("click", setColor);
 
+
   // Append Canvas to Body
   var appendCanvas = function() {
     // Create Canvas
-    for (i = 0; i < 20; i++) {
+    for (i = 0; i < 8000; i++) {
       $("body").append("<div class='square'></div>");
     }
     // Add Event Listeners for divs
-    $(".square").on("click", function() {
-      $(this).css("background", "green");
+    $(".square").on("mouseover", function() {
+      $(this).css("background", userInput);
     });
   }
 
   appendCanvas();
+
+  // Set Square Class Items to New Parameters
+  // **I think this is already set in CSS
+  $(".square").css({
+    "height": "10px",
+    "width": "10px",
+    "margin": 0
+  })
+
 });
