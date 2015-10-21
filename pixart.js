68814fbd8cb63fc915068c84471eaf77c1059091
Andrew Johnson <andrewjohnson8819@gmail.com>
2015-10-21 12:39:53 -0400

    input and button affects brush color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
+var colorField = $("#color-field")
+var setColor = $("#set-color")
+var color
+$(document).ready(function(){
 
+  setColor.on("click",function(evt){
+    evt.preventDefault()
+    color = colorField.val()
+    $(".brush").css("background", color)
+  })
+})
