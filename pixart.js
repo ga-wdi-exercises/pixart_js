53ff2b9b9a85ed3324892689eec3aed6aeff04f7
Rachel Porter <rachelandrewsporter@gmail.com>
2015-10-21 21:14:39 -0400

    commit 5 - square on click is color from input field. also preset color value to the default blue so user can use that swatch without setting color

--- pixart.js
+++ pixart.js
@@ -1,5 +1,6 @@
 // * When I click the "Set Color" button, it should change the color of the "brush" box to the color I specify in the input field. (**Hint:** You will need to use `event.preventDefault()` somewhere in your code.)
 // use jQuery to select the element, and `addEventListener` to handle clicks
+var color = "#1C4273";
 
 $("button")[0].addEventListener("click", function(event) {
   event.preventDefault();
@@ -20,10 +21,12 @@ $("button")[0].addEventListener("click", function(event) {
 //Create 20 divs of the "square" class and append them to the body - use .append()
 for (i = 0; i < 20; i++) {
   $(document.createElement("div")).attr("class", "square").appendTo('body').click( function () {
-    $(this).css("background", "green");
+    $(this).css("background", color);
   });
 }
 
 //commit 4
 // Add functionality so that when I click on each "square", it changes the color of that individual square to "green"
-//   * **Hint**: either add the event listener while creating the squares, or listen for events on the `body` element
+
+//commit 5
+//Modify your code so that when I click on each "square", it changes to the color I set using my input instead of "green" every time.
