5ea42ae2134d054a40afb01104d818cf536ae3ee
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2015-10-22 08:51:55 -0400

    completed all but bonus
    i tried to loop the mouseover, and it worked, but would crash my browser if i added 8k divs, so i moved the mouseover event outside of the loop and left that option in as a comment

--- pixart.js
+++ pixart.js
@@ -6,17 +6,23 @@ $(document).ready(function(){
     $(".brush").css("background", colorText);
   })
 
-  for (var i=0; i<=1000; i++){
+  for (var i=0; i<=8000; i++){
     $( "body" ).append( "<div class = 'square'></div>" );
 
     // $( ".square" ).eq(i).attr( "name","sqr"+[i]);
-    $(".square").each(function(index){
-      $(this).on("mouseover", function(){
-        colorText = $("#color-field").val();
-        $(".square").eq(index).css("background", colorText);
-      })
-    });
+    // $(".square").each(function(index){
+    //   $(this).on("mouseover", function(){
+    //     colorText = $("#color-field").val();
+    //     $(this).css("background", colorText);
+    //   })
+    // });
   }
+
+
+    $(".square").on("mouseover", function(){
+      colorText = $("#color-field").val();
+      $(this).css("background", colorText);
+    });
 
   $(".square").css({"height": "10px","width":"10px","margin":"0"});
 
