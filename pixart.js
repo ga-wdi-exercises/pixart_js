89ac15ee9d762f76cc1047bcb755877b1fc410ae
Jared Nichols <jnichols300@gmail.com>
2015-10-22 15:24:40 -0400

    Squares work, pretty easy. Couldn't figure out my problem for about an hour, turns out I was just missing a parenthesis and a bracket

--- pixart.js
+++ pixart.js
@@ -5,6 +5,12 @@ $("#set-color").on("click", function(event){
         $(".brush").css("background-color", $("#color-field").val())
 
 });
-for(var i = 0; i < 20; i++ ){
+for(var i = 0; i < 8000; i++ ){
     $("body").append("<div class='square'></div>")
 };
+
+$(".square").each(function(){
+      $(this).on("mouseover", function() {
+          $(this).css("background-color", $("#color-field").val())
+})
+});
