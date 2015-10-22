5f449f48c9fd46c6e9a1b21dbd35b3914723ca9d
Britton Walker <bwalker1801@gmail.com>
2015-10-22 14:14:03 -0400

    Keypress function

--- pixart.js
+++ pixart.js
@@ -1,4 +1,9 @@
-  $('#set-color').on('click', function(evt) {
-    evt.preventDefault();
-    $('.brush').css('background', $('.input').val());
-  })
+$('#set-color').on('click', function(evt) {
+  evt.preventDefault();
+  $('.brush').css('background', $('.input').val());
+})
+
+$('#set-color').on('keypress', function(evt) {
+  evt.preventDefault();
+  $('.brush').css('background', $('.input').val());
+})
