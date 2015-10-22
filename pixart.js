875b6eff7471e8925c21eef9f4a57fcba12dce34
Aurelio Avelleyra <Ao@Aurelios-MacBook-Pro.local>
2015-10-22 14:29:46 -0400

    Added 20 squares

--- pixart.js
+++ pixart.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
-  $("#set-color").on("click",function(evt) {
+  $("#set-color").on("click", function(evt) {
     evt.preventDefault();
     var newColor = $("#color-field").val();
     $(".brush").css("background", newColor)
   })
+
+  var divSq = []
+  for (var i = 0; i < 20; i++) {
+    divSq.push('<div class="square"></div>');
+  }
+  $("body").append(divSq);
+
+  
 })
