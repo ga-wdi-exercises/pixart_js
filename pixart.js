16a45ce581c558d8bbac3fb16174f37e2f20421c
Bryce Lively <bryce.lively@gmail.com>
2015-10-22 14:33:48 -0400

    cant get pointer to take selected brush color

--- pixart.js
+++ pixart.js
@@ -1,10 +1,6 @@
 $(document).ready(function() {
-  // Add a color swatch. You should have 3 boxes with the most recent 3 colors used. When you click on each of those boxes, it should set the current brush color back to that color.
-  /*for (var o = 0; o < 2; o++) {
-    $("form").after($('<div />'), {
-    "class": "brush",
-  });
-}  I decided to add the divs directly to the HTML because the client requested a specific number of brushes*/
+  // I decided to add the divs directly to the HTML because the client requested a specific number of brushes
+  // I should totaly do this with jQuery instead.
 });
 var allBrushes = $(".brush");
 allBrushes.css("display", "inline-block");
@@ -12,24 +8,27 @@ var clickCounter = 0;
 $(document).ready(function() {
   $("button").on("click", function(evt) {
     var whichBrush = $(".brush")[clickCounter];
-      evt.preventDefault();
-      colorSelected = $("input:text").val(); //This is correct.
-      console.log("User input is: " + colorSelected);
-      $(whichBrush).css("background-color", colorSelected);
-      clickCounter =  clickCounter + 1;
-      if (clickCounter >= 3) {
-        clickCounter = 0;
-      };
-    })
+    evt.preventDefault();
+    colorSelected = $("input:text").val(); //This is correct.
+    console.log("User input is: " + colorSelected);
+    $(whichBrush).css("background-color", colorSelected);
+    clickCounter = clickCounter + 1;
+    if (clickCounter >= 3) {
+      clickCounter = 0;
+    };
+  })
 });
-function addColor() {
-  return function(e) {
-    var colorSelected2 = $("input:text").val();
-    e.preventDefault;
-    $(this).css("background-color", colorSelected2);
-  };
-}
+
 $(document).ready(function() {
+  var currentButton = $(".button")[0];
+  $(".brush").on("click", currentButton = $(".brush")[this])
+  function addColor() {
+    return function(e) {
+      var colorSelected2 = currentButton.style("background-color");
+      e.preventDefault;
+      $(this).css("background-color", colorSelected2);
+    };
+  }
   for (var i = 0; i < 8000; i++) {
     $(".controls").after($('<div />', {
       mouseover: addColor(),
