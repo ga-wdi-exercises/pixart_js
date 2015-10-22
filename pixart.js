90e821dea247a1042bd0c04ddf132a6b343c1a3b
Dennis Truong <dtruong02@gmail.com>
2015-10-22 10:00:40 -0400

    second commit

--- pixart.js
+++ pixart.js
@@ -1,1 +1,13 @@
+$(document).ready(function(){
+	beginArt();
 
+	function beginArt() {
+	  $("#set-color").on("click", newColor);
+	}
+
+	function newColor(evt) {
+	      evt.preventDefault();
+	      color = $("#color-field").val();
+	      $(".brush").css("background", color);
+	}
+})
