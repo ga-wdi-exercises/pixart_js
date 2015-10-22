c860578d7886d89ca560e0ec0fafe1ca6ada40d1
Alexandra <akscreven@gmail.com>
2015-10-22 01:43:51 -0400

    brush changes according to input

--- pixart.js
+++ pixart.js
@@ -1,1 +1,8 @@
-
+// assign color entered in input field to brush
+$( document ).ready( function () {
+$("button").on('click',function(event){
+    event.preventDefault();
+    var colorSet = $('#color-field').val();
+    $('.brush').css('background-color',colorSet);
+  });
+})
