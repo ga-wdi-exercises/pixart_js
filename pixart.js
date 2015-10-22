ce73ad6d7890c6ce6401395adf6daf60d53b4400
Tyler <tylerscottcrosse@gmail.com>
2015-10-22 17:57:09 -0400

    solution with working bonus

--- pixart.js
+++ pixart.js
@@ -1,30 +1,50 @@
+var inputCounter = 0;
+
 $(document).ready(function() {
-// When I click the "Set Color" button, it should change the color of the "brush" box to the color I specify in the input field. (Hint: You will need to use event.preventDefault() somewhere in your code.)
-// Use jQuery to select the element, and addEventListener to handle clicks
-// $("body")[0].addEventListener...
 
-  $("#set-color").on("click", function(){
-    var color = $("#color-field").val();
-    $(".brush").css("background", color);
-    event.preventDefault();
-  });
-
-  $("input[name='input']").keypress(function(e) {
-    if (e.keyCode == 13) {
-      var color = $("#color-field").val();
-      $(".brush").css("background", color);
-      event.preventDefault();
-    }
-  });
-  for (var i = 0; i < 20; i++) {
+  for (var i = 0; i < 8000; i++) {
     $("body").append("<div class='square'></div>");
   }
 
-  var brushColor = "";
-
-  $(".square").on("click", function() {
-    brushColor = $(".brush").css("background-color");
-    $(this).css("background", brushColor);
+  $(".square").css({
+    "width": "10px",
+    "height": "10px",
+    "margin": "0"
   });
+
+  $(".controls").append("<div class='brush'></div>");
+  $(".controls").append("<div class='brush'></div>");
+
+  $("#set-color").on("click", function() {
+    var color = $("#color-field").val();
+    $(".brush").eq(inputCounter).css("background", color);
+    $(".square").on("mouseover", function() {
+      // brushColor = $(".brush").eq(inputCounter-1).css("background-color");
+      $(this).css("background", color);
+    });
+    if (inputCounter<2) {
+      inputCounter++;
+      console.log(inputCounter);
+    } else {
+      inputCounter = 0;
+    }
+    event.preventDefault();
+  });
+
+
+  $(".brush").on("click", function(e) {
+    var brushColor = $(this).css("background-color");
+    inputCounter = e.value;
+    console.log(inputCounter);
+    event.preventDefault();
+    $(".square").on("mouseover", function() {
+      $(this).css("background", brushColor);
+    });
+  });
+
+  // $(".square").on("mouseover", function() {
+  //   brushColor = $(".brush").eq(inputCounter-1).css("background-color");
+  //   $(this).css("background", brushColor);
+  // });
 
 });
