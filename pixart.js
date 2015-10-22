9c97f0c367b0c2c8d5ec45e23dbef5bc4bddf282
Mohamed Goodwin <mohamed.goodwin@gmail.com>
2015-10-22 14:21:33 -0400

    git_final_pixel_art

--- pixart.js
+++ pixart.js
@@ -1,1 +1,21 @@
+var color;
 
+$("#set-color").click(updateC)
+function updateC(evt){
+  color =$("#color-field").val();
+  evt.preventDefault();
+  $("div.brush").css("background", color)
+  console.log(color);
+}
+
+function divMaker() {
+for(i = 0; i < 5000; i++) {
+  $("body").append("<div class = 'square'></div>")
+  }
+}
+divMaker();
+//put a click event on each individual square
+// so that each square turns to green
+$(".square").on("mouseover", function () {
+  $(this).css("background", color);
+});
