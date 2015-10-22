484b64fd2903b9d28f56bb2eda128327204f4972
Sigmund Nathan Tang <shindigira@gmail.com>
2015-10-22 14:08:03 -0400

    First Commit

--- pixart.js
+++ pixart.js
@@ -1,1 +1,15 @@
+$(document).ready(function(){
+  $(".brush").css("background-color","red");
+});
 
+var pixArt = {
+    setColorButton: $("#set-color"),
+    clickSetColorListener: function(){
+      this.setColorButton.on("click", this.changeColorBrush);
+    },
+    changeColorBrush: function(event){
+      event.preventDefault();
+      $(".brush").css("background-color",$("#color-field").val());
+    },
+}
+pixArt.clickSetColorListener();
