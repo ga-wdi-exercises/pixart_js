cd2a043eca0a6726584c8eea5000fd0f18d34756
CHRYSTAL BANKS <Chrystal.Banks1@gmail.com>
2015-10-22 13:36:43 -0400

    first commit

--- pixart.js
+++ pixart.js
@@ -1,1 +1,15 @@
+$(document).ready(function(){
 
+
+
+$("#set-color").on("click", function(){
+  event.preventDefault();
+  var girl = $("#color-field").val();
+  console.log(girl);
+ });
+
+
+// $("div div:first-of-type").on("click", function(){
+
+
+});
