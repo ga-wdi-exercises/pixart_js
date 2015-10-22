221f29e2dd54a8aaeefc8d958741fc9369bea548
Matt Popovich <mattpopovich@outlook.com>
2015-10-22 14:12:10 -0400

    Realized keydown event failure was obscured by click event. Added diagnostic console logs.

--- pixart.js
+++ pixart.js
@@ -1,6 +1,6 @@
  $( document ).ready( function(){
 
-   //Appends canvas squares to body
+   //Appends canvas squares to body on load
    for(i = 0; i < 20; i++){
      $("body").append("<div class=\"square\"></div>");
    }
@@ -10,6 +10,7 @@
       if(e.keyCode == 13){
         event.preventDefault();
         $(".brush").css("background-color", $("#color-field").val());
+        console.log("the enter key was pressed");
       }
     });
 
@@ -17,6 +18,12 @@
     $("#set-color").click(function(){
       event.preventDefault();
       $(".brush").css("background-color", $("#color-field").val());
+      console.log("a click occurred");
+    });
+
+    //Changes canvas square color when clicked
+    $(".square").mousedown(function(){
+
     });
 
 });
