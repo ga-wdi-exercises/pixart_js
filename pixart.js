a71ecfa249ec2ed07d8be6703f0a16081f45f452
Alexandra <akscreven@gmail.com>
2015-10-22 02:33:17 -0400

    made 20 square divs

--- pixart.js
+++ pixart.js
@@ -6,3 +6,11 @@ $("button").on('click'||'13.keycode',function(event){
     $('.brush').css('background-color',colorSet);
   });
 })
+
+//make 20 squares
+var makeSquares = []
+for (i = 0; i < 20; i++){
+  makeSquares = $(document.createElement('div'));
+  makeSquares.addClass('square')
+}
+$('body').append(makeSquares);
