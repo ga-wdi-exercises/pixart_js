f9c195717b047f48946657022deb6703955f75ee
Becky Beauchamp <beckybeauchamp1@gmail.com>
2015-10-22 15:07:48 -0400

    First Commit

--- pixart.js
+++ pixart.js
@@ -1,26 +1,44 @@
-  var pixart = {
-    clickID: 0,
-    colorID: "",
-
-    chooseColor: function(){
-      event.preventDefault();
-      pixart.clickID++;
-      pixart.colorID = $("input:text").val();
-      alert("This is your color: " + pixart.colorID);
-      $(".brush").css("background", pixart.colorID);
-      if (pixart.clickID <= 1){
-        for(var i = 0; i < 20; i++){
-          var createDiv = $("body").append("<div></div>");
-          var eachDiv = $("div").eq(i).addClass("square");
-        }
-      }
-    },
-
-    clickButton: function(){
-      $("button").on("click",pixart.chooseColor);
-    }
-  };
-
-  $(document).ready(function(){
-    pixart.clickButton();
+$(document).ready(function(){
+  var colorID = $("input:text").val();
+  $("#set-color").on("click", function(e){
+    e.preventDefault();
+    console.log("test!");
+    $(".brush").css("background-color", $("#color-field").val());
   });
+});
+
+
+/*var pixart = {
+ clickID: 0,
+ colorID: "",
+
+ chooseColor: function(){
+   event.preventDefault();
+   pixart.clickDiv();
+   pixart.clickID++;
+   pixart.colorID = $("input:text").val();
+   alert("This is your color: " + pixart.colorID);
+   $(".brush").css("background", pixart.colorID);
+   if (pixart.clickID <= 1){
+     for(var i = 0; i < 20; i++){
+       var createDiv = $("body").append("<div></div>");
+       $("div").eq(i).addClass("square");
+     }
+   }
+ },
+
+ clickButton: function(){
+   $("button").on("click",pixart.chooseColor);
+ },
+
+ clickDiv: function(){
+   $("div.square").on("click", function(){
+     alert("its a div!");
+   });
+ }
+};
+
+$(document).ready(function(){
+ pixart.clickButton();
+});
+*/
