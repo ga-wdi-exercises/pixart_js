1169e643288d5c773fa0172872ef84375a6725c1
Becky Beauchamp <beckybeauchamp1@gmail.com>
2015-10-22 13:27:05 -0400

    Put in Object Pixar and Create Div

--- pixart.js
+++ pixart.js
@@ -1,19 +1,26 @@
-var clickID = 0;
-$(document).ready(function(){
-    $("button").on("click", (function(){
-      clickID++;
-      event.preventDefault();
-      var colorID = $("input:text").val();
-      if(colorID === " " || colorID === ""){
-        alert("Please choose a CSS color!");
-      }
-      else{
-        alert("This is your color: " + colorID);
-        $(".brush").css("background", colorID);
-      }
-      $("form").on("click", (function(){
-        $(".brush").css("background-color", colorID);
-      }));
-  }));
+  var pixart = {
+    clickID: 0,
+    colorID: "",
 
-});
+    chooseColor: function(){
+      event.preventDefault();
+      pixart.clickID++;
+      pixart.colorID = $("input:text").val();
+      alert("This is your color: " + pixart.colorID);
+      $(".brush").css("background", pixart.colorID);
+      if (pixart.clickID <= 1){
+        for(var i = 0; i < 20; i++){
+          var createDiv = $("body").append("<div></div>");
+          var eachDiv = $("div").eq(i).addClass("square");
+        }
+      }
+    },
+
+    clickButton: function(){
+      $("button").on("click",pixart.chooseColor);
+    }
+  };
+
+  $(document).ready(function(){
+    pixart.clickButton();
+  });
