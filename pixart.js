63ddfff213e8fed23883c7a184657cbd1a27bcc0
ANDRE SHONUBI <andre@codedre.com>
2015-10-22 06:14:28 -0400

    commit 2 + 3. I need to go back and make sure step two was down correctly

--- pixart.js
+++ pixart.js
@@ -4,4 +4,17 @@ $(document).ready(function(){
     var color = $("#color-field").val();
     $(".brush").css("background", color);
   });
+
+  // $("#color-field").keydown(function(e){
+  //   if (e.keyCode == 13){
+  //     alert('I was pressed');
+  //   }
+  //   var color = $("#color-field").val();
+  //   $(".brush").css("background", color);
+  // });
+  i =0;
+  while (i < 20){
+    $(".controls").after("<div class='square'></div>");
+    i++;
+  }
 });
