39a1fa17cd2212c4760536b7ad818ed2d8ebccf7
Thomas Solow <thomassolow@gmail.com>
2015-10-22 13:14:27 -0400

    no longer uses dot noation to refer to obj, also collapsed several functions.  to do: find way to disallow non color entries

--- pixart.js
+++ pixart.js
@@ -1,10 +1,9 @@
-
 //object to hold all vars and functions
 var pixArtPainter = {
   currentColor: "#1B4370", //this is the current color being drawn
   clickButton: $("#set-color"), //stores the button
   numPixels: 4000, //number of pixels in the canvas
-  numExtraBrushes: 3, //number of extra brushes, if 0 there will still be 1 brush
+  numExtraBrushes: 2, //number of extra brushes, if 0 there will still be 1 brush
   //sets up the extra brushes
   setUpBrushes: function() {
     for(var i=1; i <= this.numExtraBrushes; i++){
@@ -14,32 +13,27 @@ var pixArtPainter = {
     $(".brush").css("display", "inline-block");
     $(".brush").css("margin", "10px 10px 10px 10px");
   },
-  //this function sets current color to the color of whichever brush is clicked on
-  pickColorBrush: function(event){
-    pixArtPainter.currentColor = $(event.target).css("background");
-  },
-  //adds listeners to each brush
   addBrushListeners: function(){
+    var self = this;
      for(var i=0; i <= this.numExtraBrushes; i++){
-       $(".brush").eq(i).on("click", this.pickColorBrush);
+       $(".brush").eq(i).on("click", function(event){
+         self.currentColor = $(event.target).css("background");
+       });
      }
   },
   //attaches a listener to the input box's button
   setUpBrushListener: function (){
-    this.clickButton.bind("click", this.pickColor);
+    var self = this;
+    this.clickButton.bind("click", function(event) {
+      event.preventDefault(); //cancels default event on click
+      for(var i=self.numExtraBrushes; i>=1; i-=1){
+        //changes each additional brush background
+        $(".brush").eq(i).css("background",$(".brush").eq(i-1).css("background"));
+      }
+      $(".brush").eq(0).css("background",$("#color-field").val()); //changes primary brush background to the value entered in the input box
+      self.currentColor = $("#color-field").val(); //changes current color to the input box's value
+    });
   },
-  //sets the  current color to the brush that was clicked on OR to the value of the input box
-  pickColor: function(event) {
-    //console.log(pixArtPainter.numExtraBrushes)
-    event.preventDefault(); //cancels default event on click
-    for(var i=pixArtPainter.numExtraBrushes; i>=1; i-=1){
-       //changes each additional brush background
-      $(".brush").eq(i).css("background",$(".brush").eq(i-1).css("background"));
-    }
-    $(".brush").eq(0).css("background",$("#color-field").val()); //changes primary brush background to the value entered in the input box
-    pixArtPainter.currentColor = $("#color-field").val(); //changes current color to the input box's value
-  },
-  //adds the pixels to the body
   setUpCanvas: function(pixelCount){
     for (var i =0; i < pixelCount; i++){
       $("body").append("<div class = 'square'></div>");
@@ -47,9 +41,10 @@ var pixArtPainter = {
   },
   //adds listeners to the pixels
   addCanvasListeners: function() {
+    var self = this;
     $(".square").each(function(index){
       $(this).on("mouseover", function(){
-        $(this).css("background", pixArtPainter.currentColor)
+        $(this).css("background", self.currentColor)
       })
     })
   },
