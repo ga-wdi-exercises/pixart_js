0a2944433eed0412d7f47ccc25fe3b75011b83b2
Becky Beauchamp <beckybeauchamp1@gmail.com>
2015-10-22 15:40:48 -0400

    Commit 6

--- pixart.js
+++ pixart.js
@@ -4,10 +4,10 @@ $(document).ready(function(){
     console.log("test!");
     $(".brush").css("background-color", $("#color-field").val());
   });
-    for(var i = 0; i < 20; i++){
+    for(var i = 0; i < 8000; i++){
       $("body").append("<div class = 'square'></div>");
     }
-    $(".square").on("click", function(){
+    $(".square").on("mouseover", function(){
       $(this).css("background-color", $("#color-field").val());
     });
 });
