402d9e61c3b6ee387792cafab289de197112cc91
Chauncey Manson <Mansonchauncey@gmail.com>
2015-10-22 13:28:26 -0400

    finish 3rd commit

--- pixart.js
+++ pixart.js
@@ -1,1 +1,16 @@
+var color;
 
+$("#set-color").click(updateC)
+function updateC(evt){
+  color = $("#color-field").val();
+  evt.preventDefault();
+  $("div.brush").css("background", color)
+  console.log(color);
+}
+
+function divMaker() {
+for(i = 0; i < 20; i++) {
+  $("body").append("<div class ='square'></div>");
+  }
+}
+divMaker()
