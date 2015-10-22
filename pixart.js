e4d0bd3731613d45ee7caef796dbdbb9e1a2f024
Alexis Johnson <alexis.johnson052@gmail.com>
2015-10-22 13:45:00 -0400

    partially functioning swatches

--- pixart.js
+++ pixart.js
@@ -1,23 +1,68 @@
 $(document).ready(function(){
-  var color = $('.brush').css('background')
+  color = $('.brush').css('background')
+  var color_p = color
+  var color_pp = color
+  var color_ppp = color
+
   $('button').on('click', function(e){
     e.preventDefault();
+    color_ppp = color_pp
+    color_pp = color_p
+    color_p = color
     color = $('input').val()
     $('.brush').css('background', color);
+    $('#swatch1').css('background', color_p)
+    $('#swatch2').css('background', color_pp)
+    $('#swatch3').css('background', color_ppp)
   })
 
-  $('#color-field').on('keyup', function(e){
-    if(e.keyCode === 13){
-      e.preventDefault();
-      $('.brush').css('background', color);
-    }
+  // $('#color-field').on('keyup', function(e){
+  //   if(e.keyCode === 13){
+  //     e.preventDefault();
+  //     color_ppp = color_pp
+  //     color_pp = color_p
+  //     color_p = color
+  //     color = $('input').val()
+  //     $('.brush').css('background', color);
+  //     $('#swatch1').css('background', color_p)
+  //     $('#swatch2').css('background', color_pp)
+  //     $('#swatch3').css('background', color_ppp)
+  //   }
+  // })
+
+
+
+  for(i=1; i<=3; i++){
+    $('.brush').append('<div></div>');
+    $('div').eq(i+1).attr('class', 'swatch');
+    $('div').eq(i+1).attr('id', 'swatch'+i);
+  }
+
+  $('.swatch').css('height', '30px');
+  $('.swatch').css('width', '30px');
+  $('.swatch').css('margin', '5px 5px 5px 130px')
+
+  $('.swatch').each(function(){
+    $(this).on('click', function(){
+        color_ppp = color_pp;
+        color_pp = color_p;
+        color_p = color;
+        color = color_pp;
+        $('.brush').css('background', color);
+        $('#swatch1').css('background', color_p);
+        $('#swatch2').css('background', color_pp);
+        $('#swatch3').css('background', color_ppp);
+      })
   })
+
+
 
   for(i=1; i<=8000; i++){
-    divNum = i+1;
+    var divNum = i+4;
     $('body').append('<div></div>');
     $('div').eq(divNum).attr('class', 'square');
-    }
+  }
+
   $('.square').css('height', '10px');
   $('.square').css('width', '10px');
   $('.square').css('margin', '0');
