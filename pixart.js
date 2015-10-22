fa62a05d9739ba0d1659f5891f54b7f0d02e4489
MiaKoda Grodsky <mcgrodsky@gmail.com>
2015-10-21 21:28:36 -0400

    it is working! I am a rebel and i didn't commit exactly when the instructions told us to. but this version is WORKING and i'm going to attempt the color swatch

--- pixart.js
+++ pixart.js
@@ -4,24 +4,25 @@ $(document).ready(function() {
 
   var setColor = $("#set-color");
   var brushColor = $(".brush");
+  var userInput;
 
   var setColor = function(evt) {
     evt.preventDefault();
-    var userInput = $("#color-field").val();
+    userInput = $("#color-field").val();
     brushColor.css("background", userInput);
   }
   $("#set-color").on("click", setColor);
 
-  //create 20 divs .square append to body
-  for (i = 0; i < 8000; i++) {
-    +$("body").append("<div class='square'></div>");
-  }
 
-  //working to click individual squares green, but not to drag and draw. hmm
-  $(".square").on("click", function(){
-    $(this).css("background", "green");
+  var canvasColor = function() {}
+  for (i = 0; i < 8000; i++) {
+    $("body").append("<div class='square'></div>");
+  }
+  $(".square").on("mouseover", function() {
+    $(this).css("background", userInput);
   });
-})
+
+});
 
 
 
