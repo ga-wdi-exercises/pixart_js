875896a6ea02c94aa0f8073b4e01135ef534cda8
Starr Z. Chen <chenstarr@gmail.com>
2015-10-21 23:34:45 -0400

    Click changes square to green

--- pixart.js
+++ pixart.js
@@ -1,4 +1,4 @@
-//change $("brush").css("background-color") to $("#form").val() on click of $("#set-color")
+//change $(".brush").css("background-color") to $("#color-field").val() on click of $("#set-color")
 $("#set-color").on("click", function(e){
   e.preventDefault();
   // console.log("click"); -- for testing
@@ -9,3 +9,9 @@ $("#set-color").on("click", function(e){
 for (var i = 0; i <= 20; i++) {
   $("body").append("<div class = 'square'></div>")
 }
+
+// for EACH $(".square"): change color set in $("#color-field") on click -- not change ALL squares
+
+$(".square").on("click", function(){
+  $(this).css("background-color", "green")
+})
