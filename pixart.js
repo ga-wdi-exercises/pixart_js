eadffad03c86b8cd6ef9b793f769ce1a0b0c0df6
Alice Frumin <alifrumin@yahoo.com>
2015-10-22 16:45:50 -0400

    changed color variable to be global

--- pixart.js
+++ pixart.js
@@ -1,7 +1,7 @@
 // change brush color to input on click
 $("#set-color").on("click", function(){
   event.preventDefault();
-  var color = ($("#color-field").val());
+  color = ($("#color-field").val());
   $(".brush").css("background", color);
 })
 
@@ -19,7 +19,7 @@ for( var i = 0; i < 8000; i++ ){
 $("body").append($("<div class='square'/>"));
 }
 
-/*Add functionality so that when I click on each "square", it changes the color of that individual square to the color set Hint**: either add the event listener while creating the squares, or listen for events on the `body` element */
+/*Add functionality so that when I mouseover on each "square", it changes the color of that individual square to the color set Hint**: either add the event listener while creating the squares, or listen for events on the `body` element */
 $(".square").on("mouseover", function () {
 $(this).css("background", color);
 });
