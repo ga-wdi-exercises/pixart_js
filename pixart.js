7a2e0aaeb3faff8ac052da57597a95209c499b56
Matt Popovich <mattpopovich@outlook.com>
2015-10-22 13:44:43 -0400

    Keydown enter in input field also changes brush box color

--- pixart.js
+++ pixart.js
@@ -1,8 +1,15 @@
  $( document ).ready( function(){
 
-   $("#set-color").on("click", function(){
+  $("color-field").keydown(function(){
+    if(e.keyCode == 13){
       event.preventDefault();
-     $(".brush").css("background-color", $("#color-field").val());
+      $(".brush").css("background-color", $("#color-field").val());
+    }
+  });
+
+  $("#set-color").click(function(){
+     event.preventDefault();
+    $(".brush").css("background-color", $("#color-field").val());
   });
 
 });
