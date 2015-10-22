44bf18bc9b55536b24c958a2340fe3a09bee1238
Jared Nichols <jnichols300@gmail.com>
2015-10-22 13:56:06 -0400

    made squares

--- pixart.js
+++ pixart.js
@@ -2,6 +2,9 @@
 
 $("#set-color").on("click", function(event){
         event.preventDefault()
-        $(".brush").css("background-color",$("#color-field").val())
+        $(".brush").css("background-color", $("#color-field").val())
 
-})
+});
+for(var i = 0; i < 20; i++ ){
+    $("body").append("<div class='square'></div>")
+};
