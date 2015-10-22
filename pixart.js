2005301a8bd1c3c2b3ea97dd8570dce71127f856
Roshana Devkota <roshana.devkota@gmail.COM>
2015-10-21 20:53:59 -0400

    third commit

--- pixart.js
+++ pixart.js
@@ -4,6 +4,12 @@ $( document ).ready(function() {
     var getColor = $( "input" ).val();
     event.preventDefault();
     $(".brush").css("background-color", getColor);
-    
   });
 });
+
+function addingDivs(){
+	for (var i=0; i<20; i++) {
+    $("body").append('<div class="square"></div>');
+    }
+}
+addingDivs();
