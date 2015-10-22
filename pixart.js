9eab894df469235307786517a40c54da47fde9b7
Tory Burgett <vdburgett@gmail.com>
2015-10-22 19:19:02 -0400

    Working, will add bonueses

--- pixart.js
+++ pixart.js
@@ -1,7 +1,3 @@
-// var userColor;
-// var getColor = function(varX){
-//   $(varX).css("background", userColor);
-// };
 
 $(document).ready(function(){
   //don't reload the page
@@ -15,81 +11,27 @@ $(document).ready(function(){
   };
   //put this color in the box
   var getColor = function(varX){
+    userColor = $("#color-field").val();
     $(varX).css("background", userColor);
     event.preventDefault();
   };
-
   //get the color on button click
   $("button").on("click", function(){
     userColorChoose();
     getColor(".brush");
   });
-  //setting container div
-    // $(".controls").after("<div></div>");
-    // $("div:eq(2)").attr("id","squareBlock");
-
-  for(i=0; i<20; i++){
-      $(".controls").after("<div></div>");
-      $("div:eq(2)").attr("class","square");
-      $("div:eq(2)").on("click", getColor("div:eq(2)"));
+  //create 20 divs
+  for(i=0; i<8000; i++){
+      $("body").append("<div class=\"square\"></div>");
+      // $("div:eq(2)").attr("id",i);
+      // $("#"+i).on("click", getColor("#"+i));
   }
 
 
 
-// var canvasRows = 5;
-// var canvasColumns = 4;
-// //Columns
-//   for(i=0; i<canvasColumns; i++){
-//     $("#squareBlock").after("<div></div>");
-//     $("div:eq(3)").attr("id",(i+"Column"));
-//     $("div:eq(3)").attr("class","square");
-//     //Rows
-//     for(j=0; j<canvasRows; j++){
-//       $(".square:eq(0)").append("<div></div>");
-//       $(".square:eq(0)").children().eq(j).attr("id",i+"Column"+j+"Row");
-//       $(".square:eq(0)").children().eq(j).attr("class","squareCell");
-//       $(".square:eq(0)").children().eq(j).css("float", "left");
-//       $(".square:eq(0)").children().eq(j).css("width","10px");
-//       $(".square:eq(0)").children().eq(j).css("height", "10px");
-//       $(".square:eq(0)").children().eq(j).css("background","white");
-//       var placer = $(".square:eq(0)").children().eq(j);
-//       placer.on("click", getColor(placer));
-//     }
-//   }
-  //appending to container div
+$(".square").on("mouseover", function(){
+  $(this).css("background", userColor);
 
-  // $(".square").appendTo("#squareBlock");
-  // $("#squareBlock").css("margin","auto");
-  // $("#squareBlock").css("width","500");
-
-
-
-  // $(".squareCell").on("click", function () {
-  //   // userColorChoose();
-  //   $(".squareCell").css("background",userColor);
-  // });
-
-
-
-
-  // for(i=0; i<canvasColumns; i++){
-  //   for(j=0; j<canvasRows; j++){
-  //     $("#"+i+"Column"+j+"Row").on("click",function(){
-  //       userColorChoose();
-  //       $("#"+i+"Column"+j+"Row").css("background", userColor);
-  //       event.preventDefault();
-  //     })
-  //   }
-  // }
-
-  // // I don't know if I need this?
-  // $("body")[0].addEventListener("click", function(){
-  //   console.log("hello");
-  //   event.preventDefault();
-  // }
-  // );
-
-
-
+});
 
 });
