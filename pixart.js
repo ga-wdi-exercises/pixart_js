13c3047c88da0fc86d7719dc9e34e80639aed318
Alexandra <akscreven@gmail.com>
2015-10-22 12:36:20 -0400

    squares change to chosen color

--- pixart.js
+++ pixart.js
@@ -1,16 +1,16 @@
 // assign color entered in input field to brush
 $( document ).ready( function () {
-$("button").on('click'||'13.keycode',function(event){
+$("button").on('click',function(event){
     event.preventDefault();
     var colorSet = $('#color-field').val();
     $('.brush').css('background-color',colorSet);
   });
-})
-
 //make 20 squares
-var makeSquares = []
-for (i = 0; i < 20; i++){
-  makeSquares = $(document.createElement('div'));
-  makeSquares.addClass('square')
+for(var i = 0;i<20;i++){
+$('body').append($('<div class="square">'))
 }
-$('body').append(makeSquares);
+//change color of square that's clicked on
+  $('.square').on('mouseover',function(){
+    $(this).css('background',$('#color-field').val())
+  })
+})
