66d17cac204560422cda7e81b5834732a45d71d0
James Moon <mooniker@gmail.com>
2015-10-22 14:34:52 -0400

    Added multiple brushes and selectable palette.

--- pixart.js
+++ pixart.js
@@ -6,13 +6,41 @@ $(document).ready(function() {
 
     brushColor: "#1B4370",
 
+    palette: ["#1B4370",],
+
+    borderSelectedBrush: "4px solid darkgray",
+
+    showSelectedBrush: function () {
+      var self = this;
+      $("div.brush").each( function (divBrush) {
+        if ($(this).css("background") == self.brushColor) {
+          $(this).css("border", self.borderSelectedBrush);
+          console.log("Brush for", self.brushColor, "selected. (Palatte:", self.palette + ")");
+        } else { // this div.brush ain't the currently selected color
+          $(this).css("border", "0px none rgb(0, 0, 0)");
+        }
+      });
+    },
+
     setColor: function () {
       var color = $("#color-field").val();
-      console.log("Set color to", color);
       if ( $("div.brush").css("background") != color ) {
-        $("div.brush").css("background", color).attr("title", color);
-        this.brushColor = color;
-      }
+        var self = this;
+        if ( color in this.palette ) { // existing color in palette
+          this.brushColor = color;
+        } else { // completely new color
+          this.brushColor = color;
+          this.palette.push(color);
+          $("div.controls").append($(document.createElement("div")).attr("class", "brush").css("background", color).attr("title", color).css("border", self.borderSelectedBrush ).on("click", function (e) {
+            e.preventDefault();
+            console.log("clicked on brush div");
+            self.brushColor = $(this).css("background");
+          }));
+
+        }
+      } // else  { do nothing because user reselected the current color. }
+      this.showSelectedBrush();
+
 
     },
     setup: function () {
@@ -32,6 +60,12 @@ $(document).ready(function() {
           e.preventDefault();
           $(this).css("background", self.brushColor);
         });
+      });
+
+      $("div.brush").on("click", function (e) {
+        e.preventDefault();
+        console.log("clicked on brush div");
+        self.brushColor = $(this).css("background");
       });
 
     },
