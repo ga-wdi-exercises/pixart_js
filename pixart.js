b9c3bb6dab78e60664574d7243f74618a3c6bb89
Timothy Gregg <timtgregg@gmail.com>
2015-10-22 14:33:58 -0400

    removed a few lines

--- pixart.js
+++ pixart.js
@@ -1,22 +1,16 @@
 var setColorButton = $('#set-color');
 var inputSelector = $('#color-field');
 var swathDiv = $('.brush');
-var box1 = $('#box1');
-var box2 = $('#box2');
-var box3 = $('#box3');
-var box4 = $('#box4');
 var drawColor=null;
 
 swathDiv.on('click',function(){
   drawColor=$(this).css('backgroundColor');
-  console.log($(this).css('backgroundColor'));
 });
 
 var cycle = 0;
 var arr = $('.brush');
 setColorButton.on('click', function(evt){
   evt.preventDefault();
-  console.log('click');
   arr.eq(cycle).css('backgroundColor',inputSelector.val());
   cycle++;
   if (cycle===4){
@@ -29,7 +23,5 @@ for(var a=0;a<8000;a++){
 }
 
 $('.square').on('mouseover',function(evt){
-  console.log('div clicked');
-  console.log(evt.target);
-  $(evt.target).css('background', drawColor|| swathDiv.css('backgroundColor'));
+  $(this).css('background', drawColor || swathDiv.css('backgroundColor'));
 });
