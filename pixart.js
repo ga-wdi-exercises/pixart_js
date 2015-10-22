acec1327bb43a4a41a04131109ff2c9c41d710f9
Armani Hall <MRBOJANGLES226@GMAIL.COM>
2015-10-22 09:58:57 -0400

    color brush color changing

--- pixart.js
+++ pixart.js
@@ -1,1 +1,10 @@
+var colorSelect;
 
+$('#set-color').click(color)
+function color(evt){
+  colorSelect = $('#color-field').val();
+  console.log(colorSelect);
+  console.log('color select');
+  evt.preventDefault();
+  $("div.brush").css("background", colorSelect)
+}
