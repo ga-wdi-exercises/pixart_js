3db6bc09facde739777c76c4a76a823d64525aec
Thomas Clark <tclark@soteriagiving.com>
2015-10-22 14:07:09 -0400

    add document selectors by class, id

--- pixart.js
+++ pixart.js
@@ -1,3 +1,4 @@
 var button = document.querySelector("#set-color");
 var colorField = document.querySelector("#color-field");
-console.log("this happened");
+var brush = document.querySelector(".brush");
+console.log("this is happening");
