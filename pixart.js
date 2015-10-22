2ee5d199ac60d5181e627600789b46aea628d39f
ckib16 <ckib16@gmail.com>
2015-10-22 17:31:41 -0400

    fourth commit done

--- pixart.js
+++ pixart.js
@@ -1,17 +1,31 @@
+/* global $ */
+
+var userInput
+
 $(document).ready(function () {
   var inputField = $('input:text')
   console.log(inputField)
   $('#set-color').on('click', function (evt) {
     evt.preventDefault()
-    var userInput = $('input').val()
+    userInput = $('input').val()
     $('.brush').css('background', userInput)
     console.log(userInput)
   })
 
-  // * Create 20 divs of the "square" class and append them to the body
-  //   * **Hint**: use `.append()`
+  // ###Commit 4
+  //
+  // * Add functionality so that when I click on each "square", it changes the color of that individual square to "green"
+  //   * **Hint**: either add the event listener while creating the squares, or listen for events on the `body` element
+
   var amountSquareDivs = 20
   for (var i = 0; i < amountSquareDivs; i++) {
     $('body').append('<div class="square"></div>')
   }
+
+  $('.square').each(function (evt) {
+    $(this).on('click', function (evt) {
+      console.log($(this))
+      $(this).css('background', userInput)
+    })
+  })
 })
