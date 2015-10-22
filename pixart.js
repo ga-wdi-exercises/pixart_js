9ffc3be6994963b9437644b79c1e353b90c45a45
Peter Zhang <pzhang87@gmail.com>
2015-10-22 12:38:08 -0400

    add event listener to input field and color button

--- pixart.js
+++ pixart.js
@@ -1,1 +1,14 @@
+$("document").ready(function(){
+//add event listener to button
+  $("#set-color").on("click", function(evt){
+    //prevent default reload on button
+    evt.preventDefault();
+    //debug comment
+    console.log("clicked");
+    //get value of text input field and assign it to brush
+    var brushInput = $("#color-field").val();
+    $(".brush").css("background", brushInput);
 
+  })
+
+})
