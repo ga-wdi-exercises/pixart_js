0ea914161f6a7b8babcd06d2666c8e63280d46ef
Aurelio Avelleyra <Ao@Aurelios-MacBook-Pro.local>
2015-10-22 13:40:42 -0400

    Set Color Button

--- pixart.js
+++ pixart.js
@@ -1,1 +1,7 @@
-
+$(document).ready(function() {
+  $("#set-color").on("click",function(evt) {
+    evt.preventDefault();
+    var newColor = $("#color-field").val();
+    $(".brush").css("background", newColor)
+  })
+})
