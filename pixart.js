082283ca06a4fc2a4044e8e67eb15afd1fa0eaa0
Ly Nguyen <Ly900@aol.com>
2015-10-21 21:33:04 -0400

    finish part 1

--- pixart.js
+++ pixart.js
@@ -1,12 +1,13 @@
 $(document).ready(function (){
 
+// // Put a click event handler on the button Set Color
+// // Get value from input?
+// // Replace background color of brush box with input from form
+$("#set-color").on("click", function(event) {
+  var userColor = $("#color-field").eq(0).val();
+  $("div.brush").css("background-color", userColor);
+  event.preventDefault();
+}); //end on event handler
 
 
-
-
-
-
-
-
-
-});
+}); //ends document.ready
