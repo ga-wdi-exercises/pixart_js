229b61b9433c33d741e032ca17bd043e87260ce9
Rachel Porter <rachelandrewsporter@gmail.com>
2015-10-21 20:38:52 -0400

    second commit to add redundant keypress listener

--- pixart.js
+++ pixart.js
@@ -1,7 +1,16 @@
 // * When I click the "Set Color" button, it should change the color of the "brush" box to the color I specify in the input field. (**Hint:** You will need to use `event.preventDefault()` somewhere in your code.)
 // use jQuery to select the element, and `addEventListener` to handle clicks
-$("button")[0].addEventListener("click", function(event) {
-  event.preventDefault();
-  color = $("[type]").val();
-  $("div.brush").css("background", color);
+// $("button")[0].addEventListener("click", function(event) {
+//   event.preventDefault();
+//   color = $("[type]").val();
+//   $("div.brush").css("background", color);
+// });
+
+//this is for commit 2, but it was actually already working before writing this code, based on the above. I tried to write it anyway for practice but can't get it to work..
+$("#set-color").keypress( function(event) {
+  if ( event.keyCode == 13 ) {
+     event.preventDefault();
+     color = $("[type]").val();
+     $("div.brush").css("background", color);
+   }
 });
