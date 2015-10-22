d569e49b5c242e5b09fb0e3f48d89554c34b24a0
MiaKoda Grodsky <mcgrodsky@gmail.com>
2015-10-21 21:17:05 -0400

    added click event to newly created divs. green when clicked

--- pixart.js
+++ pixart.js
@@ -1,44 +1,27 @@
-//1)type color you want (hex code, rgb) in input-field ID = "#color-field", then hit "set-color", then have the box
-// "brush" change color to the one you specified
-
 //on click have the box "brush" change to color you specified in pickColor
 
 $(document).ready(function() {
 
-var setColor = $("#set-color");
-var brushColor = $(".brush");
-
+  var setColor = $("#set-color");
+  var brushColor = $(".brush");
 
   var setColor = function(evt) {
     evt.preventDefault();
     var userInput = $("#color-field").val();
     brushColor.css("background", userInput);
   }
-$("#set-color").on("click", setColor);
+  $("#set-color").on("click", setColor);
 
+  //create 20 divs .square append to body
+  for (i = 0; i < 8000; i++) {
+    +$("body").append("<div class='square'></div>");
+  }
 
-//create 20 divs .square append to body
-for (i = 0; i < 20; i++) {
-+    $("body").append("<div class='square'></div>");
-}
+  //working to click individual squares green, but not to drag and draw. hmm
+  $(".square").on("click", function(){
+    $(this).css("background", "green");
+  });
 })
-
-
-
-// $(".awesome").on("click", function(){
-//   $(this).css("background-color", "red");
-// })
-
-
-
-
-
-
-
-
-
-
-
 
 
 
