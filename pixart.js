f8e0b24d6b19637c87a507e0d17bad7523232420
James Moon <mooniker@gmail.com>
2015-10-22 11:04:40 -0400

    Add functionality for div color changes on mouse over

--- pixart.js
+++ pixart.js
@@ -6,30 +6,44 @@ $(document).ready(function() {
 
   var drawing = {
 
+    pixelCount: 20,
+
+    brushColor: "black",
+
     setColor: function () {
       var color = $("#color-field").val();
       console.log("Set color to", color);
       $("div.brush").css("background", color);
+      this.brushColor = color;
     },
-    setupListeners: function () {
+    setup: function () {
       $("#set-color").on ("click", function (e) {
         e.preventDefault();
         this.setColor();
       }.bind(this));
+
+      for ( var i = 0; i < this.pixelCount; i++ ) {
+        //var newSquare = document.createElement("div");
+        //newSquare.setAttribute("class", "square");
+        // newSquare.addEventListener("click")
+        //$("body").append(newSquare);
+        $("body").append("<div class=\"square\"></div>");
+      }
+      console.log("Just created", this.pixelCount, "square divs.");
+
+      $("div.square").each( function() {
+        $(this).on("mouseover", function (e) {
+          e.preventDefault();
+          //console.log("hovering");
+          // $(this).css("background", color);
+          $(this).css("background", drawing.brushColor); // not ideal
+        });
+      });
+
     },
+
   };
 
-
-  // function setColor() {
-  //   var color = $("#color-field").val();
-  //   console.log("Set color to", color);
-  //   $("div.brush").css("background", color);
-  // }
-
-  // for ( var i = 0; i < 20; i++ ) {
-  //   $("body").append()
-  // }
-
-  drawing.setupListeners();
+  drawing.setup();
 
 });
