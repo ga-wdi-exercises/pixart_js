746961dc392f05ffae7bcb839b3266b2cea8382e
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2015-10-22 14:27:07 -0400

    set three swatches

--- pixart.js
+++ pixart.js
@@ -1,10 +1,23 @@
+var clickCount = 0;
+
 $(document).ready(function(){
+  $( ".controls" ).append( "<div class = 'brush'></div>" );
+  // $(".brush2").css({"height": "100px","width":"100px","background":"#1B4370","margin":"15px auto"})
+
+  $( ".controls" ).append( "<div class = 'brush'></div>" );
+  // $(".brush3").css({"height": "100px","width":"100px","background":"#1B4370","margin":"15px auto"})
 
   $("#set-color").on("click", function(evt){
     var colorText = $("#color-field").val();
     evt.preventDefault();
-    $(".brush").css("background", colorText);
+    $(".brush").eq(clickCount).css("background", colorText);
+    if (clickCount < 2) {
+      clickCount++
+    } else {
+      clickCount = 0
+    }
   })
+
 
   for (var i=0; i<=8000; i++){
     $( "body" ).append( "<div class = 'square'></div>" );
@@ -19,10 +32,10 @@ $(document).ready(function(){
   }
 
 
-    $(".square").on("mouseover", function(){
-      colorText = $("#color-field").val();
-      $(this).css("background", colorText);
-    });
+  $(".square").on("mouseover", function(){
+    colorText = $("#color-field").val();
+    $(this).css("background", colorText);
+  });
 
   $(".square").css({"height": "10px","width":"10px","margin":"0"});
 
@@ -30,3 +43,48 @@ $(document).ready(function(){
 
 
 // git checkout test
+
+
+
+  // if(clickCount === 0){
+  //   $("#set-color").on("click", function(evt){
+  //     var colorText = $("#color-field").val();
+  //     evt.preventDefault();
+  //     $(".brush").eq(clickCount).css("background", colorText);
+  //     clickCount++
+  //   })
+  //   $("#set-color").keydown(function(evt) {
+  //     if ( evt.keycode == 13 ) {
+  //       event.preventDefault();
+  //       $(".brush").eq(clickCount).css("background", colorText);
+  //     }
+  //   })
+  // } else if (clickCount === 1) {
+  //   $("#set-color").on("click", function(evt){
+  //     var colorText = $("#color-field").val();
+  //     evt.preventDefault();
+  //     $(".brush").eq(clickCount).css("background", colorText);
+  //     clickCount++
+  //   })
+  //   $("#set-color").keydown(function(evt) {
+  //     if ( evt.keycode == 13 ) {
+  //       event.preventDefault();
+  //       $(".brush").eq(clickCount).css("background", colorText);
+  //     }
+  //   })
+  //
+  // } else if (clickCount >1) {
+  //   $("#set-color").on("click", function(evt){
+  //     var colorText = $("#color-field").val();
+  //     evt.preventDefault();
+  //     $(".brush").eq(clickCount).css("background", colorText);
+  //     clickCount=0;
+  //   })
+  //   $("#set-color").keydown(function(evt) {
+  //     if ( evt.keycode == 13 ) {
+  //       event.preventDefault();
+  //       $(".brush").eq(clickCount).css("background", colorText);
+  //       clickCount=0;
+  //     }
+  //   })
+  // }
