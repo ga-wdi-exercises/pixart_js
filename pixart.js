5329c8a7b3aa4a95a1f72669540f34981f97271a
Armani Hall <MRBOJANGLES226@GMAIL.COM>
2015-10-22 18:52:17 -0400

    squares change color and 8000 made

--- pixart.js
+++ pixart.js
@@ -1,3 +1,4 @@
+$(document).ready(function() {
 var colorSelect;
 
 $('#set-color').click(color)
@@ -7,4 +8,34 @@ function color(evt){
   console.log('color select');
   evt.preventDefault();
   $("div.brush").css("background", colorSelect)
+
+
+$('.square').click(function(){
+  $(this).css("background", colorSelect)
+  // colorSelect = $('#brush').val();
+  // evt.preventDefault();
+
+// var div = $("<div></div>");
+// $("#box").append(div);
+})
+
 }
+// $(function() {
+// $('.square').click(function(){
+//     $(this).css('background', 'yellow');
+
+//   });
+// });
+
+$('body').append(
+  $('<div>')
+    .attr("id", "squareDiv")
+    .addClass("square")
+  )
+    // .append("<span/>")
+var pageLimit=8000;
+     for(var i = 1; i <= pageLimit; i++) {
+      $('body').append('<div id="squareDiv' + i + '" class="square"></ div>' )
+    }
+    }
+);
