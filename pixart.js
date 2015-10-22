e22ba2771c7e93104388e675e3309f2c28c50c04
Rachel Porter <rachelandrewsporter@gmail.com>
2015-10-21 21:07:23 -0400

    commit 4 - add green on click to square divs

--- pixart.js
+++ pixart.js
@@ -1,5 +1,6 @@
 // * When I click the "Set Color" button, it should change the color of the "brush" box to the color I specify in the input field. (**Hint:** You will need to use `event.preventDefault()` somewhere in your code.)
 // use jQuery to select the element, and `addEventListener` to handle clicks
+
 $("button")[0].addEventListener("click", function(event) {
   event.preventDefault();
   color = $("[type]").val();
@@ -18,5 +19,11 @@ $("button")[0].addEventListener("click", function(event) {
 //commit 3
 //Create 20 divs of the "square" class and append them to the body - use .append()
 for (i = 0; i < 20; i++) {
-  $(document.createElement("div")).attr("class", "square").appendTo('body');
+  $(document.createElement("div")).attr("class", "square").appendTo('body').click( function () {
+    $(this).css("background", "green");
+  });
 }
+
+//commit 4
+// Add functionality so that when I click on each "square", it changes the color of that individual square to "green"
+//   * **Hint**: either add the event listener while creating the squares, or listen for events on the `body` element
