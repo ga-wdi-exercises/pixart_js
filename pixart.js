521d9b72cdcfa4906ca69b875fbaf7f2ebb6a642
jen pengelly <jgpengelly@gmail.com>
2015-10-22 17:59:49 -0400

    pixart.js: square's click with input color.

--- pixart.js
+++ pixart.js
@@ -2,14 +2,14 @@
 
 var button = $("#set-color");
 var textField = $('input:text');
-
+var colorSelected = $("#color-field").val();
 //Change brush color on click
 
 function changeBrushColorClick() {
   button.on("click", function(evt){
     evt.preventDefault()
-    $(".brush").css("background", $("#color-field").val())
-    console.log("Clicking the button works!")
+    $(".brush").css("background", $("#color-field").val());
+//    console.log("Clicking the button works!")
   })
 }
 
@@ -20,32 +20,31 @@ function changeBrushColorEnter() {
   textField.keyup(function (evt) {
     if (evt.keyCode === 13) {
         $(".brush").css("background", textField.val());
-        console.log("The enter key works!");
+//        console.log("The enter key works!");
     }
   })
 }
-
 changeBrushColorEnter();
 
 // Create 20 divs and append to body
-for (var i = 0; i < 20; i++) {
+for (var i = 0; i < 8000; i++) {
   $("body").append("<div class='square'></div>");
-  $(".square").on("click", squareColorUpdate)
-  console.log("I made 20 divs")
 }
 
+// Event listener for .square
+function squareEvents () {
+  $(".square").on("click", squareColorUpdate)
+}
+squareEvents()
 
 // Make the individual DIV change color
 function squareColorUpdate(evt) {
+
   var target = $(evt.target);
   if (target.is(".square")) {
-    target.css("background", "green")
+    target.css("background", $("#color-field").val() )
   }
-  console.log("The 20 divs are changing color");
 }
-
-// Commit #5:
-// Change so that on click, square changes to the color set using input.
 
 // Commit 6:
 // Modify square class css == height 10 px; width 10 px, margin 0
