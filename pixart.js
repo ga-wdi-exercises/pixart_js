071c89eb95c1e23812bb68a50db97a5cd5449f11
Markham Shofner <markham.shofner@gmail.com>
2015-10-22 13:31:54 -0400

    add into code that tests action  on the color swatch

--- pixart.js
+++ pixart.js
@@ -1,1 +1,40 @@
+$(document).ready(function() {
 
+  $("div.brush").css("background","red");
+  $("#set-color").on("click", function(){
+    event.preventDefault();
+    $("div.brush").css("background","green");
+  });
+
+});
+
+
+
+
+// Events
+// $(document).ready(function(){
+//  $(".awesome").on("click",function(){
+//   console.log("awesome div was clicked);
+//  })
+// })
+//
+//
+//
+// $(document).ready(function(){
+//  
+//$(".awesome").eq(0).children().each(function(index){
+//    $(this).on("click",function(){
+//      $(this).html("bob")
+//    })
+//  })
+//
+// })
+//
+// Can cycle through using .each(function(index){code}
+// $(document).ready(function(){
+//  $(".awesome").eq(0).children().each(function(index){
+//    $(this).addClass("peachpuff")
+//    $(this).html(index)
+//  })
+// })
+//
