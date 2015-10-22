548459a5dd29f9e085b98b8ced04960a061db619
Matt Popovich <mattpopovich@outlook.com>
2015-10-22 14:05:24 -0400

    20 canvas squares append to body  on load from a for loop

--- pixart.js
+++ pixart.js
@@ -1,15 +1,22 @@
  $( document ).ready( function(){
 
-  $("color-field").keydown(function(){
-    if(e.keyCode == 13){
+   //Appends canvas squares to body
+   for(i = 0; i < 20; i++){
+     $("body").append("<div class=\"square\"></div>");
+   }
+
+   //Fills brush box bg with hex color from input on Enter keydown
+   $("color-field").keydown(function(){
+      if(e.keyCode == 13){
+        event.preventDefault();
+        $(".brush").css("background-color", $("#color-field").val());
+      }
+    });
+
+    //Fills brush box bg with hex color from input on button click
+    $("#set-color").click(function(){
       event.preventDefault();
       $(".brush").css("background-color", $("#color-field").val());
-    }
-  });
-
-  $("#set-color").click(function(){
-     event.preventDefault();
-    $(".brush").css("background-color", $("#color-field").val());
-  });
+    });
 
 });
