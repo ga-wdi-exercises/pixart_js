40360e2efc20b3f5d0e491ffbd8b8d58dad5010e
Roshana Devkota <roshana.devkota@gmail.COM>
2015-10-21 20:46:06 -0400

    Commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,8 @@
-
+$( document ).ready(function() {
+    console.log( "ready!" );
+    $('#set-color').click(function(){
+    var getColor = $( "input" ).val();
+    event.preventDefault();
+    $(".brush").css("background-color", getColor); 
+  });
+});
