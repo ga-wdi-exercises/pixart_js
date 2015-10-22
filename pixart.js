b2ae6f6ce56ce0b1d30521ea125b09b6e7223088
Aurelio Avelleyra <Ao@Aurelios-MacBook-Pro.local>
2015-10-22 14:45:56 -0400

    Painting Pics

--- pixart.js
+++ pixart.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
-  $("#set-color").on("click", function(evt) {
+  $("#set-color").on("mouseover", function(evt) {
     evt.preventDefault();
     var newColor = $("#color-field").val();
     $(".brush").css("background", newColor)
   })
 
   var divSq = []
-  for (var i = 0; i < 20; i++) {
+  for (var i = 0; i < 8000; i++) {
     divSq.push('<div class="square"></div>');
   }
   $("body").append(divSq);
 
-  $(".square").on("click", function(){
+  $(".square").on("mouseover", function(){
     var newColor = $("#color-field").val();
     $(this).css("background", newColor)
   })
