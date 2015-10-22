e3a1449f021dae81cc21d2d72c5a46815be93b0a
Starr Z. Chen <chenstarr@gmail.com>
2015-10-22 00:03:15 -0400

    Pixel Art working w/ mouseover on 8000 squares

--- pixart.js
+++ pixart.js
@@ -5,15 +5,12 @@ $("#set-color").on("click", function(e){
   $(".brush").css("background-color", $("#color-field").val())
 })
 
-//append 20 of <div class = "square"></div> to body
-for (var i = 0; i <= 20; i++) {
+//append 8000 of <div class = "square"></div> to body
+for (var i = 0; i <= 8000; i++) {
   $("body").append("<div class = 'square'></div>")
 }
 
-//when each $('.square') is clicked, change color to $(#color-field).val
-
-$(".square").on("click", function(){
+//when each $('.square') is mouseover-ed, change color to $(#color-field).val
+$(".square").on("mouseover", function(){
   $(this).css("background-color", $("#color-field").val())
 })
-//Uncaught TypeError: this.each is not a function
-//this.each is from the minified jquery CDN???
