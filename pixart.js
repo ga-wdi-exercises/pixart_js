37e5e7ce0b3bfc2899bdc96ab383a831c80c0369
Armani Hall <MRBOJANGLES226@GMAIL.COM>
2015-10-22 19:04:04 -0400

    paintbrush works

--- pixart.js
+++ pixart.js
@@ -10,32 +10,19 @@ function color(evt){
   $("div.brush").css("background", colorSelect)
 
 
-$('.square').click(function(){
+$('.square').mouseover(function(){
   $(this).css("background", colorSelect)
-  // colorSelect = $('#brush').val();
-  // evt.preventDefault();
-
-// var div = $("<div></div>");
-// $("#box").append(div);
-})
-
+  })
 }
-// $(function() {
-// $('.square').click(function(){
-//     $(this).css('background', 'yellow');
-
-//   });
-// });
 
 $('body').append(
   $('<div>')
     .attr("id", "squareDiv")
     .addClass("square")
   )
-    // .append("<span/>")
+
 var pageLimit=8000;
      for(var i = 1; i <= pageLimit; i++) {
       $('body').append('<div id="squareDiv' + i + '" class="square"></ div>' )
     }
-    }
-);
+  })
