1a5080f1134e8a182667f36b63b667f89efad13b
alea q <aleaq@aleas-AppleSmoothie.local>
2015-10-22 10:30:23 -0400

    got style to work

--- pixart.js
+++ pixart.js
@@ -7,14 +7,59 @@
    })
 
 //2
-$("input[type='text']").keyup(function(event){
+$("input[type='text']").keyup(function(){
   $(".brush").css("background" , "pink") //gets same background when enter is pushed in the input section
 })
 
 //3
 //create div tag put the class in the tag and then append to body / loop however many times
-for(i=0; i<20; i++){
-var lotsOfDivs = document.createElement("div");
-lotsOfDivs.className = "square"
-document.body.appendChild(lotsOfDivs)
+var createDiv = function() {
+  for(var i=0; i<20; i++){
+var div = document.createElement("div");
+
+// $(this).css("width", "50px");
+$("body").append("<div class = 'square'></div>");
 }
+  }
+  createDiv();
+
+$(".square").css({
+  width: "10px"
+  // margin: "0"
+})
+
+$(".square").css({
+  margin: "0px"
+})
+
+
+
+  // $("div").each(function(){
+  //   document.querySelector(".square").style.width="50px"
+  //     document.querySelector(".square").style.margin="0px"
+  // });
+// });
+
+// $(function lotsOfDivs() {
+//   $("div").width(10)
+//   $("div").css({'margin':'0px'});
+  // $(this).addClass("square");
+  // $.appendTo(document.body)('<div class= "square"></div>')
+// })
+// for(i=0; i<20; i++){
+// var lotsOfDivs = document.createElement("div");
+// lotsOfDivs.className = "square"
+// document.body.appendChild(lotsOfDivs)
+// }
+// lotsOfDivs.style.width = 10px;
+// lotsOfDivs.style.margin = 0px;
+
+//4
+// function changeColor(){
+//   $("div").on("click", function(){
+//     $(".square").css("background" , "green")
+//   })
+// }
+
+
+//5
