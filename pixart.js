d52ec071816665502f7ae6d46cb9a526c98a69ed
Markham Shofner <markham.shofner@gmail.com>
2015-10-23 09:04:29 -0400

    adjust css style so that div boxes are smaller, allowing more granular art.

--- pixart.js
+++ pixart.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
   });
 
   // create 20 divs of the "square" class and append to body
-  for (var i = 0; i<8000; i++) {
+  for (var i = 0; i<16000; i++) {
     var div = document.createElement("div");
     $("body").append(div);
     $(div).attr("class","square");
@@ -26,30 +26,3 @@ $(document).ready(function() {
 
 
 });
-
-
-// Commit 6
-//
-// Modify the CSS so that the "square" class has a height and width of 10px and a margin of 0.
-// Modify your code so that you are creating 8000 divs instead of 20.
-// Change the event that changes your box colors from 'click' to 'mouseover'
-
-
-
-
-
-
-//$(".awesome").eq(0).children().each(function(index){
-//    $(this).on("click",function(){
-//      $(this).html("bob")
-//    })
-//  })
-
-
-// Can cycle through using .each(function(index){code}
-// $(document).ready(function(){
-//  $(".awesome").eq(0).children().each(function(index){
-//    $(this).addClass("peachpuff")
-//    $(this).html(index)
-//  })
-// })
