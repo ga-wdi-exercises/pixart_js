65bcd59906de2f8a7c121b78e9d2a6a41869d905
Josh Cohen <jacohen10@EMAIL.COM>
2015-10-23 08:48:18 -0400

    finished  bonus

--- pixart.js
+++ pixart.js
@@ -1,39 +1,49 @@
+var clickCount = 0;
+var squareColor;
 $(document).ready(function(){
 
+  for (i = 0; i < 1000; i++) {
+    $("<div></div>").attr("class", "square").appendTo($("body"));
+  }
+
+  $(".square").css({
+    "width": "30px",
+    "height": "30px",
+    "margin": "0",
+  });
+
+
+
+  // add 2 boxes for the most recently used colors
+  for (i = 0; i < 2; i++) {
+    $("<div></div>").attr("class", "brush").appendTo($(".controls"));
+  }
 
   $("#set-color").on("click", function(){
     event.preventDefault();
-    $(".brush").css("background", $("input").val());
-  });
-
-  $("#set-color").keydown(function(event){
-    if (event.keyCode === 13); {
-      event.preventDefault();
-    $(".brush").css("background", $("input").val());
+    $(".brush").eq(clickCount).css("background", $("input").val());
+    $(".square").on("mouseover", function(){
+      $(this).css("background", $("input").val());
+    });
+    if(clickCount < 2){
+      clickCount++;
+    } else {
+       clickCount = 0;
     }
   });
 
-  for (i = 0; i < 8000; i++) {
-    $("<div></div>").attr("class", "square").appendTo($("body"));
-}
+// listen for click on brush
+// get the value of that brush
+// change the background of the square with the value of that brush that was clicked on
+// will original mouseover cause issues?
 
 
-    // $(".square").css({
-    //   "width": "10px",
-    //   "height": "10px",
-    //   "margin": "0",
-    // });
-
-$(".square").on("mouseover", function(){
-  $(this).css("background", $("input").val());
-});
-    // $(".square"+[i]).on("click", function(){
-    //   $(".square"+[i]).css({
-    //     "background": "green",
-    //   });
-    // });
-
-
+  $(".brush").on("click", function(){
+  squareColor = $(this).css("background") ;
+    $(".square").on("mouseover", function(){
+      $(this).css("background", squareColor);
+    });
+  });
 
 
 
