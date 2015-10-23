2d269e8f59adde37f57a7ab10acee4ded3bf85df
Shawn Campbell <321campbell@gmail.com>
2015-10-23 07:49:18 -0400

    Final

--- pixart.js
+++ pixart.js
@@ -6,16 +6,17 @@ var brush = document.querySelector('.brush');
 /*onclick="document.getElementById('brush').style.color = 'red'">*/
 
 //here is a function with the suggested event.preventDefault
-$("set-color").click('submit', function(event){
+/*$("set-color").click('submit', function(event){
     event.preventDefault();
     $(".brush").css("backgroundcolor", "input.value");
 });
-};
+};*/
 
-/*form.addEventListener('submit', function(event){
+
+form.addEventListener('submit', function(event){
   event.preventDefault();
   brush.style.backgroundColor = input.value;
-});*/
+});
 
 for( var i = 0; i < 8000; i++){
   var div = document.createElement('div');
@@ -25,3 +26,5 @@ for( var i = 0; i < 8000; i++){
   });
   document.body.appendChild(div);
 }
+
+//Next step, attempting to add the .png file with CSS Sprites:https://css-tricks.com/css-sprites/ NOT COMPLETED.
