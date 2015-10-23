dec88ae5b72182b8ceffcf266dbb1d85fb791412
Andrew Johnson <andrewjohnson8819@gmail.com>
2015-10-23 05:42:59 -0400

    appended div/squares to body

--- pixart.js
+++ pixart.js
@@ -1,6 +1,7 @@
 var colorField = $("#color-field")
 var setColor = $("#set-color")
 var color
+
 $(document).ready(function(){
 
   setColor.on("click",function(evt){
@@ -8,4 +9,10 @@ $(document).ready(function(){
     color = colorField.val()
     $(".brush").css("background", color)
   })
+
+  for(i = 0; i<20; i++) {
+    $("body").append($("<div class='square'/>"))
+  }
+
+
 })
