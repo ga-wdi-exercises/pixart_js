a0e4227b638661da7fd812e55f064c0c537567a5
Matiullah Amin <matiullah243@gmail.com>
2015-10-22 21:35:38 -0400

    implemented all the steps successfully.

--- pixart.js
+++ pixart.js
@@ -1,14 +1,45 @@
-
 $(document).ready(function(){
+
+  var clickcount=1;
+
   $("#set-color").on("click", function(){
-  event.preventDefault();
-  var brushColor = $("input").val();
-  $(".brush").css("background-color", brushColor);
-  $("#color-field").keypress(fucntion(){
-    if (keyCode == 13) {$(".brush").css("background-color", brushCollor);
+    event.preventDefault();
+    var brushColor = $("input").val();
+    if (clickcount==1) {
+      $(".brush").css("background-color", brushColor);
+      clickcount++;
+    } else if (clickcount==2) {
+      $(".swatch").eq(0).css("background-color", brushColor);
+      clickcount++;
+    } else if (clickcount==3) {
+      $(".swatch").eq(1).css("background-color", brushColor);
+      clickcount=1;
     }
   });
 
 
-});
+  var $newdiv;
+  for (var i=0; i<=8000; i++){
+    $newdiv = $('<div class="square" />');
+    $('body').append($newdiv);
+  }
+
+  var $newbrush;
+  for (i=0; i<=1; i++){
+    $newbrush= $('<div class="swatch"/>');
+    $(".controls").append($newbrush);
+  }
+
+  // var $swatchcolor;
+  // $(".swatch").on("click", function (){
+  //   var $swatchcolor = $(this).css("background-color");
+  //   console.log(swatchcolor);
+  // });
+
+
+  $(".square").on("mouseover", function(){
+    var brushColor = $("input").val();
+    $(this).css("background-color", brushColor);
+  });
+
 });
