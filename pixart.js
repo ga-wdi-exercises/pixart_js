c68ee281ee94aabaef2d3eccd82a1b823953a45d
Shawn Campbell <321campbell@gmail.com>
2015-10-23 07:35:44 -0400

    Commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,27 @@
+var form = document.getElementById('form');
+var input = document.querySelector('#color-field');
+var brush = document.querySelector('.brush');
 
+//another attempt at getting the color to switch
+/*onclick="document.getElementById('brush').style.color = 'red'">*/
+
+//here is a function with the suggested event.preventDefault
+$("set-color").click('submit', function(event){
+    event.preventDefault();
+    $(".brush").css("backgroundcolor", "input.value");
+});
+};
+
+/*form.addEventListener('submit', function(event){
+  event.preventDefault();
+  brush.style.backgroundColor = input.value;
+});*/
+
+for( var i = 0; i < 8000; i++){
+  var div = document.createElement('div');
+  div.classList.add('square');
+  div.addEventListener('mouseover', function(){
+    this.style.backgroundColor = brush.style.backgroundColor;
+  });
+  document.body.appendChild(div);
+}
