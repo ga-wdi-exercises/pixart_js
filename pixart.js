9c9b322d6b6b96db7e1e8abab11efbf90ffac732
Andrew Johnson <andrewjohnson8819@gmail.com>
2015-10-23 07:21:46 -0400

    combined commits 5 and 6 (mouseover and color input)

--- pixart.js
+++ pixart.js
@@ -9,13 +9,13 @@ $(document).ready(function(){
     $(".brush").css("background", color)
   })
 
-  for(i = 0; i<20; i++) {
+  for(i = 0; i<8000; i++) {
     $("body").append($("<div class='square'></div>"))
   }
 
-  $(".square").on("click", function(){
-
-    $(this).css("background-color", green);
+  $(".square").on("mouseover", function(){
+    color = colorField.val()
+    $(this).css("background", color);
   })
 
 })
