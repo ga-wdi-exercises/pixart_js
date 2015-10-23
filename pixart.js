8602c0a96f84b150652986dc0922159847db1e0a
Mariia Kotylevska <mkotylevska@gmail.com>
2015-10-23 10:01:58 -0400

    initial commit

--- pixart.js
+++ pixart.js
@@ -1,1 +1,18 @@
+$(document).ready(function() {
+  $('button').click(function(evt) {
+    evt.preventDefault()
+    console.log("Clicked")
+  });
+});
+$('#Set Color').bind('keypress', function(e) {
+ if (e.which == 13) {
+     console.log('Enter clicked');
+ }
+ });
 
+$('body').append('<div class = "square">' + toAdd + '</div>');
+$(function() {
+  $('.square').click(function() {
+    $(this).css('.square', 'yellow');
+  });
+});
