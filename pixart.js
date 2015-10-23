4d37637329fda12ef036cab047db4facced3c033
Brendan Mahoney <BMahoney08@gmail.com>
2015-10-22 23:09:19 -0400

    bonus

--- pixart.js
+++ pixart.js
@@ -1,13 +1,43 @@
 var button = $("#set-color");
 var input = $("#color-field");
+var swatch = []
+var color;
+var swatch0 = $(".brush").eq(0)
+var swatch1 = $(".brush").eq(1)
+var swatch2 = $(".brush").eq(2)
 
 var newColor = function(evt){
   evt.preventDefault();
-  var color = $("#color-field").val();
-  $(".brush").css("background-color",color);
+  color = $("#color-field").val();
+  // $(".brush").css("background-color",color);
+  swatch.unshift(color);
+  $(".brush").eq(0).css("background-color",swatch[0]);
+  $(".brush").eq(1).css("background-color",swatch[1]);
+  $(".brush").eq(2).css("background-color",swatch[2]);
 }
 
 button.on("click", newColor);
+
+//Why does this for loop work????
+// for(var i = 0; i < 3; i++) {
+//   $(".brush").eq(i).on("click", function() {
+//     color = swatch[i];
+//   })
+// }
+
+// swatch0.on("click", function() {
+//   color = swatch[0]
+// })
+// swatch1.on("click", function() {
+//   color = swatch[1]
+// })
+// swatch2.on("click", function() {
+//   color = swatch[2]
+// })
+
+$(".brush").on("click", function() {
+  color = this.style["background-color"];
+})
 
 input.keypress(function (evt) {
   var key = evt.which;
@@ -17,11 +47,23 @@ input.keypress(function (evt) {
   }
 })
 
-for(var i = 1; i <+ 20; i++){
+for(var i = 1; i <+ 8000; i++){
   var newDiv = $("<div/>", {"class": "square"});
-  newDiv.on("click", function() {
-    color = $("#color-field").val();
+  newDiv.css({"width": "10px"})
+  newDiv.mouseover( function() {
+    // color = $("#color-field").val();
     $(this).css("background-color", color)
   });
   $("body").append(newDiv);
 }
+
+$(".brush").css("display","inline-block")
+
+
+
+// swatch2.on("click", function() {
+//   var color2 = swatch[1]
+//   newDiv.mouseover( function() {
+//     $(this).css("background-color", color1)
+//   });
+// })
