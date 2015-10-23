b22badf8af765791aa86fe1edcd98088303049f7
CHRYSTAL BANKS <Chrystal.Banks1@gmail.com>
2015-10-22 21:05:41 -0400

    Sixth Commit

--- pixart.js
+++ pixart.js
@@ -1,51 +1,17 @@
 $(document).ready(function(){
+  var color;
 
+  $("#set-color").on("click", function(){
+    event.preventDefault();
+    color = $("#color-field").val();
+    $(".brush").css("background", color);
+  });
+  for (var i = 0; i < 8000; i++ ) {
+    $("body").append("<div class='square'></div>");
+  }
+  $(".square").on("mouseover", function(event){
+      var $square = $(event.target);
+      $square.css("background", color);
 
-
-$("#set-color").on("click", function(){
-  event.preventDefault();
-  var color = $("#color-field").val();
-  $(".brush").css("background", color);
- });
-
-
-// $("div div:first-of-type").on("click", function(){
-
-for (var i = 0; i < 20; i++ ) {
-$("body").append("<div class='square'></div>");
-}
-$(".square").each(function(index){
-  $(this).on("click", function(){
-  var brush = $(".brush").css("background-color");
-  $(this).css("background", brush);
-
-});
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  });
 });
