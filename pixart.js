9a1ead3a71e0afd8bd2b78ad4b9eee2640e3017a
Kory Campbell <korycampbell@Dwights-MacBook-Pro.local>
2015-10-23 09:09:31 -0400

    commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
+var form = document.getElementById('form');
+var input = $("#color-field");
+var brush = $(".brush");
+var button = document.getElementById('set-color');
 
+var hello = function() {
+  event.preventDefault();
+  document.style.backgroundColor = 'red';
+  };
+
+button.addEventListener("click", hello);
