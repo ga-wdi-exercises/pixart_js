6c5a8237c9a798e8fef69db78930a35cbacf9b1f
Thomas Clark <tclark@soteriagiving.com>
2015-10-23 05:25:02 -0400

    added event listeners

--- pixart.js
+++ pixart.js
@@ -1,4 +1,16 @@
 var button = document.querySelector("#set-color");
 var colorField = document.querySelector("#color-field");
 var brush = document.querySelector(".brush");
-console.log("this is happening");
+var brushList = document.querySelector(".brush-list");
+var canvas = document.querySelector(".canvas");
+var color = "#1B4370";
+
+startPixart();
+
+function startPixart() {
+  button.addEventListener("click", setBrushColor);
+  canvas.addEventListener("mouseover", paintSquare);
+  canvas.addEventListener("mousedown", function() { event.preventDefault(); this.mouseDown = true;})
+  canvas.addEventListener("mouseup", function() { event.preventDefault(); this.mouseDown = false;})
+  makeCanvas();
+}
