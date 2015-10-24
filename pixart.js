fb2f83803a024026a8b7e10442c1f235bbda237c
Jordan Brauner <jordan.brauner@gmail.com>
2015-10-24 11:57:12 -0400

    Revised code for steps 1 and 2.

--- pixart.js
+++ pixart.js
@@ -1,19 +1,87 @@
-$(document).ready(createCanvas)
-
-var colorName = $('#color-field');
-var colorSet = $('#set-color');
-var brushBox = $('.brush');
-
-colorSet.on("click", function(evt) {
-  evt.preventDefault();
-  brushBox.css("background", colorName.val());
-});
+$(document).ready(createCanvas);
 
 function createCanvas () {
+  // Create pixels contained within the div .canvas
   for (var i = 0; i < 20; i++) {
-    var canvasBlock = document.createElement('div');
-    canvasBlock.className = "square";
-    $('body').append(canvasBlock);
+    $('.canvas').append("<div class=\"pixel\" id=\""+i+"pixel\"></div>");
   }
+  // Click set color button to change the current color to the color input.
+  $('#setColor').on('click', function(evt) {
+    evt.preventDefault();
+    var newColor = $('#colorInput').val()
+    $('#brushColor').css('background', newColor);
+  });
 }
- 
+
+
+
+//run function changeColor on click of .square
+//   $('.square').on('click', function () {
+//     this.css("background", "green");
+// });
+// }
+
+// var canvas = {
+//   changeColor: function {
+//     $('.square').on('click', function() {
+//     this.
+//   });
+// }
+
+// function changeColor() {
+//   newColor = $('.brush').css('background');
+//   $('.square').css('background', newColor);
+// }
+
+// function changeColor() {
+//   newColor = $('.brush').css('background');
+//   $('.square').css('background', newColor);
+// }
+
+
+
+/************************************************
+ORIGINAL
+************************************************/
+
+// $(document).ready(createCanvas);
+//
+// function createCanvas () {
+//   // create variables
+//   colorName = $('#color-field');
+//   colorSet = $('#set-color');
+//   brushBox = $('.brush');
+//   // create canvas
+//   for (var i = 0; i < 20; i++) {
+//     canvasSquare = document.createElement('div');
+//     canvasSquare.className = "square";
+//     $('body').append(canvasSquare);
+//   }
+//   // click set color to change the current color to the input color
+//   colorSet.on("click", function(evt) {
+//     evt.preventDefault();
+//     brushBox.css("background", colorName.val());
+//   });
+// }
+//   //run function changeColor on click of .square
+// //   $('.square').on('click', function () {
+// //     this.css("background", "green");
+// // });
+// // }
+//
+// // var canvas = {
+// //   changeColor: function {
+// //     $('.square').on('click', function() {
+// //     this.
+// //   });
+// // }
+//
+// // function changeColor() {
+// //   newColor = $('.brush').css('background');
+// //   $('.square').css('background', newColor);
+// // }
+//
+// // function changeColor() {
+// //   newColor = $('.brush').css('background');
+// //   $('.square').css('background', newColor);
+// // }
