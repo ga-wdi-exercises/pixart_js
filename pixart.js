de2c14f3955582706f055394b6fbe729b5f38f32
Jordan Brauner <jordan.brauner@gmail.com>
2015-10-24 12:04:20 -0400

    Commit for Step 5

--- pixart.js
+++ pixart.js
@@ -9,83 +9,11 @@ function createCanvas () {
   $('#setColor').on('click', function(evt) {
     evt.preventDefault();
     var newColor = $('#colorInput').val()
-    $('#brushColor').css('background', newColor);
+    $('#brushColor').css('background-color', newColor);
   });
   // Click pixel to change color to green.
   $('.pixel').on('click', function () {
-    $(this).css('background', 'green');
+    var newColor = $('#brushColor').css('background-color');
+    $(this).css('background-color', newColor);
   });
 }
-
-
-
-//run function changeColor on click of .square
-//   $('.square').on('click', function () {
-//     this.css("background", "green");
-// });
-// }
-
-// var canvas = {
-//   changeColor: function {
-//     $('.square').on('click', function() {
-//     this.
-//   });
-// }
-
-// function changeColor() {
-//   newColor = $('.brush').css('background');
-//   $('.square').css('background', newColor);
-// }
-
-// function changeColor() {
-//   newColor = $('.brush').css('background');
-//   $('.square').css('background', newColor);
-// }
-
-
-
-/************************************************
-ORIGINAL
-************************************************/
-
-// $(document).ready(createCanvas);
-//
-// function createCanvas () {
-//   // create variables
-//   colorName = $('#color-field');
-//   colorSet = $('#set-color');
-//   brushBox = $('.brush');
-//   // create canvas
-//   for (var i = 0; i < 20; i++) {
-//     canvasSquare = document.createElement('div');
-//     canvasSquare.className = "square";
-//     $('body').append(canvasSquare);
-//   }
-//   // click set color to change the current color to the input color
-//   colorSet.on("click", function(evt) {
-//     evt.preventDefault();
-//     brushBox.css("background", colorName.val());
-//   });
-// }
-//   //run function changeColor on click of .square
-// //   $('.square').on('click', function () {
-// //     this.css("background", "green");
-// // });
-// // }
-//
-// // var canvas = {
-// //   changeColor: function {
-// //     $('.square').on('click', function() {
-// //     this.
-// //   });
-// // }
-//
-// // function changeColor() {
-// //   newColor = $('.brush').css('background');
-// //   $('.square').css('background', newColor);
-// // }
-//
-// // function changeColor() {
-// //   newColor = $('.brush').css('background');
-// //   $('.square').css('background', newColor);
-// // }
