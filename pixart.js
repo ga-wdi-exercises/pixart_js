edc8e4689c5fa5541d1b3c5f7fced6e5421eb279
Sigmund Nathan Tang <shindigira@gmail.com>
2015-10-25 22:29:44 -0400

    third commit

--- pixart.js
+++ pixart.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 var pixArt = {
     setColorButton: $("#set-color"),
     setColorBox: $("#color-field"),
+    numSquares: 20,
     ColorListeners: function(){
       this.setColorButton.on("click", this.changeColorBrush);
       this.setColorBox.on("keypress", function(e){
@@ -18,5 +19,12 @@ var pixArt = {
       e.preventDefault();
       $(".brush").css("background-color",$("#color-field").val());
     },
+    makeSquares: function(){
+      for(var x = 0; x < this.numSquares; x++){
+        $("body").append("<div class='square'></div>");
+      }
+    }
 }
+
+pixArt.makeSquares();
 pixArt.ColorListeners();
