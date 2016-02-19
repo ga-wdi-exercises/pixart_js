7aeebb29a7d3db9093cc3846166754a828ad7524
Matthew Stevens <matthew.lee.stevens@gmail.com>
2016-02-19 17:37:58 -0500

    set color button changes brush to specified color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
+var button = document.querySelector("#set-color");
 
+var color_field = document.querySelector("#color-field");
+
+var brush = document.querySelector(".brush");
+
+button.addEventListener("click", function()
+{
+  event.preventDefault();
+  brush.style.background = color_field.value;
+});
