31d7dbd14f6062191f4052167f97cf943ef0cefe
Matthew Stevens <matthew.lee.stevens@gmail.com>
2016-02-19 17:44:10 -0500

    pressing enter inside the input field changes the color

--- pixart.js
+++ pixart.js
@@ -1,11 +1,15 @@
 var button = document.querySelector("#set-color");
-
 var color_field = document.querySelector("#color-field");
-
 var brush = document.querySelector(".brush");
 
 button.addEventListener("click", function()
-{
-  event.preventDefault();
-  brush.style.background = color_field.value;
+  {
+    event.preventDefault();
+    brush.style.background = color_field.value;
+  });
+
+color_field.addEventListener("keypress", function(e){
+  if (13 == e.keyCode) {
+   brush.style.background = color_field.value;
+  }
 });
