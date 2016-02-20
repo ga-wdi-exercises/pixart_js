6a32159ab8a65396a5797c957b0f629934dc47bb
Patrick Moran <patrick_moran@mac.com>
2016-02-20 11:14:56 -0500

    commit 1, brush color changes with user input

--- pixart.js
+++ pixart.js
@@ -1,1 +1,9 @@
+var setColorButton = document.getElementById("set-color");
+var brushBox = document.querySelector(".brush");
+var userInput = document.getElementById("color-field");
 
+setColorButton.addEventListener("click", function(evt) {
+  brushBox.style.background = userInput.value;
+  // Why does the color reset without preventDefault() ??
+  evt.preventDefault();
+});
