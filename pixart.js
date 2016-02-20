d4746273226ec363e1f676672eb3d3a65e3f1c26
Matthew Stevens <matthew.lee.stevens@gmail.com>
2016-02-20 16:21:45 -0500

    on mouseover squares change to brush color not color field

--- pixart.js
+++ pixart.js
@@ -1,19 +1,16 @@
 var button = document.querySelector("#set-color");
 var color_field = document.querySelector("#color-field");
 var brush = document.querySelector(".brush");
-
 button.addEventListener("click", function()
   {
     event.preventDefault();
     brush.style.background = color_field.value;
   });
-
 color_field.addEventListener("keypress", function(e){
   if (13 == e.keyCode) {
    brush.style.background = color_field.value;
   }
 });
-
 function createDivs() {
   for (var i = 0; i < 8000; i++) {
     var newDiv = document.createElement("div");
@@ -21,14 +18,13 @@ function createDivs() {
     document.body.appendChild(newDiv);
   }
 }
-
-function colorOnClick (){
+function colorOnMouseover (){
   var squares = document.querySelectorAll(".square");
-    for (var i=0;i<squares.length;i++){
+    for (var i = 0; i < squares.length; i++) {
         squares[i].addEventListener('mouseover', function(){
-          this.style.background = color_field.value;
+          this.style.background = brush.style.background;
         });
     }
 }
 createDivs();
-colorOnClick();
+colorOnMouseover();
