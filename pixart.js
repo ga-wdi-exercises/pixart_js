4a27ece67ca0a675261ffef14051580919654509
Christine Movius <christine.movius@gmail.com>
2016-02-20 21:46:13 -0500

    Initial commit. Created some DOM variables and set up simple event that changes color box to red when button is clicked.

--- pixart.js
+++ pixart.js
@@ -1,1 +1,8 @@
+var colorButton = document.querySelector("#set-color");
+var colorSquare = document.querySelector(".brush");
+var colorText = document.querySelector("#color-field");
 
+colorButton.addEventListener("click", function(event) {
+  event.preventDefault();
+  colorSquare.style.backgroundColor = "red";
+});
