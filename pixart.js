1d7b29eaea0c37c166fb230489becb106574aa7c
Cam <camlatimer@gmail.com>
2016-02-20 19:43:28 -0500

    commit 2 -- hooked up keydown behavior

--- pixart.js
+++ pixart.js
@@ -7,16 +7,25 @@ var colorButton = document.getElementById('set-color');
 
 //EVENT HANDLERS
 //change brush box color when colorButton is clicked...
-colorButton.addEventListener('click', changeColor)
+colorButton.addEventListener('click', buttonTrigger)
+colorInput.addEventListener('keydown', keyTrigger)
 
-function changeColor(e) {
+function keyTrigger(e) {
+  if(event.code == 'Enter') {
+    e.preventDefault();
+    colorBox.style.backgroundColor = colorInput.value;
+    console.log('enter key was pressed. color: ' + colorInput.value);
+  }
+}
+
+function buttonTrigger(e) {
   e.preventDefault();
-  //why doesn't this work on click:
+  //why doesn't this work as expected on click ...?:
     //var colorInput = document.getElementById('color-field').value;
     //colorBox.style.backgroundColor = colorInput;
   //but this does:
     //var colorInput = document.getElementById('color-field');
     //colorBox.style.backgroundColor = colorInput.value;
   colorBox.style.backgroundColor = colorInput.value;
-  console.log(colorInput.value);
+  console.log('set color button was pressd. color: ' + colorInput.value);
 }
