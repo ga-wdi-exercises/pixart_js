e1699886da6d5d41e90f1ab07c86912df09e3d0a
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-02-21 14:28:28 -0500

    fixed div class, first pixel now changes to green on click

--- pixart.js
+++ pixart.js
@@ -6,3 +6,14 @@ $("button")[0].addEventListener("click", function() {
   event.preventDefault();
   document.body.querySelector(".brush").style.background = document.body.querySelector("input").value;
 });
+
+//appends 20 div squares... seems redundant
+$("body").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>");
+
+var changeSquareColor = document.body.querySelector("square")
+$(".square")[0].addEventListener("click", function() {
+  event.preventDefault();
+  document.body.querySelector(".square").style.background = "green";
+  // document.body.querySelector("input").value;
+}
+)
