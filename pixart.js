815704e6c2c2ae5e557f773de4fd2135e3cbbe80
Christine Movius <christine.movius@gmail.com>
2016-02-20 22:49:13 -0500

    Color of square changes to color typed in box when button is pressed or enter is pushed.

--- pixart.js
+++ pixart.js
@@ -1,8 +1,23 @@
-var colorButton = document.querySelector("#set-color");
-var colorSquare = document.querySelector(".brush");
+var button = document.querySelector("#set-color");
 var colorText = document.querySelector("#color-field");
+var colorSquare = document.querySelector(".brush");
+var color= "#1B4370";
 
-colorButton.addEventListener("click", function(event) {
+playPixart();
+
+function playPixart() {
+  button.addEventListener("click", setColor);
+}
+
+function setColor(event) {
   event.preventDefault();
-  colorSquare.style.backgroundColor = "red";
-});
+  color = colorText.value;
+  colorSquare.style.backgroundColor = color;
+}
+
+
+//Simple event to change square's color to green when buttoned is clicked.
+// button.addEventListener("click", function(event) {
+//   event.preventDefault();
+//   colorSquare.style.backgroundColor = "green";
+// });
