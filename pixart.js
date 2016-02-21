76267595cdeb41bde8d7812342c95872207aefeb
ROBEL TEKESTE <ROBEL.TEKESTE@GMAIL.COM>
2016-02-21 17:12:48 -0500

    1st Commit

--- pixart.js
+++ pixart.js
@@ -1,1 +1,15 @@
+//set variables
+var setColor = document.getElementById("set-color");
+var brush = document.querySelector(".brush");
+var colorField = document.getElementById("color-field");
+var newColor = document.getElementById("form");
+var color = "#E7E5DB";
 
+//add event listener and call on function when clicked
+setColor.addEventListener("click", changeBackgroundColor);
+
+//change box color to color typed into colorField
+function changeBackgroundColor(event) {
+  brush.style.backgroundColor = colorField.value;
+  event.preventDefault();
+}
