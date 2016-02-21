bc85aaf531ee6611acf62a9418dbd7d8f091b9ca
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-02-21 14:58:16 -0500

    allows user to change color of the brush box

--- pixart.js
+++ pixart.js
@@ -1,1 +1,10 @@
+var brush = document.querySelector(".brush");
+var button = document.querySelector("#set-color");
 
+function changeBrush(event) {
+  event.preventDefault();
+  var choice = $("#color-field").val();
+  brush.style.backgroundColor = choice;
+}
+
+button.addEventListener("click", changeBrush);
