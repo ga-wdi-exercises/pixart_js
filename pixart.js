473412b618297c12e03f6ce6334073f56232ce75
Caitlin Daitch <caitlin.daitch@gmail.com>
2016-02-21 14:35:09 -0500

    Set color button changes background of brush

--- pixart.js
+++ pixart.js
@@ -1,1 +1,18 @@
+var brush = document.querySelector(".brush");
+var button = document.querySelector("#set-color");
+var colorInput = document.querySelector("#color-field");
+var color = "#1B4370";
 
+function startGame() {
+  button.addEventListener("click", setColor);
+}
+
+setColor = function(){
+  event.preventDefault();
+
+  var color = colorInput.value;
+
+  brush.style.backgroundColor = color;
+};
+
+startGame();
