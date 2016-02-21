d0c694ec09baa464b9a18f594ab1292eb09c4057
Clarissa Stark <clarissa.stark@gmail.com>
2016-02-21 13:39:21 -0500

    completed commit #3 and added 20 boxes

--- pixart.js
+++ pixart.js
@@ -3,8 +3,23 @@ var previewBox = document.querySelector(".brush");
 var form = document.querySelector("#set-color");
 
 function boxColor(){
+  if (typedColor.value === null || typedColor.value === ""){
+      alert("Please type a color into the form field.");
+      return false;
+  }else{
   previewBox.style.backgroundColor = typedColor.value;
   event.preventDefault();
+  }
+}
+
+function addBoxes(){
+  var newBox;
+  for(var b = 0; b <= 20; b++){
+    newBox = $("<div class ='brush'/>");
+    $("body").append(newBox);
+  }
 }
 
 form.addEventListener("click",boxColor);
+
+addBoxes();
