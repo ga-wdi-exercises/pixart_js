a17571aace7b0cad94c1a173fce2e866202b317d
ROBEL TEKESTE <ROBEL.TEKESTE@GMAIL.COM>
2016-02-21 17:22:42 -0500

    2nd Commit

--- pixart.js
+++ pixart.js
@@ -9,6 +9,7 @@ var color = "#E7E5DB";
 setColor.addEventListener("click", changeBackgroundColor);
 
 //change box color to color typed into colorField
+//not exactly sure why and how, but my first commit also works for my 2nd commit. I havent changed anything, but it works to press enter as well.
 function changeBackgroundColor(event) {
   brush.style.backgroundColor = colorField.value;
   event.preventDefault();
