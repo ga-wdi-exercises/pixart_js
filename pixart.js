de1e35f008e352eef95b5414f4dfe2744eac5e23
John Blakeman <blakeman.john@gmail.com>
2016-02-21 14:53:49 -0500

    Add event to change box color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
+var setColor = document.getElementById("set-color");
+var colorField = document.getElementById("color-field");
+
+function setBrushBoxColor(event) {
+    var brush = document.getElementsByClassName("brush")[0];
+    brush.style.backgroundColor = colorField.value;
+    event.preventDefault();
+}
+
+setColor.addEventListener("click", setBrushBoxColor);
 
