e130cc69afa0a4eff895a1d4d2fa6c147d977065
Cam <camlatimer@gmail.com>
2016-02-20 19:32:36 -0500

    commit 1 -- took a while b/c I was confused about setting properties correctly. see inline comments

--- pixart.js
+++ pixart.js
@@ -1,1 +1,22 @@
+console.log('script.js loaded...');
+//globals
+var pixelForm = document.getElementById('form');
+var colorBox = document.querySelector('.brush');
+var colorInput = document.getElementById('color-field');
+var colorButton = document.getElementById('set-color');
 
+//EVENT HANDLERS
+//change brush box color when colorButton is clicked...
+colorButton.addEventListener('click', changeColor)
+
+function changeColor(e) {
+  e.preventDefault();
+  //why doesn't this work on click:
+    //var colorInput = document.getElementById('color-field').value;
+    //colorBox.style.backgroundColor = colorInput;
+  //but this does:
+    //var colorInput = document.getElementById('color-field');
+    //colorBox.style.backgroundColor = colorInput.value;
+  colorBox.style.backgroundColor = colorInput.value;
+  console.log(colorInput.value);
+}
