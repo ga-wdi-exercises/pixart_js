a380c7d4fba2b6696a5dd2f6d09c8047faf5905d
Melissa Miller <lis.d.miller@gmail.com>
2016-02-21 17:28:13 -0500

    Changes the color of the brush box when user inputs a new color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,7 @@
+var form = document.getElementById('form');
 
+form.addEventListener('submit', function(event){
+  var color =  form.querySelector('#color-field').value;
+  document.querySelector('.brush').style.background = color;
+  event.preventDefault();
+});
