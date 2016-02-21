fa8abe7862aee5c0dd59c372a3c52f25c564b7e9
Christine Movius <christine.movius@gmail.com>
2016-02-21 09:27:42 -0500

    Function addSquares adds 20 divs with class square to the body of document.

--- pixart.js
+++ pixart.js
@@ -24,20 +24,10 @@ function setColor(event) {
 // });
 
 //Create 20 divs of the "square" class and append them to the body
-// div.classList.add("anotherclass");
-// function addElement () {
-//   // create a new div element
-//   // and give it some content
-//   var newDiv = document.createElement("div");
-//   var newContent = document.createTextNode("Hi there and greetings!");
-//   newDiv.appendChild(newContent); //add the text node to the newly created div.
-//
-//   // add the newly created element and its content into the DOM
-//   var currentDiv = document.getElementById("div1");
-//   document.body.insertBefore(newDiv, currentDiv);
-// }
-
 function addSquares() {
-  var newDiv = document.createElement("div");
-  newDiv.classList.add("square");
+  for (var i = 0; i < 20; i++) {
+    var newDiv = document.createElement("div");
+    newDiv.classList.add("square");
+    document.body.appendChild(newDiv);
+  }
 }
