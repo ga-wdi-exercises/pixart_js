310722e1cba4926e5bcf7c293d0b297718100573
Clarissa Stark <clarissa.stark@gmail.com>
2016-02-21 12:10:15 -0500

    completed commit #1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,10 @@
+var typedColor = document.querySelector("#color-field");
+var previewBox = document.querySelector(".brush");
+var btn = document.getElementById("set-color");
 
+function boxColor(){
+  previewBox.style.backgroundColor = typedColor.value;
+  event.preventDefault();
+}
+
+btn.addEventListener("click",boxColor);
