0c572d19df5193c9413954c052dabe745deac517
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-02-21 13:59:52 -0500

    finished part 1, brush color changes based on user input: clicks and enter

--- pixart.js
+++ pixart.js
@@ -1,1 +1,8 @@
 
+var changeColor = document.body.querySelector("button")
+
+//this changes the div brush color based on user input
+$("button")[0].addEventListener("click", function() {
+  event.preventDefault();
+  document.body.querySelector(".brush").style.background = document.body.querySelector("input").value;
+});
