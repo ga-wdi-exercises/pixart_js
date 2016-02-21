40a6f10aa36176f756eacd78cbb0643d954cd868
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-02-21 16:19:30 -0500

    allows user to enter color with enter key

--- pixart.js
+++ pixart.js
@@ -1,10 +1,19 @@
 var brush = document.querySelector(".brush");
 var button = document.querySelector("#set-color");
+var form = document.querySelector("#form");
 
-function changeBrush(event) {
+function changeBrushButton(event) {
   event.preventDefault();
   var choice = $("#color-field").val();
   brush.style.backgroundColor = choice;
 }
 
-button.addEventListener("click", changeBrush);
+function changeBrushKey(event) {
+  if (13 == event.keyCode) {
+    var choice = $("#color-field").val();
+    brush.style.backgroundColor = choice;
+  }
+}
+
+button.addEventListener("click", changeBrushButton);
+form.addEventListener("keydown", changeBrushKey);
