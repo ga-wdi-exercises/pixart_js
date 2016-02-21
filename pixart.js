0074f23f3249afda2ea5f2d76a28047b83ec0883
Clarissa Stark <clarissa.stark@gmail.com>
2016-02-21 12:16:33 -0500

    turns out solution to commit #1 also satisfies commit #2 - but did another solution here

--- pixart.js
+++ pixart.js
@@ -1,10 +1,10 @@
 var typedColor = document.querySelector("#color-field");
 var previewBox = document.querySelector(".brush");
-var btn = document.getElementById("set-color");
+var form = document.querySelector("#set-color");
 
 function boxColor(){
   previewBox.style.backgroundColor = typedColor.value;
   event.preventDefault();
 }
 
-btn.addEventListener("click",boxColor);
+form.addEventListener("click",boxColor);
