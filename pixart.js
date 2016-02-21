4db830fb9a1e058b40f52ff877f34ec953189cfb
John Blakeman <blakeman.john@gmail.com>
2016-02-21 15:28:18 -0500

    Append elements to body

--- pixart.js
+++ pixart.js
@@ -12,3 +12,12 @@ colorField.addEventListener("keypress", function(event) {
     if (event.keyCode === 13) setBrushBoxColor(event);
 });
 
+function appendBody(tagName, className) {
+    newElement = document.createElement(tagName);
+    if (className) newElement.className = className;
+    document.body.appendChild(newElement);
+}
+
+for (var i = 0; i < 20; i++) {
+    appendBody("div", "square");
+}
