b7291ac0bb845c946600283acd360dcc95cff284
Brendan Magee <bmagee@gmail.com>
2016-02-22 18:01:12 -0500

    commit 3

--- pixart.js
+++ pixart.js
@@ -3,8 +3,9 @@ var button = document.getElementById( "set-color" );
 var brush = document.querySelector( ".brush" );
 
 
-// The function below seems to meet the requirements for commit 1 and commit 2.
+// The function immediately below seems to meet the requirements for commit 1 and commit 2.
 // What is the best way to do this with the enter key?
+// All the answers I found used jQuery.
 
 var setColorClick = function(){
   button.addEventListener("click" , function(evt){
@@ -13,4 +14,15 @@ var setColorClick = function(){
   })
 }
 
+// Article on `Document.createElement()` on MDN is the only reason I think I figured this out.
+
+var createDivs = function() {
+  for (i = 0; i < 20; i++) {
+    var newDiv = document.createElement("div");
+    newDiv.setAttribute("class" , "square");
+    document.body.appendChild(newDiv);
+  }
+}
+
 setColorClick();
+createDivs();
