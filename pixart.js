319314c0715cdd11fdf62419da1b86471584d8bb
Craig Eisen <Ceisen91@gmail.com>
2016-02-22 12:16:16 -0500

    Commit #4

--- pixart.js
+++ pixart.js
@@ -2,12 +2,16 @@
 var colorField = document.querySelector("#color-field");
 var brush = document.querySelector(".brush");
 
-$("#form")[0].addEventListener('submit', function(event){
+$("#form")[0].addEventListener("submit", function(event){
   event.preventDefault();
   brush.style.backgroundColor = colorField.value;
 });
 
 for (var i = 0; i<20; i++) {
-  var divSquare = $( "<div class='square'/>" );
+  var divSquare = document.createElement("div");
+  divSquare.classList.add("square");
+  divSquare.addEventListener("click", function(){
+    this.style.backgroundColor = "green";
+  });
   $("body").append(divSquare);
 }
