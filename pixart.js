ec594fc0be81b8a9121ee61770908a3113347823
Craig Eisen <Ceisen91@gmail.com>
2016-02-22 11:52:16 -0500

    Commit #3

--- pixart.js
+++ pixart.js
@@ -6,3 +6,8 @@ $("#form")[0].addEventListener('submit', function(event){
   event.preventDefault();
   brush.style.backgroundColor = colorField.value;
 });
+
+for (var i = 0; i<20; i++) {
+  var divSquare = $( "<div class='square'/>" );
+  $("body").append(divSquare);
+}
