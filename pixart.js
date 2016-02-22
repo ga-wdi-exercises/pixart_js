0a4d97767692aded5c956d12da09bd3f922dc373
Ryan Krolick <rkrolick@gmail.com>
2016-02-22 08:41:48 -0500

    js now adds 20 divs to DOM

--- pixart.js
+++ pixart.js
@@ -1,6 +1,9 @@
 var brushBox = document.getElementsByClassName("brush")[0];
 var colorField = document.getElementById("color-field");
 var colorBtn = document.getElementById("set-color");
+var numPxls = 20;
+
+createGrid();
 
 colorField.addEventListener("keypress", checkKey);
 colorBtn.addEventListener("click",changeColor);
@@ -15,4 +18,14 @@ function changeColor(event){
   event.preventDefault();
   brushBox.style = "background-color: " + colorField.value; +";";
   colorField.value = "";
+}
+
+function createGrid(){
+  var div;
+  for (var i = 0; i < numPxls; i++){
+    div = document.createElement('div');
+    div.className = "square";
+    document.body.appendChild(div);
+  }
+
 }
