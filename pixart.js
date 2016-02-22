c6b91b72c9e1d898c65a38ebaf60573c6a59b114
Anissa Jones <anissajones10@gmail.com>
2016-02-21 21:07:24 -0500

    Need clafification on how to get input information.

--- pixart.js
+++ pixart.js
@@ -1,1 +1,10 @@
+var button = document.querySelector("button");
+var input = document.querySelector("input");
 
+$("button")[0].addEventListener("click", grabInputInfo);
+
+//I want to pull information from input.
+function grabInputInfo(){
+  console.log(input);
+  return input;
+}
