d119fe95e0aeb8b079c2e4227b82565b9e795394
Anissa Jones <anissajones10@gmail.com>
2016-02-22 08:42:48 -0500

    Still unclear about the e but I understand I need it to get the method keycode.

--- pixart.js
+++ pixart.js
@@ -6,7 +6,11 @@ var brush = document.querySelector(".brush");
 
 
 button.addEventListener("click", grabInputInfo);
-
+color_field.addEventListener("keyup", function(e){
+  if(e.keycode === 13){
+  brush.style.background = color_field.value;
+  }
+});
 //I want to pull information from input.
 function grabInputInfo(){
   event.preventDefault();
