8909aa8fd16abdf5ab8ec033eff7931807ea7d3e
Ryan Krolick <rkrolick@gmail.com>
2016-02-21 23:47:31 -0500

    textarea and button will now change brush box color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,18 @@
+var brushBox = document.getElementsByClassName("brush")[0];
+var colorField = document.getElementById("color-field");
+var colorBtn = document.getElementById("set-color");
 
+colorField.addEventListener("keypress", checkKey);
+colorBtn.addEventListener("click",changeColor);
+
+function checkKey(event){
+  if(event.keyCode == 13){
+    changeColor(event);
+  }
+}
+
+function changeColor(event){
+  event.preventDefault();
+  brushBox.style = "background-color: " + colorField.value; +";";
+  colorField.value = "";
+}
