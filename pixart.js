a654551651d6bf405cae4f9f59fe1c09d2bceeff
Brendan Magee <bmagee@gmail.com>
2016-02-22 17:15:45 -0500

    commit 2, same as commit 1

--- pixart.js
+++ pixart.js
@@ -2,6 +2,10 @@ var colorInput = document.getElementById( "color-field" );
 var button = document.getElementById( "set-color" );
 var brush = document.querySelector( ".brush" );
 
+
+// The function below seems to meet the requirements for commit 1 and commit 2.
+// What is the best way to do this with the enter key?
+
 var setColorClick = function(){
   button.addEventListener("click" , function(evt){
     evt.preventDefault();
@@ -9,4 +13,4 @@ var setColorClick = function(){
   })
 }
 
-  setColorClick();
+setColorClick();
