db7bbf07d055c849c9cef94261c9ead3b034e43b
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-02-21 19:54:37 -0500

    added new method

--- pixart.js
+++ pixart.js
@@ -1,20 +1,27 @@
 //this changes the div brush color based on user input
 $("button")[0].addEventListener("click", function() {
   event.preventDefault();
-  document.body.querySelector(".brush").style.background = document.body.querySelector("input").value;
+  document.body.querySelector(".brush").style.background =
+  document.body.querySelector("input").value;
 });
 
 //appends 20 div squares using loop, woohoo! Thanks Stack Overflow!
 for (var i = 0; i < 20; i++) {
-  var myoption = document.createElement("div");
-  myoption.setAttribute("class", "square");
-  document.body.appendChild(myoption);
+  var newDiv = document.createElement("div");
+  newDiv.setAttribute("class", "square");
+  //attempting to add event listener upon creating squares...
+  addEventListener("click", changeColor)
+  document.body.appendChild(newDiv);
 }
 
 //This should change the pixel colors to green upon clicking
 //Well, at least one square turns green -_-
-$(".square")[0].addEventListener("click", function() {
-  event.preventDefault();
-  document.body.querySelector(".square").style.background = "green";
-  // saving this for later? document.body.querySelector("input").value;
-});
+function changeColor() {
+  document.body.querySelector(".square").style.background = 'green';}
+
+//attempted diff method for green color clicks:
+// $(".square")[0].addEventListener("click", function() {
+//   event.preventDefault();
+//   document.body.querySelector(".square").style.background = "green";
+//   // saving this for later? document.body.querySelector("input").value;
+// });
