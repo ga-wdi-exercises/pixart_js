191c77ee53059f057135ddc9cbabb4bfd585320d
Fatai Agiri <yinka.fatai@gmail.com>
2016-02-22 00:51:09 -0500

    commit 1; change brush color when clicked

--- pixart.js
+++ pixart.js
@@ -1,1 +1,16 @@
-var
+  var colorField = document.getElementById("color-field").value;
+  var setColor = document.getElementById("set-color");
+
+  var brushColor = document.querySelector(".brush");
+
+var pixart = {
+  // store text input in a var and use it to manipulate square(brush) color when set-color button is clicked.
+
+// perfom function when button is clicked
+  setColor.addEventListener('click', userColor);
+
+// function to set brush color to user input
+  userColor: function (){
+    e.preventDefault();
+    brushColor.backgroundColor = colorField;
+}
