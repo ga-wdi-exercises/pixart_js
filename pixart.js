fa1320bdd6ed3f19311b7cc37d6508a1d13f0a84
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-02-21 19:32:12 -0500

    Found loop to create 20 pixels, cleaned up code

--- pixart.js
+++ pixart.js
@@ -1,19 +1,20 @@
-
-var changeColor = document.body.querySelector("button")
-
 //this changes the div brush color based on user input
 $("button")[0].addEventListener("click", function() {
   event.preventDefault();
   document.body.querySelector(".brush").style.background = document.body.querySelector("input").value;
 });
 
-//appends 20 div squares... seems redundant
-$("body").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>").append("<div class='square'></div>");
+//appends 20 div squares using loop, woohoo! Thanks Stack Overflow!
+for (var i = 0; i < 20; i++) {
+  var myoption = document.createElement("div");
+  myoption.setAttribute("class", "square");
+  document.body.appendChild(myoption);
+}
 
-var changeSquareColor = document.body.querySelector("square")
+//This should change the pixel colors to green upon clicking
+//Well, at least one square turns green -_-
 $(".square")[0].addEventListener("click", function() {
   event.preventDefault();
   document.body.querySelector(".square").style.background = "green";
-  // document.body.querySelector("input").value;
-}
-)
+  // saving this for later? document.body.querySelector("input").value;
+});
