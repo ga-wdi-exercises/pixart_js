00c501b65f5d7542e41c7d524e4a1d840db3add1
Bipin Neupane <icomp2@hotmail.com>
2016-02-22 17:16:27 -0500

    commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,25 @@
+// <div class="controls">
+//   <form id="form">
+//     <input type="text" id="color-field"></input>
+//     <button id="set-color">Set Color</button>
+//   </form>
+//   <div class="brush"></div>
+// </div>
 
+var colorField = document.querySelector("#color-field");
+var button = document.querySelector("#set-color");
+var brush = document.querySelector(".brush");
+
+
+button.addEventListener("click", function(event){
+  // stop the button from doing its default action
+  // OR prevent it from doing its normal action
+
+  //get color from the colorField
+  // and assign it as the color on the brush
+  event.preventDefault();
+  var color = colorField.value;
+  console.log(color);
+  brush.style.backgroundColor  = color;
+
+});
