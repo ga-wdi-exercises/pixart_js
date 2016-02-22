397bfdebb16665f710fda560dd5cb4680a5f5a85
Craig Eisen <Ceisen91@gmail.com>
2016-02-22 11:30:32 -0500

    Initial commit, also includes functionality included in commit 2

--- pixart.js
+++ pixart.js
@@ -1,1 +1,8 @@
 
+var colorField = document.querySelector("#color-field");
+var brush = document.querySelector(".brush");
+
+$("#form")[0].addEventListener('submit', function(event){
+  event.preventDefault();
+  brush.style.backgroundColor = colorField.value;
+});
