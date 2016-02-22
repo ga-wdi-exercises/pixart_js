961c20939615116aaf1982d6cc6968b462a508ff
Clarissa Stark <clarissa.stark@gmail.com>
2016-02-22 17:31:44 -0500

    completed commit #4 and turned those mofuggas green AT LAST

--- pixart.js
+++ pixart.js
@@ -1,25 +1,39 @@
 var typedColor = document.querySelector("#color-field");
-var previewBox = document.querySelector(".brush");
 var form = document.querySelector("#set-color");
+var previewBox = document.getElementsByClassName("brush");
+var allSquares = document.querySelectorAll("square");
 
 function boxColor(){
   if (typedColor.value === null || typedColor.value === ""){
-      alert("Please type a color into the form field.");
-      return false;
+    alert("Please type a color into the form field.");
+    return false;
   }else{
-  previewBox.style.backgroundColor = typedColor.value;
-  event.preventDefault();
-  }
-}
-
-function addBoxes(){
-  var newBox;
-  for(var b = 0; b <= 20; b++){
-    newBox = $("<div class ='brush'/>");
-    $("body").append(newBox);
+    var self = this;
+    for (var b = 0; b < previewBox.length; b++){
+      previewBox[b].style.backgroundColor = typedColor.value;
+      event.preventDefault();
+    }
   }
 }
 
 form.addEventListener("click",boxColor);
 
-addBoxes();
+// for( var i = 0; i < 19; i++){
+//   var div = document.createElement('div');
+//   var self = this);
+//   div.classList.add('square');
+//   div.addEventListener('mouseover', function(){
+//     self.style.backgroundColor = previewBox.style.backgroundColor;
+//   });
+//   document.body.appendChild(div);
+// }
+
+for(var b = 0; b < 19; b++){
+  var newBox = $("<div class ='brush'/>");
+  $("body").append(newBox);
+  }
+
+$(".brush").click(function(){
+    this.style.backgroundColor = "green";
+    event.preventDefault();
+  });
