44cc29590399240890faf9ddbcc6a360fc1058d4
Woo Yun <hh747hh@gmail.com>
2016-02-23 13:57:07 -0500

    first commit

--- pixart.js
+++ pixart.js
@@ -1,1 +1,8 @@
+var form = document.getElementById('form');
+var input = document.querySelector('#color-field');
+var brush = document.querySelector('.brush');
 
+form.addEventListener('click', function(e){
+  e.preventDefault();
+  brush.style.backgroundColor = input.value;
+});
