7b8d4f105d224ff06f4d2beb84b19f39afaa4464
Bipin Neupane <icomp2@hotmail.com>
2016-02-23 09:48:57 -0500

    checkpoint 6, hw complete.

--- pixart.js
+++ pixart.js
@@ -1,10 +1,4 @@
-// <div class="controls">
-//   <form id="form">
-//     <input type="text" id="color-field"></input>
-//     <button id="set-color">Set Color</button>
-//   </form>
-//   <div class="brush"></div>
-// </div>
+
 
 var colorField = document.querySelector("#color-field");
 var button = document.querySelector("#set-color");
@@ -25,20 +19,16 @@ var brush = document.querySelector(".brush");
 //Code to get a div boxes
 //Create 20 divs of the "square" class and append them to the body
 function createSquare() {
-for(i=0; i<5000;i++) {
+for(i=0; i<8000;i++) {
     square = document.createElement("div");
     document.body.appendChild(square);
     square.className = "square";
     //code to paint div boxes green when i click them.
     // * Add functionality so that when I click on each "square", it changes the color of that individual square to "green"
     //   * **Hint**: either add the event listener while creating the squares, or listen for events on the `body` element
-    square.addEventListener("click",  function (evt){
+    square.addEventListener("mouseover",  function (evt){
     evt.target.style.backgroundColor = brush.style.backgroundColor;
   });
 }
 }
 createSquare();
-
-// ###Commit 5
-//
-// * Modify your code so that when I click on each "square", it changes to the color I set using my input instead of "green" every time.
