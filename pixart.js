2f42ec92c3f0ea359824583c86699820693ecdd4
Andrew Smith-Mui <arsm800@gmail.com>
2016-02-23 21:36:42 -0500

    create 20 divs function complete

--- pixart.js
+++ pixart.js
@@ -1,1 +1,24 @@
+//enter text
 
+//var textColor = document.getElementByID("color-field");
+var brush = document.getElementsByClassName(".brush");
+
+//click button and apply change of event to click.
+
+var button = document.querySelector("button");
+var handleClickEvent = function() {
+  document.getElementsByClassName(div.brush).background = textColor;
+};
+button.addEventListener("click", handleClickEvent);
+
+///Create 20 divs
+
+var createDivs = function() {
+  for (var i = 0; i < 20; i ++) {
+    var newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "square");
+    document.body.appendChild(newDiv);
+  }
+};
+
+createDivs();
