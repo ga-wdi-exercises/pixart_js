df0224f5f549f67d434e842c5f40589b571a6ca8
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-02-24 16:50:03 -0500

    commit 2

--- pixart.js
+++ pixart.js
@@ -1,11 +1,17 @@
 var button = document.querySelector("#set-color");
 var brush = document.querySelector(".brush");
 var colorField = document.querySelector("#color-field");
+var color = "#1B4370"
 
-
-setColor.addEventListener("click", changeColor);
+button.addEventListener("click", changeColor);
+colorField.addEventListener("keypress", enterButtonColor);
 
 function changeColor(event){
       event.preventDefault();
-      brush.style.backgroundcolor = color;
+      brush.style.backgroundcolor = color.value;
+}
+
+function enterButtonColor() {
+  var color = colorField.value;
+  brush.style.backgroundcolor = color.value;
 }
