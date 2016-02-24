62038c08c0f479524c837c9cc901444bca2e74c4
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-02-24 18:13:07 -0500

    finals

--- pixart.js
+++ pixart.js
@@ -1,4 +1,4 @@
-/*//clicking buttons to specify the colors.//
+//clicking buttons to specify the colors.//
 var button = document.querySelector("#set-color");
 var brush = document.querySelector(".brush");
 var colorField = document.querySelector("#color-field");
@@ -6,26 +6,26 @@ var color = "#1B4370";
 //function to run the game from the beginning//
 function beginGame() {
   button.addEventListener("click", changeColor);
-  colorField.addEventListener("keypress", enterButtonColor);
+  colorField.addEventListener("mouseover", enterButtonColor);
 };
 //function to change the color of the box//
 function changeColor(){
+    event.preventDefault();
     var color= colorField.value;
     brush.style.backgroundColor = color;
 };
 //function to change the color when hit the enter button//
 
 function enterButtonColor() {
-  event.preventDefault();
   var color = colorField.value;
   brush.style.backgroundColor = color;
 };
 
 // function to CREATE 20 NEW DIVS of class "square"//
-function createDivs() {
-  for (var i= 0; i<8000; i++) {
+var createDivs = function() {
+  for (i= 0; i<8000; i++) {
   var newDiv = document.createElement("div");
-  newDiv.className = "square";
+  newDiv.setAttribute("class", "square");
   document.body.appendChild(newDiv);
   newDiv.addEventListener("mouseover", changeDivColor);
 };
@@ -34,49 +34,7 @@ function createDivs() {
 function changeDivColor(){
   this.style.backgroundColor = colorField.value;
 };
+
 //run it//
 beginGame();
-createDivs(); */
-var brush = document.querySelector(".brush");
-var button = document.querySelector("#set-color");
-var colorInput = document.querySelector("#color-field");
-var color = "#1B4370";
-
-function startGame() {
-  button.addEventListener("click", setColorButton);
-  colorInput.addEventListener("keydown", enterColorInput);
-};
-
-enterColorInput = function() {
-  var color = colorInput.value;
-
-  brush.style.backgroundColor = color;
-};
-
-setColorButton = function() {
-  event.preventDefault();
-
-  var color = colorInput.value;
-
-  brush.style.backgroundColor = color;
-};
-
-createNewBrushes = function() {
-  for (var i=0; i<8000; i++) {
-    var newBrush = document.createElement("div");
-    var page = document.querySelector(".controls");
-
-    newBrush.classList.add("square");
-
-    page.appendChild(newBrush);
-
-    newBrush.addEventListener("mouseover", changeBoxColor);
-  };
-};
-
-changeBoxColor = function() {
-  this.style.backgroundColor = colorInput.value;
-};
-
-startGame();
-createNewBrushes();
+createDivs();
