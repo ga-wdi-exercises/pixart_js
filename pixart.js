8ddf0133b4a1c4825a76a96af4b573b61fa2424f
Woo Yun <hh747hh@gmail.com>
2016-02-23 23:29:32 -0500

    commit 3

--- pixart.js
+++ pixart.js
@@ -3,6 +3,15 @@ var input = document.querySelector('#color-field');
 var brush = document.querySelector('.brush');
 
 form.addEventListener('click', function(e){
-  e.preventDefault();
+  event.preventDefault();
   brush.style.backgroundColor = input.value;
 });
+
+var squareDiv = document.createElement('div');
+squareDiv.setAttribute('class', '.square');
+
+for(var i = 0; i<20; i++){
+  var squareDiv = document.createElement('div');
+  squareDiv.setAttribute('class', '.square');
+  document.body.appendChild(squareDiv);
+}
