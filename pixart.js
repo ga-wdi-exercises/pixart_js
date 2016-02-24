1a9bf08d1a368318df1ec18adde7c6055ed325a3
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-02-23 22:41:52 -0500

    first commit not right but something

--- pixart.js
+++ pixart.js
@@ -1,1 +1,10 @@
 
+var setColor = document.querySelector("#set-color");
+console.log(setColor);
+
+setColor.addEventListener("click", function(){
+      UserColorChoice
+      document.brush.style.background = UserColorChoice = (input);
+      console.log(UserColorChoice);
+    })
+//* okay... i really know that this doesn't work but I'm still kinda proud of it because something happens in the console log... I have to google that whole (event)default thing... brb. <--commit#1 
