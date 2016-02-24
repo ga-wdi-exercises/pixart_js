4afa34ea325eac6e3c446d50f85c0c2c6ebbf06f
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-02-23 23:16:27 -0500

    added more attempts

--- pixart.js
+++ pixart.js
@@ -1,15 +1,25 @@
 var button = document.getElementById("set-color");
-var userColor = document.getElementById("color-field");
+var userInput = document.getElementById("color-field");
 
 var brush = document.querySelector("brush");
 
 var setColor = function(){
   button.addEventListener("click", function(event){
     event.preventDefault(); //googled this - not sure exactly what it is doing still.
-    document.brush.style.backgroundColor = userColor.value;
-    console.log(setColor)
+    brush.style.background = UserInput.value;
   });
 };
-============
+//*============
 
-//great now nothing works. Back to the drawing board... 
+//skipping ahead. not sure.
+
+var moreDivs = function(){
+  for(i=0; i<8000; i++){
+      moreDivs = document.createElement("div");
+        moreDivs.setAttribute("class","sqare");
+        document.body.appendChild(moreDivs);
+        moreDivs.addEventListener("mouseover") // must figure out the rest. still not sure about how this works.)
+
+}}
+moreDivs();
+setColor();
