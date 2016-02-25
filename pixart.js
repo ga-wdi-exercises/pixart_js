cbc4648e8ce5cbbe88f95a4d2f70a229c1770a4e
Crucnhwrap78 <Hayesapples78@gmail.com>
2016-02-25 07:27:54 -0500

    change color good

--- pixart.js
+++ pixart.js
@@ -1,5 +1,11 @@
 
 $("form").submit(function(event){
   event.preventDefault();
-  $(".brush").css("background-color",$("#color-field").val);
+  $(".brush").css("background",$("#color-field").val());
 });
+
+output = "";
+for (var i = 0; i<20 ; i++){
+  board += '<div class = "square"> </div>'
+}
+document.getElementsByTagName("body")[0].innerHTML = output;
