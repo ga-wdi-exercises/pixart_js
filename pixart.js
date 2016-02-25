aac791c07bf77ed433976653f74b9ed0be75c519
caseyagil <caseyagil@gmail.com>
2016-02-25 08:03:27 -0500

    first

--- pixart.js
+++ pixart.js
@@ -1,1 +1,12 @@
+var colorInput = document.getElementById( "color-field" );
+ var button = document.getElementById( "set-color" );
+ var brush = document.querySelector( ".brush" );
 
+ var setColorClick = function(){
+   button.addEventListener("click" ,function(evt){
+     evt.preventDefault();
+     brush.style.backgroundColor = colorInput.value;
+   });
+ }
+
+   setColorClick();
