ed2ee5d011b2c83bf93f3a4a35280f9cc11d3bac
YOUR FULL NAME <farishta.haider@gmail.com>
2016-02-25 01:28:38 -0500

    variables

--- pixart.js
+++ pixart.js
@@ -1,1 +1,5 @@
-
+var brush = document.querySelector(".brush");
+var square = document.querySelector(".square");
+var form = document.querySelector(".form");
+var input = document.querySelector(".set-color");
+var controls = document.querySelector(".controls");
