8ab972ce3dc30910b93c860a78a545598e5dd90d
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-02-24 23:43:45 -0500

    Added eventListener. Commit 1.

--- pixart.js
+++ pixart.js
@@ -1,3 +1,5 @@
 var colors =document.querySelector(".color");
 var square =document.querySelector(".square");
 var brush = document.querySelector(".brush");
+
+$("body")[0].addEventlistener("clicks", handleClickEvent);
