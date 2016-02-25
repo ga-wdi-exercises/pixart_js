62282f02fdbf2c5c2ffcfb58551d07f593e4e02e
caseyagil <caseyagil@gmail.com>
2016-02-25 08:08:06 -0500

    commits 2 and 3

--- pixart.js
+++ pixart.js
@@ -1,12 +1,21 @@
-var colorInput = document.getElementById( "color-field" );
- var button = document.getElementById( "set-color" );
- var brush = document.querySelector( ".brush" );
+var brush = document.querySelector( ".brush" );
 
- var setColorClick = function(){
-   button.addEventListener("click" ,function(evt){
-     evt.preventDefault();
-     brush.style.backgroundColor = colorInput.value;
-   });
+// I thought this works for both one and two of the commits?
+// After looking around I could only find a jquery solution...
+var setColorClick = function(){
+    button.addEventListener("click" , function(evt){		    button.addEventListener("click" , function(evt){
+    })
+  };
+
+
+
+// Thank you Mozilla, Doucment.creatElement() helped solve this one.
+      var createDivs = function() {
+   for (i = 0; i < 20; i) {
+     var newDiv = document.createElement("div");
+     newDiv.setAttribute("class" , "square");
+     document.body.appendChild(newDiv);
+   }
  }
-
-   setColorClick();
+ setColorClick();
+ createDivs();
