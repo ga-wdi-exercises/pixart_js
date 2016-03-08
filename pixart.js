96f6c39ae32cd79622e64765a9162640c5eea55a
KAIRA N. JOHNSON <KJOHNSON1913@GMAIL.COM>
2016-03-08 13:42:45 -0500

    my first commit

--- pixart.js
+++ pixart.js
@@ -1,1 +1,49 @@
+$("#set-color").click(function (evt) {
+  evt.preventDefault()
+  var color = $("#color-field").val()
+console.log("clicked");
+$(".brush").css("background-color", color)
+})
 
+
+
+
+// var brush = $(".brush");
+// var setColor=$("#set-color");
+// var colorField=$("#color-field");
+// var colors = ["#00ffff", "#ffff00", "#ff00ff", "#00ff00", "#700a99", "ff007f", "#0000ff" ];
+// var form = $("#form");
+//
+//
+// //create divs for square color
+// var square = '<div class="square"></div>' +  '<div class="square"></div>' +
+//              '<div class="square"></div>' + '<div class="square"></div>' +
+//              '<div class="square"></div>' + '<div class="square"></div>' +
+//              '<div class="square"></div>' + '<div class="square"></div>' +
+//              '<div class="square"></div>' + '<div class="square"></div>' +
+//              '<div class="square"></div>' + '<div class="square"></div>' +
+//              '<div class="square"></div>' + '<div class="square"></div>' +
+//              '<div class="square"></div>' + '<div class="square"></div>' +
+//              '<div class="square"></div>' + '<div class="square"></div>' +
+//              '<div class="square"></div>' + '<div class="square"></div>' ;
+//
+// $("body").append(square);
+//
+// function changeColor (){
+//   $(".square").click(function () {
+//   this.css("background-color", "green")}
+//   )};
+//
+// $.each(square, changeColor)
+//
+//
+//
+//
+// document.getElementById("form").addEventListener("click", function(event){
+//     event.preventDefault()
+//
+// });
+
+// brush.click(function () {
+// brush.css("background-color", random())
+// })
