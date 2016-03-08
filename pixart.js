88b738db7b0c35d6dd089e2db90e96937643346e
KAIRA N. JOHNSON <KJOHNSON1913@GMAIL.COM>
2016-03-08 14:08:15 -0500

    commit 5

--- pixart.js
+++ pixart.js
@@ -1,53 +1,12 @@
 $("#set-color").click(function (evt) {
   evt.preventDefault()
   var color = $("#color-field").val()
-console.log("clicked");
-$(".brush").css("background-color", color)
-})
+  $(".brush").css("background-color", color)
+  $(".square").css("background-color", color)
+});
 
 for (var i =0; i < 20; i++) {
 var div = $("<div class=square></div>")
 console.log(div)
 $("body").append(div)
-}
-
-
-// var brush = $(".brush");
-// var setColor=$("#set-color");
-// var colorField=$("#color-field");
-// var colors = ["#00ffff", "#ffff00", "#ff00ff", "#00ff00", "#700a99", "ff007f", "#0000ff" ];
-// var form = $("#form");
-//
-//
-// //create divs for square color
-// var square = '<div class="square"></div>' +  '<div class="square"></div>' +
-//              '<div class="square"></div>' + '<div class="square"></div>' +
-//              '<div class="square"></div>' + '<div class="square"></div>' +
-//              '<div class="square"></div>' + '<div class="square"></div>' +
-//              '<div class="square"></div>' + '<div class="square"></div>' +
-//              '<div class="square"></div>' + '<div class="square"></div>' +
-//              '<div class="square"></div>' + '<div class="square"></div>' +
-//              '<div class="square"></div>' + '<div class="square"></div>' +
-//              '<div class="square"></div>' + '<div class="square"></div>' +
-//              '<div class="square"></div>' + '<div class="square"></div>' ;
-//
-// $("body").append(square);
-//
-// function changeColor (){
-//   $(".square").click(function () {
-//   this.css("background-color", "green")}
-//   )};
-//
-// $.each(square, changeColor)
-//
-//
-//
-//
-// document.getElementById("form").addEventListener("click", function(event){
-//     event.preventDefault()
-//
-// });
-
-// brush.click(function () {
-// brush.css("background-color", random())
-// })
+};
