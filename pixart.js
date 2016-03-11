e47409c2b39e30e69c79f786f443d13ab8004608
KAIRA N. JOHNSON <KJOHNSON1913@GMAIL.COM>
2016-03-10 21:59:11 -0500

    made some updates. Will ask some questions during office hours tomorrow

--- pixart.js
+++ pixart.js
@@ -1,12 +1,30 @@
 $("#set-color").click(function (evt) {
-  evt.preventDefault()
-  var color = $("#color-field").val()
-  $(".brush").css("background-color", color)
-  $(".square").css("background-color", color)
+evt.preventDefault()
+var color = $("#color-field").val()
+$(".brush").css("background-color", color)
+$(".square").css("background-color", color)
+
+var mouseX = 0, mouseY = 0;
+var $xp = 0, $yp = 0;
+
+$(document).mousemove(function(paint){
+       $mouseX = paint.pageX;
+       $mouseY = paint.pageY;
+
+    var $loop = setInterval(function(){
+      $xp += (($mouseX - $xp)/5);
+      $yp += (($mouseY - $yp)/5);
+      $(".square").css({left:$xp +'px', top:$yp +'px'});
+    }, 60);
+  });
 });
+
+
 
 for (var i =0; i < 20; i++) {
 var div = $("<div class=square></div>")
 console.log(div)
 $("body").append(div)
 };
+
+var square = $(".square")
