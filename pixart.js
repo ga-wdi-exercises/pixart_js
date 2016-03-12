dffd212e8a558a770d428ef2cf0b96051ecd8346
KAIRA N. JOHNSON <KJOHNSON1913@GMAIL.COM>
2016-03-12 16:32:31 -0500

    all commits acheived. working on bonus

--- pixart.js
+++ pixart.js
@@ -1,30 +1,21 @@
+
+
 $("#set-color").click(function (evt) {
-evt.preventDefault()
-var color = $("#color-field").val()
-$(".brush").css("background-color", color)
-$(".square").css("background-color", color)
-
-var mouseX = 0, mouseY = 0;
-var $xp = 0, $yp = 0;
-
-$(document).mousemove(function(paint){
-       $mouseX = paint.pageX;
-       $mouseY = paint.pageY;
-
-    var $loop = setInterval(function(){
-      $xp += (($mouseX - $xp)/5);
-      $yp += (($mouseY - $yp)/5);
-      $(".square").css({left:$xp +'px', top:$yp +'px'});
-    }, 60);
-  });
+  evt.preventDefault()
+  var color = $("#color-field").val()
+  $(".brush").css("background-color", color)
 });
 
 
-
-for (var i =0; i < 20; i++) {
+for (var i =0; i < 10000; i++) {
 var div = $("<div class=square></div>")
-console.log(div)
 $("body").append(div)
-};
+}
 
 var square = $(".square")
+
+
+square.on("mouseover", function() {
+var color = $("#color-field").val()
+$(this).css("background-color", color)
+})
