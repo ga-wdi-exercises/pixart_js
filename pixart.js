d8a67898b6e33c12c5b17fe59bf6bff306c4328b
Lauren C. Kennedy <laurenckennedy@gmail.com>
2016-03-23 18:55:46 -0400

    commit 1; edited js to change color of brush div on button click

--- pixart.js
+++ pixart.js
@@ -1,1 +1,9 @@
-
+var setColorButton = document.getElementById("set-color");
+var brushDiv = document.querySelector("div.brush");
+function changeColor(){
+  var color = document.getElementById("color-field").value;
+  event.preventDefault();
+  brushDiv.style.background = color
+}
+setColorButton.addEventListener("click", changeColor)
+// setColorButton.addEventListener("")
