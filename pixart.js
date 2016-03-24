65313b07f79b61bcba877dcb43aa055e67d0586b
Lauren C. Kennedy <laurenckennedy@gmail.com>
2016-03-24 19:37:11 -0400

    revisions to commits 2/3

--- pixart.js
+++ pixart.js
@@ -4,15 +4,16 @@ var brushDiv = document.querySelector("div.brush");
 function changeColor(){
   var color = document.getElementById("color-field").value;
   event.preventDefault();
-  brushDiv.style.background = color
+  brushDiv.style.background = color;
+  console.log("pizza")
 };
 
-setColorButton.addEventListener("click", changeColor);
-setColorButton.addEventListener("compositionend", changeColor);
+setColorButton.addEventListener("click" || "compositionend", changeColor);
 
 for (i=0; i<=20; i++){
-var h1 = document.querySelector("h1")
 var newDiv = document.createElement("div");
 newDiv.className = 'square';
 newDiv.innerHTML = "";
-document.body.appendChild(newDiv)}
+document.body.appendChild(newDiv);
+newDiv.addEventListener("click", changeColor)
+}
