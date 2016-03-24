d04bfe0b48a43f2286f67a44790c7f28fc18c2ae
Joshua <Joshua.Nezianya@gmail.com>
2016-03-23 20:20:47 -0400

    played with code from today's class

--- pixart.js
+++ pixart.js
@@ -1,1 +1,9 @@
-var div = document.querySelector("#brush")
+var button = document.querySelector("#set-color");
+
+
+button.addEventListener ("click",changeTheme)
+
+function changeTheme() {
+  var themeClass = this.className;
+  document.body.className= themeClass;
+}
