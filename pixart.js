1feb64a9836dc33168ed9a16543be8a5ab58085a
Spicy Legato <jpqreichard@gmail.com>
2016-03-24 15:13:27 -0400

    added 20 squares with class square

--- pixart.js
+++ pixart.js
@@ -3,9 +3,15 @@ var setColorButton = document.getElementById('set-color');
 var inputField= document.getElementById('color-field');
 var brushColor = document.querySelector('.brush');
 var docForm = document.querySelector('#form');
+var bunchaSquaresMaaan = [];
 setColorButton.addEventListener( "click", setColor);
 
 function setColor() {
   event.preventDefault();
   brushColor.style.backgroundColor=inputField.value;
+}
+
+for (i=0; i < 20; i++){
+  bunchaSquaresMaaan[i]=document.body.appendChild(document.createElement("div"));
+  bunchaSquaresMaaan[i].className="square";
 }
