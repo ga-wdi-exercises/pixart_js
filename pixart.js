e95849df8d6cee88b4a1f8953188d9419255f36b
Lauren C. Kennedy <laurenckennedy@gmail.com>
2016-03-24 19:28:16 -0400

    commit 3: added 20 divs

--- pixart.js
+++ pixart.js
@@ -1,9 +1,18 @@
-var setColorButton = document.getElementById("set-color");
+var setColorButton = document.querySelector("#set-color");
 var brushDiv = document.querySelector("div.brush");
+
 function changeColor(){
   var color = document.getElementById("color-field").value;
   event.preventDefault();
   brushDiv.style.background = color
-}
-setColorButton.addEventListener("click", changeColor)
-setColorButton.addEventListener("compositionend", changeColor)
+};
+
+setColorButton.addEventListener("click", changeColor);
+setColorButton.addEventListener("compositionend", changeColor);
+
+for (i=0; i<=20; i++){
+var h1 = document.querySelector("h1")
+var newDiv = document.createElement("div");
+newDiv.className = 'square';
+newDiv.innerHTML = "";
+document.body.appendChild(newDiv)}
