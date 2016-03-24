759baf921494784651312e38ed69b75f0c55a4e5
Zaneh Williams <zanehmw@gmail.com>
2016-03-24 09:29:07 -0400

    first commit

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
+var brush = document.querySelector(".brush")
+var button = document.querySelector("#set-color");
 
+button.addEventListener( "click", changeColor);
+
+
+function changeColor(event) {
+  event.preventDefault()
+  brush.style.background = "red"
+
+};
