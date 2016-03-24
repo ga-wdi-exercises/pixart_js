03a271c8f259c3f9517567ac2b398196f4d3ad0e
Jordan Ballard <ballard.jordan@gmail.com>
2016-03-24 08:55:29 -0400

    made divs appear on page load

--- pixart.js
+++ pixart.js
@@ -3,19 +3,20 @@ var brushBox = document.querySelector(".brush");
 var colorBox = document.querySelector("#color-field");
 var square = document.querySelectorAll(".square");
 
+//Add 20 <div> elements with the class "square" when page is loaded
+window.onload = function() {
+    for(var i = 0; i < 20; i++) {
+      newDiv = document.createElement("div");
+      document.body.appendChild(newDiv);
+      newDiv.className = "square";
+    }
+};
 
 function pixArt() {
 //Make text input change the color of the brush box to the color specified
   event.preventDefault();
   colorBox.setAttribute("value", "");
   brushBox.style.background = colorBox.value;
-
-//Add 20 <div> elements with the class "square"
-  for(var i = 0; i < 20; i++) {
-    newDiv = document.createElement("div");
-    document.body.appendChild(newDiv);
-    newDiv.className = "square";
-  }
 }
 
 button.addEventListener( "click", pixArt );
