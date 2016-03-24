b6a785918c6154b7cbadc781e4a9da3533737190
Jordan Ballard <ballard.jordan@gmail.com>
2016-03-23 22:21:05 -0400

    pixart commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,12 @@
+var button = document.querySelector("button");
+var brushBox = document.querySelector(".brush");
+var colorBox = document.querySelector("#color-field");
 
+
+function changeBoxColor() {
+  event.preventDefault();
+  colorBox.setAttribute("value", "");
+  brushBox.style.background = colorBox.value;
+}
+
+button.addEventListener( "click", changeBoxColor );
