5c705ab85f59431b257486bf395dad552c553dd8
Spicy Legato <jpqreichard@gmail.com>
2016-03-24 13:16:57 -0400

    added set square background color functionality

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
+// Like the A-Team, but with variables
+var setColorButton = document.getElementById('set-color');
+var inputField= document.getElementById('color-field');
+var brushColor = document.querySelector('.brush');
+var docForm = document.querySelector('#form');
+setColorButton.addEventListener( "click", setColor);
 
+function setColor() {
+  event.preventDefault();
+  brushColor.style.backgroundColor=inputField.value;
+}
