7900f8654ae21d19a1c327102dc1d2d11fc1e979
Jordan Ballard <ballard.jordan@gmail.com>
2016-03-24 08:38:13 -0400

    pixart commit 3...add 20 divs

--- pixart.js
+++ pixart.js
@@ -7,6 +7,12 @@ function changeBoxColor() {
   event.preventDefault();
   colorBox.setAttribute("value", "");
   brushBox.style.background = colorBox.value;
+
+  for(var i = 0; i < 20; i++) {
+    newDiv = document.createElement("div");
+    document.body.appendChild(newDiv);
+    newDiv.className = "square";
+  }
 }
 
 button.addEventListener( "click", changeBoxColor );
