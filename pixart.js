3b731db2a7802ba0b6718fc381ed08cae9aaada3
Jordan Ballard <ballard.jordan@gmail.com>
2016-03-24 08:50:51 -0400

    made some changes on commit 3

--- pixart.js
+++ pixart.js
@@ -1,13 +1,16 @@
 var button = document.querySelector("button");
 var brushBox = document.querySelector(".brush");
 var colorBox = document.querySelector("#color-field");
+var square = document.querySelectorAll(".square");
 
 
-function changeBoxColor() {
+function pixArt() {
+//Make text input change the color of the brush box to the color specified
   event.preventDefault();
   colorBox.setAttribute("value", "");
   brushBox.style.background = colorBox.value;
 
+//Add 20 <div> elements with the class "square"
   for(var i = 0; i < 20; i++) {
     newDiv = document.createElement("div");
     document.body.appendChild(newDiv);
@@ -15,4 +18,4 @@ function changeBoxColor() {
   }
 }
 
-button.addEventListener( "click", changeBoxColor );
+button.addEventListener( "click", pixArt );
