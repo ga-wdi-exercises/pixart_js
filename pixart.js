fff919b8097a7e33d7d04bc9209595941067fbe7
Spicy Legato <jpqreichard@gmail.com>
2016-03-24 18:25:50 -0400

    squares turn green when clicked. kneel before zod

--- pixart.js
+++ pixart.js
@@ -4,9 +4,9 @@ var inputField= document.getElementById('color-field');
 var brushColor = document.querySelector('.brush');
 var docForm = document.querySelector('#form');
 var bunchaSquaresMaaan = [];
-setColorButton.addEventListener( "click", setColor);
+setColorButton.addEventListener( "click", setBrush);
 
-function setColor() {
+function setBrush() {
   event.preventDefault();
   brushColor.style.backgroundColor=inputField.value;
 }
@@ -14,4 +14,9 @@ function setColor() {
 for (i=0; i < 20; i++){
   bunchaSquaresMaaan[i]=document.body.appendChild(document.createElement("div"));
   bunchaSquaresMaaan[i].className="square";
+  bunchaSquaresMaaan[i].addEventListener("click", setColor)
+}
+
+function setColor () {
+  this.style.backgroundColor="green";
 }
