47554007a4c6f299e34b6babd14e8335a4d198a0
Habby Whyte <bahybaj@hotmail.com>
2016-03-24 21:42:37 -0400

    added variables

--- pixart.js
+++ pixart.js
@@ -1,1 +1,18 @@
+var brushBox = document.querySelector(".brush")
+var inputBox = document.querySelector("#color-field")
+var colorButton = document.querySelector(#set-color)
 
+function () {
+
+}
+
+function setColor() {
+  document.body.style.background = "#1B4370";
+  document.body.style.
+}
+
+// function changeRed() {
+//  document.body.style.backgroundColor = "red";
+//  document.body.style.color = "white";
+// };
+// redButton.addEventListener( "click", changeRed );
