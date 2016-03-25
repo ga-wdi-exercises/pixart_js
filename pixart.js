c7a13fc1aec9901f89b4ec677eaecca99ea48c1f
Wes Reed <wesley.a.reed@gmail.com>
2016-03-24 20:03:29 -0400

    Set color button and enter key to change color of brush box

--- pixart.js
+++ pixart.js
@@ -1,1 +1,15 @@
+var setColorButton = document.querySelector("#set-color");
+var inputColor = document.querySelector("#color-field");
+var input = document.querySelector("#color-field");
+var brush = document.querySelector(".brush");
 
+
+
+
+function setColor(event) {
+  event.preventDefault();
+  brush.style.backgroundColor = input.value;
+}
+
+
+setColorButton.addEventListener("click", setColor);
