9f2a4e3d256a4c631e99e6fe528dd624d4be20e9
Mary Griffus <mary.e.shultz@gmail.com>
2016-03-24 21:24:57 -0400

    c1 - change paintbrush color w form

--- pixart.js
+++ pixart.js
@@ -1,1 +1,9 @@
+//color handling
+var pBrush = document.querySelector(".brush");
+var form = document.querySelector("form");
+var newColor = document.querySelector("#color-field")
 
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  pBrush.style.backgroundColor = newColor.value;
+});
