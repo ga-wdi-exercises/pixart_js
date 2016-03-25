4ed6b779cccb233e9a6fdf90951afe80beea7dc1
Spicy Legato <jpqreichard@gmail.com>
2016-03-24 23:45:51 -0400

    squares now change color based on input field. workaround for uncomputed stylesheet on load

--- pixart.js
+++ pixart.js
@@ -1,22 +1,31 @@
 // Like the A-Team, but with variables
+
 var setColorButton = document.getElementById('set-color');
 var inputField= document.getElementById('color-field');
-var brushColor = document.querySelector('.brush');
-var docForm = document.querySelector('#form');
+var brushElement = document.querySelector('.brush');
+//var style = document.getComputedStyle(brushElement, null);
+var brushColor = "#1B4370";
+console.log("brush color is "+brushColor)
 var bunchaSquaresMaaan = [];
+
 setColorButton.addEventListener( "click", setBrush);
 
 function setBrush() {
   event.preventDefault();
-  brushColor.style.backgroundColor=inputField.value;
+  brushElement.style.backgroundColor=inputField.value;
 }
 
 for (i=0; i < 20; i++){
   bunchaSquaresMaaan[i]=document.body.appendChild(document.createElement("div"));
   bunchaSquaresMaaan[i].className="square";
-  bunchaSquaresMaaan[i].addEventListener("click", setColor)
+  bunchaSquaresMaaan[i].addEventListener("click", setSquareColor)
 }
 
-function setColor () {
-  this.style.backgroundColor="green";
+function setSquareColor () {
+  //brushColor = brushElement.style.backgroundColor;
+  if (brushElement.style.backgroundColor) {
+    brushColor=brushElement.style.backgroundColor;
+  }
+  this.style.backgroundColor= brushColor;
+  console.log("welp");
 }
