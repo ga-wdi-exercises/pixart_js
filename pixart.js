58635d70f5eaf7f5a945a72ce5c67b7025a78758
Leland Jordon <lelandjordon@mac.com>
2016-03-25 08:46:50 -0400

    Pair computing with the stuff I know.  Dumped jQuery for now and got the color box to change.

--- pixart.js
+++ pixart.js
@@ -4,6 +4,23 @@
 // * Use jQuery to select the element, and `addEventListener` to handle clicks
 //   * `$("body")[0].addEventListener...`
 //
-// -- which means: addEventListener to get the input from the text box
+// -- which means: addEventListener to listen for a click on the button
+//                get the input from the text box
 //                Change the color of the "brush" box, which is div class = "brush"
-var myColor = $( "#color-field" ) [0].addEventListener("click", )
+//
+// var setColor = $( "set-color" )[0]addEventListener("click", )
+//
+// $( "set-color" )[0].addEventListener("click", val())
+// console.log(val);
+//
+// Okay, forget jQuery for now.
+
+var setColorButton = document.querySelector("#set-color");
+var brushBox = document.querySelector(".brush");
+var input = document.querySelector("#color-field");
+
+function setColor (){
+  event.preventDefault()
+  brushBox.style.backgroundColor = input.value;
+}
+setColorButton.addEventListener("click", setColor);
