164e9671d6f974ec491805fbc17b3601c2e48672
Wes Reed <wesley.a.reed@gmail.com>
2016-03-24 23:23:12 -0400

    Add square class and append. Add 8000 squares

--- pixart.js
+++ pixart.js
@@ -1,15 +1,31 @@
 var setColorButton = document.querySelector("#set-color");
 var inputColor = document.querySelector("#color-field");
-var input = document.querySelector("#color-field");
+var colorField = document.querySelector("#color-field");
 var brush = document.querySelector(".brush");
-
+var body = document.querySelector("body");
 
 
 
 function setColor(event) {
   event.preventDefault();
-  brush.style.backgroundColor = input.value;
+  brush.style.backgroundColor = colorField.value;
 }
+
+function pixArt() {
+  for (var i = 0; i < 8000; i ++) {
+    var div = document.createElement("div");
+    div.classList.add("square");
+    div.addEventListener("mouseover", paint);
+    body.appendChild(div);
+  }
+}
+
+function paint() {
+  this.style.backgroundColor = brush.style.backgroundColor;
+}
+
+
+
 
 
 setColorButton.addEventListener("click", setColor);
