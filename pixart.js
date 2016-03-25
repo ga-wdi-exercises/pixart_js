3e74a4e9430048f0b6e9b872e59cee658d9b3e31
Louis <LOHankins@gmail.com>
2016-03-24 21:34:50 -0400

    pixart first commit

--- pixart.js
+++ pixart.js
@@ -1,1 +1,13 @@
 
+var setColorButton = document.querySelector("#set-color");
+
+function handleClickButton() {
+	var chosenColor = document.querySelector("#color-field").value;
+	var brushBox = document.querySelector(".brush");
+	brushBox.style.background = chosenColor;
+	event.preventDefault();
+	document.querySelector("#color-field").value = "";
+}
+
+
+setColorButton.addEventListener("click", handleClickButton);
