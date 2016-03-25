fedd91b2fa57537207362208a7c800c80050bd38
Zaneh Williams <zanehmw@gmail.com>
2016-03-24 22:05:11 -0400

    commit 1 final

--- pixart.js
+++ pixart.js
@@ -1,11 +1,23 @@
+var form = document.querySelector("#form")
 var brush = document.querySelector(".brush")
+var input = document.querySelector("#color-field")
 var button = document.querySelector("#set-color");
 
-button.addEventListener( "click", changeColor);
 
+form.addEventListener('submit', function(event){
+  event.preventDefault();
+  brush.style.backgroundColor = input.value;
+});
 
-function changeColor(event) {
-  event.preventDefault()
-  brush.style.background = "red"
-
-};
+// function changeColor(event) {
+//   event.preventDefault()
+//   if ( == "red") {
+//     brush.style.background = "red"
+//   }
+//   else if (input == "white") {
+//     brush.style.background = "white"
+//   }
+//   else if (input == "black") {
+//     brush.style.background = "black"
+//   }
+// };
