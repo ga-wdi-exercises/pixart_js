6dc40276aacaac3c51df8594a5138225b7054cdc
Mary Griffus <mary.e.shultz@gmail.com>
2016-03-24 21:42:08 -0400

    c4 - click changes squares green

--- pixart.js
+++ pixart.js
@@ -5,11 +5,13 @@ var newColor = document.querySelector("#color-field")
 
 form.addEventListener("submit", function(event) {
   event.preventDefault();
-  pBrush.style.backgroundColor = newColor.value;
+  pBrush.style.background = newColor.value;
 });
 
 for (var i = 0; i < 20; i ++){
   var div = document.createElement("div");
-  div.classList.add(".square");
+  div.classList.add("square");
   document.querySelector("body").appendChild(div);
+  div.addEventListener("click", function() {
+  this.style.background = "green"})
 }
