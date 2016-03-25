9bda02986dc6072ad874a3b40a99e7e0e7a5747a
Hewan Abreha <henebys@gmail.com>
2016-03-25 08:49:02 -0400

    homework on pixart_js

--- pixart.js
+++ pixart.js
@@ -1,1 +1,18 @@
+var setColorButton = document.querySelector("#set-color");
+var input = document.querySelector("#color-field");
+var brushBox = document.querySelector(".brush");
 
+
+function setColor() {
+  event.preventDefault();
+  brushBox.style.backgroundColor = input.value;
+}
+setColorButton.addEventListener("click", setColor);
+
+
+
+
+// setColorButton.addEventListener("submit", function (event) {
+//    event.preventDefault();
+//   brushBox.style.backgroundColor = input.value;
+// });
