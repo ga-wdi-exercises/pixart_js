50dee955f15d60c2f6815f2f91c406a02063a7da
Louis <LOHankins@gmail.com>
2016-03-24 22:35:01 -0400

    add enter key function

--- pixart.js
+++ pixart.js
@@ -2,12 +2,26 @@
 var setColorButton = document.querySelector("#set-color");
 
 function handleClickButton() {
-	var chosenColor = document.querySelector("#color-field").value;
+	var chosenColor = document.querySelector("#color-field");
 	var brushBox = document.querySelector(".brush");
-	brushBox.style.background = chosenColor;
+	brushBox.style.background = chosenColor.value;
 	event.preventDefault();
 	document.querySelector("#color-field").value = "";
+	debugger;
+}
+
+function handlePressEnter() {
+	var chosenColor = document.querySelector("#color-field");
+	var brushBox = document.querySelector(".brush");
+	var key =  event.keyCode;
+	if (key === 13) {
+		brushBox.style.background = chosenColor.value;
+		document.querySelector("#color-field").value = "";
+		event.preventDefault();
+	}
 }
 
 
+
 setColorButton.addEventListener("click", handleClickButton);
+document.querySelector("#color-field").addEventListener("keypress", handlePressEnter);
