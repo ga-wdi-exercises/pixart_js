b13e2cceb950d9262d99ba9ed8e35921ab3eef8b
Leland Jordon <lelandjordon@mac.com>
2016-03-24 23:53:00 -0400

    Step one - try to figure out jQuery from scratch.

--- pixart.js
+++ pixart.js
@@ -1,1 +1,9 @@
-
+//From the README:
+// FIRST COMMIT:
+//* When I click the "Set Color" button, it should change the color of the "brush" box to the color I specify in the input field. (**Hint:** You will need to use `event.preventDefault()` somewhere in your code.)
+// * Use jQuery to select the element, and `addEventListener` to handle clicks
+//   * `$("body")[0].addEventListener...`
+//
+// -- which means: addEventListener to get the input from the text box
+//                Change the color of the "brush" box, which is div class = "brush"
+var myColor = $( "#color-field" ) [0].addEventListener("click", )
