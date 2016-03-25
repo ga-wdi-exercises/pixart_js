a6333e9a27db4b55a7f9bab5ec7c6cbf7e68f91e
Matt <mvaden08@gmail.com>
2016-03-25 02:55:21 -0400

    pixart homework

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
+// variables for paint brush colors
+var form = document.querySelection("form").value;
+var input = document.querySelection("color-field").value;
+var brush = document.querySelection(".brush").value;
 
+
+// instructions for changing color
+form.addEventListener("click", function("event") {
+      event.preventDefault();
+      brush.style.backgroundColor = newColor.value;
+});
