ccd70ed5e7a6810e3a727191539b31b6d3922024
Jordan Ballard <ballard.jordan@gmail.com>
2016-03-24 22:56:05 -0400

    added divs and brush color box works

--- pixart.js
+++ pixart.js
@@ -2,21 +2,34 @@ var button = document.querySelector("button");
 var brushBox = document.querySelector(".brush");
 var colorBox = document.querySelector("#color-field");
 var square = document.querySelectorAll(".square");
+var body = document.body;
+var pixel = 20;
 
 //Add 20 <div> elements with the class "square" when page is loaded
-window.onload = function() {
-    for(var i = 0; i < 20; i++) {
-      newDiv = document.createElement("div");
-      document.body.appendChild(newDiv);
+function addDivs() {
+    for(var i = 0; i < 8000; i++) {
+      var newDiv = document.createElement("div");
+      var child = document.body.appendChild(newDiv);
       newDiv.className = "square";
     }
-};
+}
 
-function pixArt() {
+function paintDivs() {
+  var color = colorBox.value;
+  document.querySelectorAll(".square")[pixel].style.background = color;
+}
+
+
 //Make text input change the color of the brush box to the color specified
+function brushColor() {
   event.preventDefault();
   colorBox.setAttribute("value", "");
   brushBox.style.background = colorBox.value;
+  paintColor = colorBox.value;
+  return paintColor;
 }
 
-button.addEventListener( "click", pixArt );
+
+window.addEventListener("mouseover", paintDivs );
+window.addEventListener( "load", addDivs );
+button.addEventListener( "click", brushColor );
