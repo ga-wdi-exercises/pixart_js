6bb68d4bd9b71354504e703f710db1dc9f3262b0
Habby Whyte <bahybaj@hotmail.com>
2016-03-25 06:16:35 -0400

    working on adding functions and events

--- pixart.js
+++ pixart.js
@@ -1,18 +1,21 @@
-var brushBox = document.querySelector(".brush")
-var inputBox = document.querySelector("#color-field")
-var colorButton = document.querySelector(#set-color)
-
-function () {
-
-}
+var brushBox = document.querySelector(".brush");
+var inputBox = document.innerHTML.querySelector("#color-field").value;
+var colorButton = document.querySelector("#set-color")/
 
 function setColor() {
   document.body.style.background = "#1B4370";
-  document.body.style.
-}
+  document.body.style.background = ('inputBox').value
+};
+
+function inputBox() {
+  document.body.innerHTML('input[text]')
+};
+
+brushBox.addEventListener ("click", setColor);
+inputBox.addEventListener ("click", inputBox);
+
 
 // function changeRed() {
 //  document.body.style.backgroundColor = "red";
 //  document.body.style.color = "white";
 // };
-// redButton.addEventListener( "click", changeRed );
