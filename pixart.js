1a5a99af232debf8dee8373429126fae2b572e5d
Joshua <Joshua.Nezianya@gmail.com>
2016-03-25 06:30:22 -0400

    changed the color of box by using input value.

--- pixart.js
+++ pixart.js
@@ -1,9 +1,18 @@
 var button = document.querySelector("#set-color");
+var colors = document.querySelector("#color-field");
+var brusher = document.querySelector('.brush');
+
+form.addEventListener('submit', function(event){
+  event.preventDefault();
+  brusher.style.backgroundColor = colors.value;
+});
 
 
-button.addEventListener ("click",changeTheme)
 
-function changeTheme() {
-  var themeClass = this.className;
-  document.body.className= themeClass;
-}
+// function colored (input) {
+//   console.log (this);
+//
+// }
+
+// button.addEventListener("click", console.log("I was clicked") );
+// colored.addEventListener ("input", console.log ('') );
