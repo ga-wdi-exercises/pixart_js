16213b65e06b3b5df43d15ac3346b6684d5ff5df
Jordan Ballard <ballard.jordan@gmail.com>
2016-03-25 08:49:06 -0400

    conquered pixartgit add pixart.js

--- pixart.js
+++ pixart.js
@@ -1,35 +1,21 @@
 var button = document.querySelector("button");
 var brushBox = document.querySelector(".brush");
 var colorBox = document.querySelector("#color-field");
-var square = document.querySelectorAll(".square");
-var body = document.body;
-var pixel = 20;
 
-//Add 20 <div> elements with the class "square" when page is loaded
-function addDivs() {
-    for(var i = 0; i < 8000; i++) {
-      var newDiv = document.createElement("div");
-      var child = document.body.appendChild(newDiv);
-      newDiv.className = "square";
-    }
-}
-
-function paintDivs() {
-  var color = colorBox.value;
-  document.querySelectorAll(".square")[pixel].style.background = color;
-}
-
-
-//Make text input change the color of the brush box to the color specified
-function brushColor() {
+button.addEventListener( "click", function() {
   event.preventDefault();
   colorBox.setAttribute("value", "");
   brushBox.style.background = colorBox.value;
   paintColor = colorBox.value;
   return paintColor;
+} );
+
+
+for(var i = 0; i < 8000; i++) {
+  var newDiv = document.createElement("div");
+  document.body.appendChild(newDiv);
+  newDiv.className = "square";
+  newDiv.addEventListener("mouseover", function() {
+    this.style.background = colorBox.value;
+  });
 }
-
-
-window.addEventListener("mouseover", paintDivs );
-window.addEventListener( "load", addDivs );
-button.addEventListener( "click", brushColor );
