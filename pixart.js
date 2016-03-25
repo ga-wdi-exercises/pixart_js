01c72cc1b994596887fd8b4e762c43474ab89a15
Zaneh Williams <zanehmw@gmail.com>
2016-03-24 22:43:44 -0400

    second commit done

--- pixart.js
+++ pixart.js
@@ -3,8 +3,12 @@ var brush = document.querySelector(".brush")
 var input = document.querySelector("#color-field")
 var button = document.querySelector("#set-color");
 
-
 form.addEventListener('submit', function(event){
+  event.preventDefault();
+  brush.style.backgroundColor = input.value;
+});
+
+form.addEventListener('onkeydown', function(event){
   event.preventDefault();
   brush.style.backgroundColor = input.value;
 });
