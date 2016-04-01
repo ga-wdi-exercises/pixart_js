774bcf60e4dc83184ff32b5b6a6de12f1dfc839e
Martin Terskin <martinas.terskin@gmail.com>
2016-04-01 13:50:38 -0400

    rewrote in Jquery

--- pixart.js
+++ pixart.js
@@ -1,1 +1,14 @@
 
+$('#set-color').click(function(){
+  event.preventDefault();
+  $('.brush').eq(0).css('background',$('#color-field').prop('value'));
+});
+
+
+for (var i=0;i<8000;i++) {
+  $('body').append($('<div class="square"></div>'));
+}
+
+$('.square').mouseover(function(){
+  $(this).css('background',$('#color-field').prop('value'));
+});
