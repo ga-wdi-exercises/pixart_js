787a7c38156c95995eaeb9cf58f2cf1e31dccd16
Jordan Ballard <ballard.jordan@gmail.com>
2016-04-02 10:23:35 -0400

    hooked up input to change color box on submit with jquery

--- pixart.js
+++ pixart.js
@@ -1,1 +1,8 @@
+$("#form").submit(getInput);
 
+function getInput() {
+  event.preventDefault();
+  var userInput = $("#color-field").val();
+  var colorBox = $(".brush").eq(0);
+  $(colorBox).css("background-color", userInput);
+}
