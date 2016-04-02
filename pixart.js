9464d493c0aee97d57e04b1c41838038658354c9
Jordan Ballard <ballard.jordan@gmail.com>
2016-04-02 11:09:31 -0400

    added 20 divs that change to desired color on click

--- pixart.js
+++ pixart.js
@@ -1,8 +1,23 @@
-$("#form").submit(getInput);
+$("#form").submit(setColor);
 
-function getInput() {
+
+function setColor() {
   event.preventDefault();
-  var userInput = $("#color-field").val();
-  var colorBox = $(".brush").eq(0);
-  $(colorBox).css("background-color", userInput);
+  $userInput = $("#color-field").val();
+  var $colorBox = $(".brush").eq(0);
+  $($colorBox).css("background-color", $userInput);
+}
+
+
+function makeSquares() {
+  for(var i = 0; i < 20; i++) {
+    var $div = $("<div>", {class: "square"});
+    $("body").append($div);
+  }
+  $(".square").click(squareColor);
+}
+makeSquares();
+
+function squareColor() {
+  $(this).css("background-color", $userInput);
 }
