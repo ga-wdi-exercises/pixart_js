346e1b2ab30612a515cd95d0b8986ac67a4f7189
Michael Nahum <USGrant63@gmail.com>
2016-04-03 14:45:36 -0400

    nailed appending divs.

--- pixart.js
+++ pixart.js
@@ -1,3 +1,5 @@
+// var squareDiv =
+
 $("#set-color").on("click", function(){
   event.preventDefault();
   $(".brush").css("backgroundColor", $("#color-field").val());
@@ -9,3 +11,11 @@ $("#set-color").on("keypress", function(){
   $(".brush").css("backgroundColor", $("#color-field").val());
   console.log("heya")
 })
+
+function makeManySquares () {
+  for (var i = 0; i <8000; i++){
+    $("body").append("<div class='square'></div>");
+  }
+}
+
+makeManySquares()
