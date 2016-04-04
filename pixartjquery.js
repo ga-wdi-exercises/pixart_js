2efb14719d30b2dcd7629d4f3298edcecc347424
Zaneh Williams <zanehmw@gmail.com>
2016-04-03 22:15:00 -0400

    struggling with jquery

--- pixartjquery.js
+++ pixartjquery.js
@@ -4,26 +4,21 @@ $(document).on("ready", function(){
   var input = $("#color-field")
   var button = $("#set-color")
 
-  $("#form").on("submit", function(){
-    event.preventDefault();
-    console.log("happy")
-    brush.css("backgroundColor", input.val())
-
-  });
-
+$("#form").on("submit", function(){
+  event.preventDefault();
+  console.log("happy")
+  brush.css("backgroundColor", input.val());
 });
 
 
-// form.on('onkeydown', function(event){
-//   event.preventDefault();
-//   brush.style.backgroundColor = input.val();
-// });
-//
-// for(x = 0; x < 10000; x++) {
-//     var div = document.createElement('div');
-//     div.classList.add('square')
-//       div.addEventListener('mouseover', function() {
-//       this.style.backgroundColor = brush.style.backgroundColor
-//   })
-//     document.body.appendChild(div);
-//   };
+for(x = 0; x < 10000; x++) {
+  var div = $("body").append($("<div class='square'/>"))
+};
+
+('body').classList.add('square')
+div.on('mouseover', function() {
+$(this).css("backgroundColor", brush.css("backgroundColor")
+  )})
+
+
+});
