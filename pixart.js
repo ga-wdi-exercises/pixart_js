2c4be44f85540d396d226771cdd95c9afca67079
Michael Nahum <USGrant63@gmail.com>
2016-04-03 22:25:55 -0400

    finished nailing things

--- pixart.js
+++ pixart.js
@@ -1,21 +1,27 @@
-// var squareDiv =
 
 $("#set-color").on("click", function(){
   event.preventDefault();
   $(".brush").css("backgroundColor", $("#color-field").val());
-  console.log("heya")
+
+  //should be a way of abstracting $("#color-field").val() into a variable, since it repeats three times.
 })
 
 $("#set-color").on("keypress", function(){
   event.preventDefault();
   $(".brush").css("backgroundColor", $("#color-field").val());
-  console.log("heya")
 })
 
-function makeManySquares () {
+function makeManySquares() {
   for (var i = 0; i <8000; i++){
     $("body").append("<div class='square'></div>");
   }
 }
 
+function paintThings() {
+  $(".square").on("mouseover", function(){
+  $(this).css("backgroundColor", $("#color-field").val())
+})
+}
+
 makeManySquares()
+paintThings()
