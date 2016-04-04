978e8b9e3c6bc98aa797685d3134c93945bad961
Lauren C. Kennedy <laurenckennedy@gmail.com>
2016-04-03 23:31:50 -0400

    commit 1: adds event listener for 'set color' button to change the color of the 'brush' box to the color in the input field

--- pixart.js
+++ pixart.js
@@ -1,23 +1,11 @@
+$(document).ready(function(){
+
 function changeBrushColor(){
-  var color = document.getElementById("color-field").value;
-  event.preventDefault();
-  document.querySelector("div.brush").style.background = color;
-};
-
-function changeSquareColor(){
-  var color = document.getElementById("color-field").value;
-  this.style.background = color;
-  var self = this;
-  setTimeout(function buddhaBoard(){
-    self.style.background = "#E7E5DB"
-  }, 8000)
+  event.preventDefault('button');
+  brushColor = $('#color-field').val();
+  $('div.brush').css("background-color", brushColor);
 }
 
-document.querySelector("#set-color").addEventListener("click" || "compositionend", changeBrushColor);
+$('#set-color').on("click", changeBrushColor);
 
-for (i=0; i<=8000; i++){
-var newDiv = document.createElement("div");
-newDiv.className = 'square';
-document.body.appendChild(newDiv);
-newDiv.addEventListener("mouseover", changeSquareColor)
-}
+}); //ends document.ready
