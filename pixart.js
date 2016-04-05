137b77f5b3dea74590ea0679ddc5a6903f1b80ea
antony mastroniani <afm2105@gmail.com>
2016-04-05 07:49:14 -0400

    altered pixart.js

--- pixart.js
+++ pixart.js
@@ -1,1 +1,43 @@
+// var submitForm = document.getElementById("color-field");
+// var form = $("form");
+// var box = document.querySelector(".brush");
+// var setColor= document.getElementById("set-color");
+// // $("#set-color");
+// ///
+// $("form").click(function(event){
+//     event.preventDefault();
+// });
+//
+//
+// box.addEventListener("click", handleClickEvent);
+//
+// function handleClickEvent() {
+// // submitForm.innerHTML = submitForm;
+//   // box.innerHTML = submitForm;
+//   box.style.backgroundColor = form;
+// }
+// ///
+// submitForm.addEventListener("submit", handleSubmitEvent);
+//
+// function handleSubmitEvent() {
+//   box.innerHTML=submitForm;
+// }
 
+
+var form = $("form");
+var input = document.querySelector('#color-field');
+var brush = document.querySelector('.brush');
+
+$("form").click(function(event){
+    event.preventDefault();
+  brush.style.backgroundColor = input.value;
+});
+
+// for( var i = 0; i < 8000; i++){
+//   var div = document.createElement('div');
+//   div.classList.add('square');
+//   div.addEventListener('mouseover', function(){
+//     this.style.backgroundColor = brush.style.backgroundColor;
+//   });
+//   document.body.appendChild(div);
+// }
