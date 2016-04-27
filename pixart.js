a17126389789c1323e0815d5d9f9b8da61eacd32
RobertAKARobin <robertgfthomas@gmail.com>
2016-04-27 08:34:39 -0400

    Minor style changes so it all fits on one page

--- pixart.js
+++ pixart.js
@@ -1,17 +1,18 @@
 var form = document.getElementById('form');
 var input = document.querySelector('#color-field');
 var brush = document.querySelector('.brush');
+var canvas = document.getElementById('canvas');
 
 form.addEventListener('submit', function(event){
   event.preventDefault();
   brush.style.backgroundColor = input.value;
 });
 
-for( var i = 0; i < 8000; i++){
+for( var i = 0; i < 4440; i++){
   var div = document.createElement('div');
   div.classList.add('square');
   div.addEventListener('mouseover', function(){
     this.style.backgroundColor = brush.style.backgroundColor;
   });
-  document.body.appendChild(div);
+  canvas.appendChild(div);
 }
