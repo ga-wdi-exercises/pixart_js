6f8944933eb668937c6c9bc02d8d2b3cef0c9514
Kristin Pasive <kristin.pasive@gmail.com>
2016-06-01 14:24:05 -0400

    initial commit

--- pixart.js
+++ pixart.js
@@ -1,1 +1,15 @@
+$(document).on("ready", function(){
 
+var brush = $(".brush");
+var setColor = $("#set-color");
+var colorInput = $("#color-field");
+var form = $("#form");
+
+
+form.on("submit", function(form){
+  form.preventDefault();
+  brush.css("background", colorInput.val());
+})
+
+
+})
