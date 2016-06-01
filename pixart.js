b37fdb06a16a7801ffbf28dd4d216c0f9801816c
Julia Najafi <Julia.Najafi@gmail.com>
2016-06-01 17:50:12 -0400

    append 20 divs to body

--- pixart.js
+++ pixart.js
@@ -2,7 +2,16 @@
 $("#set-color").on("click", changeColor);
 var colorInput = $("#color-field");
 
+//changing color of one box by inputing color
 function changeColor() {
   event.preventDefault();
   $(".brush").css("background-color", colorInput.val());
+}
+
+
+//creating divs and appending them
+for (var i =1 ; i < 20; i++) {
+
+  $("body").append("<div class='square'></div>");
+
 }
