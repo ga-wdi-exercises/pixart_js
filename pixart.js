6cc91ddd5c39455764b849e9809fe38cf3b0de7c
Sean Tay <seantay@Seans-MacBook-Pro-2.local>
2016-06-01 17:39:21 -0400

    created 20 squares

--- pixart.js
+++ pixart.js
@@ -6,4 +6,18 @@ $(document).ready(function(){
       $(".brush").css("background",color);
       event.preventDefault();
     })
+
+    function loadSquares(){
+      for (a = 0; a < 21; a++){
+        $('body').append("<div class='square'></div>")
+      }
+    };
+
+    loadSquares();
+
+
+    $(".square").on("click"), function(){
+      var color = $("#color-field").val();
+      $(this).css("background",color);
+    }
 });
