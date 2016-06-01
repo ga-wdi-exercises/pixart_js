110bbf64a3eb818fe83d2b0d8af550b1aa6b75ac
James <james@team-wombat.com>
2016-06-01 16:17:18 -0400

    Added enter press functionality

--- pixart.js
+++ pixart.js
@@ -4,3 +4,9 @@ $("#set-color").on("click", function(){
     userColor = $('#color-field').val();
     event.preventDefault();
 });
+
+$("#color-field").on("keydown", function(event){
+    if(event.keyCode == 13){
+        $("#iset-color").click();
+    }
+});
