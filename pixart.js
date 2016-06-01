4d5e45fed58b65767300c8a482de16c58db9ff25
Adina Pollack <adinap16@gmail.com>
2016-06-01 10:45:44 -0400

    Enter key fucntionality

--- pixart.js
+++ pixart.js
@@ -4,3 +4,17 @@ $("button").click(function(e){
   e.preventDefault();
   brush.css("background-color", color)
 });
+
+$("#form").keypress(function(e){
+  enterKey;
+})
+
+function enterKey(){
+  var keycode = (event.keyCode ? event.keyCode : event.which);
+  if(e.keycode == '13'){
+    var brush = $(".brush");
+    var color = $("#color-field").val();
+    brush.css("background-color", color)
+    return true
+  };
+}
