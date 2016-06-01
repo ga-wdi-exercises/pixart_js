87b2dcbcfec5922085727631cb5b4c5491e92bca
Matthew Scott <matthewruelscott@gmail.com>
2016-06-01 13:59:19 -0400

    Adds div boxes

--- pixart.js
+++ pixart.js
@@ -1,6 +1,13 @@
-$("#set-color").on("click", function(e) {
+$("#set-color").on("click", function() {
   var color = $("#color-field").val()
   var brush = $(".brush")
   brush.css("background-color", color)
   event.preventDefault()
 })
+
+var counter = 0
+while(counter < 21){
+  var square = "<div class='square'></div>"
+  $("body").append(square)
+  counter++
+}
