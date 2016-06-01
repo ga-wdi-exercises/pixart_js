e4625465e160cf1fbd441d16a96730caa85f88f6
James <james@team-wombat.com>
2016-06-01 16:36:05 -0400

    Added those bizarre little squares

--- pixart.js
+++ pixart.js
@@ -1,7 +1,14 @@
 var userColor;
 
+function addOn() {
+    for (i=0; i<20; i++) {
+        $("body").append("<div class='square'></div>");
+    }
+}
+
 $("#set-color").on("click", function(){
     userColor = $('#color-field').val();
+    addOn();
     event.preventDefault();
 });
 
