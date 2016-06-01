2d72f203e43a0ad2360c05294bc004c74c671d59
Julia Schultz <julia.schultz91@gmail.com>
2016-06-01 17:25:22 -0400

    finishing bonus portion

--- pixart.js
+++ pixart.js
@@ -1,6 +1,7 @@
 
 var body = $("body");
 var colors = []
+$(".brush").attr('id', 'brush0');
 
 
 
@@ -19,9 +20,6 @@ function setColor(){
   $("#brush2").css("background-color", colors[colors.length-3]);
 
 }
-
-$(".brush").attr('id', 'brush0');
-
 for (i=1; i<3; i++){
   $(".controls").append('<div class="brush" id="brush' + i + '"/>');
 }
@@ -30,26 +28,17 @@ for(i = 0; i < 8000; i++) {
   $("body").append('<div class="square"/>');
 }
 
-
 $(".square").on("mouseover",function(){
   var colorInput = $("#color-field").val();
   $(this).css("background-color", colorInput);
 });
 
 
+$(".brush").on("click", function() {
+  var brushColor = $(this).css("background-color");
+  console.log(brushColor);
 
-//
-//
-//
-//
-// var inputs = new Array();
-//  $("input").each(function(){
-//      inputs.push($(this).val());
-//  })
-//
-//  console.log(inputs);
-
-
-// you should have 3 boxes with the most recent 3 colors used. When you click on each of those boxes, it should set the current brush color back to that color.
-//
-// append 2 more .brush divs
+  $(".square").on("mouseover",function(){
+    $(this).css("background-color", brushColor);
+  })
+})
