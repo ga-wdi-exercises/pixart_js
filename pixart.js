cac2cefc4b0441e11e7ad5176defca44f17ec7e1
David <dcordz@gmail.com>
2016-05-31 21:06:27 -0400

    added the 20 divs

--- pixart.js
+++ pixart.js
@@ -3,4 +3,10 @@ $(document).ready(function() {
     evt.preventDefault();
     $(".brush").css("background", $("#color-field").val());
   });
+  function squareFactory() {
+    for (var i = 0; i < 20; i++) {
+      $("body").append("<div class='square'></div>");
+    }
+  }
+  squareFactory();
 });
