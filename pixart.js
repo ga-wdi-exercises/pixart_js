d8ff7be5bdeae6c4160f3843bbc5cda9fb9a380e
Julia Najafi <Julia.Najafi@gmail.com>
2016-06-01 17:10:13 -0400

    Box color changes with input into color field

--- pixart.js
+++ pixart.js
@@ -1,1 +1,8 @@
 
+$("#set-color").on("click", changeColor);
+var colorInput = $("#color-field");
+
+function changeColor() {
+  event.preventDefault();
+  $(".brush").css("background-color", colorInput.val());
+}
