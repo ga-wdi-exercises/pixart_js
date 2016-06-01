b7dd7c9d3980f4302693b74242a0edde044b7be6
Julia Schultz <julia.schultz91@gmail.com>
2016-06-01 10:48:04 -0400

    when you click a square it changes to a color that the user inputs

--- pixart.js
+++ pixart.js
@@ -17,3 +17,10 @@ function setColor(){
 for(i = 0; i < 20; i++) {
   $("body").append('<div class="square"/>');
 }
+
+
+$(".square").on("click",function(){
+  var colorInput = $("#color-field").val();
+
+  $(event.target).css("background-color", colorInput);
+});
