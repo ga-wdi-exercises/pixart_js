7cc2a8de1663b4b80bd0687ea2cf0de6119461a4
Sean Tay <seantay@Seans-MacBook-Pro-2.local>
2016-06-01 17:11:10 -0400

    brush color set

--- pixart.js
+++ pixart.js
@@ -1,1 +1,9 @@
 
+$(document).ready(function(){
+    $("button").on("click", function(){
+      var color = $("#color-field").val();
+      console.log(color);
+      $(".brush").css("background",color);
+      event.preventDefault();
+    })
+});
