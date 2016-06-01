352cd55fa9b76335f42bd4982d324f40eb7bc9f3
Mara Sanders <sandersmara@gmail.com>
2016-06-01 13:59:06 -0400

    Added 20 div squares

--- pixart.js
+++ pixart.js
@@ -1,9 +1,15 @@
 
+var counter = 0
 
 $("#set-color").on("click",function(){
   var brush = $(".brush")
   var color = $("#color-field").val();
   brush.css("background-color",color);
   event.preventDefault();
-
 })
+var counter = 0
+while(counter < 21){
+  var square = "<div class = 'square'></div>"
+  $("body").append(square)
+  counter++
+}
