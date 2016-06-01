6044804195d45c213dd0568b528b96d4eac11372
Mara Sanders <sandersmara@gmail.com>
2016-06-01 13:35:06 -0400

    Set Brush Box to Specified Color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,9 @@
 
+
+$("#set-color").on("click",function(){
+  var brush = $(".brush")
+  var color = $("#color-field").val();
+  brush.css("background-color",color);
+  event.preventDefault();
+
+})
