b2a6da97c8d999bf7d437a47c64ba838e0081bbf
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2016-05-31 20:41:04 -0400

    Add the functionality of setting the brush color to the specified color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,32 @@
+/*
+<h1>Pixel Art!</h1>
+<div class="controls">
+  <form id="form">
+    <input type="text" id="color-field"></input>
+    <button id="set-color">Set Color</button>
+  </form>
+  <div class="brush"></div>
+</div>
+ */
 
+function initPixelArt() {
+
+  // Get references to the elements.
+  var $colorField = $( '#color-field' );
+  var $button     = $( '#set-color' );
+  var $brush      = $( '.brush' );
+
+  // Add click listener on the "Set Color" button.
+  $button.on( 'click', function( evt ) {
+
+    // Disable the button's default behavior.
+    evt.preventDefault();
+
+    // Set the colot of "brush" box to the color that is specified in the input field.
+    $brush.css( 'background', $colorField.val() );
+  });
+}
+
+$( document ).ready( function(){
+  initPixelArt();
+});
