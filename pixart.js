a35504ebcfe386e8856bf624806bd0d1b7dfb8a5
Thaisun Green <Tlgreen16@gmail.COM>
2016-06-01 23:00:15 -0400

    divs created, css created,canvas created

--- pixart.js
+++ pixart.js
@@ -1,1 +1,9 @@
-
+$("#set-color").on("click", function(evt){
+  evt.preventDefault();
+  var input = $("#color-field");
+  console.log(input.val());
+  $(".brush").css("background-color" , input.val());
+  $(".square").on("mouseover",function(){
+    $(this).css('background-color',input.val())
+  })
+})
