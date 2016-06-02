d500e5b936b9e049f88b8c6955cf7ee429fc3a43
Michael Holzheimer <michael.r.holzheimer@gmail.com>
2016-06-01 22:09:59 -0400

    commit #6

--- pixart.js
+++ pixart.js
@@ -1,22 +1,11 @@
-
-
-
-
-
-form.addEventListener("click", function() {
-    form.getElementById("set-color").text;
-});
-
 //event.preventDefault()  unsure of how to use this in the code.
-
-
 var i = 0
 
-while i <=20 {
+while i <=8000 {
   i =+ 1
   document.append("<div class=square" + i + "></div>")
   }
 
-body.addEventListener("click", function() {
+body.addEventListener("mouseover", function() {
     this.getElementById("set-color").text
 });
