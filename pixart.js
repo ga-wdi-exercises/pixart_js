cd0eecd66863ac8f1430bb01a5dfa9e4b804c77e
Van Phan <VanPhan514@gmail.com>
2016-07-05 17:17:43 -0400

    Added function that changes the square divs to green when clicked

--- pixart.js
+++ pixart.js
@@ -4,10 +4,15 @@ $(document).ready(function(){
 	set_color.click(function(e) {
 		e.preventDefault();
 		$("body").css("background", ($('input#color-field').val()));
- 	});
+ 	})
 
 	for (var i = 0; i < 20; i++) {
 		$("body").append("<div class='square'></div>");
 	}
-	
+
+	$(".square").click(function(e) {
+		e.preventDefault();
+		$(this).css("background", "green");
+	})
+
 });
