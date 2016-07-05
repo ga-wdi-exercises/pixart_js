bf5d9c5d8a1b611eee5096b14721bcc1ce8fe61a
Marc Fournier <marc.fournier91@gmail.com>
2016-07-05 17:54:35 -0400

    Commit one. Added event listener to change brush box color to input field value when clicking button

--- pixart.js
+++ pixart.js
@@ -1,1 +1,15 @@
+$(document).on("ready", function(){
+  // console.log("jquery");
+  var field = $("#color-field")
+  var button = $("#set-color");
+  var brushBox = $(".brush");
 
+  button.on("click", setColor);
+
+  function setColor(e){
+    e.preventDefault();
+    console.log("clicked");
+    brushBox.css("background", field.val());
+  }
+
+});
