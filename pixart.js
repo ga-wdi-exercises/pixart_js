656d606c2a0054ed8648465a1f45f4736fae2e23
Rasheeda Alexander <sheedabeeda@gmail.com>
2016-07-05 19:13:46 -0400

    Changed square color using input instead of green

--- pixart.js
+++ pixart.js
@@ -19,17 +19,33 @@ function changeColorBox(evt) {
   $(".brush").css("background", color);
 }
 
+//Commit 3
 for(var i=0; i<=20; i++) {
   $('body').append('<div class="square"></div>');
   console.log("WTF");
 }
 
-
-
+//Commit 4
 var square = $('.square');
-square.on("click", changeColorGreen);
+square.on("click", changeColorNotGreen);
 
+//Commit 4 (continued)
+/*
 function changeColorGreen() {
   console.log("Green Color");
   $('.square').css("background", "green");
+}
+*/
+
+//Commit 5
+function changeColorNotGreen(evt) {
+  console.log("Not Green");
+
+  //Define input
+  var input = $('input');
+
+  evt.preventDefault();
+  //.val() is for input fields
+  var color = input.val();
+  $('.square').css("background", color);
 }
