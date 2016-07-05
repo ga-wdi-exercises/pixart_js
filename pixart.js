b9f43c2c53c383398a66c788ae70219d7825825c
Taylor Edwards <tedwards1884@gmail.com>
2016-07-05 17:53:58 -0400

    commits 1,2: added color text  input functionality

--- pixart.js
+++ pixart.js
@@ -1,1 +1,14 @@
+$(document).on("ready", function() {
+    console.log('jquery ready broh');
 
+    $('button').on("click", getInput);
+})
+
+function getInput(evt) {
+  // console.log('button clicked broh');
+  evt.preventDefault();
+  var colorName = $('#color-field').val();
+  // console.log(colorName);
+  $('.brush').css("background-color", colorName);
+  // console.log($('.brush').css("background-color"));
+}
