9acf67a31f227f2d7bddecc844e23bdabc23ed56
Van Phan <VanPhan514@gmail.com>
2016-07-05 16:58:06 -0400

    Finished setting color function and changing the background color to the color set

--- pixart.js
+++ pixart.js
@@ -1,1 +1,8 @@
+$(document).ready(function(){
+	var set_color = $("#set-color");
 
+	set_color.click(function(e) {
+		e.preventDefault();
+		$("body").css("background", ($('input#color-field').val()));
+  });
+});
