53a677b19299958172fd9261b921d6cc8f2ec931
johnestes4 <johnestes4@gmail.com>
2016-07-05 14:14:30 -0400

    Added functionality for color changes

--- pixart.js
+++ pixart.js
@@ -1,1 +1,15 @@
+// below is the standard code for the document ready event.
+$(document).ready(function(){
+   // jQuery goes here...
+  //  $("#inputThing").val("Hi!");
+  //  var inputText = $("input").val();
+  //  console.log(inputText);
 
+
+  $("#set-color").click( function(){
+    event.preventDefault();
+    var colorIn = $("input").val();
+    $(".brush").css("backgroundColor", colorIn);
+  });
+
+});
