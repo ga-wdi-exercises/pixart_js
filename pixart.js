fbebc0c9fe138f9343cb09368270356d29bfa4be
Winston Davis Mclean <winston.davis.mclean@gmail.com>
2016-07-05 15:42:47 -0400

    made 20 divs with class .square

--- pixart.js
+++ pixart.js
@@ -9,6 +9,16 @@ $(document).ready(function(){
     $('.brush').css({'background': color});
     event.preventDefault();
   });
+  var count = 20;
+  while( count > 0) {
+    var new_div = $('<div></div>').addClass("square");
+    $('body').append(new_div);
+    count--;
+    console.log(count);
+  };
+  // $('.square').on("click", function(){
+  //   this.style.backgroundColor = 'green';
+  // });
 });
 
 
