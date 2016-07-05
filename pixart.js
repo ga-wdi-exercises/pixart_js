4ef72d4a178310a09a824d165a49911b1f4a27c1
Tyler Rosenberg <tyryanr@gmail.com>
2016-07-05 17:05:29 -0400

    Commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,12 @@
+$(document).ready(function() {
 
+  $("button").on("click", colorCollector);
+
+  function colorCollector(evt) {
+    evt.preventDefault();
+    var color = $("input").val();
+    $(".brush").css("background", color);
+    }
+
+
+})
