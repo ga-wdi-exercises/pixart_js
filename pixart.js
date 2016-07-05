b7a8a4dd643d864714e3a8d0f7c687d0768e787f
Christopher Phillips <cphillips918@gmail.com>
2016-07-05 17:58:39 -0400

    added both enter keydown and click functionality to .on trigger

--- pixart.js
+++ pixart.js
@@ -3,12 +3,18 @@ var brushBox = $('.brush');
 var inputField = $('#color-field');
 
 
+/*this has both onClick and enter functionality in chrome for some reason if it doesn't work for the other browsers I'd create my own call back function starting with the function in line seven and feed that into the button.on selector listening for both click and enter*/
 $(document).ready(function() {
-    button.on('click', function(event) {
+    button.on({
+        click: mainColorEvent,
+        onKeyDown: mainColorEvent,
+    })
+
+    function mainColorEvent(event) {
         event.preventDefault();
         function changeColor(){
             brushBox.css('background-color', inputField.val());
         }
         changeColor();
-    });
+    };
 });
