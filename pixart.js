b59788b6e3bb4abfe95af7b81f5aabf83c441356
Van Phan <VanPhan514@gmail.com>
2016-07-05 19:03:30 -0400

    Added a swatch that saves previous colors

--- pixart.js
+++ pixart.js
@@ -1,18 +1,41 @@
 $(document).ready(function(){
 	var set_color = $("#set-color");
+	var swatch = [];
 
 	set_color.click(function(e) {
 		e.preventDefault();
-		$("body").css("background", ($('input#color-field').val()));
+		$(".brush").css("background", ($('input#color-field').val()));
+
  	})
 
-	for (var i = 0; i < 20; i++) {
+	function set(color) {
+		if (swatch.length > 2) {
+			var trash = swatch.pop();
+		}
+		swatch.unshift(color);
+		$("#0").css("background", swatch[0]);
+		$("#1").css("background", swatch[1]);
+		$("#2").css("background", swatch[2]);
+	}
+
+	for (var i = 0; i < 8000; i++) {
 		$("body").append("<div class='square'></div>");
 	}
 
-	$(".square").click(function(e) {
+	$(".square").mouseover(function(e) {
 		e.preventDefault();
 		$(this).css("background", ($('input#color-field').val()));
 	})
+
+	$(".square").click(function() {
+  		var color = $(this).css("background");
+  		set(color);
+	});
+	$(".pix").click(function() {
+  		var color = $(this).css("background");
+  		set(color);
+	});
+
+
 
 });
