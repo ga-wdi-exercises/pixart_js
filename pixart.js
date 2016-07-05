5779c628c332b85c9b27e0b5023fce0c3501f34f
Winston Davis Mclean <winston.davis.mclean@gmail.com>
2016-07-05 13:07:59 -0400

    set color button

--- pixart.js
+++ pixart.js
@@ -1,1 +1,16 @@
+$(document).ready(function(){
+  $('button').on('click', function() {
+    var color = $('input').val();
+    $('.brush').css({'background': color});
+    event.preventDefault();
+  });
+  // $('button').keypress(function() {
+  //   var color = $('input').val();
+  //   $('.brush').css({'background': color});
+  //   event.preventDefault();
+  // });
+});
+
+
+
 
