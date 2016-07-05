a12fb28236f1e80f76c505b6114fb1ae0fffdb8b
MrNelley <Kelleysoftware@gmail.com>
2016-07-05 18:05:49 -0400

    initial commit

--- pixart.js
+++ pixart.js
@@ -1,1 +1,18 @@
+$(document).ready(function(){
+  console.log("JQuery Works!");
 
+  var button = $('button');
+  button.on("click", changeColorBox);
+});
+
+function changeColorBox(evt)
+{
+  var input=$('input');
+
+  evt.preventDefault();
+  //.val() only exists for input fields
+  var color = input.val();
+  console.log(color);
+
+  $('.brush').css('background',color);
+}
