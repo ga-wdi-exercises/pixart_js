b66212e2ba76e231f669176c945ce0ef8f38d256
Taylor Edwards <tedwards1884@gmail.com>
2016-07-05 18:25:26 -0400

    squares green on click

--- pixart.js
+++ pixart.js
@@ -1,14 +1,16 @@
+var square = $('<div class="square"></div>');
+
+for (var i=0; i<20; i++){
+  $('body').append(square.clone());
+  // console.log('so square broh');
+}
+
 $(document).on("ready", function() {
   // console.log('jquery ready broh');
 
   $('button').on("click", getColor);
+  $('.square').on("click", changeSquare);
 
-  var square = $('<div class="square"></div>');
-
-  for (var i=0; i<20; i++){
-    $('body').append(square.clone());
-    console.log('so square broh');
-  }
 })
 
 function getColor(evt) {
@@ -18,4 +20,9 @@ function getColor(evt) {
   // console.log(colorName);
   $('.brush').css("background-color", colorName);
   // console.log($('.brush').css("background-color"));
+}
+
+function changeSquare() {
+  // console.log(this);
+  $(this).css("background-color", "green");
 }
