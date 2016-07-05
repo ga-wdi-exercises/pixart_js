4246cc6370c331a9f9ae99c277ba1479db3b7296
Jacqui Ahn <jsahn23@gmail.com>
2016-07-05 18:03:59 -0400

    creates new set color button

--- pixart.js
+++ pixart.js
@@ -1,1 +1,18 @@
+$(document).ready(function(){
+  console.log("I'm ready");
 
+    var button = $("button");
+
+    button.on("click", changeColorBox);
+});
+    function changeColorBox(evt){
+      //Define input within the function to avoid error
+      var input = $("input");
+
+      evt.preventDefault();
+      //.val() is for input fields
+      var color = input.val();
+      console.log(color);
+
+      $(".brush").css("background", color)
+    }
