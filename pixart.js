79b64ede412cb48eba35d8165107cec30aadaeac
Chase <chasefreeman13@gmail.com>
2016-07-05 22:19:47 -0400

    finally added the divs

--- pixart.js
+++ pixart.js
@@ -1,6 +1,5 @@
 
 $(document).ready(function(){
-  console.log("Working?")
 
 var button = $("button");
 var input = $("input");
@@ -8,12 +7,20 @@ var input = $("input");
 
 
   button.on("click", changeBrushColor);
+  button.on("keypress", changeBrushColor);
 
 
   function changeBrushColor(evt) {
     evt.preventDefault();
 
+
     var newColor = input.val();
   $(".brush").css("background", newColor);
-      }
-  });
+}
+
+
+  for ( var i = 0; i < 20; i++) {
+    $("body").append("<div class='square'></div>");
+};
+
+});
