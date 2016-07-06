878027cf286cccad9b1630a2c688bb3cd2fa7b8f
Ben Brizzi <brizzitime@gmail.com>
2016-07-06 18:10:56 -0400

    user input color determines square color on click

--- pixart.js
+++ pixart.js
@@ -6,11 +6,11 @@ $(document).ready(function() {
     e.preventDefault();
   }
   //creating 20 of the square div class, commit 3
-  for (var i = 0; i <= 8000; i++) {
+  for (var i = 0; i <= 20; i++) {
     $("body").append("<div class='square'></div>")
   }
 
-  $(".square").on("mouseover", colorSquare)
+  $(".square").on("click", colorSquare)
 //commits 4 and 5, storing the input string in a variable to be used in the line 17 background change
   function colorSquare() {
     var inputColor = $("input").val();
