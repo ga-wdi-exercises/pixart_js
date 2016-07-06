21adfe1f6e6acd6e822b7ae68f2d4671cd8dcfff
Ben Brizzi <brizzitime@gmail.com>
2016-07-06 18:08:33 -0400

    added mouseover painting, and created extra square divs

--- pixart.js
+++ pixart.js
@@ -6,12 +6,15 @@ $(document).ready(function() {
     e.preventDefault();
   }
   //creating 20 of the square div class, commit 3
-  for (var i = 0; i <= 20; i++) {
+  for (var i = 0; i <= 8000; i++) {
     $("body").append("<div class='square'></div>")
   }
-  $(".square").on("click", greenSquare)
 
-  function greenSquare() {
-    $(this).css("background", "green");
+  $(".square").on("mouseover", colorSquare)
+//commits 4 and 5, storing the input string in a variable to be used in the line 17 background change
+  function colorSquare() {
+    var inputColor = $("input").val();
+    $(this).css("background", inputColor);
   }
+
 });
