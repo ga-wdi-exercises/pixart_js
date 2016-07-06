6aa23d9696ce03b5cbb6dd5410dfbc30a5bae6e2
pamelaolney <pjolney12@gmail.com>
2016-07-06 05:19:08 -0400

    Commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,13 @@
+$(document).on("ready", function()
+{
+var setcolor=$("#set-color");
+setcolor.on("click", changeBoxToInput);
 
+var input=$(":input").val();
+
+
+function changeBoxToInput(){
+  $(".brush").css("background", ":input");
+  event.preventDefault()
+}
+});
