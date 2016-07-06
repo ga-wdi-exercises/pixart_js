8608954473ffe4bf31d32b6e12bb5c99e502429c
Ben Brizzi <brizzitime@gmail.com>
2016-07-06 17:57:15 -0400

    created 20 square divs

--- pixart.js
+++ pixart.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
   $("button").on("click", colorBrush);
-
+  //input.val allows it to work for both click and return key for commits 1 and 2
   function colorBrush(e) {
     $(".brush").css("background", $("input").val());
     e.preventDefault();
   }
-
+  //creating 20 of the square div class, commit 3
+  for (var i = 0; i <= 20; i++) {
+    $("body").append("<div class='square'></div>")
+  }
 });
