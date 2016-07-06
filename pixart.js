5b851d031473dd106ad0a55a7f47eb723bee5913
AlexanderHolland <aholland@mystudybank.com>
2016-07-05 22:10:40 -0400

    setColor Functionality

--- pixart.js
+++ pixart.js
@@ -1,1 +1,13 @@
 
+$(document).on("ready", function() {
+    console.log('Ready');
+
+    $('button').on("click", userInput);
+
+ function userInput(event) {
+   event.preventDefault();
+   var setColor = $('#color-field').val();
+   $('.brush').css("background-color", setColor);
+ }
+
+ });
