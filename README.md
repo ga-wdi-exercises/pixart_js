35bd6ee9764184eb9e52667a7a2dbe90bf54c518
Christopher Phillips <cphillips918@gmail.com>
2016-07-05 22:12:40 -0400

    reimplemented functionality from old feature implementation

--- README.md
+++ README.md
@@ -8,10 +8,6 @@ Use the starter code and commit each step of the exercise.
 - When you're finished, push your solution branch to your fork (`git push origin <branch-name>`) and issue a **pull request**.
 - Feel free to use jQuery.
 
-###Commit 6
-
-* Paint a picture!
-
 ## Bonus
 
 * Add a color swatch. You should have 3 boxes with the most recent 3 colors used. When you click on each of those boxes, it should set the current brush color back to that color.
--- pixart.js
+++ pixart.js
@@ -2,13 +2,12 @@ $(document).ready(function() {
     var button = $('button');
     var brushBox = $('.brush');
     var inputField = $('#color-field');
-
+    var boxField = $('')
     /*this has both onClick and enter functionality in chrome for some reason if it     doesn't work for the other browsers I'd create my own call back function starting   with the function in line seven and feed that into the button.on selector listening for both click and enter*/
     button.on({
         click: colorSelect,
         onKeyDown: colorSelect,
     })
-
     // changes the selected color to the text input
     function colorSelect(event) {
         event.preventDefault();
@@ -17,9 +16,8 @@ $(document).ready(function() {
         }
         changeColor();
     };
-
     //creates static div elements for the site with the onClick color change
-    for(i=1;i<8000; i++ ){
+    for(i=1;i<8000;i++){
       $("body").append($("<div class='square'/>"))
     }
     $('.square').on('mouseover',function(event) {
