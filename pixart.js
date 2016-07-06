ba49c787a71e3d7fed8ba50af455aaf76b0b9fbe
Sarah K Shultis <skshultis@Sks-MacBook-Air.local>
2016-07-05 21:09:34 -0400

    input changes color box when set color or enter are clicked (Commit 1 and 2)

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
+$(document).ready(function() {
+  console.log("Your Mom");
 
+$("button").on("click", brushColorChange);
+
+  function brushColorChange() {
+    $(".brush").css("background", $("input").val());
+    event.preventDefault();
+  }
+
+});
