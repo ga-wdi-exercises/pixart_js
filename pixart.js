630f6e8edf6988a00687266ab788fd933e63bd88
Chase <chasefreeman13@gmail.com>
2016-07-05 21:48:10 -0400

    Color switch feature working

--- pixart.js
+++ pixart.js
@@ -1,1 +1,19 @@
 
+$(document).ready(function(){
+  console.log("Working?")
+
+var button = $("button");
+var input = $("input");
+
+
+
+  button.on("click", changeBrushColor);
+
+
+  function changeBrushColor(evt) {
+    evt.preventDefault();
+
+    var newColor = input.val();
+  $(".brush").css("background", newColor);
+      }
+  });
