96a728836396c760a91a5e572048fac6a81ad2b9
Daniela Noir <daniela@danielanoir.com>
2016-07-05 23:15:02 -0400

    adjusted javascript

--- pixart.js
+++ pixart.js
@@ -1,1 +1,36 @@
+var brush = $(".brush");
+var button = document.getElementById("set-color");
+var input = document.getElementById("color-field");
+function changeBrushColor (inputColor) {
+  brush.css("background", inputColor);
+}
 
+
+//I got very stuck on the part where I needed to enter the input.value, so I refered to the
+//solutions, but I'm not sure what the function(event) part of this does.
+
+button.addEventListener("click", function(event){
+  event.preventDefault();
+  changeBrushColor(input.value);
+})
+
+
+
+
+//the below are my own crazy notes trying to figure out how to get this to work...
+
+//
+// $(".four").on( "click", changebackgroundColortoYellow );
+//
+// function changeBackgroundColorToRed (){
+//   body.css("background","red");
+// }
+//
+
+
+// brush.addEventListener("click", changeBrushColor)
+
+//
+// function changeBoxColor () {
+//   $("input:text").val("#brush:background");
+// }
