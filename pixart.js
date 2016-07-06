1ceae275babda1ea9d1c4712d462f4802197aa09
AUBREY NORTHAM <AENORTHAM@GMAIL.COM>
2016-07-05 23:04:48 -0400

    step 1, make color input work

--- pixart.js
+++ pixart.js
@@ -1,1 +1,12 @@
+//getting html elements
+var brush = $(".brush");
+var button = $("button");
 
+function changeColor(e){
+  e.preventDefault();
+  var color = $("#color-field");
+  //brush.style.backgroundColor = color.value;
+  brush.css("background", color.val());
+}
+button.on("click", changeColor);
+//event(action, event handler slash function to call)
