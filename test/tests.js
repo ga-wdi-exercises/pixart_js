a60bd9a7e2c7f35f197685ce87eb0241a1f9059b
Nick Olds <nolds9@yahoo.com>
2016-08-31 16:05:17 -0400

    tests allow for starter code

--- test/tests.js
+++ test/tests.js
@@ -3,7 +3,9 @@
     $("#color-field").val("red")
     var form = $("#form")[0]
     var listener = listeners[form.getAttribute("data-listener-id")]
-    listener.cb(new Event("Hi"))
+    if(listener) {
+      listener.cb(new Event("Hi"))
+    }
     var color = $(".brush").css("background-color")
     assert.equal( color, "rgb(255, 0, 0)");
   });
@@ -14,23 +16,28 @@
 
   test("when I hover over a square, the square's background changes color", function (assert) {
     $("#color-field").val("red")
+    var squares = document.querySelectorAll(".square")
     var listener;
     if($("body").attr("data-listener-id")) {
       var id = $("body").attr("data-listener-id")
       listener = listeners[id]
-      var squares = document.querySelectorAll(".square")
       var customEvent = $.Event("mouseover")
       customEvent.target = squares[0]
-      listener.cb(customEvent)
+      if(listener){
+        listener.cb(customEvent)
+      }
       assert.equal($(squares[0]).css("background-color"), "rgb(255, 0, 0)")
     }
     if($(".square").attr("data-listener-id")) {
       var id = $(".square").attr("data-listener-id")
       listener = listeners[id]
-      var squares = document.querySelectorAll(".square")
       var customEvent = $.Event("mouseover")
       customEvent.target = squares[0]
-      listener.cb(customEvent)
+      if(listener){
+        listener.cb(customEvent)
+      }
+      assert.equal($(squares[0]).css("background-color"), "rgb(255, 0, 0)")
+    } else {
       assert.equal($(squares[0]).css("background-color"), "rgb(255, 0, 0)")
     }
   })
