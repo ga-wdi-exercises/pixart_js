3508f860f75e52d8dfe239dc72d6ca712fb92ec5
Mark Sweet <marks828@gmail.com>
2016-09-13 13:50:00 -0400

    added button functionality

--- pixart.js
+++ pixart.js
@@ -1,1 +1,16 @@
+// get input colorl val
+// set color
 
+
+$('#set-color').on('click', function(evt){
+  var color = $('input').val()
+  $('.brush').css('background', color)
+  evt.preventDefault();
+})
+
+
+$('#set-color').on('keypress', 'enter', function(evt){
+  var color = $('input').val()
+  $('.brush').css('background', color)
+  evt.preventDefault();
+})
