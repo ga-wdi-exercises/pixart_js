c285bd54a0602b7b2b4a0b040c10d005076f84bd
Michael Rubin <michael@rubinsworld.com>
2016-09-13 19:55:21 -0400

    Commit 1 Set color button

--- pixart.js
+++ pixart.js
@@ -1,1 +1,18 @@
 
+
+function getAndSetColor() {
+  var color = $("#color-field").val();
+  $(".brush").css("background",color);
+  event.preventDefault();
+}
+
+/*  Main Line code */
+if(typeof jQuery === "function"){
+  console.log('jQuery loaded!')
+}
+/* register click events */
+
+var button=$("button");
+button.on( "click", getAndSetColor );
+
+// $(".brush").css("background","red");
