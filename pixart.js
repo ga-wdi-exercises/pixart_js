8ef949058cea994ef5b438824950cd6a4844d7c0
Nick Oki <nick.oki@live.com>
2016-09-14 18:16:47 -0400

    Step 2: Enter key

--- pixart.js
+++ pixart.js
@@ -1,6 +1,18 @@
 // pixart.js
 
+// Set Brush Color
+function setColor() {
+	$('.brush').css('background', ($('#color-field').val()));
+}
+
 $('#set-color').on('click', function(e) {
 	e.preventDefault();
-	$('.brush').css('background', ($('#color-field').val()));
+	setColor();
+})
+
+$('#set-color').keypress(function(e) {
+	var key = e.which;
+	if (key == 13) {
+		setColor();
+	}
 })
