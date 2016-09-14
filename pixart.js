9915218cd3a7e954452a330a345e7ef34b14faa2
Michael Rubin <michael@rubinsworld.com>
2016-09-14 13:49:18 -0400

    Commit 3 20 DIVS

--- pixart.js
+++ pixart.js
@@ -7,6 +7,7 @@ function getAndSetColor() {
   event.preventDefault();
 }
 
+/* commit 2*/
 function handleKeyboardPress(keyChar) {
   var key = keyChar.which;
   switch (key) {
@@ -26,12 +27,15 @@ if(typeof jQuery === "function"){
 }
 /* register click events */
 
+/* commit 1 */
 var button=$("button");
 button.on( "click", getAndSetColor );
 
+/* commit 2 */
 var textBox=$('#form');
 textBox.on('keydown', '#color-field', handleKeyboardPress);
 
-
-
-// $(".brush").css("background","red");
+/* commit 3*/
+for(var i=0;i<20;i++) {
+  $("body").append("<div class='square'></div>")
+}
