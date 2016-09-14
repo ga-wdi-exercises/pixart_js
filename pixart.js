2f4aefb78a3842145df2dcbaa2c7488491607e87
Liza Floyd <lizafloyd@gmail.com>
2016-09-14 18:07:16 -0400

    set color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,13 @@
 
+var button = $("#set-color")
+var brush = $(".brush")
+
+
+function change(e) {
+  // colorSel.
+  var colorSel = $("#color-field").val()
+  brush.css("background", colorSel)
+  e.preventDefault()
+
+}
+button.on("click", change)
