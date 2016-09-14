9a47423106f4c8b91e85fc67577693d9a0864558
Barrett Cope <barrettcope@gmail.com>
2016-09-14 17:50:33 -0400

    Brush changes to user input color.

--- pixart.js
+++ pixart.js
@@ -1,1 +1,15 @@
+var input = $("#color-field");
+var button = $("#set-color");
+var brush = $(".brush");
+var form = $("#form");
 
+var x = function(){
+  color = input.val();
+  brush.css("background",color);
+}
+
+form.submit(function(e) {
+    e.preventDefault();
+});
+
+button.on("click",x);
