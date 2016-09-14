6ee7a6ad5c43030b11a911f1f38678ddaa0596ac
YOUR FULL NAME <lun.mariancik@gmail.com>
2016-09-14 19:56:41 -0400

    added a for loop

--- pixart.js
+++ pixart.js
@@ -6,3 +6,8 @@ function changeColor() {
   e.preventDefault();
 }
 setColor.on("click", changeColor);
+
+var addDiv = $("<div class='square'></div>")
+for (var i = 0; i < 21; i++){
+  $("body").append("<div class='square'></div>");
+}
