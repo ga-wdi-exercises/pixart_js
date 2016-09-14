8ea4a70e8ae5a618b1b1ea628ee7d913cc339f58
YOUR FULL NAME <lun.mariancik@gmail.com>
2016-09-14 19:36:52 -0400

    first commit

--- pixart.js
+++ pixart.js
@@ -1,1 +1,8 @@
 
+var anyColor = $("#set-color")
+function changeColor() {
+  var color = $("#color-field").val();
+  $(".brush").css("background-color", color);
+  e.preventDefault();
+}
+anyColor.on("click", changeColor);
