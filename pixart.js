0e8a093f772a6a43e4bce9021e75d86ebbd3aa0f
Nick Oki <nick.oki@live.com>
2016-09-14 19:09:09 -0400

    Bonus: Added swatches

--- pixart.js
+++ pixart.js
@@ -1,21 +1,22 @@
 // pixart.js
 
 // Set Brush Color
-function setColor() {
-	$('.brush').css('background', ($('#color-field').val()));
+function setColor(color) {
+	updateSwatches();
+	$('.brush').css('background', color);
 }
 
 // Click listener for Set Color
 $('#set-color').on('click', function(e) {
 	e.preventDefault();
-	setColor();
+	setColor($('#color-field').val());
 })
 
 // Enter key listener for Set Color
 $('#set-color').keypress(function(e) {
 	var key = e.which;
 	if (key == 13) {
-		setColor();
+		setColor($('#color-field').val());
 	}
 })
 
@@ -27,5 +28,24 @@ for (var i = 0; i < 8000; i++) {
 // Click listener for .square
 $('body .square').on('mouseover', function(e) {
 	e.preventDefault();
-	$(this).css('background', $('#color-field').val());
+	$(this).css('background', $('.brush').css('background'));
 })
+
+// Color Swatches
+function updateSwatches() {
+	$('.swatch').eq(2).css('background', $('.swatch').eq(1).css('background'));
+	$('.swatch').eq(1).css('background', $('.swatch').eq(0).css('background'));
+	$('.swatch').eq(0).css('background', $('.brush').css('background'));
+}
+
+// Click listener for Swatches
+$('.swatch').on('click', function() {
+	chooseSwatch($(this));
+})
+
+// Swatch logic
+function chooseSwatch($swatch) {
+	var temp = $('.brush').css('background');
+	$('.brush').css('background', $swatch.css('background'));
+	$swatch.css('background', temp);
+}
