c64fa75eec2af310dc8736af6b0d9a92c9043bfe
Michael Lai <mike24567@gmail.com>
2016-09-14 18:06:12 -0400

    Set the color for commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,12 @@
 
+//Get and Set color//
+
+var clickColor = $("#set-color");
+
+function getAndSetColor(){
+  var inputColor = $("#color-field").val();
+  $(".brush").css("background", inputColor);
+  event.preventDefault();
+}
+
+clickColor.on("click", getAndSetColor);
