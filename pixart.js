e019899f52cd29b41fc7080dcfd4076affa06ffe
Barrett Cope <barrettcope@gmail.com>
2016-09-14 18:18:44 -0400

    Squares are turning user input color

--- pixart.js
+++ pixart.js
@@ -3,19 +3,24 @@ var button = $("#set-color");
 var brush = $(".brush");
 var form = $("#form");
 var body = $("body");
+var color;
 
 var y = function(){
-  $(this).css("background","green");
+  $(this).css("background",color);
 }
 
-var x = function(){
-  color = input.val();
+var z = function(){
   brush.css("background",color);
   for (var i = 0; i < 20; i++) {
     newSquare = $("<div class='square'></div>");
     newSquare.on("click",y);
     body.append(newSquare);
   }
+}
+
+var x = function(){
+  color = input.val();
+  z();
 }
 
 form.submit(function(e) {
