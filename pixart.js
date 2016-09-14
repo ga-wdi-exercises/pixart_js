a8cea3ef983bafb71f3e55ce31e8d89afa2cc7cf
Tim Foley <thetimfoley@gmail.com>
2016-09-14 09:11:33 -0400

    setting the color pallette

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
 
+var $setColorButton = $('#set-color');
+$setColorButton.on('click',setColor);
+
+function setColor(e) {
+  e.preventDefault();
+  var $colorField = $('#color-field');
+  var color = $colorField.val();
+  $('.brush').css('background', color);
+  $colorField.val('');
+}
