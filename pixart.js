3a39fe0e4b835f213b03c3f9dd5dfbfd81c961c4
RYANBROPHY <BROPHY2321@ME.COM>
2016-09-14 18:23:00 -0400

    2

--- pixart.js
+++ pixart.js
@@ -1,9 +1,13 @@
 
 var setColor = $("#set-color")
 
-function changeColor (){
+function changeColor (e){
   var color = $('#color-field').val()
   $(".brush").css("background", color);
-  event.preventDefault()
+  e.preventDefault()
 }
 setColor.on("click", changeColor)
+
+for (i=0; i<20; i++){
+  $("body").append("<div class='square'></div>");
+}
