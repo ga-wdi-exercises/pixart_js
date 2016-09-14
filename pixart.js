5df799b19d8340b9b8227e067b343686f9675d37
Kevin Mahoney <kjmahoney13@gmail.com>
2016-09-14 18:03:34 -0400

    step 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,13 @@
+var updateColor = $(":input")
+var setColor = $('#set-color')
 
+
+function changeColor(){
+  var color = $('#color-field').val()
+  $(".brush").css("background",color);
+  event.preventDefault()
+}
+
+setColor.on("click",changeColor);
+
+//.val
