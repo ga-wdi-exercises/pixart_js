3fc13a8d521b682c9fa2dbc2144bccaa8e475bcb
Khoi Le <khoileee89@gmail.com>
2016-09-14 18:31:40 -0400

    #1 changes color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,10 @@
+var button = $("#set-color")
 
+var colorField = $("#color-field")
+
+function changeColor(){
+  var color = colorField.val()
+  $(".brush").css("background", color);
+  event.preventDefault()
+}
+button.on("click", changeColor);
