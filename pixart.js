cabafb592820cf6a5a9fd9aef8b34ad6cdf6ca56
Domonique Griffin <griffind2009@gmail.com>
2016-09-14 19:06:48 -0400

    Changed color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,14 @@
 
+$('#set-color').click(function(){
+  var $inputColor = $('#color-field').val();
+ $('.brush').css("background", $inputColor);
+
+});
+
+function changeColor(color, pixel) {
+  color = $("#color").val();
+  pixel.css("background-color", color);
+}
+$( "form" ).submit(function( event ) {
+  event.preventDefault();
+});
