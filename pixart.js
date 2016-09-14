6d39e8d1dde8f9b9d98d88f09db7a35abb2842c8
Alex Manno <alman92@gmail.com>
2016-09-14 19:06:43 -0400

    First Commit

--- pixart.js
+++ pixart.js
@@ -1,1 +1,12 @@
+var button = $("#set-color");
+var brush = $(".brush");
+var form = $("#form");
+var colorField = $("#color-field")
 
+function changeColor(){
+  var color = colorField.val()
+  brush.css("background-color" , color)
+  event.preventDefault();
+}
+
+button.on("click" , changeColor) 
