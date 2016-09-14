403c255cdc0e9992858f5f9d07acbdd17b51c81e
RYANBROPHY <BROPHY2321@ME.COM>
2016-09-14 18:08:50 -0400

    firstcommit

--- pixart.js
+++ pixart.js
@@ -1,1 +1,9 @@
 
+var setColor = $("#set-color")
+
+function changeColor (){
+  var color = $('#color-field').val()
+  $(".brush").css("background", color);
+  event.preventDefault()
+}
+setColor.on("click", changeColor)
