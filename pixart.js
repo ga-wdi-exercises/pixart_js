a2f86a18cc0ed20e7a26b0efc2557f1548ce169c
Tim Foley <thetimfoley@gmail.com>
2016-09-14 09:51:22 -0400

    created square divs

--- pixart.js
+++ pixart.js
@@ -1,6 +1,7 @@
 
 var $setColorButton = $('#set-color');
 $setColorButton.on('click',setColor);
+addSquares();
 
 function setColor(e) {
   e.preventDefault();
@@ -8,4 +9,17 @@ function setColor(e) {
   var color = $colorField.val();
   $('.brush').css('background', color);
   $colorField.val('');
+}
+
+function addSquares(){
+  var square = "<div class='square'></div>";
+  var squares = '';
+  // this feels SO sloppy, but I can't think of a better way just yet...
+  for (i = 0; i < 20; i++) {
+    squares += square;
+  }
+  console.log(squares);
+  var $squares = $(squares);
+  var $body = $('body');
+  $body.append($squares);
 }
