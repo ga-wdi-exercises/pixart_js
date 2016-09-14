83f4504bffa6695b13c65e3173eb17cd3fb0f902
Poh Chuan Kuan <pohckuan@yahoo.com>
2016-09-14 18:04:09 -0400

    commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,10 @@
+// var setColor = $("#set-color");
+// var inPut = $("#color-field");
 
+$("#set-color").on("click", changeBackground);
+function changeBackground(){
+  var color = $("#color-field").val();
+$(".brush").css("background", color);
+event.preventDefault()
+}
+// setColor.on("click",changeBackground);
