88263da2a330a75d17cd673f52051e6d317f44a8
Barrett Cope <barrettcope@gmail.com>
2016-09-14 18:05:57 -0400

    20 squares are working

--- pixart.js
+++ pixart.js
@@ -1,12 +1,16 @@
-// no changes needed for commit 2
 var input = $("#color-field");
 var button = $("#set-color");
 var brush = $(".brush");
 var form = $("#form");
+var body = $("body");
 
 var x = function(){
   color = input.val();
   brush.css("background",color);
+  for (var i = 0; i < 20; i++) {
+    newSquare = $("<div class='square'></div>");
+    body.append(newSquare);
+  }
 }
 
 form.submit(function(e) {
