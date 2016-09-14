5b1c276e065f4a554f4b9f492c1d0e67a03a2abf
William Keating <keating49@gmail.com>
2016-09-14 17:54:27 -0400

    step 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,14 @@
+ var inputColor = $("#color-field");
+ var button = $("#set-color");
+ var brush = $(".brush");
+ var form = $("#form");
 
+ var x = function(){
+   color = inputColor.val();
+   brush.css("background", color)
+ }
+form.submit(function(e) {
+  e.preventDefault();
+});
+
+button.on("click", x);
