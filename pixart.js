3d8d5a317835a595d52058615bb43d318ce94a63
Poh Chuan Kuan <pohckuan@yahoo.com>
2016-09-14 19:15:10 -0400

    commit 6

--- pixart.js
+++ pixart.js
@@ -8,12 +8,12 @@ $(".brush").css("background", color);
 event.preventDefault()
 }
 $("#set-color").on("click",changeBackground);
-for (i = 0; i <21; i+=1){
+for (i = 0; i < 8001; i+=1){
   var addDiv = $("<div class='square'></div>");
   $("body").append(addDiv);
 };
 
-$(".square").on("click", changeToGreen);
+$(".square").on("mouseover", changeToGreen);
 function changeToGreen(){
   var color = $("#color-field").val();
 $(this).css("background", color);
