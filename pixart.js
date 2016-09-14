8b635a6ea9bac285d5d9cb0362a33bd06aad2c3c
Alex Manno <alman92@gmail.com>
2016-09-14 19:23:26 -0400

    Third Commit

--- pixart.js
+++ pixart.js
@@ -9,4 +9,9 @@ function changeColor(){
   event.preventDefault();
 }
 
-button.on("click" , changeColor) 
+button.on("click" , changeColor)
+
+for(var i = 0; i < 20; i++){
+  var newDiv = $("<div class='square'></div>")
+  $("body").append(newDiv);
+}
