5c5c132172546d5d185a141a6611c2397ed19df8
Nick Oki <nick.oki@live.com>
2016-09-14 18:22:57 -0400

    Step 4: Added click listener for squares

--- pixart.js
+++ pixart.js
@@ -5,11 +5,13 @@ function setColor() {
 	$('.brush').css('background', ($('#color-field').val()));
 }
 
+// Click listener for Set Color
 $('#set-color').on('click', function(e) {
 	e.preventDefault();
 	setColor();
 })
 
+// Enter key listener for Set Color
 $('#set-color').keypress(function(e) {
 	var key = e.which;
 	if (key == 13) {
@@ -17,6 +19,13 @@ $('#set-color').keypress(function(e) {
 	}
 })
 
+// Create 20 .square divs
 for (var i = 0; i < 20; i++) {
 	$('body').append('<div class="square"></div>');
 }
+
+// Click listener for .square
+$('body .square').on('click', function(e) {
+	e.preventDefault();
+	$(this).css('background', 'green');
+})
