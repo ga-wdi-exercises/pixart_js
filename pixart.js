573ad9525c9db0f9a75700dfe282f4969a63cba5
Ian Hollands <ihollands@outlook.com>
2016-09-15 01:09:17 -0400

    commit 4

--- pixart.js
+++ pixart.js
@@ -1,16 +1,21 @@
+function createDivSquares() {
+  for (i=1;i<21;i++) {
+    $("body").append('<div class="square"></div>')
+  }
+}
+createDivSquares();
 
 var colorBox = $(".brush");
 var button = $("button");
+var newDivs = $("body div.square")
 
 var updateColor = function(e) {
   e.preventDefault();
   colorBox.css("background", $("input").val())
 }
 
-function createDivSquares() {
-  for (i=1;i<21;i++) {
-    $("body").append('<div class="square"></div>')
-  }
+var divToGreen = function() {
+  $(this).css("background", "green")
 }
 
 function stopReload(evt) {
@@ -18,3 +23,5 @@ function stopReload(evt) {
 }
 
 button.on('click', updateColor)
+
+newDivs.on('click', divToGreen)
