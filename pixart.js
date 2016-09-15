15927cb34f1942e0cd34a6df97e3f919df873f19
Mike Flores <michaelprflores@gmail.com>
2016-09-14 22:58:29 -0400

    Tried working on first commit problem

--- pixart.js
+++ pixart.js
@@ -1,1 +1,10 @@
+// for commit 1
+// when user enters hex color, store value
+// when user clicks button, div color changes
+
+$("#set-color")on.click(".brush", function() {
+    console.log("Ok")
+   // ("background","hex-value")
+})
+
 
