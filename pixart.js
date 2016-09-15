834df1ecebdb1298d8148a8d5d0634cdca77c568
brown2206 <brown.angelo.jr@gmail.com>
2016-09-15 03:04:32 -0400

    Commit 2

--- pixart.js
+++ pixart.js
@@ -1,4 +1,12 @@
+//Set Color functionality and event listener
 $("#set-color").on("click",function(epd){
+  var color = $("input").val()
+  $(".brush").css("background",color)
+  epd.preventDefault();
+})
+
+//set event on keypress enter
+$("#set-color").on("keypress","enter",function(epd){
   var color = $("input").val()
   $(".brush").css("background",color)
   epd.preventDefault();
