19fcafa30887b5b940705da7285fa31dd66303b7
manish pradhan <pradhanmanish33@gmail.com>
2016-09-15 02:12:32 -0400

    commit 3

--- pixart.js
+++ pixart.js
@@ -5,8 +5,13 @@ var color = $("input").val()
 $(".brush").css("background", color)
 
 })
-
 //The .val() method returns or sets the value attribute of the selected elements.
-
 //The .preventDefault() method cancels the event if it is cancelable,
 //meaning that the default action that belongs to the event will not occur.
+
+
+for (i = 1; i <= 20; i++) {
+  var newDiv = $("<div class = square></div");
+  $("body").append(newDiv)
+};
+//The .append() method inserts specified content at the end of the selected elements.
