9ec00e17db67277504e042b22a496746f0bbe451
manish pradhan <pradhanmanish33@gmail.com>
2016-09-15 03:27:16 -0400

    commit 4

--- pixart.js
+++ pixart.js
@@ -15,3 +15,11 @@ for (i = 1; i <= 20; i++) {
   $("body").append(newDiv)
 };
 //The .append() method inserts specified content at the end of the selected elements.
+
+
+$( ".square" ).on( "click", function() {
+  $(this).css("background", "green")
+  });
+//$(this) selector refers or stands for the current selected object in a loop or event.
+//For example, if you have an unordered list <ul> with five list items <li> inside and you run a click event for each list item,
+//when you click on a single item,  $(this) selector will refer exactly to that clicked item.
