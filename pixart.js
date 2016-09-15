7c5c242898d73eb5ccd042ffb05844607516a858
manish pradhan <pradhanmanish33@gmail.com>
2016-09-15 04:05:22 -0400

    commit 6

--- pixart.js
+++ pixart.js
@@ -10,16 +10,27 @@ $(".brush").css("background", color)
 //meaning that the default action that belongs to the event will not occur.
 
 
-for (i = 1; i <= 20; i++) {
+/*for (i = 1; i <= 20; i++) {
+  var newDiv = $("<div class = square></div");
+  $("body").append(newDiv)
+};*/
+for (i = 1; i <= 8000; i++) {
   var newDiv = $("<div class = square></div");
   $("body").append(newDiv)
 };
+
 //The .append() method inserts specified content at the end of the selected elements.
 
 
-$( ".square" ).on( "click", function() {
+/*$( ".square" ).on( "click", function() {
   $(this).css("background", "green")
   });
+*/
+$( ".square" ).on( "mouseover", function() {
+     var boxColor = $("input").val()
+    $(this).css("background", boxColor)
+    });
+
 //$(this) selector refers or stands for the current selected object in a loop or event.
 //For example, if you have an unordered list <ul> with five list items <li> inside and you run a click event for each list item,
 //when you click on a single item,  $(this) selector will refer exactly to that clicked item.
@@ -29,3 +40,5 @@ $( ".square" ).click(function(event) {
   var boxColor = $("input").val()
   $(this).css("background", boxColor)
   });
+
+//used same code as first one.
