94ee1048c0ab4338b9a1ed3fc0e2eba861e65ed8
manish pradhan <pradhanmanish33@gmail.com>
2016-09-15 01:34:30 -0400

    commit 2

--- pixart.js
+++ pixart.js
@@ -1,6 +1,12 @@
 
-$('#set-color').on('click', function(event){
-var color = $('input').val()
-$('.brush').css('background', color)
+$("#set-color").click(function(event){
 event.preventDefault();
+var color = $("input").val()
+$(".brush").css("background", color)
+
 })
+
+//The .val() method returns or sets the value attribute of the selected elements.
+
+//The .preventDefault() method cancels the event if it is cancelable,
+//meaning that the default action that belongs to the event will not occur.
