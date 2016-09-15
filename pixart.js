057c6eeee0ea1f29e3d606b77fcb10662ce9dc59
Ian Hollands <ihollands@outlook.com>
2016-09-15 00:26:41 -0400

    commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,15 @@
 
+var colorBox = $(".brush");
+//var input = $("input").val();
+var button = $("button");
+
+var updateColor = function(e) {
+  e.preventDefault();
+  colorBox.css("background", $("input").val())
+}
+
+
+function stopReload(evt) {
+        evt.preventDefault();
+}
+button.on('click', updateColor)
