54c9f4424ff3a29102afa774bea1969664244ef5
Mike Flores <michaelprflores@gmail.com>
2016-09-16 15:31:47 -0400

    worked with Jesse to make this work

--- pixart.js
+++ pixart.js
@@ -2,9 +2,19 @@
 // when user enters hex color, store value
 // when user clicks button, div color changes
 
-$("#set-color")on.click(".brush", function() {
-    console.log("Ok")
-   // ("background","hex-value")
+$("#set-color").on("click", function(event) {
+    event.preventDefault()
+    var color = $("#color-field").val()
+    console.log(color)
+    $(".brush").css("background", color)
 })
 
+for (var i = 0; i < 8000; i++){
+    $("body").append("<div class='square'></div>")
+}
+
+$(".square").on("mouseover", function() {
+    var color = $("#color-field").val()
+    $(this).css("background", color)
+})
 
