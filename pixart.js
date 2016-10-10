f714ffe3ec87b2180d32498c694a2f23b3e3f80c
Robert Zehnder <robzcodes@gmail.com>
2016-10-10 14:10:42 -0400

    Adds Step 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,12 @@
+//Example code - modify
+$("#set-color").on("click", function(){
+  event.preventDefault();
+  var x = $('#color-field').select().val();
+    $('.brush').css('background-color', x);
+}).preventDefault();
 
+// $('#usersSearch').keypress(function(e){
+//   event.preventDefault();
+//   var x = $('#color-field').select().val();
+//     $('.brush').css('background-color', x);
+// }).preventDefault();
