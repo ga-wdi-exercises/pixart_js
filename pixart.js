bb407a09a4a16437a87f03136026b30f3ff018de
Robert Zehnder <robzcodes@gmail.com>
2016-10-10 16:35:22 -0400

    adds commit 5

--- pixart.js
+++ pixart.js
@@ -1,22 +1,35 @@
 
 
-//Commit 1
-$("#set-color").on("click", function(){
-  event.preventDefault();
-  var x = $('#color-field').select().val();
-    $('.brush').css('background-color', x);
-});
 
-//Commit 2
-$('#usersSearch').keypress(function(e){
-  event.preventDefault();
-  var x = $('#color-field').select().val();
-    $('.brush').css('background-color', x);
-});
 
-//Commit 3
+
 $(document).ready(function() {
+
+  //Commit 1
+  $("#set-color").on("click", function(){
+    event.preventDefault();
+    var x = $('#color-field').select().val();
+      $('.brush').css('background-color', x);
+  });
+
+  //Commit 2
+  $('#usersSearch').keypress(function(e){
+    event.preventDefault();
+    var x = $('#color-field').select().val();
+      $('.brush').css('background-color', x);
+  });
+
+  //Commit 3
     for (i=0;i<20;i++) {
       $("body").append("<div class='square'></div>");
     }
+
+  //Commit 4
+  $('.square').on('click', function () {
+    $(this).css('background-color', 'green');
+  });
+
+  //Commit 5
+  
+
 });
