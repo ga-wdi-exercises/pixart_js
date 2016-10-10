25de43ab0650d0e50012bfb77d459d094cf5584a
Travis Batts <Travisbatts6@gmail.com>
2016-10-10 18:10:16 -0400

    Initial commit

--- pixart.js
+++ pixart.js
@@ -1,1 +1,12 @@
 
+
+$(document).ready(function(){ //To start a document
+  var setColor = $("#set-color");
+
+  setColor.on("click", changeColor);
+
+  function changeColor(){
+    $(".brush").css("background", $("#color-field").val())
+    event.preventDefault();
+  }
+})
