9cf4b6b75c2827dd5fbf30c51372179b4bacc856
Robert Zehnder <robzcodes@gmail.com>
2016-10-10 18:43:05 -0400

    Adds bonus step (without changing the paint brush

--- pixart.js
+++ pixart.js
@@ -4,32 +4,65 @@
 
 
 $(document).ready(function() {
+  var color;
+  var storedColors = ['white','white','white'];
+//storedColors[0);
+  $('#brush1').css('background-color', storedColors[0]);
+  $('#brush2').css('background-color', storedColors[1]);
+  $('#brush3').css('background-color', storedColors[2]);
 
   //Commit 1
   $("#set-color").on("click", function(){
     event.preventDefault();
-    var x = $('#color-field').select().val();
-      $('.brush').css('background-color', x);
+    color = $('#color-field').select().val();
+    if (color === ""){
+      alert('Please enter a valid color name, hex value, or rgb value');
+    }
+    else {
+      $('.brush').css('background-color', color);
+      changeColor();
+    }
   });
+
+    $(".brush").on("click", function(){
+      event.preventDefault();
+      color = $(this).css('background-color');
+      changeColor();
+  });
+
+  function changeColor() {
+    storedColors.unshift(color);
+    storedColors.pop();
+    $('#brush1').css('background-color', storedColors[0]);
+    $('#brush2').css('background-color', storedColors[1]);
+    $('#brush3').css('background-color', storedColors[2]);
+  }
 
   //Commit 2
   $('#usersSearch').keypress(function(e){
     event.preventDefault();
-    var x = $('#color-field').select().val();
-      $('.brush').css('background-color', x);
+    color = $('#color-field').select().val();
+      $('.brush').css('background-color', color);
   });
 
-  //Commit 3
-    for (i=0;i<20;i++) {
+  //Commit 3/6
+    for (i=0;i<8000;i++) {
       $("body").append("<div class='square'></div>");
     }
 
-  //Commit 4
-  $('.square').on('click', function () {
-    $(this).css('background-color', 'green');
+  //Commit 4/5
+  $('.square').mouseover(function () {
+    event.preventDefault();
+    color = $('#color-field').select().val();
+
+    if (color === ""){
+      $(this).css('background-color', '#E7E5DB');
+    }
+    else {
+      $(this).css('background-color', color);
+    }
+
+  //Commit Bonus
+
   });
-
-  //Commit 5
-  
-
 });
