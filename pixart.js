989e3c1d09bbd6bbb0cecf51cf498ea8dee60e14
Kevin Herrera <1herrerake@gmail.com>
2016-10-10 18:10:12 -0400

    commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,14 @@
+$(document).ready(function(){
+var setColor = $("#set-color");
 
+setColor.on("click", changeColor);
+function changeColor(){
+  $(".brush").css("background",$("#color-field").val());
+  event.preventDefault()
+}
+
+
+
+
+
+});
