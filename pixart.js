f6415cda8e8bcd09360027370ac557d4c2bd0abf
Sam Mans <smans123@gmail.com>
2016-10-10 18:09:07 -0400

    commit 1 color changes and page refreshes

--- pixart.js
+++ pixart.js
@@ -1,1 +1,17 @@
+var button = $("#set-color");
 
+
+button.on("click", colorCheck);
+button.on("click", preventReset);
+
+//*checks for color
+function colorCheck() {
+// alert($("input").val());
+$(".brush").css("background", $("input").val());
+
+}
+
+function preventReset() {
+  event.preventDefault();
+
+}
