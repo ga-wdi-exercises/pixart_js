488192f8003c530df64f19b860d60b52293eaa64
Robert Zehnder <robzcodes@gmail.com>
2016-10-10 14:35:58 -0400

    Adds Step 3

--- pixart.js
+++ pixart.js
@@ -1,12 +1,22 @@
-//Example code - modify
+
+
+//Commit 1
 $("#set-color").on("click", function(){
   event.preventDefault();
   var x = $('#color-field').select().val();
     $('.brush').css('background-color', x);
-}).preventDefault();
+});
 
+//Commit 2
 $('#usersSearch').keypress(function(e){
   event.preventDefault();
   var x = $('#color-field').select().val();
     $('.brush').css('background-color', x);
-}).preventDefault();
+});
+
+//Commit 3
+$(document).ready(function() {
+    for (i=0;i<20;i++) {
+      $("body").append("<div class='square'></div>");
+    }
+});
