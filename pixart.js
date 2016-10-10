09f232f9b019ce23419c2527824a6ef241ebdf37
Kevin Herrera <1herrerake@gmail.com>
2016-10-10 18:49:14 -0400

    commit 2

--- pixart.js
+++ pixart.js
@@ -5,10 +5,10 @@ setColor.on("click", changeColor);
 function changeColor(){
   $(".brush").css("background",$("#color-field").val());
   event.preventDefault()
+
 }
 
-
-
-
-
+for(i=0; i<20; i++){
+    $("body").append(("<div class=square></div>"));
+}
 });
