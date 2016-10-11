8951e0fb9d4121b9a7449d05c941c08b05342b97
Jonathan Grunewald <jonathan.grunewald1@gmail.com>
2016-10-10 21:00:49 -0400

    adds functionality for pixel art

--- pixart.js
+++ pixart.js
@@ -1,7 +1,4 @@
-// $(document).ready(function(){
-// 	console.log("hello world")
-// })
-
+$(document).ready(function(){
 
 
 var color = $("#color-field").val();
@@ -12,4 +9,15 @@ function changeColor() {
 	event.preventDefault();
 }
 
+function updatePixel() {
+	$(this).css("background-color", $("#color-field").val())
+}
+
 $("#set-color").on("click", changeColor);
+
+for (i = 0; i < 2000; i++){
+	$("body").append("<div class='square'></div>");
+	$(".square").on("mouseover", updatePixel)
+}
+}
+)
