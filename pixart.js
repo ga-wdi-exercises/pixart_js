ea56ff9aa5f6a7c1b6ac8536fab477b2da1695b9
Alex Wandres <alex.r.wandres@gmail.com>
2016-10-11 02:21:51 -0400

    stage1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,13 @@
+var button = $("button")
+var colorInput
+var brush = $(".brush");
 
+var colorSet= function() {
+  var colorInput = $("#color-field").val();
+  brush.css("background", colorInput)
+};
+
+button.on("click", colorSet)
+button.click(function(event) {
+  event.preventDefault();
+});
