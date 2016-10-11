81541976369fef10c23329287f6ef82be5483e25
Thinh Nguyen <nguyendthinh@outlook.com>
2016-10-10 21:06:38 -0400

    set color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,10 @@
+var input = $("#color-field");
+var button = $("#set-color");
+var brush = $(".brush");
 
+function changeColor() {
+  brush.css("background", input.val());
+  event.preventDefault();
+}
+
+button.on("click", changeColor);
