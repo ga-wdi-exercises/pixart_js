acc2f52d0c211464f444d348db6685eaa0372d7b
Rob Romaniw <robertromaniw@gmail.com>
2016-10-10 21:42:12 -0400

    Commit 4

--- pixart.js
+++ pixart.js
@@ -1,17 +1,20 @@
-
-$("#set-color").on("click", function(){
+$("#set-color").on("click", function() {
     event.preventDefault();
-    var x = $('#color-field').select().val();
-      $('.brush').css('background-color', x);
-  });
+    var color = $('#color-field').select().val();
+    $('.brush').css('background-color', color);
+});
 
 
-  $("#set-color").on("keypress", "return", function(){
-      event.preventDefault();
-      var x = $('#color-field').select().val();
-        $('.brush').css('background-color', x);
-    });
+$("#set-color").on("keypress", "return", function() {
+    event.preventDefault();
+    var color = $('#color-field').select().val();
+    $('.brush').css('background-color', color);
+});
 
-for (i=0; i<20; i++) {
-$("body").append("<div class = 'square'></div>");
+for (i = 0; i < 20; i++) {
+    $("body").append("<div class = 'square'></div>");
 }
+
+$(".square").on("click", function() {
+    $(this).css("background", $("#color-field").val());
+});
