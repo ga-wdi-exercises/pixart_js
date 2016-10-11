33cb266f4f8bc8e3b6244d831bedfe8a3873161b
AdrienneD <adriennedao@gmail.com>
2016-10-11 18:37:03 -0400

    initial commit

--- pixart.js
+++ pixart.js
@@ -1,1 +1,27 @@
+$(document).ready(function() {
+  var button = $("#set-color");
+  var brush = $(".brush");
+  var input = $("input");
 
+  console.log(button);
+
+  button.on("click", setColorButton);
+  //doSomething() - invoking a function
+  //doSomething is a reference
+
+  function setColorButton() {
+    brush.css("background-color", input.val());
+    event.preventDefault();
+  }
+
+});
+// $("button").click(function(){
+//     $("p").css("color", "red");
+// });
+//
+//
+//
+//
+//
+
+ 
