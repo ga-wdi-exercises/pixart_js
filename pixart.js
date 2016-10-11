c85c822dc933ade77b8b70b565597d903da546c0
StefKabatova <stefkabatova@gmail.com>
2016-10-10 22:21:25 -0400

    changed default on form submission n functionality to button

--- pixart.js
+++ pixart.js
@@ -1,1 +1,24 @@
+// 1 commit - on click of the set color button change color of brush box to color in the input field
+$(document).ready(function(){
 
+  var colorbutton = $("#set-color");
+  var newColor;
+
+// prevents form from submitting
+  $( "form" ).submit(function( event ) {
+  event.preventDefault();
+});
+
+// changes the color of brush to new color by calling another function
+function colorChanger(){
+  $(".brush").css("background", colorSetr);
+}
+  // gets color from input field
+   function colorSetr(){
+       return $("#color-field").val();
+
+  }
+
+    colorbutton.on("click",colorChanger);
+
+})
