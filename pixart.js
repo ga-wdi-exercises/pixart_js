2aa167fe6eb459ccf72bcbb484486b89e0a1a719
Juliet Berman <berman.juliet@gmail.com>
2016-10-10 21:34:44 -0400

    adds event listener to change color selection

--- pixart.js
+++ pixart.js
@@ -1,1 +1,19 @@
+$( document ).ready(function() {
+  console.log("hello jquery")
 
+
+$("#set-color").on("click", changeColor);
+
+function changeColor() {
+        event.preventDefault();
+        var color = $("input").val();
+        if (color !==""){
+          $(".brush").css("background-color", color);
+          $("input").val("");
+          }
+        else {
+          alert("Please submit a valid response.");
+        }
+      };
+      
+});
