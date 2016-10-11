da85e79c138c2d5d85233a3e011c36d89942fa1b
robert <robert.k25@gmail.com>
2016-10-10 20:28:33 -0400

    adds ability to set color with button

--- pixart.js
+++ pixart.js
@@ -1,1 +1,22 @@
+$(document).ready(function() {
+  var button = $('button');
 
+  button.on("click", setColor);
+
+
+  //need to prevent the reloading default
+
+  function setColor(event) {
+    event.preventDefault() //event automatically passed?
+    var inputColor = $("#color-field").val();
+    $(".brush").css('background-color', inputColor);
+  }
+
+});
+
+  // $("#set-color").click(function(event) {
+  // event.preventDefault();
+  // var inputColor = $('#color-field').val();
+  // console.log(inputColor);  //sets properly
+  // $(".brush").css({background-color: inputColor});
+  // })
