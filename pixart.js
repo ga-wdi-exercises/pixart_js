fa19cdc25113282efffa5dc488a43c9b04c3a778
Sam Catherman <Sam.Catherman@gmail.com>
2016-10-10 20:34:17 -0400

    commit #1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,13 @@
+$(document).ready(function(){
 
+var setColor = $("#set-color");
+var brush = $(".brush");
+var colorField = $("#color-field");
+
+setColor.on("click", changeColor);
+
+function changeColor (){
+  brush.css("background", colorField.val());
+  event.preventDefault();
+  }
+});
