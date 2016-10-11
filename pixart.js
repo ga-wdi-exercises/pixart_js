046c3053f15158b7a961f3d58db78aac19420eeb
Drew Swinburne <drewswinburne@gmail.com>
2016-10-10 20:48:30 -0400

    add event listener & input

--- pixart.js
+++ pixart.js
@@ -1,1 +1,20 @@
+$(document).ready(function(){
 
+var button = $("button");
+var brush = $(".brush");
+// var handleClickEvent = function(e){
+//   e.preventDefault();
+
+button.on("click", function(e) {
+  // var color = $("#form").submit();
+  e.preventDefault();
+});
+
+
+  button.on("click", function() {
+    var color = $( "input" ).val();
+    brush.css("background", color);
+  })
+
+
+})
