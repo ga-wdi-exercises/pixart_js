13566ee404f814c13de4596ba32e17d0a47a6dfb
YOUR FULL NAME <THE_EMAIL_YOU_USE_FOR_GITHUB@EMAIL.COM>
2016-10-11 18:20:13 -0400

    V's Pixart-js

--- pixart.js
+++ pixart.js
@@ -1,1 +1,22 @@
+var button = $("#set-color");
+var mouseBrush = $(".brush");
+var inputBox = $("#color-field");
 
+button.on("click", changeColor);
+
+function changeColor() {
+  mouseBrush.css("background", inputBox.val());
+  event.preventDefault();
+}
+
+$("body").append("<div class=square></div>");
+
+for (i = 0; i < 8000; i++) {
+  $("body").append("<div class=square></div>");
+}
+
+$(".square").on("mouseover", changeToGreen);
+
+function changeToGreen() {
+  $(this).css("background", inputBox.val());
+}
