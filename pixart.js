6c8987f8d26d36b6098e6370e97d6e381a52e0b2
Drew Swinburne <drewswinburne@gmail.com>
2016-10-10 22:10:22 -0400

    add event listener to small squares

--- pixart.js
+++ pixart.js
@@ -2,14 +2,25 @@ $(document).ready(function(){
 
 var button = $("button");
 var brush = $(".brush");
+// var lilSquare = $(".square")
 // var div = $("<div/>").addClass("square")
 // var handleClickEvent = function(e){
 //   e.preventDefault();
+// $("body").on("click", "div", function() {
+//   this.css("background", "green")}
+// );
+
+
 
 button.on("click", function(e) {
   // var color = $("#form").submit();
   e.preventDefault();
 });
+
+// lilSquare.on("click," function() {
+//   this.css("background", "green")
+//
+// })
 
 
   button.on("click", function() {
@@ -17,9 +28,15 @@ button.on("click", function(e) {
     brush.css("background", color);
   })
 
+function changeGreen() {
+  $(this).css("background", "green");
+}
+
+
 var i = 0;
 do {
   $("body").append('<div class="square"></div>');
+  $(".square").on("click", changeGreen);
   i++;}
   while (i < 20);
 
