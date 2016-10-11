c604e09dfae5e0aaa19ccb451323b3e94c5d2489
Angel Valant <angelgithub@gmail.com>
2016-10-10 20:54:54 -0400

    commit 1 updates pixart.js

--- pixart.js
+++ pixart.js
@@ -1,1 +1,19 @@
+$(document).ready(function() {
+  console.log("js file is attached");
 
+// When I click the "Set Color" button, it should change the color of the "brush" box to the color I specify in the input field.
+  var setColor = $("#set-color"); //set color button
+
+  var colorField = $("#color-field").val(); //input field
+
+  var brushBox = $(".brush") //brush box
+
+  function handleClickEvent () {
+    $("brushBox").addClass("colorField");
+    event.preventDefault();
+
+  }
+
+  $("setColor").on("click", handleClickEvent);
+
+})
