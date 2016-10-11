b915e8930216e5500b54bf3e4095b9da7c75bfdc
Jonathan Grunewald <jonathan.grunewald1@gmail.com>
2016-10-10 20:14:01 -0400

    commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,15 @@
+// $(document).ready(function(){
+// 	console.log("hello world")
+// })
 
+
+
+var color = $("#color-field").val();
+
+
+function changeColor() {
+	$(".brush").css("background-color", $("#color-field").val());
+	event.preventDefault();
+}
+
+$("#set-color").on("click", changeColor);
