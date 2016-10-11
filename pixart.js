13401d0f506376a0028593e15ee161a4c43620df
Juliet Berman <berman.juliet@gmail.com>
2016-10-11 14:12:32 -0400

    final game with slight revisions in code to promote efficiency

--- pixart.js
+++ pixart.js
@@ -1,13 +1,23 @@
 $( document ).ready(function() {
-  console.log("hello jquery")
+
+console.log("hello jquery")
+
+
+var swatchColors = [];
+var color;
 
 $("#set-color").on("click", changeColor);
 $("#set-color").keyup(changeColor);
-$("#erase").on("click", eraseColor);
-$(".swatch").on("click", swatchColor);
 
-var swatch = [];
-var color;
+$("#erase").on("click", function(){
+  event.preventDefault();
+  color = "#E7E5DB";
+});
+
+$(".swatch").on("click", function(){
+  color=$(this).css("background-color");
+}
+);
 
 function changeColor() {
         event.preventDefault();
@@ -16,7 +26,11 @@ function changeColor() {
         if (color !==""){
           $(".brush").css("background-color", color);
           $("input").val("");
-          // push color value into swatch array and change div background-color in a loop
+          swatchColors.unshift(color);
+          $("#swatchOne").css("background-color", swatchColors[0]);
+          $("#swatchTwo").css("background-color", swatchColors[1]);
+          $("#swatchThree").css("background-color", swatchColors[2]);
+          // console.log(swatchColors);
           }
         else {
           alert("Please submit a valid response.");
@@ -31,14 +45,16 @@ function pixelColor() {
   $(this).css("background-color", color);
 };
 
-function eraseColor() {
-  event.preventDefault();
-  color= "#E7E5DB";
-};
 
-function swatchColor(){
+// function swatch(){
+//   swatchColors.unshift(color);
+//   $("#brushOne").css("background-color", swatchColors[0]);
+//   $("#brushTwo").css("background-color", swatchColors[1]);
+//   $("#brushThree").css("background-color", swatchColors[2]);
+//   console.log(swatchColors);
+
   // insert code here so that:
   // color = value of specific div background-color
-};
+// };
 
 });
