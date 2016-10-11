6bdfaed3e9e5c9b3c105c2689969dec685297bb9
Sam Catherman <Sam.Catherman@gmail.com>
2016-10-10 21:08:51 -0400

    4th commit

--- pixart.js
+++ pixart.js
@@ -4,11 +4,19 @@ var setColor = $("#set-color");
 var brush = $(".brush");
 var colorField = $("#color-field");
 
+// setColor.on("click", changeColor);
+
+// function changeColor (){
   brush.css("background", colorField.val());
   event.preventDefault();
+//   }
+//   colorField.on("keyup", changeColor);
 
-var body = $("body");
-for (var i=0; i<20; i++) {
-body.append("<div class="square"></div>");
+for (i=0; i<20; i++) {
+  $("body").append("<div class=square></div>");
   }
+var square = $(".square");
+square.on("click", function(){
+  $(this).css("background", "green");
+})
 });
