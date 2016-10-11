bb41ea6413ea8373f544efcafe56bb1650a9cdeb
Juliet Berman <berman.juliet@gmail.com>
2016-10-10 23:13:53 -0400

    Juliet Berman Pixel Art HW

--- pixart.js
+++ pixart.js
@@ -3,28 +3,42 @@ $( document ).ready(function() {
 
 $("#set-color").on("click", changeColor);
 $("#set-color").keyup(changeColor);
+$("#erase").on("click", eraseColor);
+$(".swatch").on("click", swatchColor);
 
+var swatch = [];
 var color;
 
 function changeColor() {
         event.preventDefault();
         color = $("input").val();
+
         if (color !==""){
           $(".brush").css("background-color", color);
           $("input").val("");
+          // push color value into swatch array and change div background-color in a loop
           }
         else {
           alert("Please submit a valid response.");
         }
       };
 
-for (var i = 0; i< 20; i++){
-$("<div>",{"class":"square"}).appendTo("body").on("click", pixelColor);
+for (var i = 0; i< 8000; i++){
+$("<div>",{"class":"square"}).appendTo("body").on("mouseover", pixelColor);
 };
 
 function pixelColor() {
   $(this).css("background-color", color);
 };
 
+function eraseColor() {
+  event.preventDefault();
+  color= "#E7E5DB";
+};
+
+function swatchColor(){
+  // insert code here so that:
+  // color = value of specific div background-color
+};
 
 });
