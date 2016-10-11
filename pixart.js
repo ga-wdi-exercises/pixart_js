2f378aaedb4dd0a2d7f2244bac3f096392ac3129
Ba Van Phan <bavphan@gmail.com>
2016-10-11 08:37:57 -0400

    stage 1 commit

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
+$(document).ready(function(){
+  console.log("YOOOO")
 
+  var colorInput = $("#color-field").val();
+
+  function changeColor (){
+    $(".brush").css("background-color", $("#color-field").val());
+    event.preventDefault();
+
+  }
+})
