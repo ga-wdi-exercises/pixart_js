3ce5b2c2787e834e76069d4c08a1e7c780edd4a5
Diana B. Gobar <dianabgobar@gmail.com>
2016-10-12 00:09:10 -0400

    color input fixed

--- pixart.js
+++ pixart.js
@@ -2,7 +2,16 @@ console.log("hello!")
 
 var color = $("color-field").val();
 
+$("#set-color").on("click", colorChange);
+
+
+
 function colorChange (){
-$(".brush").css("background", color);
-  var color = $("")
+  $(".brush").css("background-color", $("#color-field").val());
+  event.preventDefault(); //saves color..val(), for input field
+}
+
+function colorInput (){
+  $(this).css("background-color", $("color-field").val());
+//this, used to describe colorInput
 }
