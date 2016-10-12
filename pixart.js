6c6ac4a9163eee9cc6e5898b9e88285ae3abc620
Angel Valant <angelgithub@gmail.com>
2016-10-11 21:49:42 -0400

    fixes pixart.js

--- pixart.js
+++ pixart.js
@@ -1,42 +1,40 @@
 $(document).ready(function() {
   console.log("js file is attached");
 
-// When I click the "Set Color" button, it should change the color of the "brush" box to the color I specify in the input field.
   var setColor = $("#set-color"); //set color button
+  var colorField = $("#color-field"); //input field
+  var brushBox = $(".brush"); //brush box
+  var squareBox = $(".square");
 
-  var colorField = $("#color-field").val(); //input field
-
-  var brushBox = $(".brush") //brush box
-
-  var squareBox = $(".square"); //square box in css
-
-  function handleClickEvent () {
-    $("brushBox").css("background-color", "colorField");
+  //function for changing color of brush box
+  function handleClickEvent() {
     event.preventDefault();
-
+    brushBox.css("background", colorField.val());
+    console.log(colorField.val());
   }
 
-  //enter key being pressed
-  $("colorField").keypress(function(event){
-      var keycode = (event.keyCode ? event.keyCode : event.which);
-      if(keycode == '13'){
-        $("brushBox").css("background-color", "colorField");
-        event.preventDefault();
-      }
+  // enter key being pressed
+  colorField.keypress(function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+    event.preventDefault();
+    brushBox.css("background-color", colorField.val());
+    }
   });
 
+  //function that executes for loop for drawing area
   function count() {
-    for (var i = 8000; i > 0; i -- ) {
+    for (var i = 0; i < 8000; i++) {
     $("body").append("<div class=square></div>");
     }
   }
 
-  $("squareBox").on("mouseover", function() {
-    $(this).css("background-color", "colorField");
-
+  //click on square and it fills with color--not working
+  squareBox.on("mouseover", function() {
+    $(this).css("background", colorField.val());
   });
 
-  $("setColor").on("click", handleClickEvent);
-  count();
+  setColor.on("click", handleClickEvent); //button
+  count(); //runs loop for drawing area
 
-})
+});
