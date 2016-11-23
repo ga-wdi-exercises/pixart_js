682c965a0f09120bd9ec1ea92e698926dced2d4e
dmozzoni <cyric@hotmail.com>
2016-11-22 23:59:28 -0500

    click on square and set color

--- pixart.js
+++ pixart.js
@@ -1,5 +1,5 @@
 
-
+let obj = {selColor: ""};
 
 $('form').on('submit', function(e){
   e.preventDefault();
@@ -7,9 +7,14 @@ $('form').on('submit', function(e){
   var input = $('input').eq(0).val();
   console.log(input);
   $(".brush").css("background-color",input);
+  obj.selColor = input;
 });
 
 for (let i=0;i<20;i++) {
   $("body").append("<div class='square'></div>");
-
 }
+
+$(".square").on("click", function() {
+  $(this).css("background-color",obj.selColor);
+
+});
