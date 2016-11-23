078fd19645a190c2ec62219e044002810e907fc9
dmozzoni <cyric@hotmail.com>
2016-11-22 23:47:11 -0500

    20 square divs

--- pixart.js
+++ pixart.js
@@ -1,1 +1,15 @@
 
+
+
+$('form').on('submit', function(e){
+  e.preventDefault();
+  console.log('form submitted');
+  var input = $('input').eq(0).val();
+  console.log(input);
+  $(".brush").css("background-color",input);
+});
+
+for (let i=0;i<20;i++) {
+  $("body").append("<div class='square'></div>");
+
+}
