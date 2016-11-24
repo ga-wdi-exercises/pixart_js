b5640ee0c9cd9da070781f93e6995b9329dc99a6
luke popwell <lukepopwell@mac.com>
2016-11-24 12:11:00 -0600

    commit 1. background changes with button press

--- pixart.js
+++ pixart.js
@@ -1,1 +1,15 @@
+$(function(){
+	console.log('js works');
 
+	$('button').click(function(e) {
+	    e.preventDefault();
+	});
+
+	$('button').on('click', function(){
+		console.log(' clicked on color-field');
+
+		var color = $('#color-field').val();
+		$('.brush').css("background-color", color);
+
+	});
+});
