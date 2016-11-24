b336ac421c676de2b94a038ba6f5a9c0cf2bf808
luke popwell <lukepopwell@mac.com>
2016-11-24 14:22:34 -0600

    added mouseover and painted a picture

--- pixart.js
+++ pixart.js
@@ -9,29 +9,17 @@ $(function(){
 
 		var color = $('#color-field').val();
 		$('.brush').css("background-color", color);
-
-
 		});
 	});
 
-	// var div = document.createElement('div');
-	// document.body.appendChild(div);
+	for (var i=1; i<=8000; i++){
+		$('body').append('<div class="square">');
+	}
 
-	$(document).ready(function(){
-		for (var i=1; i<=20; i++){
-			$('body').append('<div class="square">');
-		}
-
-		$(".square").on("click", function(){
-			var color = $('#color-field').val();
-			$(this).css("background-color", color + $(this).val());
+	$(".square").on("mouseover", function(){
+		var color = $('#color-field').val();
+			$(this).css("background-color", color);
 			console.log("squares are clicked");
 		});
 
-	});
-});
-
-
-$('#activite_color').on('keyup', function() {
-    $('.col-lg-1').css('background', '#' + $(this).val());
 });
