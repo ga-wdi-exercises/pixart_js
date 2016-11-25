21a7451756e3ba18eb3b5ec1dbbd7efefba2c000
awanderlyss <alyssa.felix7@gmail.com>
2016-11-25 18:58:06 -0500

    Change brush box to specified color in jquery

--- pixart.js
+++ pixart.js
@@ -1,1 +1,14 @@
+// setColor equal to submit button
+const getColor = $('#set-color');
+// colorInput equal to input field
+const colorInput = $('#color-field');
 
+// Change the color of the brush box
+const setBrushColor = function (e){
+  // Prevents the page from refreshing
+  e.preventDefault();
+  let brushColor = $('.brush');
+  brushColor.css("background", colorInput.val());
+};
+
+getColor.on("click", setBrushColor);
