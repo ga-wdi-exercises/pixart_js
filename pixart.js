3736c9f6c0def933ffc9e014c806e87a600be4c9
dfwg4@hotmail.com <dfwg4@hotmail.com>
2016-11-26 17:15:23 -0500

    step two

--- pixart.js
+++ pixart.js
@@ -1,5 +1,5 @@
 //('#set-color') of the brush to color of specified input ("#color-field")
-
+var brush = document.getElementByClassName("brush");
 
 
 //EventListener(){
@@ -11,5 +11,6 @@
 document.addEventListener('click', colChaFunction);
 colChaFunction(){
   document.getElementById("color-field")
-  //and make document.brush = background = ('color-field')
+  //and make document.brush.background = ('color-field')
 };
+//need to have the input field become equal to the brush
