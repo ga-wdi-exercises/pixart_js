831e179e4d72f2d39cc5be0fbfabbf47298085e5
erikslewis <Adelina198527@gmail.com>
2016-11-26 12:06:35 -0500

    partOne

--- pixart.js
+++ pixart.js
@@ -1,1 +1,10 @@
+let form = $("#form");
+let button = $("#set-color");
+let brush = $(".brush");
+let input = $("#color-field");
 
+
+form.on("submit", function(event){
+  event.preventDefault();
+  brush.css('background-color', input.val());
+})
