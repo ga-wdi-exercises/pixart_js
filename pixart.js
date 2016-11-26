1c1df4274803e82313bfd904dfe6afa82510e597
dfwg4@hotmail.com <dfwg4@hotmail.com>
2016-11-26 16:48:29 -0500

    psuh 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,15 @@
+//('#set-color') of the brush to color of specified input ("#color-field")
 
+
+
+//EventListener(){
+//$(the thing.click (function(event){
+  //event.preventDefault();
+//});
+//};
+
+document.addEventListener('click', colChaFunction);
+colChaFunction(){
+  document.getElementById("color-field")
+  //and make document.brush = background = ('color-field')
+};
