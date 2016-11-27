21a7a010bf5f1d9195cee5871d430f48a0c4c099
jtellis <jessetellis@gmail.com>
2016-11-27 14:32:48 -0500

    Make Set Color button assign input color to brush control

--- pixart.js
+++ pixart.js
@@ -1,1 +1,10 @@
+$(function(){
+  let $colorField = $("#color-field");
+  let $setColor = $("#set-color");
+  let $controlsBrush = $(".controls > .brush");
 
+  $setColor.on('click', function(event) {
+    event.preventDefault();
+    $controlsBrush.css('background-color', $colorField.val());
+  });
+});
