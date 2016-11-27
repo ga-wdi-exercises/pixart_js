c1d51e0cfc8248afb99692aa2090c29bb501b203
jtellis <jessetellis@gmail.com>
2016-11-27 15:22:59 -0500

    Selection color swatches for 3 most recent colors

--- pixart.js
+++ pixart.js
@@ -1,18 +1,33 @@
 $(function(){
   let $colorField = $("#color-field");
   let $setColor = $("#set-color");
-  let $controlsBrush = $(".controls > .brush");
+  let $brushes = $('.brush');
+  let $firstBrush = $brushes.first();
+  let $secondBrush = $firstBrush.next();
+  let $thirdBrush = $secondBrush.next();
+  let brushColor = $colorField.val();
 
   $setColor.on('click', function(event) {
     event.preventDefault();
-    $controlsBrush.css('background-color', $colorField.val());
+    $thirdBrush.css('background-color', $secondBrush.css('background-color'));
+    $secondBrush.css('background-color', $firstBrush.css('background-color'));
+    $firstBrush.css('background-color', $colorField.val());
+    $brushes.removeClass('selected');
+    $firstBrush.addClass('selected');
+    brushColor = $colorField.val();
+  });
+
+  $('.brush').on('click', function(event) {
+    $brushes.removeClass('selected');
+    $(this).addClass('selected');
+    brushColor = $(this).css('background-color');
   });
 
   for(let i = 0; i < 8000; i++) {
     $('body').append($('<div>', {class: 'square'}));
   }
   $('.square').on('mouseover', function(event) {
-    $(this).css('background-color', $colorField.val());
+    $(this).css('background-color', brushColor);
   });
 
 });
