dd97a6fcec7f9ca928c1a89ef9bda594a5266581
erikslewis <Adelina198527@gmail.com>
2016-11-26 19:35:03 -0500

    commit

--- pixart.js
+++ pixart.js
@@ -8,3 +8,7 @@ form.on("submit", function(event){
   event.preventDefault();
   brush.css('background-color', input.val());
 })
+ for(let i = 0; i < 8000; i++){
+   let square = $("<div class='square'/>");
+   $("body").append(square);
+ }
