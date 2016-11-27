8caf2dddb829ecb2dbc6959c62f0ae241ca5674e
myztajay <myztajay@hotmail.com>
2016-11-27 14:37:39 -0500

    Added enter key functionality to app

--- pixart.js
+++ pixart.js
@@ -3,6 +3,7 @@ let $colorField = $(colorField);
 let $brush = $(brush);
 
 $setColor.on("click", updateBrush);
+$setColor.keypress("13", updateBrush);
 
 function updateBrush(){
   let color = $colorField.val();
