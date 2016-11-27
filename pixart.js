45a994edf35179d8cecc0caacdf8563f3bee6b23
jtellis <jessetellis@gmail.com>
2016-11-27 14:43:34 -0500

    Append 20 div.square to the body

--- pixart.js
+++ pixart.js
@@ -7,4 +7,9 @@ $(function(){
     event.preventDefault();
     $controlsBrush.css('background-color', $colorField.val());
   });
+
+  for(let i = 0; i < 20; i++) {
+    $('body').append($('<div>', {class: 'square'}));
+  }
+
 });
