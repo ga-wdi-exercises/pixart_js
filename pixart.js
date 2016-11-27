12ab7232642ae06ca5f64b89fabad908101f05b5
vinh huynh <vvvhuynh@yahoo.com>
2016-11-27 15:59:51 -0600

    added changing-color through input

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
+$(document).ready(function() {
 
+  $('#set-color').on('click', function(){
+    //prevents page from refreshing
+    event.preventDefault();
+
+    var color = $('#color-field').val();
+    $('.brush').css('background-color', color);
+  })
+
+});
