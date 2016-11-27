166783692b3a217eef352067c242f0d4bc1dbb5e
myztajay <myztajay@hotmail.com>
2016-11-27 14:33:43 -0500

    Created brush update function to update color of brush div

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
+let $setColor = $(setColor);
+let $colorField = $(colorField);
+let $brush = $(brush);
 
+$setColor.on("click", updateBrush);
+
+function updateBrush(){
+  let color = $colorField.val();
+  $brush.css("background-color", color);
+  return false;
+}
