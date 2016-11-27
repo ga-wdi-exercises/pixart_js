c3730104767bc4ad5bf4eed6de380df45ce825ab
vinh huynh <vvvhuynh@yahoo.com>
2016-11-27 16:31:31 -0600

    added keypress functionality on input

--- pixart.js
+++ pixart.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
 
+  var color;
+
   $('#set-color').on('click', function(){
     //prevents page from refreshing
     event.preventDefault();
-
-    var color = $('#color-field').val();
+    color = $('#color-field').val();
     $('.brush').css('background-color', color);
+  })
+
+  $('#color-field').on('keypress', function(e){
+    var key = e.which;
+    color = $(this).val();
+    if (key === 13) {
+      $('.brush').css('background-color', color);
+    }
   })
 
 });
