9801d7b064524f70795fac828e6ff02bd9a6ce09
Stephanie Workman <sworkman2014@gmail.com>
2016-11-27 18:55:19 -0600

    gave set color button functionality

--- pixart.js
+++ pixart.js
@@ -1,1 +1,13 @@
+$ (function(){
 
+  $('button').click(function(e) {
+    e.preventDefault();
+  });
+
+  $('button').on('click', function(){
+    console.log('button works');
+
+    var color = $('#color-field').val();
+    $('.brush').css('background-color', color);
+  });
+});
