5ec8bee6ada6f63d5694e182db2a5c9af423ee46
Kyle Crichton <kylecrichton@gmail.com>
2016-11-27 22:09:15 -0600

    added enter functionality

--- pixart.js
+++ pixart.js
@@ -3,6 +3,11 @@ $('#set-color').one("click", function(e){
   var color = $('#color-field').val();
   $('.brush').css({"background" : color});
   e.preventDefault();
+  $("#id_of_textbox").keyup(function(event){
+    if(event.keyCode == 13){
+        $("#id_of_button").click();
+    }
+});
 });
 
 
