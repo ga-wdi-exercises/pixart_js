b98bf74aa9b157df60c2d07ffd1ebfcef52af701
yourUsername <your_email@example.com>
2016-11-27 23:45:18 -0500

    just added the event listener but cant change brush to input color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,31 @@
 
+let $form = $(form);
+let $brush = $(brush);
+
+let $setColor = $(setColor);
+
+var button = document.querySelector('button');
+var brush = document.querySelector('.brush');
+//this will keep form from refreshing on click
+$("setColor").on(function(form){
+  return false;
+});
+var colorField = document.getElementById('#colorField');
+button.addEventListener('click', function() {
+  document.body.div.eq[1] = 'colorField';
+});
+
+
+
+ /*
+ $(".brush").click(function (){
+     $(this).css("background-color", "red")
+ });
+*/
+
+
+
+// $( ".brush" ).change(function() {
+//     $('#colorField').css('background-color', '#colorField');
+//     console.log("brush changed");
+// });
