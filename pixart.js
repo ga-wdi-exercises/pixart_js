c07ac32f8844446e98458dc083a1ba71c23e2044
yourUsername <your_email@example.com>
2016-11-27 19:28:24 -0500

    DaraHoy Week 02 Day 02 HW

--- pixart.js
+++ pixart.js
@@ -1,1 +1,12 @@
+//Set Color button
+$setColor = $('#set-color');
 
+//Setup color-field to event handler
+//function updates color from form
+let updateColor = function (){
+  newColor = $("#color-field").val();
+  $('.brush').css('background', newColor);
+  return false;
+};
+
+$setColor.on("click", updateColor);
