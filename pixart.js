8f965a21a44ce5d1cc55ae7cebf91a963a3d4046
Kyle Crichton <kylecrichton@gmail.com>
2016-11-27 22:07:22 -0600

    added click functionality

--- pixart.js
+++ pixart.js
@@ -1,1 +1,12 @@
+$(function () {
+$('#set-color').one("click", function(e){
+  var color = $('#color-field').val();
+  $('.brush').css({"background" : color});
+  e.preventDefault();
+});
 
+
+
+
+
+});
