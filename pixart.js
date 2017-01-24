9ddaa1229d10b9ae3e8eae6a05e7a644bb4c8959
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2017-01-24 18:48:43 -0500

    js now fires when you press enter anywhere on the document

--- pixart.js
+++ pixart.js
@@ -1,4 +1,21 @@
-$('button').on('click', function() {
-  var color = $('input').val();
-  $('.brush').css('background-color', color);
+$( document ).ready(function() {
+  $('button').on('click', function() {
+    var color = $('input').val();
+    $('.brush').css('background-color', color);
+
+  });
+  $(document).on('keydown', function() {
+    if (event.which === 13) {
+      event.preventDefault();
+      var color = $('input').val();
+      $('.brush').css('background-color', color);
+    }
+    else {
+    }
+  });
 });
+
+// $(document).ready(function() {
+//   var div = $("<div></div>");
+//   $("#box").append(div);
+// })
