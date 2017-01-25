18a1e3e46d5a055b578d4e3e3e02c34cba947d57
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2017-01-24 19:52:29 -0500

    picture complete

--- pixart.js
+++ pixart.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  for (i=0;i<20;i++) {
+  for (i=0;i<8000;i++) {
     $('body').append("<div class='square'></div>");
   }
 })
@@ -18,7 +18,7 @@ $( document ).ready(function() {
     else {
     }
   });
-  $('.square').on('click', function() {
+  $('.square').on('mouseover', function() {
     var color = $('input').val();
     $(this).css('background-color', color);
   })
