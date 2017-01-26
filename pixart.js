a5da4b8b710541383c1f23a49069c5d648f85306
Andy Flickinger <atflick@gmail.com>
2017-01-26 14:31:12 -0500

    q2 done

--- pixart.js
+++ pixart.js
@@ -1,7 +1,18 @@
-$("#set-color").click(setColor);
+$("#set-color").click(function(){event.preventDefault()});
+
+// $("#set-color").click(setColor);
+
 
 function setColor() {
-  event.preventDefault()
+  // event.preventDefault()
   var color = $("#color-field").val();
   $(".brush").css("background", color);
 }
+
+$("#color-field").keypress(function(event){
+  if (event.which == 13) {
+    setColor();
+  } else {
+    return;
+  }
+})
