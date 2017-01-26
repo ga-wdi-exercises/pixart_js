5c53c23e3825c77ecf5640c149dcac49b395178d
Nat Carter <n.goldman.carter@gmail.com>
2017-01-26 18:06:06 -0500

    Commit 3 (commit 2 worked automatically)

--- pixart.js
+++ pixart.js
@@ -1,6 +1,12 @@
+var body = $('body');
 var field = $('#color-field');
 var button = $('#set-color');
 var brush = $('.brush');
+// var square = $('.square');
+
+for (var i = 0; i < 20; i++) {
+  body.append('<div class="square"></div>');
+}
 
 button.click(function(event) {
   event.preventDefault();
