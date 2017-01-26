c5f349f0faecec10739151df29b547c74b36a902
Bill Bobbitt <wmbobbitt@gmail.com>
2017-01-26 18:52:51 -0500

    enter now works

--- pixart.js
+++ pixart.js
@@ -3,3 +3,10 @@ $("button").on("click", function(e) {
   $(".brush").css("background", $("input").val());
   e.preventDefault();
 })
+
+$(document).on("keypress", function(e) {
+  if (e.which == 13) {
+    $(".brush").css("background", $("input").val());
+    e.preventDefault();
+  }
+})
