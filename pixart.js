f0433f2c9fd9637190a9baf904e6c083718c43d0
Vince Campanale <vcampanale0294@gmail.com>
2017-01-26 14:29:44 -0500

    appending 20 divs w/ square class to body

--- pixart.js
+++ pixart.js
@@ -1,6 +1,13 @@
-$(document).ready(
+$(document).ready(function() {
   $("#set-color").click(function(event) {
     event.preventDefault();
-    $(".brush").css('background', $("#color-field").val());
-  })
-);
+    let brushColor = $("#color-field").val();
+    $(".brush").css('background', brushColor);
+  });
+
+  for(var i = 0; i <= 20; i++){
+    let html = `<div class="square"></div>`;
+    $("body").append(html);
+  }
+  
+});
