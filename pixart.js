b1c5d63b6050f6a3971ff3b2c1a95ae36cd2295e
Wes <wes.coffay@gmail.com>
2017-01-26 18:25:33 -0500

    increased number of squares, added mouseover, functional

--- pixart.js
+++ pixart.js
@@ -1,12 +1,12 @@
 $("#set-color").click(function(event) {
   event.preventDefault()
   $(".brush").css('background', $('#color-field').val())
-  console.log($(".brush").css('background'))
+  // console.log($(".brush").css('background'))
 })
-for (i=0; i<20; i++) {
+for (i=0; i<8000; i++) {
 $('body').append('<div class="square"></div>')
 }
-$('.square').on('click', function(){
+$('.square').on('mouseover', function(){
   $(this).css('background', $('#color-field').val())
-  console.log($(this).css('background'))
+  // console.log($(this).css('background'))
 })
