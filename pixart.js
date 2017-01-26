cafc5d219fc7f6adcc080f0767677f0eeaf1d765
Justin Shin <jjs3b@virginia.edu>
2017-01-26 14:21:36 -0500

    part five of assignment

--- pixart.js
+++ pixart.js
@@ -7,7 +7,7 @@ button.on('click',function(){
   brush.css('background',colorField.val());
   return false;
 })
-colorField.on('keypress', function(){
+colorField.on('keypress', function(e){
     if (e.keyCode == 13) {
       brush.css('background',colorField.val());
     }
@@ -17,4 +17,4 @@ for (i=0; i<20; i++) {
   $('body').append('<div class="square"></div>');
 }
 
-$('body').children('.square').on('click', function(){$(this).css('background','green')})
+$('body').children('.square').on('click', function(){$(this).css('background',brush.css('background'))})
