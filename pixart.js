9f0b360a1304740e3801193820ccf72af02fab0b
Eva Lai <evalai.dc@gmail.com>
2017-01-26 15:45:07 -0500

    commit1 set box to change by clicking input

--- pixart.js
+++ pixart.js
@@ -1,1 +1,9 @@
+// changing color of brush box to
+var colorButton = $('button')
 
+
+colorButton.on('click',function(){
+  event.preventDefault();
+  var colorSelected = document.getElementById('color-field').value;
+  $('.brush').css('background',colorSelected)
+})
