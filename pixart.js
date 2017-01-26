dd953d7d03e86f0e00d832e7115c4071e97dc49d
Dominick Houston <dominick.houston14@gmail.com>
2017-01-26 17:41:26 -0500

    Commit #2 When Enter is pressed

--- pixart.js
+++ pixart.js
@@ -1,1 +1,20 @@
 
+//on click switch color of .square to another color
+//use JQuery to select element and add an event listener
+
+//Commit #1
+
+    //blank
+  // })
+$("button").on("click", function() {
+  var color = $("input").val();
+  // $(".brush").css("background-color", color);
+  event.preventDefault();
+  alert("color has changed");
+})
+
+// $(".square").on("click", function() {
+//   $("this").css("background-color", "orange");
+//   //^^use Orange as "COLOR"
+
+// });
