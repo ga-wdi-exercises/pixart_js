af80c166c9ffdd8acb40f453f01cbd35c8ddabea
Erica Norby <ericanorby@gmail.com>
2017-01-26 15:45:20 -0500

    added function to change color based on user input

--- pixart.js
+++ pixart.js
@@ -1,1 +1,7 @@
+$("button").click(changeColor)
 
+function changeColor(){
+  event.preventDefault();
+  var userInput = document.getElementById("color-field").value;
+  $(".brush").css("background", userInput)
+}
