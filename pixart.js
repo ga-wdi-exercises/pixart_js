492de872530c632321b1d2e420b2d9eea7da66ec
Andy Flickinger <atflick@gmail.com>
2017-01-26 15:53:34 -0500

    q3 done

--- pixart.js
+++ pixart.js
@@ -16,3 +16,12 @@ $("#color-field").keypress(function(event){
     return;
   }
 })
+
+
+function addSquares(){
+  for(var i=0;i<20;i++){
+    $("body").append('<div class="square"></div>');
+  }
+}
+
+addSquares();
