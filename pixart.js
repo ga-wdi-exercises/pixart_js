9a9a87942f921f298fc6920470487e6973528b05
Eva Lai <evalai.dc@gmail.com>
2017-01-26 17:29:19 -0500

    commit4 change mini squares to green

--- pixart.js
+++ pixart.js
@@ -1,12 +1,12 @@
 // changing color of brush box to color inputted commit 1 & 2
 var colorButton = $('button')
-
-
-colorButton.on('click',function(){
+function colorChange(){
   event.preventDefault();
   var colorSelected = document.getElementById('color-field').value;
   $('.brush').css('background',colorSelected)
-})
+}
+
+colorButton.on('click',colorChange)
 
 // commit 3- 20 divs of square class and append them to the body
 function addDiv(){
@@ -14,6 +14,13 @@ function addDiv(){
     var div = document.createElement('div')
     div.className = 'square'
     $('body').append(div);
+    //add function to click to change square.
+    $('.square').on('click',onClick)
   }
 }
 addDiv(20);
+
+//commit 4 - change for each square
+function onClick(){
+  $(this).css('background','green')
+};
