63e6ca9a97533a20fe4ef6b73ccd80139b99dc09
Eva Lai <evalai.dc@gmail.com>
2017-01-26 17:04:06 -0500

    commit3 created 20 divs

--- pixart.js
+++ pixart.js
@@ -1,4 +1,4 @@
-// changing color of brush box to
+// changing color of brush box to color inputted commit 1 & 2
 var colorButton = $('button')
 
 
@@ -7,3 +7,13 @@ colorButton.on('click',function(){
   var colorSelected = document.getElementById('color-field').value;
   $('.brush').css('background',colorSelected)
 })
+
+// commit 3- 20 divs of square class and append them to the body
+function addDiv(){
+  for (var i=0; i<20; i++) {
+    var div = document.createElement('div')
+    div.className = 'square'
+    $('body').append(div);
+  }
+}
+addDiv(20);
