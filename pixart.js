de57bbf7569d53c8bbd2e4df32b784b7b5bdfdb0
Erica Norby <ericanorby@gmail.com>
2017-01-26 18:45:09 -0500

    finished assignment, added color swatch

--- pixart.js
+++ pixart.js
@@ -1,17 +1,37 @@
-for (var i = 0; i < 20; i++) {
-  var square = $("<div></div>");
-  square.addClass("square")
-  $("body").append(square)
+function createSquares(num){
+  for (var i = 0; i < num; i++) {
+    var square = $("<div></div>");
+    square.addClass("square")
+    $("body").append(square)
+  }
 }
+
+var i = 0;
 
 function changeColor(){
   event.preventDefault();
   var userInput = document.getElementById("color-field").value;
   $(".brush").css("background", userInput)
+  if (i == 0) {
+    $(".swatch").eq(0).css("background", userInput)
+    i++;
+  } else if (i == 1) {
+    $(".swatch").eq(1).css("background", userInput)
+    i++;
+  } else if (i == 2) {
+    $(".swatch").eq(2).css("background", userInput)
+    i = 0;
+  }
 }
 
+createSquares(8000);
+
 $("button").click(changeColor)
-$(".square").click(function(){
+$(".square").mouseover(function(){
   var selectedColor = $(".brush").css("background");
   $(this).css("background", selectedColor)
+})
+$(".swatch").click(function(){
+  var swatchColor = $(this).css("background");
+  $(".brush").css("background", swatchColor)
 })
