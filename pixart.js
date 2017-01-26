e5195da9c06c81cc00e86fa8a43e3c87271312c0
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-01-26 14:27:35 -0500

    able to change brush color, step 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,10 @@
 
+
+var setcolor = $("#set-color")
+
+$("#set-color").on("click", function(){
+  event.preventDefault()
+  var input = document.getElementById("color-field").value;
+
+  $(".brush").css("background", input)
+})
