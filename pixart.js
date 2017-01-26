47d08970719aa520c1be90e6c44d39dbd799e99f
Wes <wes.coffay@gmail.com>
2017-01-26 18:08:08 -0500

    added 20 squares

--- pixart.js
+++ pixart.js
@@ -3,3 +3,6 @@ $("#set-color").click(function(event) {
   $(".brush").css('background', $('#color-field').val())
   console.log($(".brush").css('background'))
 })
+for (i=0; i<20; i++) {
+$('body').append('<div class="square"></div>')
+}
