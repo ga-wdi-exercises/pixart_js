b91459cde421e49a7dce13f1f23e742cad93801e
Justin Shin <jjs3b@virginia.edu>
2017-01-26 14:15:27 -0500

    part four of assignment

--- pixart.js
+++ pixart.js
@@ -1,6 +1,7 @@
 var button = $('#set-color')
 var brush = $('.brush')
 var colorField = $('#color-field')
+var square = $('.square')
 
 button.on('click',function(){
   brush.css('background',colorField.val());
@@ -11,6 +12,9 @@ colorField.on('keypress', function(){
       brush.css('background',colorField.val());
     }
 });
+
 for (i=0; i<20; i++) {
   $('body').append('<div class="square"></div>');
 }
+
+$('body').children('.square').on('click', function(){$(this).css('background','green')})
