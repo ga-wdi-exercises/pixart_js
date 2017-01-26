ea4f1dadac8ff61cce16b502eaefa88ac87f3f95
Justin Shin <jjs3b@virginia.edu>
2017-01-26 14:03:13 -0500

    part two of assignment

--- pixart.js
+++ pixart.js
@@ -6,3 +6,8 @@ button.on('click',function(){
   brush.css('background',colorField.val());
   return false;
 })
+colorField.on('keypress', function(){
+    if (e.keyCode == 13) {
+      brush.css('background',colorField.val());
+    }
+});
