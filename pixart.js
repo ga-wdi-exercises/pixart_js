d93da905ffda73417b40a66a29bf58e92bbf0ba1
Justin Shin <jjs3b@virginia.edu>
2017-01-26 14:57:46 -0500

    bonus part

--- pixart.js
+++ pixart.js
@@ -2,16 +2,33 @@ var button = $('#set-color')
 var brush = $('.brush')
 var colorField = $('#color-field')
 var square = $('.square')
+var first = $('#first')
+var second = $('#second')
+var third = $('#third')
+var holderColor = null
 
 button.on('click',function(){
-  brush.css('background',colorField.val());
+  setColor();
   return false;
 })
 colorField.on('keypress', function(e){
     if (e.keyCode == 13) {
-      brush.css('background',colorField.val());
+      setColor();
     }
 });
+$('.history').children().on('click',function(){
+  holderColor = $(this).css('background')
+  $(this).css('background',brush.css('background'))
+  brush.css('background',holderColor)
+})
+
+function setColor(){
+  third.css('background',second.css('background'));
+  second.css('background',first.css('background'));
+  first.css('background',brush.css('background'));
+  brush.css('background',colorField.val());
+
+}
 
 for (i=0; i<8000; i++) {
   $('body').append('<div class="square"></div>');
