84006ef6ab5e61e1a2e249c56313bb527e7c37d8
Justin Shin <jjs3b@virginia.edu>
2017-01-26 14:00:00 -0500

    part one of assignment

--- pixart.js
+++ pixart.js
@@ -1,1 +1,8 @@
+var button = $('#set-color')
+var brush = $('.brush')
+var colorField = $('#color-field')
 
+button.on('click',function(){
+  brush.css('background',colorField.val());
+  return false;
+})
