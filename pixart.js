e6a61886d5666403ea6f97a2e5381f7563fa3f06
Erica Norby <ericanorby@gmail.com>
2017-01-26 17:03:28 -0500

    created 20 divs in body

--- pixart.js
+++ pixart.js
@@ -1,5 +1,11 @@
 $("button").click(changeColor)
 
+for (var i = 0; i < 20; i++) {
+  var square = $("<div></div>");
+  square.addClass("square")
+  $("body").append(square)
+}
+
 function changeColor(){
   event.preventDefault();
   var userInput = document.getElementById("color-field").value;
