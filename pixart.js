a83ec5d5d46159a98b64882faf34979d86dc9f90
Nat Carter <n.goldman.carter@gmail.com>
2017-01-26 17:59:29 -0500

    Commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,8 @@
+var field = $('#color-field');
+var button = $('#set-color');
+var brush = $('.brush');
 
+button.click(function(event) {
+  event.preventDefault();
+  brush.css('background-color', field.val());
+});
