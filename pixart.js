ccb52ad7fdc7dfeb937c73538d6a5d8b95bfb280
Will Hawkins <willhawkins123@yahoo.com>
2017-01-26 17:44:03 -0500

    Brush Box Changes Color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,9 @@
-
+console.log("Hello World");
+$("button").on("click", function(event) {
+//  console.log("The button can click!");
+  event.preventDefault();
+  var value = $("#color-field").val();
+  console.log(value);
+  $(".brush").css("background", value);
+  $("#color-field").val("");
+});
