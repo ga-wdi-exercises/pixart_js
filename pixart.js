993c77aaaae3e805ce48aec4e1dd0d7803a6b30b
Ebenezer Williams <ebenezerw@me.com>
2017-01-26 19:27:20 -0500

    question 5

--- pixart.js
+++ pixart.js
@@ -1,16 +1,16 @@
 $("button").on("click", function() {
-	var newColor = $("input").val();
+	color = $("input").val();
 	event.preventDefault();
-	$(".brush").css("background-color", newColor)
+	$(".brush").css("background-color", color)
 })
-
 
 $("document").ready(function () {
 	for (var i = 0; i < 20; i++) {
+
 		$("body").append("<div class='square'></div>");
 
 		$(".square").on("click", function () {
-			$(this).css("background-color", "green")
+			$(this).css("background-color", color)
 		})
 	}
 })
