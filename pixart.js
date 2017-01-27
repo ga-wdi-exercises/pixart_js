fb18d950032a2b266617e3eb3ae921e70d2c04e8
xgironx <xgironx@gmail.com>
2017-01-27 03:17:24 -0500

    commit #1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,14 @@
 
+$(document).ready(function(){
+  console.log("hello world")
+})
+var button = $("#set-color");
+var textInput = $("#color-field");
+
+button.on('click', function (event) {
+    event.preventDefault();
+    console.log("in click handler");
+    color = textInput.val();
+    $(".brush").css("background",color);
+
+});
