c3c2630f0ce952a2de96c67039bf4a204e561c59
Wes <wes.coffay@gmail.com>
2017-01-26 19:19:37 -0500

    added memory swatch

--- pixart.js
+++ pixart.js
@@ -1,12 +1,18 @@
 $("#set-color").click(function(event) {
   event.preventDefault()
+  $("#third").css('background', $("#second").css('background'))
+  $("#second").css('background', $("#first").css('background'))
+  $("#first").css('background', $(".brush").css('background'))
+
+
   $(".brush").css('background', $('#color-field').val())
-  // console.log($(".brush").css('background'))
 })
 for (i=0; i<8000; i++) {
 $('body').append('<div class="square"></div>')
 }
 $('.square').on('mouseover', function(){
-  $(this).css('background', $('#color-field').val())
-  // console.log($(this).css('background'))
+  $(this).css('background', $('.brush').css('background'))
+})
+$(".hx").click(function(){
+  $(".brush").css('background', $(this).css('background'))
 })
