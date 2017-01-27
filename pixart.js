1fac7c19317a65e1c0e49e40dd3a296970186391
Kay Ehni <kay.ehnigmail.com>
2017-01-27 13:19:15 -0500

    updated pixart.js--got .click to work, but not .mouseover

--- pixart.js
+++ pixart.js
@@ -1,11 +1,19 @@
 var button = $("button");
-$("button").click(function(event){
-    event.preventDefault();
-});
-button.click(function(){
-  $(".brush").css("background", $("input:color-field").val()));
+var form = $("form");
+//var userColor = $("input:text").val();// Not sure why this variable doesn't work
+
+$("form").submit(function(){
+  event.preventDefault();
+  $(".brush").css("background", $("input:text").val());
 })
-button.click(function(){
-  console.log($("input:color-field").val());
-}
-)
+$("form").submit(function(event){
+  event.preventDefault();
+  console.log($("input:text").val());
+    });
+for(i = 0; i<=500; i++) {
+  $("body").append("<div></div>");
+  $("div").addClass("square");
+  }
+$("div").click(function(){$(this).css("background", $("input:text").val())})
+
+//$("div").mouseover(function(){$(this).css("background", $("input:text").val())}) //Not sure why this function doesn't work
