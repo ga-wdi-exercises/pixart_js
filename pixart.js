104dbe683cb1bc3e88803f8592ef7f87b2cc22a1
Chris Wightman <wightman_christopher@bah.com>
2017-01-26 19:50:59 -0500

    Commit 4. Turn each square green on click

--- pixart.js
+++ pixart.js
@@ -3,13 +3,12 @@ var brushBox = $(".brush")
 var	color
 var colorField = $("#color-field")
 var squareNumber = $(".square").length
+var square = $(".square")
 
 function changeBrushBox(){
 	color = $('#color-field').val()
 	brushBox.css("background", color);
-
 	return false
-
 }
 
 setColor.on("click", changeBrushBox)
@@ -18,3 +17,11 @@ colorField.on("submit", changeBrushBox)
 for (i=0; i < 20; i++) {
 	$( "body" ).append( "<div class='square'></div>" );
 }
+
+// $(".square").on("click", user.sayName);
+
+// function changeToGreen()
+
+$(".square").on("click", function(){
+	$(this).css("background-color", "green");
+} );
