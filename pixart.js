899427f6e034cfcce65cee5ed0611ef055f70a88
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-01-27 05:33:08 -0500

    made the enter button work for submitting a color

--- pixart.js
+++ pixart.js
@@ -1,8 +1,15 @@
 //get content of input field with id colorfield
 //set the squares color to match the content of the input field
-$( "#submit_button").click(function(event){
-  var color;
-  color = $('#color-field').val();
-  $('#brush').css('background', color);
-  event.preventDefault();
+$('document').ready(function(){
+  $( "#submit_button").click(function(event){
+    var color;
+    color = $('#color-field').val();
+    $('#brush').css('background', color);
+    event.preventDefault();
+    });
+  $("#color-field").keypress (function(e){
+    if(e.which == 13){
+      $('#submit_button').click();
+    }
+  });
 });
