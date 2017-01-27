7a28e8076b1a4453d28f93c6cdc2ac8860d50cf7
Bedig Galladian <bgalladian@gmail.com>
2017-01-26 20:25:14 -0500

    commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,13 @@
 
+//creat the variable button, and name it, this is my target//
+var colorButton = $('button')
+//now I have to creat the function, which is colorChange to describe the action//
+function colorChange(){
+  //prevents the click from reloading the page//
+  event.preventDefault();
+  //variable to select a color by id/color entered.
+  var colorSelected = document.getElementById('color-field').value;
+  $('.brush').css('background',colorSelected)
+}
+//so when i click, the color changes//
+colorButton.on('click',colorChange)
