21d3fdf3b6fe1cda3ecfad0dfd0dbb3405e9aa93
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-01-27 06:23:45 -0500

    commit #6 color changes + mouseover

--- pixart.js
+++ pixart.js
@@ -1,11 +1,11 @@
 //get content of input field with id colorfield
 //set the squares color to match the content of the input field
 $('document').ready(function(){
-var color;
-  for (i = 0; i < 20; i++) {
+  var color;
+  for (i = 0; i < 8000; i++) {
     $('body').append('<div class="square"></div>');
   }
-  $('.square').on("click",function(event){
+  $('.square').on("mouseover",function(event){
     $(this).css('background', color);
     event.preventDefault();
   });
