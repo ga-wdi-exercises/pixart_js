b8ad709d0b8620558ba5aa4d3b8fa81f0b81020c
xgironx <xgironx@gmail.com>
2017-01-27 03:18:56 -0500

    commit #2

--- pixart.js
+++ pixart.js
@@ -1,14 +1,23 @@
-
 $(document).ready(function(){
-  console.log("hello world")
-})
+  console.log("Commit-2")
+});
+
 var button = $("#set-color");
 var textInput = $("#color-field");
 
 button.on('click', function (event) {
     event.preventDefault();
-    console.log("in click handler");
+    console.log("in click handler 2");
     color = textInput.val();
     $(".brush").css("background",color);
 
+
+});
+
+textInput.keyup(function(event){
+    //do we have then enter key?
+    if(event.keyCode == 13){
+        console.log("have hit enter key");
+        button.click();
+    }
 });
