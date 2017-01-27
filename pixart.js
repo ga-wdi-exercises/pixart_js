e5c974529d2b71b51dfcc589ae6414e15edfaeb5
Laura Goldstein <laura.koss.goldstein@gmail.com>
2017-01-26 20:43:13 -0500

    added prevent default and color change

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
+var button = $("button");
 
+var handleClickEvent = function(e){
+  e.preventDefault();
+  $( "input#color-field" ).keyup(function() {
+      var value = $( this ).val();
+      $(".brush").css('background-color', value);
+    })
+    .keyup();
+}
+button.on("click", handleClickEvent);
