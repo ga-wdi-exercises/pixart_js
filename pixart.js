021d95d0c456d3c3c873b089048872aee9ae4c2e
Collin OConnell <ococncol@gmail.com>
2017-01-27 06:32:52 -0500

    added squares

--- pixart.js
+++ pixart.js
@@ -1,8 +1,18 @@
 var setColor = $('button');
 
-
+//add also able to press enter along with clicking set color
+//button
 setColor.on('click',function(){
   event.preventDefault();
   var colorBlock = document.getElementById('color-field').value;
   $('.brush').css("background",colorBlock);
 });
+
+function createDiv(){
+  for (var i = 0; i < 20; i++){
+    var div = $('<div class="square"></div>');
+    $('body').append(div);
+  }
+}
+
+createDiv(20);
