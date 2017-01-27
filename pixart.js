b99cfe53ebe41b0171a50eadb9b3309bda12b53b
Fiona <h.natapra@gmail.com>
2017-01-26 21:57:52 -0500

    commit #6

--- pixart.js
+++ pixart.js
@@ -7,3 +7,16 @@ setColor.on("click", function(){
 $("#set-color").click(function(event){
   event.preventDefault();
 });
+
+
+for (var i = 0; i < 8000; i++) {
+  $('body').append('<div class="square"></div>');
+}
+
+
+var square = $('.square')
+
+square.on("mouseover", function(){
+  $(this).css('background', document.getElementById('color-field').value);
+
+})
