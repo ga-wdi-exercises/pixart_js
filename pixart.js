a90432f17d9f4540d01af8698be3d3b525b68f68
jared edwards <jredwards519@gmail.com>
2017-01-26 20:55:32 -0500

    made changes for commit 6

--- pixart.js
+++ pixart.js
@@ -17,22 +17,25 @@ $("input").keypress(function(e) {
 // appends divs to body tags
 var square = $('.square');
 
-for (let i=0; i < 20; i ++){
+for (let i=0; i < 8000; i ++){
     var div = $('<div />').appendTo('body');
      div.attr('class','square').click(function(){
-     $(div).on('click').css('background','green');
+       $(div).css('background', 'green');
+     });
 }
-}
-
+// .click(function(){
+// $(div).on('click').css('background','green');
 
 //attempt to change the color of the divs
-$('.square').on('click', function(){
+$('.square').on('mouseover', function(){
   console.log("clicked");
-  $('.square').on('click').css('background','green');.click(function(){
-$(div).on('click').css('background','green')
 })
 
 
 // change box to 10px h&w and marin 0;
 // loop 8000
 // change from click to mouseover
+
+$('.square').attr('height','10px');
+$('.square').attr('widtht','10px');
+$('.square').attr('margin',0);
