52be933c057550a9550b7b1f7decbeb09188a993
jared edwards <jredwards519@gmail.com>
2017-01-26 19:32:30 -0500

    Added 20 boxes with class square

--- pixart.js
+++ pixart.js
@@ -1,3 +1,4 @@
+//This is to change the color of the brush field
 var button = $('button');
 var input = $('#color-field');
 
@@ -5,10 +6,18 @@ button.on('click', function(e){
   e.preventDefault()
   $('.brush').css('background', input.val());
 });
-
+// allows for enter key to submit form
 $("input").keypress(function(e) {
     if (e.keypress == 13) {
         e.preventDefault();
         $("form").submit();
     }
 });
+
+// appends divs to body tags
+var square = $('.square');
+
+for (let i=0; i < 20; i ++){
+  var div = $('<div />').appendTo('body');
+   div.attr('class','square');
+}
