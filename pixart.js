5640a474720d9bb44aae8e390a6e038cf280fe55
shapiro_benjamin@bah.com <benjaminshapiro@C1MS50ZQH3QK.fios-router.home>
2017-01-27 01:30:30 -0500

    Commit 4: Individual Green Boxes

--- pixart.js
+++ pixart.js
@@ -1,9 +1,17 @@
+// Commit 1-2
 $('#set-color').on('click',function(event){
   event.preventDefault();
   var color = $("#color-field").val();
   $(".brush").css("background", color);
 });
 
+// Commit 3
 for(i=0;i<20;i++){
   $('body').append('<div class="square"></div>');
 }
+
+// Commit 4
+$('.square').on('click',function(event){
+  event.preventDefault();
+  $(this).css("background","green");
+});
