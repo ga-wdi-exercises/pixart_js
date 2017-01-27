d627b964e28bfb10d1f8cf415b7954ba27ee5a02
Chris Wightman <wightman_christopher@bah.com>
2017-01-26 19:27:33 -0500

    Commit 3. Commit 2 didn't work. Adding div's

--- pixart.js
+++ pixart.js
@@ -1,1 +1,20 @@
+var setColor = $("#set-color")
+var brushBox = $(".brush")
+var	color
+var colorField = $("#color-field")
+var squareNumber = $(".square").length
 
+function changeBrushBox(){
+	color = $('#color-field').val()
+	brushBox.css("background", color);
+
+	return false
+
+}
+
+setColor.on("click", changeBrushBox)
+colorField.on("submit", changeBrushBox)
+
+for (i=0; i < 20; i++) {
+	$( "body" ).append( "<div class='square'></div>" );
+}
