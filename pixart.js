e3e3d86a9b47915f03e4bee11c48be6673b583b5
Vince Campanale <vcampanale0294@gmail.com>
2017-01-26 22:31:24 -0500

    background of individual squares changes to green when clicked

--- pixart.js
+++ pixart.js
@@ -1,13 +1,27 @@
 $(document).ready(function() {
+
+  //set brush color helper function
+  function setBrushColor() {
+    var brushColor = $("#color-field").val();
+    $(".brush").css('background', brushColor);
+  }
+
+  //add click event listner to set color button
   $("#set-color").click(function(event) {
     event.preventDefault();
-    let brushColor = $("#color-field").val();
-    $(".brush").css('background', brushColor);
+    setBrushColor();
   });
+
+  //add submit event listener to text box
+  $("#color-field").on("submit", setBrushColor);
 
   for(var i = 0; i <= 20; i++){
     let html = `<div class="square"></div>`;
     $("body").append(html);
   }
-  
+
+  $("body").delegate(".square", "click", function(){
+    $(this).css("background", "green");
+  });
+
 });
