bf94e3f9822c0d493a3b6c7c1ccff5164e5fec5f
Bedig Galladian <bgalladian@gmail.com>
2017-01-26 20:43:31 -0500

    added 20 divs

--- pixart.js
+++ pixart.js
@@ -1,13 +1,19 @@
 
-//creat the variable button, and name it, this is my target//
+///creat the variable button, and name it, this is my target//
 var colorButton = $('button')
-//now I have to creat the function, which is colorChange to describe the action//
 function colorChange(){
-  //prevents the click from reloading the page//
   event.preventDefault();
-  //variable to select a color by id/color entered.
   var colorSelected = document.getElementById('color-field').value;
   $('.brush').css('background',colorSelected)
 }
-//so when i click, the color changes//
+
 colorButton.on('click',colorChange)
+
+function addDiv(){
+  for (var i=0; i<20; i++) {
+    var div = document.createElement('div')
+    div.className = 'square'
+    $('body').append(div);
+  }
+}
+addDiv(20);
