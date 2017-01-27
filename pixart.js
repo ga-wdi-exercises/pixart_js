2e57b7255ffa989f9edcf0608917cb4e8f728cbb
Bedig Galladian <bgalladian@gmail.com>
2017-01-27 01:08:12 -0500

    commit 5

--- pixart.js
+++ pixart.js
@@ -11,14 +11,15 @@ function colorChange(){
 colorButton.on('click',colorChange)
 //commit 3//
 function addDiv(){
-  for (var i=0; i<20; i++) {
+  for (var i=0; i<8000; i++) {
     var div = document.createElement('div')
     div.className = 'square'
     $('body').append(div);
-    $('.square').on('click',onClick)
+    //add function to click to change square.
+    $('.square').on('mouseover',onClick)
   }
 }
-addDiv(20);
+addDiv(8000);
 //commit 4/5//
 function onClick(){
   var colorSelected = document.getElementById('color-field').value;
