2ffe47595529b2149e00af0d1b634a7a2ef0abe6
Habtamu Abere <habbuya@gmail.com>
2017-01-26 23:39:49 -0700

    second commit

--- pixart.js
+++ pixart.js
@@ -5,4 +5,19 @@ $(document).ready(function canvas() {
 
   $( "body" ).append($("<div class='square'></div>"))
 }
+      this.squares = $(".square")
 });
+
+inputColor(){
+    this.color = this.input.val()
+    return this.color
+  }
+
+$("button").on("click", function setColor(){
+   this.inputColor.css("background", this.color)
+ }
+
+ $("form").on("submit", function stop() {
+      stop.preventDefault()
+      this.setColor()
+    })
