c7a0df0c01b182775c30914a19d105a583864159
Chris Wightman <wightman_christopher@bah.com>
2017-01-26 19:57:14 -0500

    Commit 6. We are now painting. Pretty cool.

--- pixart.js
+++ pixart.js
@@ -14,11 +14,11 @@ function changeBrushBox(){
 setColor.on("click", changeBrushBox)
 colorField.on("submit", changeBrushBox)
 
-for (i=0; i < 20; i++) {
+for (i=0; i < 8000; i++) {
 	$( "body" ).append( "<div class='square'></div>" );
 }
 
-$(".square").on("click", function(){
+$(".square").on("mouseover", function(){
 	$(this).css("background-color", color);
 } );
 
