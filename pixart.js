61c3839f66d258797d6d8f2e0b1e1cf73458d132
Collin OConnell <ococncol@gmail.com>
2017-01-27 06:40:04 -0500

    made it so the small boxes become green

--- pixart.js
+++ pixart.js
@@ -2,17 +2,28 @@ var setColor = $('button');
 
 //add also able to press enter along with clicking set color
 //button
-setColor.on('click',function(){
+//setColor.on('click',function(){
+function setColor(){
   event.preventDefault();
   var colorBlock = document.getElementById('color-field').value;
   $('.brush').css("background",colorBlock);
-});
+}
+
+setColor.on('click', setColor);
+
 
 function createDiv(){
   for (var i = 0; i < 20; i++){
     var div = $('<div class="square"></div>');
     $('body').append(div);
+
+
+    $('.square').on('click', onClick);
   }
 }
 
 createDiv(20);
+
+function onClick(){
+  $(this).css("background", "green");
+}
