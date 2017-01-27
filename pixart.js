0903e3d4cce0aff92ed0fb97c6e46eb2575da242
Fiona <h.natapra@gmail.com>
2017-01-26 19:08:20 -0500

    commit #1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,9 @@
+var setColor = $('#set-color')
 
+setColor.on("click", function(){
+  $(".brush").css('background', document.getElementById('color-field').value)
+})
+
+$("#set-color").click(function(event){
+  event.preventDefault();
+});
