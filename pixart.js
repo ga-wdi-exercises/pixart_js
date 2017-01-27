aeaba105fe4b0fa1463f56474abb0c84647c4df8
Vince Campanale <vcampanale0294@gmail.com>
2017-01-26 23:22:08 -0500

    implemented color swatch

--- pixart.js
+++ pixart.js
@@ -1,11 +1,30 @@
 $(document).ready(function() {
-
+  var colorSwatch = [];
+  var color = '';
 
   //set brush color helper function
   function setBrushColor() {
-    var brushColor = $("#color-field").val();
-    $(".brush").css('background', brushColor);
+    var brushColor = $("#color-field").val(); //get the current brush color (from the .brush div)
+    $(".brush").css('background', brushColor); //set the brush to the new value
+    addToColorSwatch(brushColor); //add that color to the color swatch
   }
+
+  //color swatch handler ( not jquery methods :/ )
+  function addToColorSwatch(color) {
+    if(colorSwatch.length >= 3) {
+      $(".color-swatch").children().eq(0).remove();
+      colorSwatch.pop();
+    }
+    colorSwatch.unshift(color);
+    console.log(colorSwatch);
+
+    $(".color-swatch").append(`<div class="swatch-square ${color}"></div>`);
+    $(`.${color}`).css("background", color);
+  }
+
+  $(".color-swatch").delegate("div", "click", function(){
+    $(".brush").css("background", $(this).css('background'));
+  });
 
   //add click event listner to set color button
   $("#set-color").click(function(event) {
