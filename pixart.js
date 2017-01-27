6bbae740faba8d6cc4a9dc837540d87add18133a
Natasha Clark <Natasharclark@gmail.com>
2017-01-27 08:50:33 -0500

    started code structure for 1st question but couldnt figure it out... will spend the weekend studying and revisit

--- pixart.js
+++ pixart.js
@@ -1,1 +1,37 @@
+var button = $("#button");
+// var input = $(".text");
+var brush = $(".brush");
 
+function changeBrushColortoInput () {
+  var color = document.getElementById("color-field").value;
+  // var color = $("#color-field").val(); //need to get the value from form
+  $(".brush").css("background","color");
+
+}
+
+button.on("click", changeBrushColortoInput());
+
+// function elt(name, attributes) {
+//   var node = document.createElement(name);
+//   if (attributes) {
+//     for (var attr in attributes)
+//       if (attributes.hasOwnProperty(attr))
+//         node.setAttribute(attr, attributes[attr]);
+//   }
+//   for (var i = 2; i < arguments.length; i++) {
+//     var child = arguments[i];
+//     if (typeof child == "string")
+//       child = document.createTextNode(child);
+//     node.appendChild(child);
+//   }
+//   return node;
+// }
+
+// button.color = function(cx) {
+//   var input = elt("input", {type: "color"});
+//   input.addEventListener("change", function() {
+//     cx.fillStyle = input.value;
+//     cx.strokeStyle = input.value;
+//   });
+//   return elt("span", null, "Color: ", input);
+// };
