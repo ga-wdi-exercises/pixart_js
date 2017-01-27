d4056864a4251e23e1ecee4dbd6a792082d85146
Elena Santana <fanelenan7@gmail.com>
2017-01-26 19:35:15 -0500

    initial commit and commit 2, because it's happening automatically

--- pixart.js
+++ pixart.js
@@ -1,1 +1,15 @@
+var brushColor
+var brushBox
 
+// $("#set-color").on("input", function(){
+// })
+
+$("#set-color").on("click", function(e){
+  e.preventDefault();
+  brushColor = $("#color-field").val()
+  brushBox = $(".brush").css("background", brushColor)
+
+// resetting the input box
+  $("#color-field").val("")
+})
+// console.log(brushColor)
