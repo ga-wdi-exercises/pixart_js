d59df98eea4fc1cfd7d93c1348d44cec496b8075
jared edwards <jredwards519@gmail.com>
2017-01-26 20:42:13 -0500

    Missing syntax

--- pixart.js
+++ pixart.js
@@ -18,14 +18,21 @@ $("input").keypress(function(e) {
 var square = $('.square');
 
 for (let i=0; i < 20; i ++){
-  var div = $('<div />').appendTo('body');
+    var div = $('<div />').appendTo('body');
      div.attr('class','square').click(function(){
      $(div).on('click').css('background','green');
-   });
+}
 }
 
-// attempt to change the color of the divs
-// $('.square').on('click', function(){
-//   console.log("clicked");
-//   $('.square').on('click').css('background','green');
-// })
+
+//attempt to change the color of the divs
+$('.square').on('click', function(){
+  console.log("clicked");
+  $('.square').on('click').css('background','green');.click(function(){
+$(div).on('click').css('background','green')
+})
+
+
+// change box to 10px h&w and marin 0;
+// loop 8000
+// change from click to mouseover
