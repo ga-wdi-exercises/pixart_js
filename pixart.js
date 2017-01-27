ab8789316b8257c09b43f4b6c2d9cc15dd0e713a
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-01-27 06:18:35 -0500

    commit #4 green squares

--- pixart.js
+++ pixart.js
@@ -4,11 +4,15 @@ $('document').ready(function(){
   for (i = 0; i < 20; i++) {
     $('body').append('<div class="square"></div>');
   }
+  $('.square').on("click",function(event){
+    $(this).css('background', 'green');
+    event.preventDefault();
+  });
   $( "#submit_button").click(function(event){
     var color;
     color = $('#color-field').val();
     $('#brush').css('background', color);
-    event.preventDefault();
+      event.preventDefault();
     });
   $("#color-field").keypress (function(e){
     if(e.which == 13){
