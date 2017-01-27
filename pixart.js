627bc6af4f53641f50ef8082025800d3aff06446
Kay Ehni <kay.ehnigmail.com>
2017-01-27 07:56:11 -0500

    pixart HW

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
-
+var button = $("button");
+$("button").click(function(event){
+    event.preventDefault();
+});
+button.click(function(){
+  $(".brush").css("background", $("input:color-field").val()));
+})
+button.click(function(){
+  console.log($("input:color-field").val());
+}
+)
