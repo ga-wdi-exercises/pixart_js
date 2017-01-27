f1437642cbe4448efc9e53c1901f4230660000ce
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-01-27 05:25:34 -0500

    first commit 1: Set Color for Pixart_JS

--- pixart.js
+++ pixart.js
@@ -1,1 +1,8 @@
-
+//get content of input field with id colorfield
+//set the squares color to match the content of the input field
+$( "#submit_button").click(function(event){
+  var color;
+  color = $('#color-field').val();
+  $('#brush').css('background', color);
+  event.preventDefault();
+});
