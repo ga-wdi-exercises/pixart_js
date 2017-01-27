03bba9482c2a113eac8baf2a7d8baa5c1c48e063
Nat Carter <n.goldman.carter@gmail.com>
2017-01-26 20:00:09 -0500

    bonus, tweaks, and refactoring

--- pixart.js
+++ pixart.js
@@ -1,15 +1,22 @@
-var body = $('body');
 var field = $('#color-field');
-var button = $('#set-color');
 var brush = $('.brush');
+var dab = $('.dab');
 
-button.click(function(event) {
+$('#set-color').click(function(event) {
   event.preventDefault();
+  dab.eq(2).css('background-color', dab.eq(1).css('background-color'));
+  dab.eq(1).css('background-color', dab.eq(0).css('background-color'));
+  dab.eq(0).css('background-color', brush.css('background-color'));
   brush.css('background-color', field.val());
+  field.val('');
+});
+
+dab.click(function() {
+  brush.css('background-color', $(this).css('background-color'));
 });
 
 for (var i = 0; i < 8000; i++) {
-  body.append('<div class="square"></div>');
+  $('body').append('<div class="square"></div>');
 }
 
 $('.square').mouseover(function() {
