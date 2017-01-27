8465a79399c04d18831bcf92020dadfaa57d9562
Amilcar Sylvester <THE_EMAIL_YOU_USE_FOR_GITHUB@EMAIL.COM>
2017-01-26 23:42:40 -0500

    1commit

--- pixart.js
+++ pixart.js
@@ -1,1 +1,10 @@
+var brush = $('.brush')
+var colorField = $('#color-field')
+var setColorButton = $('#set-color')
 
+function setBrushColor(e) {
+  e.preventDefault()
+  var color = $('setColor')
+}
+
+setColorButton.on('click', setBrushColor)
