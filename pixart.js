862dab435f4961e22d5716a16d0db159165adf88
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-01-26 20:21:16 -0500

    #1 set button to change background color to input specified and prevent page from refreshing

--- pixart.js
+++ pixart.js
@@ -1,1 +1,9 @@
+console.log('Hello World!');
 
+// set click the "Set Color" button to change the color of the "brush" box to the color specified in the input field.
+//prevent page from refreshing when button is clicked
+
+$('button').on('click', function(inputColor) {
+  $('.button').css('background','#set-color');
+    event.preventDefault(inputColor);
+});
