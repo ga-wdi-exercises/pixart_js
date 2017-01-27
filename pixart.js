0743facd9d26f01690d3fe540d6c5a4ca304b610
Will Hawkins <willhawkins123@yahoo.com>
2017-01-26 22:04:03 -0500

    Bonused

--- pixart.js
+++ pixart.js
@@ -1,10 +1,25 @@
 console.log("Hello World");
-$("button").on("click", function(event) {
+var i = 1;
+$("#set-color").on("click", function(event) {
 //  console.log("The button can click!");
   event.preventDefault();
   var value = $("#color-field").val();
   console.log(value);
-  $(".brush").css("background", value);
+  $(".brush").css("background-color", value);
+  if (i == 1) {
+    $("#recentOne").css("background-color", value);
+    i++;
+  } else if (i == 2) {
+    $("#recentTwo").css("background-color", value);
+    i++;
+  } else if (i == 3) {
+    $("#recentThree").css("background", value);
+    i = 1;
+  } else {
+    i = 1;
+    $("#recentOne").css("background", value);
+    i++;
+  }
   $("#color-field").val("");
 });
 
@@ -23,4 +38,10 @@ appendSquares(8000);
 $(".square").on("mouseover", function() {
   var current = $(".brush").css("background");
   $(this).css("background", current);
+})
+
+$(".recent").on("click", function() {
+  var saved = $(this).css("background-color");
+  console.log(saved);
+  $(".brush").css("background", saved);
 })
