00f8a8fd772a25fc9705f5c50f7ba723e897ca68
Dominick Houston <dominick.houston14@gmail.com>
2017-01-26 19:36:39 -0500

    Commit #6 Added mouseover event

--- pixart.js
+++ pixart.js
@@ -16,15 +16,18 @@ $("button").on("click", function() {
 })
 
 $("document").ready(function() {
-  for( var i = 0; i < 20; i++) {
+  for( var i = 0; i < 1000; i++) {
     $("body").append("<div class='square'></div>");
 
-    $(".square").on("click", function() {
+    $(".square").on("mouseover", function() {
       $(this).css("background-color", color)
       // ^^use Orange as "COLOR"
     })
   }
 })
+
+//Painted a farmhouse!!
+
 
 // $(".square").on("click", function() {
 //   $("this").css("background-color", color)
