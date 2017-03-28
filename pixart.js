365d1c180271c8ea5d44f29cf68438c62eae865f
Jonathan Wright <jonwright.va@gmail.com>
2017-03-28 16:15:07 -0400

    Commit 1 - brush box color change functionality

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
+let button = $('#set-color');
+let inputSubmit = $('input');
+let brushBox = $('.brush');
 
+function changeColor() {
+  let brushColor = $('#color-field').val();
+  brushBox.css("background", brushColor);
+}
+
+button.click(changeColor);
+inputSubmit.on('submit', changeColor);
