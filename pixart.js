31d02a0637230c2198f4fe8e5a4777a244e93bc9
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-03-29 18:24:53 -0400

    added a test function to get click event working

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
+$(document).ready(function(){
 
+  var setColorButton = $('#set-color');
+
+  var changeColorFunction = function(){
+    event.preventDefault();
+    console.log('clicked');
+  };
+
+  setColorButton.on('click', changeColorFunction);
+});
