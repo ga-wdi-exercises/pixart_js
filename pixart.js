77c17c8086fbf358b0ed1cc6de7b373d982c7120
Erin <erinedawson@gmail.com>
2017-03-29 17:44:33 -0400

    change color of brush box. add event preventdefault

--- pixart.js
+++ pixart.js
@@ -1,1 +1,35 @@
+// var color = $(this.input);
+//
+// function setColor(){
+//   $(".brush").css("background", $(this.input));
+// }
+//
+// color.on('submit', setColor);
+//
+// var box {
+//     this.brush:$(".brush"),
+//     this.input:$("#color-field"),
+// }
+var input = $("#color-field");
+var brush = $(".brush");
+var brushHue = undefined;
 
+function setColor(){
+    brushHue=input.val();
+    brush.css("background", brushHue);
+}
+
+$('#set-color').click(setColor)
+
+
+$('#set-color').click(function(event) {
+  event.preventDefault();
+  $("<div>")
+    .append("default " + event.type + " prevented")
+    .appendTo("#log");
+});
+
+
+
+
+// .bind(box)
