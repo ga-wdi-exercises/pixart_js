8d6205b235443a31e1fb7f2369171ac668c3c29f
Jeong Hoon Koo <junghoone17@gmail.com>
2017-03-29 18:25:18 -0400

    Input changes box color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,10 @@
+var inputField = $("#color-field");
+var colorButton = $("#set-color");
+var brush = $(".brush")
 
+colorButton.on("click", setColor);
+
+function setColor() {
+  event.preventDefault()
+  brush.css("background-color", inputField.val());
+};
