2e15a1f218b58a93da9577d3592a8a63ac446e84
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-03-29 19:19:45 -0400

    added functionality so when I click on the squares they turn green

--- pixart.js
+++ pixart.js
@@ -3,16 +3,25 @@ $(document).ready(function(){
   var setColorButton = $('#set-color')
   // setColorButton.on('click', changeColorFunction)
   // setColorButton.keypress(changeColorFunction)
-  setColorButton.bind('on click keypress', setColorFunction)
 
   var setColorFunction = function(){
     event.preventDefault()
     console.log('clicked')
   }
+  setColorButton.bind('on click keypress', setColorFunction)
   /////
 
   for(var i = 0; i < 8000; i++) {
     var square = $('<div class="square"></div>');
     $('body').append(square);
   }
+
+  // var colorSquare = function (){
+  //   square.css('background','green')
+  //   console.log('clicked')
+  // }
+
+  $('.square').on('click',function(){
+    $(this).css('background','green');
+  })
 });
