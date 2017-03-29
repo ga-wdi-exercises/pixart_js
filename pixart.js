7ecec6873573c86018277ff61e592d41dd5031bc
Luisa Gonzalez <THE_EMAIL_YOU_USE_FOR_GITHUB@EMAIL.COM>
2017-03-29 19:13:40 -0400

    squares added

--- pixart.js
+++ pixart.js
@@ -7,3 +7,7 @@ colorButton.on("click", function() {
   event.preventDefault()
   $(".brush").css("background-color", colorField.val())
 })
+for (var i = 0; i < 8000; i++) {
+  var square = $('<div class="square"></div>');
+  $('body').append(square);
+}
