7dcc2c0d2f4a89b24293170dbde1644d51b502db
Curtis Ostle <curtisostle@gmail.com>
2017-03-29 13:43:05 -0400

    box is changing color with diff inputs

--- pixart.js
+++ pixart.js
@@ -1,1 +1,15 @@
+var colorButton = $('#set-color');
+var brushBox = $('.brush');
+var colorInput = $('#color-field');
 
+
+
+colorButton.on('click', function(event) {
+  event.preventDefault();
+});
+
+colorButton.on('click', changeColor);
+
+function changeColor() {
+  brushBox.css('background', colorInput.val());
+}
