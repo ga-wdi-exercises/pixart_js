9c4e0b4957e0adb424fe5a443b5f27946b2871fc
Matt Caplan <mattpcaplan@gmail.com>
2017-03-29 13:34:47 -0400

    got those colors showing up nicely! :D

--- pixart.js
+++ pixart.js
@@ -1,1 +1,13 @@
+$(function(){
 
+ $('button').click(function(changeColor) {
+  changeColor.preventDefault();
+   });
+
+   $('button').on('click', function(){
+     console.log('F* YEAH');
+
+     var color = $('#color-field').val();
+     $('.brush').css('background-color', color);
+   });
+ });
