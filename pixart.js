0aae7b31b681b18f441340377e71398cd0f3656f
Marcus Linn Miller Jr <marcusmiller220@gmail.com>
2017-03-29 18:20:19 -0400

    commit1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
+var colorButton = $("#set-color");
+var colorField = $("#color-field");
 
+
+function setColor(){
+    event.preventDefault()
+  $(".brush").css("background-color", colorField.val());
+  }
+
+
+colorButton.on("click", setColor);
