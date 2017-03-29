552d5fd5e15385054c392dee89dcadd9be1530f6
Jonathan Wright <jonwright.va@gmail.com>
2017-03-29 08:25:59 -0400

    Commit 3 - create 20 divs with square class '

--- pixart.js
+++ pixart.js
@@ -1,10 +1,24 @@
 let button = $('#set-color');
 let inputSubmit = $('input');
 let brushBox = $('.brush');
+let htmlBody = $('body');
+let canvasDiv = fillArray('<div class="square"></div>', 20);
+
+function fillArray(value, len) {
+  if (len == 0) return [];
+  var a = [value];
+  while (a.length * 2 <= len) a = a.concat(a);
+  if (a.length < len) a = a.concat(a.slice(0, len - a.length));
+  return a;
+}
 
 function changeColor() {
   let brushColor = $('#color-field').val();
   brushBox.css("background", brushColor);
+}
+
+function createCanvas() {
+  htmlBody.append(canvasDiv)
 }
 
 button.click(changeColor);
@@ -14,3 +28,5 @@ inputSubmit.keydown(function(e) {
     changeColor();
   }
 });
+
+createCanvas();
