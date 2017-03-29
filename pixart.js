be20eeb12bd2c52d51003e0c795fadac261f9220
Jeong Hoon Koo <junghoone17@gmail.com>
2017-03-29 19:58:04 -0400

    tried bonus but incomeplete..

--- pixart.js
+++ pixart.js
@@ -10,10 +10,15 @@ function setColor() {
   brush.css("background-color", inputField.val());
 };
 
-  for (var i = 0; i < 8000; i++) {
+for (var i = 0; i < 8000; i++) {
   $("body").append("<div class= square></div>" );
-  }
+}
 
 $(".square").hover(function() {
   $(this).css("background-color", inputField.val());
 });
+
+for (var i = 0; i <3; i++) {
+  $(".swatches").append("<div class = brush></div>");
+}
+$(".swatches").css({"display": "flex", "flex-direction": "row"});
