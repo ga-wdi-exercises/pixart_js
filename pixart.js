505a40187b7ef1981c4e4ae87227b908d7832a66
amlak befekadu <amlakgirma@yahoo.com>
2017-03-29 18:38:39 -0400

    Q-1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
 
+var inputField = $("#color-field");
+var buttonColor = $("#set-color");
+var brush = $(".brush");
+
+function setColor(){
+  event.preventDefault;
+  brush.css("background-color",inputField.val());
+
+}
+buttonColor.on("click",setColor);
