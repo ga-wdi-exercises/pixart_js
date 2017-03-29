7844e79dfc48db850b077c5c61698689c77f9a1e
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-03-29 18:40:23 -0400

    add 20 squares, append to body

--- pixart.js
+++ pixart.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
 
-  var setColorButton = $('#set-color');
+  var setColorButton = $('#set-color')
 
   var changeColorFunction = function(){
-    event.preventDefault();
-    console.log('clicked');
-  };
+    event.preventDefault()
+    console.log('clicked')
+  }
 
-  setColorButton.on('click', changeColorFunction);
-  setColorButton.keypress(changeColorFunction)
+  setColorButton.bind('on click keypress',changeColorFunction)
+  // setColorButton.on('click', changeColorFunction)
+  // setColorButton.keypress(changeColorFunction)
+  var squares = $('.square')
+  $('body').append(squares)
 });
