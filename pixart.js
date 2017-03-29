e970b64e2d5cde713c60aa73d93caf9f8fcedb94
Andres Arango <andresarangov17@gmail.com>
2017-03-29 18:30:56 -0400

    set color button changes color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,9 @@
+var inputField = $("#color-field");
+var buttonColor = $("#set-color");
 
+function setColor() {
+  event.preventDefault();
+  $(".brush").css("background", inputField.val());
+}
+
+buttonColor.on("click", setColor);
