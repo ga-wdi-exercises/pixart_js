f34642c2189ab5e2a68c9557f80a02294bb26359
Whitney <whitney.purdum@gmail.com>
2017-03-29 18:12:02 -0400

    Change square colors to green

--- pixart.js
+++ pixart.js
@@ -4,13 +4,17 @@ var currentColor
 var body = $("body");
 
 for (var i = 0; i < 20; i++) {
-	body.append('<div class='square'></div>')
+	body.append("<div class='square'></div>")
 	console.log('div added to body')
-}
+};
 
 
 function updateCSS() {
 	$(".brush").css("background-color", currentColor)
+}
+
+function changeSquareColor() {
+	$(this).css("background-color", "green");
 }
 
 //store color to global color variable
@@ -23,6 +27,8 @@ $("#form").on("submit", function (event) {
 	console.log(currentColor);
 	updateCSS();
 });
+
+$(".square").click(changeSquareColor);
 
 //create loop to add 20 DIVs to body
 
