412405caf95f9fa1d845e52b13876177b940f4b5
Whitney <whitney.purdum@gmail.com>
2017-03-29 17:53:16 -0400

    initial add

--- pixart.js
+++ pixart.js
@@ -1,1 +1,36 @@
 
+//variable
+var currentColor
+
+
+
+function updateCSS() {
+	$(".brush").css("background-color", currentColor)
+}
+
+//store color to global color variable
+
+
+
+//click event
+$("#form").on("submit", function (event) {
+	event.preventDefault()
+	console.log('preventing default');
+	currentColor = $("input").val();
+	console.log(currentColor);
+	updateCSS();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
