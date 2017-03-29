5c1f104dbe7c0606c7a27f636ad6acefd12d14dd
Ick Hyun Kwon <kwonih1020@gmail.com>
2017-03-29 18:30:38 -0400

    commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,9 @@
+var setColor = $("#set-color");
+var colorField = $("#color-field");
 
+function changeColor () {
+  event.preventDefault()
+  $(".brush").css("background", colorField.val());
+}
+
+setColor.on("click", changeColor);
