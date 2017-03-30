ac553a002e979d083f1d9eb776215933356bb7b8
Matt Caplan <mattpcaplan@gmail.com>
2017-03-29 21:14:31 -0400

    the spiders from mars...

--- pixart.js
+++ pixart.js
@@ -1,13 +1,24 @@
-$(function(){
+var setColor = $('#set-color');
+var brush = $('.brush');
+var colorSelector = $('#color-field');
 
- $('button').click(function(changeColor) {
-  changeColor.preventDefault();
-   });
 
-   $('button').on('click', function(){
-     console.log('F* YEAH');
+setColor.on('click', function(event) {
+  event.preventDefault();
+});
 
-     var color = $('#color-field').val();
-     $('.brush').css('background-color', color);
-   });
- });
+setColor.on('click', changeColor);
+
+function changeColor() {
+  brush.css('background', colorSelector.val());
+}
+
+for (var i = 9000; i > 0; i--) {
+  var div = $('<div class=square> </div>');
+  div.on('mouseover', boxColor);
+  $('body').append(div);
+}
+
+function boxColor() {
+  $(this).css('background', colorSelector.val());
+}
