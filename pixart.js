aa3c12e24e8d8ffa836d83d2b9ef34f520277319
Caroline Chen <carolinecaroline@gmail.com>
2017-03-29 21:21:16 -0400

    create 20 divs

--- pixart.js
+++ pixart.js
@@ -1,6 +1,6 @@
-var colorField = $('#color-field');
-var setColor = $('#set-color');
-var brushBox = $('.brush');
+var colorField = $("#color-field");
+var setColor = $("#set-color");
+var brushBox = $(".brush");
 
 var getAndSetColor = function (e){
 	e.preventDefault();
@@ -19,3 +19,25 @@ var getAndSetColor2 = function (e){
 setColor.on("click", getAndSetColor);
 
 setColor.on("keypress", getAndSetColor2);
+
+
+// var $square1 = $("<div/>", {
+// 	class: "square"});
+
+// var $square2 = $("<div/>", {
+// 	class: "square"});
+
+// var $square3 = $("<div/>", {
+// 	class: "square"});
+
+// $("body").append($square1);
+// $("body").append($square2);
+// $("body").append($square3);
+// can do a for loop!
+
+
+for (var i = 0; i < 20; i++) {
+	var $square = $("<div/>", {
+	class: "square"});
+	$("body").append($square);
+};
