e5513d4a3ebae6290a5eb333989eeac60ae14349
Ammar Alasadi <alasadi.ammar@gmail.com>
2017-03-30 07:37:40 -0400

    First commit / function not working

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
+var input = $('#color-value'); //this is the selected color
+var button = $("#set-color"); // confirm button
+var brush = $(".brush");// application area
 
+function changeColor() {
+	selectedColor = button.val();
+	brush.css("background", brush);
+}
+
+button.click(changeColor);
+button.on('submit', changeColor);
