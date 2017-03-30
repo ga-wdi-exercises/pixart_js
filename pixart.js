e83aa613db58fcf2b71c4731d854135ce74080f3
Caroline Chen <carolinecaroline@gmail.com>
2017-03-29 20:20:14 -0400

    make set color button functional

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
+var colorField = $('#color-field');
+var setColor = $('#set-color');
+var brushBox = $('.brush');
 
+var getInput = function (e){
+	e.preventDefault();
+	var selection = colorField.val();
+	brushBox.css("background-color", selection);
+};
+
+setColor.on("click", getInput);
