bf3671de0965aa25220d0e5aca934c0b6e1c3ad9
Caroline Chen <carolinecaroline@gmail.com>
2017-03-29 20:34:16 -0400

    make button react to enter key

--- pixart.js
+++ pixart.js
@@ -2,10 +2,20 @@ var colorField = $('#color-field');
 var setColor = $('#set-color');
 var brushBox = $('.brush');
 
-var getInput = function (e){
+var getAndSetColor = function (e){
 	e.preventDefault();
 	var selection = colorField.val();
 	brushBox.css("background-color", selection);
 };
 
-setColor.on("click", getInput);
+var getAndSetColor2 = function (e){
+	e.preventDefault();
+	if(e.which == 13) {
+	var selection = colorField.val();
+	brushBox.css("background-color", selection);
+}
+};
+
+setColor.on("click", getAndSetColor);
+
+setColor.on("keypress", getAndSetColor2);
