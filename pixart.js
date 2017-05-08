60a1156247c106936e766437b3a108c48c769d90
superbuggy <jpqreichard@gmail.com>
2017-05-08 14:01:26 -0400

    add soln

--- pixart.js
+++ pixart.js
@@ -1,1 +1,19 @@
+var $form = $("#form");
+var $button = $("#set-color");
+var $brush = $(".brush");
+var $input = $("#color-field");
 
+//When I click the "Set Color" button, it should change the color of the "brush" box to the color I specify in the input field. (Hint: You will need to use event.preventDefault() somewhere in your code.)
+
+$form.on("submit", function(event){
+  event.preventDefault();
+  $brush.css('background-color', $input.val());
+})
+for(var i = 0; i < 8000; i++){
+  var $square = $("<div class='square'/>");
+  $("body").append($square);
+}
+
+$(".square").on("mouseover", function(){
+  $(this).css('background', $input.val());
+})
