11b6a499011c348e08cdf36d2a89d8541461d609
Scott Pierce <scottpiercew@gmail.com>
2017-05-09 18:10:25 -0400

    20 divs created with for loop

--- pixart.js
+++ pixart.js
@@ -3,8 +3,18 @@ var setColor = $("#form")
 setColor.on("submit", function(event){
   event.preventDefault()
   $(".brush").css("background", $("#color-field").val())
+  })
+
+var div = "<div class='square'></div>"
+for (i = 0; i <= 20; i++) {
+  $("body").append(div)
+}
+
+// square.on("click", function(){
+
+
 
 // console.log(
 // $("#color-field").val()
 // )
-})
+// })
