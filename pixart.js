9d595501c4467982ca47d9da97780438cfa1a5ea
Scott Pierce <scottpiercew@gmail.com>
2017-05-09 13:47:26 -0400

    #1 set color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,10 @@
+var setColor = $("#set-color")
 
+setColor.on("click", function(event){
+  event.preventDefault()
+  $(".brush").css("background", $("#color-field").val())
+
+// console.log(
+// $("#color-field").val()
+// )
+})
