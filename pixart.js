02273760d816789378b1e270d7827bcdd4ee47a1
Scott Pierce <scottpiercew@gmail.com>
2017-05-09 18:32:52 -0400

    #4 clicking green

--- pixart.js
+++ pixart.js
@@ -5,12 +5,14 @@ setColor.on("submit", function(event){
   $(".brush").css("background", $("#color-field").val())
   })
 
-var div = "<div class='square'></div>"
 for (i = 0; i <= 20; i++) {
+  var div = "<div class='square'></div>"
   $("body").append(div)
 }
 
-// square.on("click", function(){
+$(".square").on("click", function(){
+  $(this).css("background", "green")
+})
 
 
 
