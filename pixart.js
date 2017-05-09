3e6e6c931a6c0d946846a14df6c0aef705b64443
Mark Moeller <MarkMoellerUVA@gmail.com>
2017-05-09 13:57:50 -0400

    Brush box color updated on set color button

--- pixart.js
+++ pixart.js
@@ -1,1 +1,14 @@
+var color_field = $("#color-field")
+var submit = $("#set-color")
+var brush = $(".brush")
+var color
 
+
+
+var setColor = function(e){
+  e.preventDefault()
+  color = color_field.val()
+  brush.css("background", color)
+}
+
+submit.on("click", setColor)
