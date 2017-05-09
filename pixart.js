d669d1c1e5de2d11f30c8e37ad9f3c5aee6ef10d
Tim Chase <tchase4544@gmail.com>
2017-05-09 17:55:21 -0400

    first click function

--- pixart.js
+++ pixart.js
@@ -1,1 +1,12 @@
+$(document).ready(function () {
+	var button = $('#set-color');
 
+	button.on("click",function(e){
+		e.preventDefault();
+		var colorVal = $('#color-field').val();
+
+		console.log(colorVal)
+
+		$('.brush').css("background", colorVal);
+	})
+})
