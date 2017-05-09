2a63b688432e8f9fbd84e691d0205ff722788c50
Tim Chase <tchase4544@gmail.com>
2017-05-09 18:17:45 -0400

    divs will change to selected color

--- pixart.js
+++ pixart.js
@@ -1,24 +1,27 @@
 $(document).ready(function () {
 	var div = "<div class='square'></div>"
-	var button = $('#set-color');
 
+	var button = $('#set-color');
+	//create div's
 	for (var i = 0; i < 21; i++) {
 		$('body').append(div)
 	}
-	$('.square').on("click",function(){
-		// console.log(this)
-		$(this).css("background","green")
-	})
 
+	//changes color to input color value
 	button.on("click",function(e){
 		e.preventDefault();
-		var colorVal = $('#color-field').val();
-
-		console.log(colorVal)
-
-		$('.brush').css("background", colorVal);
+		var aColor = colorVal()
+		$('.brush').css("background", aColor);
 	})
 
+	//div will change color when clicked
+	$('.square').on("click",function(){
+		var aColor = colorVal();
+		$(this).css("background", aColor)
+	})
 
-
+	//finds the value of input field
+	function colorVal() {
+		return $('#color-field').val();
+	}
 })
