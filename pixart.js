f8606fe8df081dc0d1fc5fa4f000b8902aebb2ce
Tim Chase <tchase4544@gmail.com>
2017-05-10 11:53:29 -0400

    completed pixart with bonus

--- pixart.js
+++ pixart.js
@@ -1,7 +1,10 @@
 $(document).ready(function () {
-	var div = "<div class='square'></div>"
-
+	var div = "<div class='square'></div>";
+	var prev = $('.prev')
+	var last = $('.last')
 	var button = $('#set-color');
+	var brush = $('.brush');
+	var currentColor;
 	//create div's
 	for (var i = 0; i < 8001; i++) {
 		$('body').append(div)
@@ -10,14 +13,28 @@ $(document).ready(function () {
 	//changes color to input color value
 	button.on("click",function(e){
 		e.preventDefault();
+
+		//send colors down the line
+		var lColor = $('.prev').css("background");
+		var pColor = $('.current').css("background");
+		last.css("background", lColor)
+		prev.css("background", pColor)
+
+		//find color value
 		var aColor = colorVal()
-		$('.brush').css("background", aColor);
+		currentColor = colorVal()
+		//set current
+		$('.current').css("background", aColor);
+
 	})
 
-	//div will change color when clicked
+	//div will change color on mouse over
 	$('.square').on("mouseover",function(){
-		var aColor = colorVal();
+		var aColor = currentColor
 		$(this).css("background", aColor)
+	})
+	brush.on("click",function(){
+		currentColor = $(this).css("background")
 	})
 
 	//finds the value of input field
