38cd74eec1844df19c8988095d5eada30350313c
SONNY KEOBOUNPHANH <SKEOBOUNPHANH@GMAIL.COM>
2017-05-10 18:18:20 -0400

    commit 3

--- pixart.js
+++ pixart.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
+  var input = $('#color-field')
+  var button = $('#set-color')
 
 //why does entering button works here?? Wouldn't it be a separte event listener?
-  $('button').on('click', function() {
+  button.on('click', function() {
     event.preventDefault();
     var input = $('input').val();
     console.log(input);
-    $('.brush').css('background', 'red')
+    $('.brush').css('background', input)
   })
+
+
+  for (var i = 0; i < 20; i++) {
+    var newDiv = $("<div class='square'/>");
+    $('body').append(newDiv);
+
+    console.log(newDiv);
+  }
 
 });
