171ae6da3e8464f32a971fe595fe53356995286c
SONNY KEOBOUNPHANH <SKEOBOUNPHANH@GMAIL.COM>
2017-05-10 17:38:13 -0400

    commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
+$(document).ready(function(){
 
+//why does entering button works here?? Wouldn't it be a separte event listener?
+  $('button').on('click', function() {
+    event.preventDefault();
+    var input = $('input').val();
+    console.log(input);
+    $('.brush').css('background', 'red')
+  })
+
+});
