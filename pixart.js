6a0a720e5672f0085a1d88b50c0de12057976729
tedlayea <tedlayea@gMAIL.com>
2017-05-10 22:43:17 -0400

    commit 2

--- pixart.js
+++ pixart.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
 
 var buttonClicked = $("#set-color");
+var colorSelected=$("#color-field").val();
 function changecolor(evt){
-
-  var colorSelected=$("#color-field").val();
   evt.preventDefault();
   $("div .brush").css("background-color",colorSelected);
   console.log("color changed to "+ colorSelected);
 }
 
 buttonClicked.on("click", changecolor );
+colorSelected.on("keypress",changecolor);
+
 })
