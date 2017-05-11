5ea0bb05711b92b630a9f464b308470c85225d80
Robert Lowe <RobLowe1992@Yahoo.com>
2017-05-11 07:51:35 -0400

    Completed

--- pixart.js
+++ pixart.js
@@ -1,1 +1,23 @@
 
+//jQuery variables
+var $form = $("#form");
+var $brush = $(".brush");
+var $colorField = $("#color-field");
+
+// Prevents the page from reloading and sets the brush color
+$form.on("submit", function(e){
+      e.preventDefault();
+      $brush.css('background-color', $colorField.val());
+
+    //Creates the squares for the canvas
+    for(var i = 0; i < 10080; i++){
+      var $squares = $("<div class='squares'></div>");
+      $("body").append($squares);
+    }
+    // Changes the color of each square when the mouse is hovering over it
+    $(".squares").on("mouseover", function(){
+      $(this).css('background', $colorField.val());
+    })
+})
+
+
