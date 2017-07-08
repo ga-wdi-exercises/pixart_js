4ed0eeae02f2f76970d81cca471e0c386cf51619
Jacob Greenwald <jeg4t@virginia.edu>
2017-07-08 08:35:31 -0500

    add constructor, not yet working

--- pixart.js
+++ pixart.js
@@ -1,2 +1,24 @@
 
- 
+class PixartHw {
+  constructor () {
+    this.brush = $('.brush')
+    this.input = $('#color-field')
+    this.color = null
+    this.addEvents()
+    $('form').on('submit', (x) => {
+      x.preventDefault()
+
+      this.brushColor()
+    })
+  }
+  inputColor () {
+    this.color = this.input.val()
+    return this.color
+  }
+  brushColor () {
+
+    this.inputColor()
+    this.brush.css('background', this.color)
+  }
+   
+}
