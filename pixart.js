3ed44ec47343e1d8d38ec69c21a94c1ef036fe3c
Jessa Marie Barre <jmlbarre@gmail.com>
2017-07-08 12:55:35 -0400

    created square divs

--- pixart.js
+++ pixart.js
@@ -10,7 +10,13 @@ setColorButton.on('click', function (e) {
 })
 
 inputBox.on('onkeypress', function (e) {
-  if(e.which === 13) {
+  if (e.which === 13) {
     brushBox.css('background', inputBox.val())
   }
 })
+
+// Create 20 divs of the "square" class and append them to the body
+//  * **Hint**: use `.append()`
+for (var i = 0; i < 20; i++) {
+  $('body').append('<div class="square"></div>')
+}
