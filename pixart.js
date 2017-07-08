44d05fd4814cdb9f5bcd98701b5e96609fb27fc7
Jacob Greenwald <jeg4t@virginia.edu>
2017-07-08 11:11:02 -0500

    step 3

--- pixart.js
+++ pixart.js
@@ -14,3 +14,11 @@ form.on('enterKey', function (event) {
   event.preventDefault()
   colorBrush.css('background-color', input.val())
 })
+
+//create loop that appends body with div elements and define div
+
+for (let i = 0; i < 20; i++) {
+  var square = $('.square')
+
+$('body').append('<div class="square"></div>')
+}
