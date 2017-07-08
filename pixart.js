7c50fc65e9425acd48cf19c899cf385a9d4a2257
Perry Fustero <pjfust@gmail.com>
2017-07-08 18:47:44 -0400

    change box when user press enter from input field

--- pixart.js
+++ pixart.js
@@ -5,3 +5,10 @@ $('#set-color').on('click', function (n) {
   var color = $('#color-field').val()
   $('.brush').css('background', color)
 })
+
+$('#color-field').keypress(function (n) {
+  if (n.which === 13) {
+    var color = $('#color-field').val()
+    $('.brush').css('background', color)
+  }
+})
