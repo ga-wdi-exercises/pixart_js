44168a62a9b4cd9cbfa5cd5e1425abdf58fee987
Colleen O'Reilly <colleenaoreilly@gmail.com>
2017-07-08 12:55:19 -0400

    added set color functionality

--- pixart.js
+++ pixart.js
@@ -1,1 +1,18 @@
+/* global $ */
+$(document).ready(function () {
 
+var button = $('#set-color')
+var form = $('#form')
+
+function setColor () {
+  color = $('#color-field').val()
+  $('.brush').css('background', color)
+}
+button.on('click', setColor)
+
+function changeFormDefault (e) {
+  e.preventDefault()
+}
+form.on('submit', changeFormDefault)
+
+})
