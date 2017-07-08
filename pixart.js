7c7b7e348dd238c81be7ceb94973d76d1de3f4f1
Kendall <adkins.kendall90@gmail.com>
2017-07-08 18:31:51 -0400

    progress

--- pixart.js
+++ pixart.js
@@ -1,1 +1,17 @@
+/* global $ */
+console.log('hello world')
 
+var colorField = $('#color-field')
+var button = $('#set-color')
+var setColor = function () {
+  var set = colorField.val()
+  $('.brush').css('background', set)
+}
+$('form').on('keypress', function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault()
+    return false
+  }
+})
+
+button.on('click', setColor)
