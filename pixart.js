af3b50125b979ecb2fa6779479e0a5538f3bfffb
Perry Fustero <pjfust@gmail.com>
2017-07-08 19:39:51 -0400

    add working color swatch

--- pixart.js
+++ pixart.js
@@ -1,23 +1,51 @@
 /* global $ */
 
+var brushes = []
+brushOne = $('.one')
+brushTwo = $('.two')
+brushThree = $('.three')
+brushes.push(brushOne, brushTwo, brushThree)
+
+// Set brush color by clicking button
 $('#set-color').on('click', function (n) {
   n.preventDefault()
-  var color = $('#color-field').val()
-  $('.brush').css('background', color)
+  colorChange()
 })
 
+// Set brush color by pressing 'Enter'
 $('#color-field').keypress(function (n) {
   if (n.which === 13) {
-    var color = $('#color-field').val()
-    $('.brush').css('background', color)
+    n.preventDefault()
+    colorChange()
   }
 })
 
-for (let i = 0; i < 20; i++) {
+// Change color of brush
+function colorChange () {
+  var color = $('#color-field').val()
+  var colorBox1 = brushes[0].css('background')
+  var colorBox2 = brushes[1].css('background')
+  brushes[2].css('background', colorBox2)
+  brushes[1].css('background', colorBox1)
+  brushes[0].css('background', color)
+}
+
+$('.brush').on('click', function () {
+  var color = $(this).css('background')
+  var colorBox1 = brushes[0].css('background')
+  var colorBox2 = brushes[1].css('background')
+  brushes[2].css('background', colorBox2)
+  brushes[1].css('background', colorBox1)
+  brushes[0].css('background', color)
+})
+
+// Create drawing board
+for (let i = 0; i < 8000; i++) {
   $('body').append('<div class="square"></div>')
 }
 
-$('.square').on('click', function () {
+// Create squares when mouse moves over drawing board
+$('.square').on('mouseover', function () {
   var getColor = $('.brush').css('background')
   $(this).css('background', getColor)
 })
