1e7e4395c79e7e28062999d71e196e0872f0d3cb
Charles Cox <cox.charles.w@gmail.com>
2017-07-08 12:03:55 -0400

    add functionality to change brush color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,8 @@
+var setColor = $('#set-color')
+var brush = $('.brush')
+var input = $('#color-field')
 
+setColor.on('click', function (evt) {
+   evt.preventDefault()
+   brush.css('background', input.val())
+})
