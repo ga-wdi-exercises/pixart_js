0abf2815eb4603f80f9d50ce48706c33aa96db32
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-07-09 11:34:53 -0400

    changed color box color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
 
+var button = $('button')
+var colorBox = $('.brush')
+var input = $('#color-field')
+
+var changeColor = function (e) {
+  e.preventDefault()
+  colorBox.css("background-color", input.val())
+}
+
+button.on('click', changeColor)
