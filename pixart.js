9236b2c51cdfc87794dc698e64334bb79ef85b7d
Sanjay Persaud <sanjaywallah@gmail.com>
2017-07-09 16:08:26 -0400

    cant figure out how to change color of each!!  Have to think this through some more.

--- pixart.js
+++ pixart.js
@@ -8,12 +8,12 @@ button.on ('click', handleClickEvent)
 
 $(document).ready(function () {
     //var $newdiv = $('<div class="square" />').css ('background','red')
-    for (var i = 0; i <= 20; i++) {
+    for (var i = 0; i <= 8000; i++) {
         $newdiv = $('<div class="square" />').css ('background','red')
         $('body').append($newdiv);
     }
 })
 
-$('body').on('click', function () {
+$('body').on('mouseover', function () {
   $('.square').css ('background', $('input').val())
 })
