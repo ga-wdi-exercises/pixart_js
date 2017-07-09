b0fe3e2042313768e0054d058c9d857067c5c796
Alex Wandres <alex.r.wandres@gmail.com>
2017-07-09 13:17:54 -0400

    commit 6

--- pixart.js
+++ pixart.js
@@ -1,19 +1,21 @@
-function setColor(evt) {
+function setColor (evt) {
   evt.preventDefault()
   $('.brush').css('background', $('color-field').val())
 }
 
 $('set-color').on('click', setColor)
-$('set-color').on('onkeypress', function (key){
-    if (key.which === 13) {
-      setColor()
-    }
-  })
+$('set-color').on('onkeypress', function (key) {
+  if (key.which === 13) {
+    setColor()
+  }
+})
 
-for (i = 0; i <= 20; i++) {
+$('.square').css({height: '10px', width: '10px', margin: '0'})
+
+for (i = 0; i <= 8000; i++) {
   $('body').append('<div class="square"></div>')
 }
 
-$('.square').on('click', function () {
+$('.square').on('mouseover', function () {
   $('this').css('background', $('color-field').val())
 })
