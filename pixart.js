6e768a20b6ab4f7a02bb0169c3dbe708e1716a39
Sanjay Persaud <sanjaywallah@gmail.com>
2017-07-09 15:31:13 -0400

    added commits 3 and 4

--- pixart.js
+++ pixart.js
@@ -2,6 +2,18 @@
 var button = $('#set-color')
 function handleClickEvent (e) {
   $('.brush').css ('background', $('input').val())
-  e.preventDefault();
+  e.preventDefault()
 }
 button.on ('click', handleClickEvent)
+
+$(document).ready(function () {
+    //var $newdiv = $('<div class="square" />').css ('background','red')
+    for (var i = 0; i <= 20; i++) {
+        $newdiv = $('<div class="square" />').css ('background','red')
+        $('body').append($newdiv);
+    }
+})
+
+$('body').on('click', function () {
+  $('.square').css ('background', 'green')
+})
