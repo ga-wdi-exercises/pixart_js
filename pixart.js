88bf8e54053ae240e28358433870b4efed90745d
Kendall <adkins.kendall90@gmail.com>
2017-07-08 22:05:11 -0400

    excitement, another problem solved, enter button works

--- pixart.js
+++ pixart.js
@@ -3,14 +3,16 @@ console.log('hello world')
 
 var colorField = $('#color-field')
 var button = $('#set-color')
+
 var setColor = function () {
   var set = colorField.val()
   $('.brush').css('background', set)
 }
-$('form').on('keypress', function (event) {
+
+$('#form').on('keypress', function (event) {
   if (event.keyCode === 13) {
     event.preventDefault()
-    return false
+    setColor()
   }
 })
 
