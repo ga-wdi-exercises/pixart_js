e22c91d78849aacdee19544a20d11c5c369062fb
Alex Wandres <alex.r.wandres@gmail.com>
2017-07-09 13:05:05 -0400

    commit 1 & 2

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
+function setColor(evt) {
+  evt.preventDefault()
+  $('.brush').css('background', $('color-field').val())
+}
 
+$('set-color').on('click', setColor)
+$('set-color').on('onkeypress', function (key){
+    if (key.which === 13) {
+      setColor()
+    }
+  })
