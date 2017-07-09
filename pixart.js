61852ed7feda0bdadd1da912a12d75d778d552fa
Admin <Tgafvert@gmail.com>
2017-07-09 12:09:01 -0400

    phase 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,10 @@
+var brush = $('.brush')
+var button = $('#set-color')
+var input = $('#color-field')
 
+var setColor = function (e) {
+  e.preventDefault()
+  brush.css('background-color', input.val())
+}
+
+button.on('click', setColor)
