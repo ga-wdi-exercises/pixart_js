a6db523326015ee5ff4f4d5a27166e4975645acf
MATT FILBERT <mcfilbert@email.wm.edu>
2017-07-09 22:51:48 -0400

    bonus commit add swatch functionality

--- pixart.js
+++ pixart.js
@@ -1,11 +1,39 @@
 /* global $ */
 
 var $input = $('#color-field')
+var swatches = []
+var $brush = $('.brush')
+var color
 
-$('#form').on('click', function (evnt) {
+$('#form').on('submit', function (evnt) {
   evnt.preventDefault()
-  var color = $input.val()
-  $('.brush').css('background', color)
+  color = $input.val()
+  $brush.css('background', color)
+  swatches.unshift(color)
+  $('#swatch1').css('background', swatches[1])
+  $('#swatch2').css('background', swatches[2])
+  $('#swatch3').css('background', swatches[3])
+  if (swatches.length > 4) {
+    swatches.pop()
+  }
+})
+
+$('#swatch1').on('click', function (chng) {
+  chng.preventDefault()
+  color = swatches[1]
+  $brush.css('background', color)
+})
+
+$('#swatch2').on('click', function (chng) {
+  chng.preventDefault()
+  color = swatches[2]
+  $brush.css('background', color)
+})
+
+$('#swatch3').on('click', function (chng) {
+  chng.preventDefault()
+  color = swatches[3]
+  $brush.css('background', color)
 })
 
 for (let i = 0; i < 8000; i++) {
@@ -13,5 +41,5 @@ for (let i = 0; i < 8000; i++) {
 }
 
 $('.square').on('mouseover', function (evnt) {
-  $(evnt.target).css('background', $input.val())
+  $(evnt.target).css('background', color)
 })
