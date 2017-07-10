2310f4996b6cef94dafe0ba44e8e219b7ab39f53
MATT FILBERT <mcfilbert@email.wm.edu>
2017-07-09 21:15:03 -0400

    1st commit checkpoint

--- pixart.js
+++ pixart.js
@@ -1,1 +1,22 @@
+/* global $ */
 
+class Pixart {
+  constructor () {
+    this.color
+    this.brush = $('.brush')
+    this.input = $('#color-field')
+    $('#set-color').on('click', evnt => {
+      evnt.preventDefault()
+      this.makeColor()
+    })
+  }
+
+  makeColor () {
+    this.takeColor()
+    this.brush.css('background', this.color)
+  }
+  takeColor () {
+    this.color = this.input.val()
+    return this.color
+  }
+}
