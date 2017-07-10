fb1ecbe39966715cf9fe81cbb21a9dc1e0a8591c
josh <josh@joshs-iMac.home>
2017-07-09 23:45:30 -0400

    brushes changes color when clicked

--- pixart.js
+++ pixart.js
@@ -1,1 +1,19 @@
+//Name: Josue Rosales
+//Date: July 9th
+//Description: drawing when mouseover
 
+var inputText= $('#color-field'); //grabbing input text element
+var button  = $('#set-color'); // grabbing button element
+var currentBrush  = $('.brush');
+
+button.on('click',changeBrush)
+
+
+
+function changeBrush(event)
+{
+var colorx = inputText.val();
+  currentBrush.css('background',colorx);
+
+  event.preventDefault();//prevents from refreshing 
+}
