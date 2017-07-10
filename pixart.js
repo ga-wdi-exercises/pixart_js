71b6c7a87a6be26463fcdd1dd0277710c0561e18
MATT FILBERT <mcfilbert@email.wm.edu>
2017-07-09 22:07:34 -0400

    third commit checkpoint

--- pixart.js
+++ pixart.js
@@ -3,3 +3,7 @@ $('#form').on('click', function(evnt){
   var color = $('#color-field').val();
   $('.brush').css('background', color);
 })
+
+for (let i = 0; i < 20; i++){
+  $('body').append("<div class='square'></div>")
+}
