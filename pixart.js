9c8933f9da33f54233eaa586e7123416ba535b32
MATT FILBERT <mcfilbert@email.wm.edu>
2017-07-09 21:55:30 -0400

    Revert "mistakes were mad"
    This reverts commit b5f70bdb77528062f84c808340ace60b4b216e9d.

---
+++ pixart.js
@@ -0,0 +1,28 @@
+/* global $ */
+
+class Pixart {
+  constructor () {
+    this.color
+    this.brush = $('.brush')
+    this.input = $('#color-field')
+    $('#form').on('submit', evnt => {
+      evnt.preventDefault()
+      this.makeColor()
+    })
+  }
+
+  makeColor () {
+    this.takeColor()
+    this.brush.css('background', this.color)
+  }
+  takeColor () {
+    this.color = this.input.val()
+    return this.color
+  }
+
+  makeCanvas () {
+    for (var i = 0; i < 20; i++) {
+      $('body').append("<div class='square'></div>")
+    }
+  }
+}
