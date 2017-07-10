c5db4b8fd7fd36f41db81a8fe1b052f57a199ad8
Richard Schram <rcschram@yahoo.com>
2017-07-10 08:52:21 -0400

    more stuff

--- pixart.js
+++ pixart.js
@@ -1,1 +1,22 @@
+var marker = $('.markerh')
+var button = $('#set-color')
+var input = $('#color-field')
+var body = $('body')
+// brush.css('background-color', '#1B4370')
+// $('brush').css('background-color', 'blue');
 
+var setColor = function (e) {
+  e.preventDefault()
+  marker.css('background-color', input.val())
+}
+
+button.on('click', setColor)
+
+for (i = 0; i < 8000; i++) {
+var square = $('<div class ="square"></div>')
+    body.append(square)
+    square.on('mouseover', function () {
+      $(this).css('background', brush.css('background-color'))
+    })
+
+  }
